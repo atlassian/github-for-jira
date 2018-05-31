360e75cb1bb6ab6a34ae24a11cbed49096a41aae
Primarily Snark <starkey.joshua@gmail.com>
2018-05-31 02:05:29 -0700

    fixup! Add minimal implementation of push update functionality.

--- test/safe/push.test.js
+++ test/safe/push.test.js
@@ -36,44 +36,42 @@ const payload = {
 }
 
 describe('GitHub Actions', () => {
-  describe('issue_comment', () => {
-    describe('created', () => {
-      it('should update the GitHub issue with a linked Jira ticket', async () => {
-        const githubApi = td.api('https://api.github.com')
-        const jiraApi = td.api('https://test-atlassian-instance.net')
+  describe('push', () => {
+    it('should update the Jira issue with the linked GitHub commit', async () => {
+      const githubApi = td.api('https://api.github.com')
+      const jiraApi = td.api('https://test-atlassian-instance.net')
 
-        td.when(githubApi.get('/repos/test-repo-owner/test-repo-name/contents/.github/jira.yml'))
-          .thenReturn({
-            content: Buffer.from('jira: https://test-atlassian-instance.net').toString('base64')
-          })
+      td.when(githubApi.get('/repos/test-repo-owner/test-repo-name/contents/.github/jira.yml'))
+        .thenReturn({
+          content: Buffer.from('jira: https://test-atlassian-instance.net').toString('base64')
+        })
 
-        await app.receive(payload)
+      await app.receive(payload)
 
-        td.verify(jiraApi.post('/rest/developmenttool/0.9/devinfo/bulk', {
-          preventTransitions: false,
-          repositories: [
-            {
-              name: 'example/test-repo-name',
-              url: 'test-repo-url',
-              id: 'test-repo-id',
-              commits: [
-                {
-                  hash: "test-commit-id",
-                  message: "[TEST-123] Test commit.",
-                  author: {
-                    name: "test-commit-author-name",
-                    email: "test-commit-author-email"
-                  },
-                  displayId: "test-c",
-                  fileCount: 3,
-                  id: "test-commit-id",
-                  issueKeys: ["TEST-123"]
-                }
-              ]
-            }
-          ]
-        }))
-      })
+      td.verify(jiraApi.post('/rest/developmenttool/0.9/devinfo/bulk', {
+        preventTransitions: false,
+        repositories: [
+          {
+            name: 'example/test-repo-name',
+            url: 'test-repo-url',
+            id: 'test-repo-id',
+            commits: [
+              {
+                hash: "test-commit-id",
+                message: "[TEST-123] Test commit.",
+                author: {
+                  name: "test-commit-author-name",
+                  email: "test-commit-author-email"
+                },
+                displayId: "test-c",
+                fileCount: 3,
+                id: "test-commit-id",
+                issueKeys: ["TEST-123"]
+              }
+            ]
+          }
+        ]
+      }))
     })
   })
 })
