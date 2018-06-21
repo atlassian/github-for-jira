372300a42cca7a6512cc3227e46462d0793ef8cb
Primarily Snark <starkey.joshua@gmail.com>
2018-06-21 11:24:56 -0700

    Add Jira lifecycle hooks.
    Added Jira lifecycle hooks for enabling, disabling, and
    uninstalling the Jira app.

---
+++ db/migrations/20180621181228-add-enable-flag-for-installations.js
@@ -0,0 +1,13 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.addColumn('Installations', 'enabled', {
+      type: Sequelize.BOOLEAN,
+      allowNull: false,
+      defaultValue: true
+    })
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.removeColumn('Installations', 'enabled')
+  }
+}
