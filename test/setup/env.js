1291a8e127c5efb735c92939908c05c89bd5dc17
Alberto Gimeno <gimenete@github.com>
2018-09-20 17:17:08 +0200

    Clarify STORAGE_SECRET in testing mode

--- test/setup/env.js
+++ test/setup/env.js
@@ -8,6 +8,8 @@ const defaults = Object.assign({
   PRIVATE_KEY_PATH: './test/setup/test-key.pem',
   GITHUB_CLIENT_SECRET: 'test-github-secret',
   LOG_LEVEL: 'fatal',
+  // Don't worry about this key. It is just for testing.
+  // Generate a secure, random one with `openssl rand -hex 32` in your .env file
   STORAGE_SECRET: '8cad66340bc92edbae2ae3a792d351f48c61d1d8efe7b2d9408b0025c1f7f845',
   SETUP: 'yes' // indicates that the setup did run
 }, process.env)
