82fe93b4454b851429491428140a38cbd30c659a
Tommy Byrd <tcbyrd@github.com>
2018-09-21 16:16:48 -0400

    Synchronize branches with related Pull Requests (#64)
    * Return undefined data object to avoid destructuring error
    
    * create jiraPayload that contains branch data from the PR
    
    * Update PR fixture to include data needed for branch payload
    
    * update PR test to expect branch payload
    
    * add sourceBranchUrl property to match related branches
    
    * add sourceBranchUrl to test case

--- test/safe/pull-request.test.js
+++ test/safe/pull-request.test.js
@@ -23,6 +23,28 @@ describe('GitHub Actions', () => {
             name: 'example/test-repo-name',
             url: 'test-repo-url',
             id: 'test-repo-id',
+            branches: [
+              {
+                createPullRequestUrl: 'test-pull-request-head-url/pull/new/test-pull-request-head-ref',
+                lastCommit: {
+                  author: {name: 'test-pull-request-author-login'},
+                  authorTimestamp: 'test-pull-request-update-time',
+                  displayId: 'test-p',
+                  fileCount: 0,
+                  hash: 'test-pull-request-sha',
+                  id: 'test-pull-request-sha',
+                  issueKeys: ['TEST-123'],
+                  message: 'n/a',
+                  updateSequenceId: 12345678,
+                  url: 'test-pull-request-head-url/commit/test-pull-request-sha'
+                },
+                id: 'test-pull-request-head-ref',
+                issueKeys: ['TEST-123'],
+                name: 'test-pull-request-head-ref',
+                url: 'test-pull-request-head-url/tree/test-pull-request-head-ref',
+                updateSequenceId: 12345678
+              }
+            ],
             pullRequests: [
               {
                 author: {
@@ -36,7 +58,8 @@ describe('GitHub Actions', () => {
                 id: 'test-pull-request-number',
                 issueKeys: ['TEST-123'],
                 lastUpdate: 'test-pull-request-update-time',
-                sourceBranch: 'test-pull-request-head-url/tree/test-pull-request-head-ref',
+                sourceBranch: 'test-pull-request-head-ref',
+                sourceBranchUrl: 'test-pull-request-head-url/tree/test-pull-request-head-ref',
                 status: 'OPEN',
                 title: '[TEST-123] Test pull request.',
                 timestamp: 'test-pull-request-update-time',
