5890a81f6fc874c2ae63a537830a8e448c6a54fa
Alberto Gimeno <gimenete@github.com>
2018-10-02 12:04:24 +0200

    CSRF protection

--- static/js/github-configuration.js
+++ static/js/github-configuration.js
@@ -3,7 +3,8 @@ $('.install-link').click(function (event) {
   event.preventDefault()
 
   $.post('/github/configuration', {
-    installationId: $(event.target).data('installation-id')
+    installationId: $(event.target).data('installation-id'),
+    _csrf: document.getElementById('_csrf').value
   }, function (data) {
     window.close()
   })
