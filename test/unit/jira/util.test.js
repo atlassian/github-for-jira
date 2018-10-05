f116aec02ca8f6f9cf733cf7d44b6f4fcae02144
Alberto Gimeno <gimenete@github.com>
2018-10-05 19:01:24 +0200

    Additional tests

--- test/unit/jira/util.test.js
+++ test/unit/jira/util.test.js
@@ -60,5 +60,7 @@ describe('Jira util', () => {
   describe('#getJiraId', () => {
     expect(getJiraId('AP-3-large_push')).toEqual('AP-3-large_push')
     expect(getJiraId('AP-3-large_push/foobar')).toEqual('~41502d332d6c617267655f707573682f666f6f626172')
+    expect(getJiraId('feature-something-cool')).toEqual('feature-something-cool')
+    expect(getJiraId('feature/something-cool')).toEqual('~666561747572652f736f6d657468696e672d636f6f6c')
   })
 })
