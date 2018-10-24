d26b88014dd38c25f48138dc97000650029481d2
Tommy Byrd <tcbyrd@github.com>
2018-10-24 19:39:55 -0400

    Tracking unique project keys (#133)
    * getting project keys in pushes
    
    * fix conflicts from master
    
    * implementation to log unique project keys
    
    * Move projects to its own table
    
    * add analytics view of projects table

---
+++ db/migrations/20181024000949-add-projects.js
@@ -0,0 +1,37 @@
+'use strict'
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Projects', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      projectKey: {
+        type: Sequelize.STRING,
+        allowNull: true
+      },
+      occurrences: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        defaultValue: 0
+      },
+      jiraHost: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    })
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Projects')
+  }
+}
---
+++ db/migrations/20181024214811-more-analytics-views.js
@@ -0,0 +1,33 @@
+'use strict'
+
+module.exports = {
+  up: async (queryInterface) => {
+    await queryInterface.sequelize.query(`
+      CREATE OR REPLACE VIEW "analytics"."subscriptions" AS SELECT "Subscriptions".id,
+        "Subscriptions"."gitHubInstallationId" AS github_installation_id,
+        "Subscriptions"."jiraHost" AS jira_host,
+        "Subscriptions"."createdAt" AS created_at,
+        "Subscriptions"."updatedAt" AS updated_at,
+        "Subscriptions"."repoSyncState" AS repo_sync_state
+       FROM "Subscriptions";
+    `)
+
+    await queryInterface.sequelize.query(`
+      CREATE VIEW analytics.Projects AS (
+        SELECT
+          id,
+          "projectKey" AS project_key,
+          "occurrences",
+          "jiraHost" as jira_host,
+          "updatedAt" as updated_at,
+          "createdAt" as created_at
+        FROM
+          "Projects"
+      )`
+    )
+  },
+
+  down: async (queryInterface) => {
+    await queryInterface.sequelize.query('DROP VIEW analytics.projects')
+  }
+}
