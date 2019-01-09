fd428fcbe15e380832cd353bb21577fb452d026a
Danny Hadley <dadleyy@gmail.com>
2019-01-09 14:42:07 -0500

    Fixing more lint issues

--- test/unit/jira/util.test.js
+++ test/unit/jira/util.test.js
@@ -1,14 +1,14 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require('fs')
+const path = require('path')
 const getJiraUtil = require('../../../lib/jira/util')
 const { getJiraId } = require('../../../lib/jira/util/id')
 
 describe('Jira util', () => {
-  function loadFixture(name) {
-    const base = path.join(__dirname, '../../fixtures/text', name);
-    const source = fs.readFileSync(base + '.source.md').toString('utf-8').trim();
-    const rendered = fs.readFileSync(base + '.rendered.md').toString('utf-8').trim();
-    return { source, rendered };
+  function loadFixture (name) {
+    const base = path.join(__dirname, '../../fixtures/text', name)
+    const source = fs.readFileSync(base + '.source.md').toString('utf-8').trim()
+    const rendered = fs.readFileSync(base + '.rendered.md').toString('utf-8').trim()
+    return { source, rendered }
   }
 
   describe('#addJiraIssueLinks', () => {
@@ -25,7 +25,7 @@ describe('Jira util', () => {
     })
 
     it('should linkify Jira references to valid issues', () => {
-      const { source, rendered } = loadFixture('existing-reference-link');
+      const { source, rendered } = loadFixture('existing-reference-link')
       const issues = [
         {
           key: 'TEST-2019',
@@ -36,11 +36,11 @@ describe('Jira util', () => {
       ]
 
       const result = util.addJiraIssueLinks(source, issues)
-      expect(result).toBe(rendered);
-    });
+      expect(result).toBe(rendered)
+    })
 
     it('should not add reference links if already present', () => {
-      const { source, rendered } = loadFixture('previously-referenced');
+      const { source, rendered } = loadFixture('previously-referenced')
       const issues = [
         {
           key: 'TEST-2019',
@@ -51,9 +51,8 @@ describe('Jira util', () => {
       ]
 
       const result = util.addJiraIssueLinks(source, issues)
-      expect(result).toBe(rendered);
-    });
-
+      expect(result).toBe(rendered)
+    })
 
     it('should not linkify Jira references to invalid issues', () => {
       const text = 'Should not linkify [TEST-123] as a link'
@@ -65,7 +64,7 @@ describe('Jira util', () => {
     })
 
     it('should linkify only Jira references to valid issues', () => {
-      const { source, rendered } = loadFixture('valid-and-invalid-issues');
+      const { source, rendered } = loadFixture('valid-and-invalid-issues')
       const issues = [
         {
           key: 'TEST-200',
