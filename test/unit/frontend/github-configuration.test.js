037f92827d185b67c0a9b84a25077b17d3e57db6
Tommy Byrd <tcbyrd@github.com>
2019-03-02 14:49:00 -0500

    Check for user installations before subscribing (#176)
    * check for user installations before installing
    
    * add test and fix response status
    
    * make errors consistent

--- test/unit/frontend/github-configuration.test.js
+++ test/unit/frontend/github-configuration.test.js
@@ -1,5 +1,6 @@
 const Keygrip = require('keygrip')
 const supertest = require('supertest')
+const nock = require('nock')
 
 function getCookieHeader (payload) {
   const cookie = Buffer.from(JSON.stringify(payload)).toString('base64')
@@ -7,6 +8,18 @@ function getCookieHeader (payload) {
 
   return [
     `session=${cookie};session.sig=${keygrip.sign(`session=${cookie}`)};`
+  ]
+}
+
+const userInstallationsResponse = {
+  'total_count': 2,
+  'installations': [
+    {
+      'id': 1
+    },
+    {
+      'id': 3
+    }
   ]
 }
 
@@ -42,6 +55,21 @@ describe('Frontend', () => {
           .expect(401)
       })
 
+      it('should return a 401 if the user doesn\'t have access to the requested installation ID', () => {
+        nock('https://api.github.com').get('/user/installations').reply(200, userInstallationsResponse)
+        return supertest(subject)
+          .post('/github/configuration')
+          .send({
+            installationId: 2
+          })
+          .type('form')
+          .set('cookie', getCookieHeader({
+            githubToken: 'test-github-token',
+            jiraHost: 'test-jira-host'
+          }))
+          .expect(401)
+      })
+
       it('should return a 400 if no installationId is present in the body', () => {
         return supertest(subject)
           .post('/github/configuration')
@@ -54,10 +82,12 @@ describe('Frontend', () => {
       })
 
       it('should return a 200 and install a Subscription', async () => {
+        nock('https://api.github.com').get('/user/installations').reply(200, userInstallationsResponse)
+
         await supertest(subject)
           .post('/github/configuration')
           .send({
-            installationId: 'test-installation-id'
+            installationId: 1
           })
           .type('form')
           .set('cookie', getCookieHeader({
@@ -67,7 +97,7 @@ describe('Frontend', () => {
           .expect(200)
 
         td.verify(models.Subscription.install({
-          installationId: 'test-installation-id',
+          installationId: '1',
           host: 'test-jira-host'
         }))
       })
