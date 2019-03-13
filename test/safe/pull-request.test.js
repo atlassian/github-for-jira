6abe3f79e36bf77d0a6f9ff38bd870f789c48400
tcbyrd <tcbyrd@github.com>
2019-03-12 19:34:18 -0700

    add test for cases where branch still has an issue key

--- test/safe/pull-request.test.js
+++ test/safe/pull-request.test.js
@@ -126,5 +126,79 @@ describe('GitHub Actions', () => {
 
       td.verify(jiraApi.delete(`/rest/devinfo/0.10/repository/${repository.id}/pull_request/${pullRequest.number}?_updateSequenceId=12345678`))
     })
+
+    it('will not delete references if a branch still has an issue key', async () => {
+      const payload = require('../fixtures/pull-request-test-changes-with-branch.json')
+      
+      const jiraApi = td.api('https://test-atlassian-instance.net')
+      const githubApi = td.api('https://api.github.com')
+
+      td.when(githubApi.get('/users/test-pull-request-user-login')).thenReturn({
+        login: 'test-pull-request-author-login',
+        avatar_url: 'test-pull-request-author-avatar',
+        html_url: 'test-pull-request-author-url'
+      })
+
+      Date.now = jest.fn(() => 12345678)
+
+      await app.receive(payload)
+      
+      td.verify(jiraApi.post('/rest/devinfo/0.10/bulk', {
+        preventTransitions: false,
+        repositories: [
+          {
+            id: "test-repo-id",
+            name: "example/test-repo-name",
+            url: "test-repo-url",
+            branches: [
+              {
+                createPullRequestUrl: "TES-3-test-pull-request-head-url/pull/new/TES-3-test-pull-request-head-ref",
+                lastCommit: {
+                  author: {name: "test-pull-request-author-login"},
+                  authorTimestamp: "test-pull-request-update-time",
+                  displayId: "test-p",
+                  fileCount: 0,
+                  hash: "test-pull-request-sha",
+                  id: "test-pull-request-sha",
+                  issueKeys: ["TES-3"],
+                  message: "n/a",
+                  updateSequenceId: 12345678,
+                  url: "TES-3-test-pull-request-head-url/commit/test-pull-request-sha"
+                },
+                id: "TES-3-test-pull-request-head-ref",
+                issueKeys: ["TES-3"],
+                name: "TES-3-test-pull-request-head-ref",
+                url: "TES-3-test-pull-request-head-url/tree/TES-3-test-pull-request-head-ref",
+                updateSequenceId: 12345678
+              }
+            ],
+            pullRequests: [
+              {
+                author: {
+                  avatar: "test-pull-request-author-avatar",
+                  name: "test-pull-request-author-login",
+                  url: "test-pull-request-author-url"
+                },
+                commentCount: "test-pull-request-comment-count",
+                destinationBranch: "test-pull-request-base-url/tree/pull-request-base-ref",
+                displayId: "#1",
+                id: 1,
+                issueKeys: ["TES-3"],
+                lastUpdate: "test-pull-request-update-time",
+                sourceBranch: "TES-3-test-pull-request-head-ref",
+                sourceBranchUrl: "TES-3-test-pull-request-head-url/tree/TES-3-test-pull-request-head-ref",
+                status: "OPEN",
+                timestamp: "test-pull-request-update-time",
+                title: "Test pull request.",
+                url: "test-pull-request-url",
+                updateSequenceId: 12345678
+              }
+            ],
+            updateSequenceId: 12345678
+          }
+        ],
+        properties: {installationId: "test-installation-id"}
+      }))
+    })
   })
 })
