1116099d8fc82ec8a5b635865f53403b0a7c4d21
tcbyrd <tcbyrd@github.com>
2019-07-16 14:48:12 -0400

    add migration to backfill clientKeys

---
+++ db/migrations/20190716143013-migrate-clientKey.js
@@ -0,0 +1,32 @@
+'use strict'
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.sequelize.query(`
+      UPDATE "Subscriptions"
+      SET "jiraClientKey" = (
+        SELECT "clientKey"
+        FROM "Installations"
+        WHERE "Subscriptions"."jiraHost" = 
+          "Installations"."jiraHost"
+      )
+      WHERE EXISTS (
+        SELECT "clientKey"
+        FROM "Installations"
+        WHERE "Subscriptions"."jiraHost" = "Installations"."jiraHost"
+      )
+    `)
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.sequelize.query(`
+      UPDATE "Subscriptions"
+      SET "jiraClientKey" = NULL
+      WHERE EXISTS (
+        SELECT "clientKey"
+        FROM "Installations"
+        WHERE "Subscriptions"."jiraHost" = "Installations"."jiraHost"
+      )
+    `)
+  }
+}
