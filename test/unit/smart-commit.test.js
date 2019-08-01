f8ba942b6102d6a0f3969f8ef774c8921f95b1c5
Jordan Raine <jnraine@github.com>
2019-08-01 12:01:19 -0700

    Uppercase issue keys
    JIRA’s developer info endpoint is strict. If you have a JIRA project that uses `JRA` as th key but submit `jra-123`, it will respond with a 400.
    
    Users want to be able to specify projects in upper or lower case so let’s upper case issue keys before submitting them to JIRA.

--- test/unit/smart-commit.test.js
+++ test/unit/smart-commit.test.js
@@ -254,6 +254,16 @@ describe('Smart commit parsing', () => {
         issueKeys: ['JRA-090']
       })
     })
+
+    it('uppercases issue keys', () => {
+      const text = 'jra-090'
+
+      const result = smartCommit(text)
+
+      expect(result).toMatchObject({
+        issueKeys: ['JRA-090']
+      })
+    })
   })
 
   it('should ignore irrelevant text', () => {
