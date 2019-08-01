137d15d04e8971ef435f7ab8420ce133750a3f0e
Tommy Byrd <tcbyrd@github.com>
2019-08-01 15:07:09 -0400

    New feature: Subscription management (#211)
    * check user's organization permissions
    
    This changes the subscription flow so that only admins of an Org can link their org to a Jira instance.
    
    * update page so it only shows admin installations
    
    * fix some linting issues
    
    * refactoring to allow for proper error handling
    
    There will likely always be some accounts tha don't accept the new permissions. For a user account it doesn't matter, but if an Org doesn't accept the permissions this will return false so we don't show the org in the list.
    
    * Refactoring the github config page to show the jira host
    
    This will help make sure it's clear which jira host you're connecting to when you setup the subscription.
    
    * link to /github/subscriptions
    
    This won't use Jira middleware since you may want to remove subscriptions from Jira instances you don't have access to anymore, so it should be namespaced under `/github`
    
    * add ui for an org admin to view wand delete all subscriptions
    
    * fixing some linting issues
    
    * get user login from github instead of session params
    
    * move admin check to middleware
    
    * pluralize this route since it's a list
    
    * remove debugging logs
    
    * move the rest of the admin checks to github-middleware
    
    * UX polish around managing and deleting subscriptions
    
    * fix some linting errors
    
    * show non-admin installations as disabled
    
    * pass installations to the template with admin attribute
    
    * ensure admin role is passed to subscription uninstall
    
    * move admin check out of the model
    
    * make isAdmin testable and add tests
    
    * don't decouple a one line response
    
    * update the language for disabled installations
    
    * add method to handle whether or not permission have been updated
    
    * don't call subscription install twice
    
    * bring back in installationswithAdmin after bad merge
    
    * add back in jiraHost and clientKey values from 26a3e81
    
    * update readme to document this new feature

--- static/js/github-configuration.js
+++ static/js/github-configuration.js
@@ -13,3 +13,18 @@ $('.install-link').click(function (event) {
     window.close()
   })
 })
+
+$('.delete-link').click(function (event) {
+  event.preventDefault()
+
+  $.post('/github/subscription', {
+    installationId: $(event.target).data('installation-id'),
+    jiraHost: $(event.target).data('jira-host'),
+    _csrf: document.getElementById('_csrf').value
+  }, function (data) {
+    if (data.err) {
+      return console.log(data.err)
+    }
+    window.close()
+  })
+})
