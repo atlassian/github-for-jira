6c632e02c11257c2d5bb0494b9a8e51d8cc122bb
tcbyrd <tcbyrd@github.com>
2019-08-07 19:49:39 -0400

    add additional indexes for jira host and client key

--- db/migrations/20190807230019-add-indexes.js
+++ db/migrations/20190807230019-add-indexes.js
@@ -4,17 +4,23 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.addIndex('Subscriptions', {
       fields: ['gitHubInstallationId', 'jiraHost'],
-      name: 'index-on-installation-and-jirahost'
+      name: 'index-on-installation-and-jira-host'
     })
 
     await queryInterface.addIndex('Subscriptions', {
-      fields: ['gitHubInstallationId'],
-      name: 'index-on-github-installation-id'
+      fields: ['jiraHost'],
+      name: 'index-on-jira-host'
+    })
+
+    await queryInterface.addIndex('Subscriptions', {
+      fields: ['jiraClientKey'],
+      name: 'index-on-jira-client-key'
     })
   },
 
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.removeIndex('Subscriptions', 'index-on-installation-and-jirahost')
-    await queryInterface.removeIndex('Subscriptions', 'index-on-github-installation-id')
+    await queryInterface.removeIndex('Subscriptions', 'index-on-installation-and-jira-host')
+    await queryInterface.removeIndex('Subscriptions', 'index-on-jira-host')
+    await queryInterface.removeIndex('Subscriptions', 'index-on-jira-client-key')
   }
 }
