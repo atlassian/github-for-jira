a18dadb8fac109a0a7fb989f72c9621e374e1ef5
Jordan Raine <jnraine@github.com>
2019-08-07 17:24:01 -0700

    Add index on Installations
    This aims to significantly reduce time spent executing a query that takes 18% of our database time.
    
    ```
     total_exec_time | prop_exec_time |  ncalls   |  sync_io_time   |                                                                                                                                         query
    -----------------+----------------+-----------+-----------------+---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
     03:39:55.74488  | 52.7%          | 2,710,622 | 00:00:00.085608 | SELECT "id", "gitHubInstallationId", "jiraHost", "selectedRepositories", "repoSyncState", "syncStatus", "jiraClientKey", "createdAt", "updatedAt" FROM "Subscriptions" AS "Subscription" WHERE "Subscription"."gitHubInstallationId" = $1
     01:28:22.313629 | 21.2%          | 715,672   | 00:00:00.000512 | SELECT "id", "projectKey", "occurrences", "jiraHost", "createdAt", "updatedAt" FROM "Projects" AS "Project" WHERE "Project"."projectKey" = $1 AND "Project"."jiraHost" = $2 LIMIT $3
     01:17:57.612923 | 18.7%          | 3,060,110 | 00:00:00.004206 | SELECT "id", "jiraHost", "secrets", "clientKey", "enabled", "createdAt", "updatedAt" FROM "Installations" AS "Installation" WHERE "Installation"."jiraHost" = $1 AND "Installation"."enabled" = $2 LIMIT $3
    ```
    
    The top two are handled elsewhere (#225, #227).

---
+++ db/migrations/20190808001912-add-indexes-on-installations.js
@@ -0,0 +1,12 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.addIndex('Installations', {
+      fields: ['jiraHost'],
+      name: 'Installations_jiraHost_idx'
+    })
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.removeIndex('Installations', 'Installations_jiraHost_idx')
+  }
+};
