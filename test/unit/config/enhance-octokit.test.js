794e0e2a162edbb22cdf0bd2b78c32ec027891a7
Jordan Raine <jnraine@github.com>
2019-09-09 13:27:50 -0700

    Add toHaveSentMetric
    This metric will make it easier to test code that sends metrics to Datadog. This commit includes tests for the Octokit request metrics.
    
    The matcher itself is rough around the edges but can be refined over time as we use it, if necessary.

---
+++ test/unit/config/enhance-octokit.test.js
@@ -0,0 +1,26 @@
+const Octokit = require('@octokit/rest')
+const nock = require('nock')
+
+const enhanceOctokit = require('../../../lib/config/enhance-octokit')
+
+describe(enhanceOctokit, () => {
+  describe('request metrics', () => {
+    it('sends reqest timing', async () => {
+      nock('https://api.github.com').get(/.+/).reply(200, [])
+
+      const log = { debug: () => {} }
+      const octokit = Octokit()
+
+      enhanceOctokit(octokit, log)
+
+      await expect(async () => {
+        await octokit.activity.listPublicEvents()
+      }).toHaveSentMetrics({
+        name: 'jira-integration.github-request',
+        type: 'h',
+        value: (value) => value > 0 && value < 20, // Value changes depending on how long nock takes
+        tags: { path: '/events' }
+      })
+    })
+  })
+})
