7d71f3de61661386c0b9b9d723357facc70be413
Jordan Raine <jnraine@github.com>
2019-09-09 23:31:09 -0700

    Instrument requests to Jira API
    This sends timing data to Datadog whenever a request is made to Jira. This generates a [histogram] using the request duration in milliseconds. It also includes method, path, and response status tags.
    
    These metrics will provide insight into the health of our interactions with the Jira API. In addition to knowing when something explictly bad happens (e.g., 401s, 500s), we’ll also see when things slow down, if our traffic spikes, or if things requests get sluggish.
    
    histogram: https://docs.datadoghq.com/developers/metrics/histograms/

---
+++ test/unit/jira/client/axios.test.js
@@ -0,0 +1,48 @@
+const nock = require('nock')
+
+const getJiraAxios = require('../../../../lib/jira/client/axios')
+const LogDouble = require('../../../setup/log-double')
+
+describe('Jira axios instance', () => {
+  describe('request metrics', () => {
+    const jiraHost = 'https://example.atlassian.net'
+
+    describe('when request successful', () => {
+      it('sends timing metric', async () => {
+        nock(jiraHost).get('/foo/bar').reply(200)
+
+        const jiraAxiosInstance = getJiraAxios(jiraHost, 'secret', new LogDouble())
+
+        await expect(async () => {
+          await jiraAxiosInstance.get('/foo/bar')
+        }).toHaveSentMetrics({
+          name: 'jira-integration.jira_request',
+          type: 'h',
+          tags: { path: '/foo/bar', method: 'GET', status: '200' },
+          value: (value) => value > 0 && value < 20
+        })
+      })
+    })
+
+    describe('when request fails', () => {
+      it('sends timing metric', async () => {
+        nock(jiraHost).get('/foo/bar').reply(500)
+
+        const jiraAxiosInstance = getJiraAxios(jiraHost, 'secret', new LogDouble())
+
+        await expect(async () => {
+          try {
+            await jiraAxiosInstance.get('/foo/bar')
+          } catch (error) {
+            // Swallow error
+          }
+        }).toHaveSentMetrics({
+          name: 'jira-integration.jira_request',
+          type: 'h',
+          tags: { path: '/foo/bar', method: 'GET', status: '500' },
+          value: (value) => value > 0 && value < 20
+        })
+      })
+    })
+  })
+})
