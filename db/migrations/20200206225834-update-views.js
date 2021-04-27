42c6455f155f73d73d7f51d155951a85ed4057e4
Rachelle Rathbone <rachellerathbone@gmail.com>
2021-04-27 11:36:58 +1000

    revert db schema changes

--- db/migrations/20200206225834-update-views.js
+++ db/migrations/20200206225834-update-views.js
@@ -1,7 +1,7 @@
 module.exports = {
   up: async (queryInterface) => {
     await queryInterface.sequelize.query(`
-    create or replace view analytics.subscriptions(id, github_installation_id, jira_host, created_at, updated_at, repo_sync_state, selected_repositories, sync_status, sync_warning, number_synced_repos) as
+    create or replace view analytics.subscriptions(id, github_installation_id, jira_host, created_at, updated_at, repo_sync_state, selected_repositories, sync_status, sync_warning) as
       SELECT "Subscriptions".id,
             "Subscriptions"."gitHubInstallationId" AS github_installation_id,
             "Subscriptions"."jiraHost"             AS jira_host,
@@ -10,23 +10,21 @@ module.exports = {
             "Subscriptions"."repoSyncState"        AS repo_sync_state,
             "Subscriptions"."selectedRepositories" AS selected_repositories,
             "Subscriptions"."syncStatus"           AS sync_status,
-            "Subscriptions"."syncWarning"          AS sync_warning,
-            "Subscriptions"."numberOfSyncedRepos"  AS number_synced_repos
+            "Subscriptions"."syncWarning"          AS sync_warning
       FROM "Subscriptions";
-    `);
+    `)
   },
 
   down: async (queryInterface) => {
     await queryInterface.sequelize.query(`
-    create or replace view analytics.subscriptions(id, github_installation_id, jira_host, created_at, updated_at, repo_sync_state, number_synced_repos) as
+    create or replace view analytics.subscriptions(id, github_installation_id, jira_host, created_at, updated_at, repo_sync_state) as
       SELECT "Subscriptions".id,
             "Subscriptions"."gitHubInstallationId" AS github_installation_id,
             "Subscriptions"."jiraHost"             AS jira_host,
             "Subscriptions"."createdAt"            AS created_at,
             "Subscriptions"."updatedAt"            AS updated_at,
-            "Subscriptions"."repoSyncState"        AS repo_sync_state,
-            "Subscriptions"."numberOfSyncedRepos"  AS number_synced_repos
+            "Subscriptions"."repoSyncState"        AS repo_sync_state
       FROM "Subscriptions";
-  `);
-  },
-};
+  `)
+  }
+}
