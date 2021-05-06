2ed84078374cccb5b0192b8b051446abd3793be1
Rachelle Rathbone <rachellerathbone@gmail.com>
2021-05-06 16:08:22 +1000

    undo changes to rename in db

--- db/migrations/20180917195216-add-sync-columns.js
+++ db/migrations/20180917195216-add-sync-columns.js
@@ -3,24 +3,24 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.addColumn('Subscriptions', 'selectedRepositories', {
       type: Sequelize.ARRAY(Sequelize.INTEGER),
-      allowNull: true,
-    });
+      allowNull: true
+    })
 
     await queryInterface.addColumn('Subscriptions', 'repoSyncState', {
       type: Sequelize.JSONB,
-      allowNull: true,
-    });
+      allowNull: true
+    })
 
     await queryInterface.addColumn('Subscriptions', 'syncStatus', {
-      type: Sequelize.ENUM('PENDING', 'COMPLETE', 'IN PROGRESS', 'FAILED'),
-      allowNull: true,
-    });
+      type: Sequelize.ENUM('PENDING', 'COMPLETE', 'ACTIVE', 'FAILED'),
+      allowNull: true
+    })
   },
 
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.removeColumn('Subscriptions', 'selectedRepositories');
-    await queryInterface.removeColumn('Subscriptions', 'repoSyncState');
-    await queryInterface.removeColumn('Subscriptions', 'syncStatus');
-    await queryInterface.sequelize.query('DROP TYPE "enum_Subscriptions_syncStatus";');
-  },
-};
+    await queryInterface.removeColumn('Subscriptions', 'selectedRepositories')
+    await queryInterface.removeColumn('Subscriptions', 'repoSyncState')
+    await queryInterface.removeColumn('Subscriptions', 'syncStatus')
+    await queryInterface.sequelize.query('DROP TYPE "enum_Subscriptions_syncStatus";')
+  }
+}
