bc504bc6322c679d9c3415e5298ba7d2e23d2beb
Michel Boudreau <michelboudreau@gmail.com>
2021-06-04 10:31:06 +1000

    latest test fixes

--- test/unit/sync/branch.test.ts
+++ test/unit/sync/branch.test.ts
@@ -1,20 +1,28 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-import parseSmartCommit from '../../../src/transforms/smart-commit';
-import {branchesNoLastCursor, branchesWithLastCursor} from '../../fixtures/api/graphql/branch-queries';
+/* eslint-disable @typescript-eslint/no-var-requires,@typescript-eslint/no-explicit-any */
+import parseSmartCommit from "../../../src/transforms/smart-commit";
+import { branchesNoLastCursor, branchesWithLastCursor } from "../../fixtures/api/graphql/branch-queries";
+import { mocked } from "ts-jest/utils";
+import { Subscription } from "../../../src/models";
+import { Application } from "probot";
+import { createApp } from "../../utils/probot";
+import createJob from "../../setup/create-job";
+import { processInstallation } from "../../../src/sync/installation";
+import nock from "nock";
 
-describe('sync/branches', () => {
-  let jiraHost;
-  let jiraApi;
-  let installationId;
+jest.mock("../../../src/models");
+
+describe("sync/branches", () => {
+  const installationId = 1234;
   let delay;
-  const branchNodesFixture = require('../../fixtures/api/graphql/branch-ref-nodes.json');
-  const emptyNodesFixture = require('../../fixtures/api/graphql/branch-empty-nodes.json');
-  const branchCommitsHaveKeys = require('../../fixtures/api/graphql/branch-commits-have-keys.json');
-  const associatedPRhasKeys = require('../../fixtures/api/graphql/branch-associated-pr-has-keys.json');
-  const branchNoIssueKeys = require('../../fixtures/api/graphql/branch-no-issue-keys.json');
+  let app: Application;
+  const branchNodesFixture = require("../../fixtures/api/graphql/branch-ref-nodes.json");
+  const emptyNodesFixture = require("../../fixtures/api/graphql/branch-empty-nodes.json");
+  const branchCommitsHaveKeys = require("../../fixtures/api/graphql/branch-commits-have-keys.json");
+  const associatedPRhasKeys = require("../../fixtures/api/graphql/branch-associated-pr-has-keys.json");
+  const branchNoIssueKeys = require("../../fixtures/api/graphql/branch-no-issue-keys.json");
 
-  function makeExpectedResponse({branchName}) {
-    const {issueKeys} = parseSmartCommit(branchName);
+  function makeExpectedResponse({ branchName }) {
+    const { issueKeys } = parseSmartCommit(branchName);
     return {
       preventTransitions: true,
       repositories: [
@@ -23,214 +31,208 @@ describe('sync/branches', () => {
             {
               createPullRequestUrl: `test-repo-url/pull/new/${branchName}`,
               id: branchName,
-              issueKeys: ['TES-123'].concat(issueKeys).reverse().filter(Boolean),
+              issueKeys: ["TES-123"].concat(issueKeys).reverse().filter(Boolean),
               lastCommit: {
                 author: {
-                  avatar: 'https://camo.githubusercontent.com/test-avatar',
-                  name: 'test-author-name',
+                  avatar: "https://camo.githubusercontent.com/test-avatar",
+                  name: "test-author-name"
                 },
-                authorTimestamp: 'test-authored-date',
-                displayId: 'test-o',
+                authorTimestamp: "test-authored-date",
+                displayId: "test-o",
                 fileCount: 0,
-                hash: 'test-oid',
-                id: 'test-oid',
-                issueKeys: ['TES-123'],
-                message: 'TES-123 test-commit-message',
-                url: 'test-repo-url/commit/test-sha',
-                updateSequenceId: 12345678,
+                hash: "test-oid",
+                id: "test-oid",
+                issueKeys: ["TES-123"],
+                message: "TES-123 test-commit-message",
+                url: "test-repo-url/commit/test-sha",
+                updateSequenceId: 12345678
               },
               name: branchName,
               url: `test-repo-url/tree/${branchName}`,
-              updateSequenceId: 12345678,
-            },
+              updateSequenceId: 12345678
+            }
           ],
           commits: [
             {
               author: {
-                avatar: 'https://camo.githubusercontent.com/test-avatar',
-                email: 'test-author-email@example.com',
-                name: 'test-author-name',
+                avatar: "https://camo.githubusercontent.com/test-avatar",
+                email: "test-author-email@example.com",
+                name: "test-author-name"
               },
-              authorTimestamp: 'test-authored-date',
-              displayId: 'test-o',
+              authorTimestamp: "test-authored-date",
+              displayId: "test-o",
               fileCount: 0,
-              hash: 'test-oid',
-              id: 'test-oid',
-              issueKeys: ['TES-123'],
-              message: 'TES-123 test-commit-message',
-              timestamp: 'test-authored-date',
-              url: 'test-repo-url/commit/test-sha',
-              updateSequenceId: 12345678,
-            },
+              hash: "test-oid",
+              id: "test-oid",
+              issueKeys: ["TES-123"],
+              message: "TES-123 test-commit-message",
+              timestamp: "test-authored-date",
+              url: "test-repo-url/commit/test-sha",
+              updateSequenceId: 12345678
+            }
           ],
-          id: 'test-repo-id',
-          name: 'test-repo-name',
-          url: 'test-repo-url',
-          updateSequenceId: 12345678,
-        },
+          id: "test-repo-id",
+          name: "test-repo-name",
+          url: "test-repo-url",
+          updateSequenceId: 12345678
+        }
       ],
       properties: {
-        installationId: 1234,
-      },
+        installationId: installationId
+      }
     };
   }
 
   function nockBranchRequest(fixture) {
-    nock('https://api.github.com')
-      .post('/graphql', branchesNoLastCursor)
-      .reply(200, payload);
-    nock('https://api.github.com')
-      .post('/graphql', branchesWithLastCursor)
+    githubNock
+      .post("/graphql", branchesNoLastCursor)
+      .reply(200, fixture)
+      .post("/graphql", branchesWithLastCursor)
       .reply(200, emptyNodesFixture);
   }
 
-  let createJob;
-  let processInstallation;
-
   beforeEach(async () => {
     const repoSyncStatus = {
-      installationId: 12345678,
-      jiraHost: 'tcbyrd.atlassian.net',
+      installationId: installationId,
+      jiraHost: "tcbyrd.atlassian.net",
       repos: {
-        'test-repo-id': {
+        "test-repo-id": {
           repository: {
-            name: 'test-repo-name',
-            owner: {login: 'integrations'},
-            html_url: 'test-repo-url',
-            id: 'test-repo-id',
+            name: "test-repo-name",
+            owner: { login: "integrations" },
+            html_url: "test-repo-url",
+            id: "test-repo-id"
           },
-          pullStatus: 'complete',
-          branchStatus: 'pending',
-          commitStatus: 'complete',
-        },
-      },
+          pullStatus: "complete",
+          branchStatus: "pending",
+          commitStatus: "complete"
+        }
+      }
     };
-    delay = process.env.LIMITER_PER_INSTALLATION = '2000';
+    delay = process.env.LIMITER_PER_INSTALLATION = "2000";
 
-    jiraHost = process.env.ATLASSIAN_URL;
-    jiraApi = td.api(process.env.ATLASSIAN_URL);
-
-    installationId = 1234;
     Date.now = jest.fn(() => 12345678);
 
-    td.when(
-      models.Subscription.getSingleInstallation(jiraHost, installationId),
-    ).thenResolve({
+    mocked(
+      Subscription.getSingleInstallation
+    ).mockResolvedValue({
       jiraHost,
       id: 1,
       get: () => repoSyncStatus,
       set: () => repoSyncStatus,
       save: () => Promise.resolve({}),
-      update: () => Promise.resolve({}),
-    });
+      update: () => Promise.resolve({})
+    } as any);
 
-    createJob = (await import('../../setup/create-job')).default;
-    processInstallation = (await import('../../../src/sync/installation')).processInstallation;
+    app = await createApp();
   });
 
-  it('should sync to Jira when branch refs have jira references', async () => {
-    const job = createJob({data: {installationId, jiraHost}, opts: {delay}});
+  it("should sync to Jira when branch refs have jira references", async () => {
+    const job = createJob({ data: { installationId, jiraHost }, opts: { delay } });
     nockBranchRequest(branchNodesFixture);
 
+    jiraNock
+      .post("/rest/devinfo/0.10/bulk",
+        makeExpectedResponse({ branchName: "TES-321-branch-name" }))
+      .reply(200);
+
     const queues = {
       installation: {
-        add: jest.fn(),
-      },
+        add: jest.fn()
+      }
     };
-    await processInstallation(app, queues)(job);
+    await expect(processInstallation(app, queues)(job)).toResolve();
     expect(queues.installation.add).toHaveBeenCalledWith(job.data, job.opts);
-
-    td.verify(
-      jiraApi.post('/rest/devinfo/0.10/bulk', makeExpectedResponse({branchName: 'TES-321-branch-name'})),
-    );
   });
 
-  it('should send data if issue keys are only present in commits', async () => {
+  it("should send data if issue keys are only present in commits", async () => {
 
-    const job = createJob({data: {installationId, jiraHost}, opts: {delay}});
+    const job = createJob({ data: { installationId, jiraHost }, opts: { delay } });
     nockBranchRequest(branchCommitsHaveKeys);
 
+    jiraNock.post("/rest/devinfo/0.10/bulk", makeExpectedResponse({
+      branchName: "dev"
+    })).reply(200)
+
     const queues = {
       installation: {
-        add: jest.fn(),
-      },
+        add: jest.fn()
+      }
     };
-    await processInstallation(app, queues)(job);
+    await expect(processInstallation(app, queues)(job)).toResolve();
     expect(queues.installation.add).toHaveBeenCalledWith(job.data, job.opts);
-
-    td.verify(jiraApi.post('/rest/devinfo/0.10/bulk', makeExpectedResponse({
-      branchName: 'dev',
-    })));
   });
 
-  it('should send data if issue keys are only present in an associatd PR title', async () => {
-    const job = createJob({data: {installationId, jiraHost}, opts: {delay}});
+  it("should send data if issue keys are only present in an associatd PR title", async () => {
+    const job = createJob({ data: { installationId, jiraHost }, opts: { delay } });
     nockBranchRequest(associatedPRhasKeys);
 
-    const queues = {
-      installation: {
-        add: jest.fn(),
-      },
-    };
-    await processInstallation(app, queues)(job);
-    expect(queues.installation.add).toHaveBeenCalledWith(job.data, job.opts);
-
-    td.verify(jiraApi.post('/rest/devinfo/0.10/bulk', {
+    jiraNock.post("/rest/devinfo/0.10/bulk", {
       preventTransitions: true,
       repositories: [
         {
           branches: [
             {
-              createPullRequestUrl: 'test-repo-url/pull/new/dev',
-              id: 'dev',
-              issueKeys: ['PULL-123'],
+              createPullRequestUrl: "test-repo-url/pull/new/dev",
+              id: "dev",
+              issueKeys: ["PULL-123"],
               lastCommit: {
                 author: {
-                  avatar: 'https://camo.githubusercontent.com/test-avatar',
-                  name: 'test-author-name',
+                  avatar: "https://camo.githubusercontent.com/test-avatar",
+                  name: "test-author-name"
                 },
-                authorTimestamp: 'test-authored-date',
-                displayId: 'test-o',
+                authorTimestamp: "test-authored-date",
+                displayId: "test-o",
                 fileCount: 0,
-                hash: 'test-oid',
-                issueKeys: ['PULL-123'],
-                id: 'test-oid',
-                message: 'test-commit-message',
-                url: 'test-repo-url/commit/test-sha',
-                updateSequenceId: 12345678,
+                hash: "test-oid",
+                issueKeys: ["PULL-123"],
+                id: "test-oid",
+                message: "test-commit-message",
+                url: "test-repo-url/commit/test-sha",
+                updateSequenceId: 12345678
               },
-              name: 'dev',
-              url: 'test-repo-url/tree/dev',
-              updateSequenceId: 12345678,
-            },
+              name: "dev",
+              url: "test-repo-url/tree/dev",
+              updateSequenceId: 12345678
+            }
           ],
           commits: [],
-          id: 'test-repo-id',
-          name: 'test-repo-name',
-          url: 'test-repo-url',
-          updateSequenceId: 12345678,
-        },
+          id: "test-repo-id",
+          name: "test-repo-name",
+          url: "test-repo-url",
+          updateSequenceId: 12345678
+        }
       ],
       properties: {
-        installationId: 1234,
-      },
-    }));
+        installationId: installationId
+      }
+    }).reply(200);
+
+    const queues = {
+      installation: {
+        add: jest.fn()
+      }
+    };
+    await expect(processInstallation(app, queues)(job)).toResolve();
+    expect(queues.installation.add).toHaveBeenCalledWith(job.data, job.opts);
   });
 
-  it('should not call Jira if no issue keys are found', async () => {
-    const job = createJob({data: {installationId, jiraHost}, opts: {delay}});
+  it("should not call Jira if no issue keys are found", async () => {
+    const job = createJob({ data: { installationId, jiraHost }, opts: { delay } });
     nockBranchRequest(branchNoIssueKeys);
 
     const queues = {
       installation: {
-        add: jest.fn(),
-      },
+        add: jest.fn()
+      }
     };
 
-    td.when(jiraApi.post(), {ignoreExtraArgs: true})
-      .thenThrow(new Error('test error'));
+    const interceptor = jiraNock.post(/.*/);
+    const scope = interceptor.reply(200);
 
-    await processInstallation(app, queues)(job);
+    await expect(processInstallation(app, queues)(job)).toResolve();
     expect(queues.installation.add).toHaveBeenCalledWith(job.data, job.opts);
+    expect(scope).not.toBeDone();
+    nock.removeInterceptor(interceptor);
   });
 });
