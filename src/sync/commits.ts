220ae882cf6b3761ec1a7b74cf0567ce05839fa8
Michel Boudreau <michelboudreau@gmail.com>
2021-06-08 15:38:56 +1000

    fixing build/tests

--- src/sync/commits.ts
+++ src/sync/commits.ts
@@ -4,20 +4,24 @@ import {GitHubAPI} from 'probot';
 
 // TODO: better typings
 export default async (github:GitHubAPI, repository, cursor, perPage:number) => {
-  const data = await github.graphql(getDefaultRef, {
+  // TODO: fix typings for graphql
+  const data = (await github.graphql(getDefaultRef, {
     owner: repository.owner.login,
     repo: repository.name,
-  });
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  }) as any);
 
   const refName = (data.repository.defaultBranchRef) ? data.repository.defaultBranchRef.name : 'master';
 
-  const commitsData = await github.graphql(getCommitsQuery, {
+  // TODO: fix typings for graphql
+  const commitsData = (await github.graphql(getCommitsQuery, {
     owner: repository.owner.login,
     repo: repository.name,
     per_page: perPage,
     cursor,
     default_ref: refName,
-  });
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  })) as any;
 
   // if the repository is empty, commitsData.repository.ref is null
   const { edges } = commitsData.repository.ref
