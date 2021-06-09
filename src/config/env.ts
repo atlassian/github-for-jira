131754def83cb80a06d1e25bac927c03d3658d12
Michel Boudreau <michelboudreau@gmail.com>
2021-06-09 16:18:48 +1000

    ignoring missing .env for production

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,11 +1,11 @@
 import dotenv from "dotenv";
 import path from "path";
-
-const filename = process.env.NODE_ENV === "test" ? ".env.test" : ".env";
+const nodeEnv = process.env.NODE_ENV;
+const filename = nodeEnv === "test" ? ".env.test" : ".env";
 const env = dotenv.config({
   path: path.resolve(process.cwd(), filename)
 });
-if (env.error) {
+if (env.error && nodeEnv !== "production") {
   throw env.error;
 }
 export default env.parsed || {};
