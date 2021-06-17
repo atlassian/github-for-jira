4981e07a8c010c172cff6be5acc18bb8356c504d
Rachelle Rathbone <rachellerathbone@gmail.com>
2021-06-17 12:17:05 +1000

    logs

--- src/config/redis-info.ts
+++ src/config/redis-info.ts
@@ -1,9 +1,16 @@
 import url from 'url';
 import Redis from 'ioredis';
+import bunyan from 'bunyan';
 
 const REDIS_URL = process.env.REDIS_BOTTLENECK_HOST || '127.0.0.1';
 const redisInfo = url.parse(REDIS_URL);
 const REDIS_PORT = process.env.REDIS_BOTTLENECK_PORT || redisInfo.port || 6379;
+const logger = bunyan.createLogger({ name: 'deepcheck' });
+
+logger.info(
+  `REDIS_BOTTLENECK_HOST: ${process.env.REDIS_BOTTLENECK_HOST} and REDIS_BOTTLENECK_PORT: ${process.env.REDIS_PORT}`,
+);
+logger.info(`REDIS_URL: ${REDIS_URL}`);
 
 let password;
 if (redisInfo.auth?.split(':').length === 2) {
