3aa070392cd01ed0e760798c0bd9dd023e1dead7
Rachelle Rathbone <37155488+rachellerathbone@users.noreply.github.com>
2021-06-18 09:45:28 +1000

    ARC-250 new relic purge (#494)
    * update config
    
    * install library
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * delete smart commit stuff
    
    * complete installing parsing library;
    
    * wip
    
    * wip
    
    * wip
    
    * remove nr

--- src/github/index.ts
+++ src/github/index.ts
@@ -1,4 +1,3 @@
-import newrelic from 'newrelic';
 import issueComment from './issue-comment';
 import issue from './issue';
 import middleware from './middleware';
@@ -10,26 +9,31 @@ import webhookTimeout from '../middleware/webhook-timeout';
 export default (robot) => {
   // TODO: Need ability to remove these listeners, especially for testing...
   robot.on('*', (context) => {
-    // TODO: update newrelic type to latest version to fix this compilation issue: https://github.com/DefinitelyTyped/DefinitelyTyped/pull/53429
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    newrelic.setControllerName(`github/events.${context.name}`);
-    context.log({ event: context.name, action: context.payload.action }, 'Event received');
+    context.log(
+      { event: context.name, action: context.payload.action },
+      'Event received',
+    );
   });
 
-  robot.on(['issue_comment.created', 'issue_comment.edited'], webhookTimeout(middleware(issueComment)));
+  robot.on(
+    ['issue_comment.created', 'issue_comment.edited'],
+    webhookTimeout(middleware(issueComment)),
+  );
 
   robot.on(['issues.opened', 'issues.edited'], middleware(issue));
 
   robot.on('push', middleware(push));
 
-  robot.on([
-    'pull_request.opened',
-    'pull_request.closed',
-    'pull_request.reopened',
-    'pull_request.edited',
-    'pull_request_review',
-  ], middleware(pullRequest));
+  robot.on(
+    [
+      'pull_request.opened',
+      'pull_request.closed',
+      'pull_request.reopened',
+      'pull_request.edited',
+      'pull_request_review',
+    ],
+    middleware(pullRequest),
+  );
 
   robot.on('create', middleware(createBranch));
   robot.on('delete', middleware(deleteBranch));
