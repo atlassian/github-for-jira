c2f45aa8c2ea260f561a0412ff98600f83b8f22c
Tom Hombergs <thombergs@atlassian.com>
2021-06-17 16:05:25 +1000

    ARC-298: simplify redis connection

--- src/config/redis-info.ts
+++ src/config/redis-info.ts
@@ -1,41 +1,24 @@
-import url from 'url';
 import Redis from 'ioredis';
 import bunyan from 'bunyan';
 
-const REDIS_URL = process.env.REDIS_URL || '127.0.0.1';
-const redisInfo = url.parse(REDIS_URL);
-const REDIS_PORT = process.env.REDIS_BOTTLENECK_PORT || redisInfo.port || 6379;
 const logger = bunyan.createLogger({ name: 'redis info' });
 
+const REDIS_URL = process.env.REDIS_BOTTLENECK_HOST || '127.0.0.1';
+const REDIS_PORT = process.env.REDIS_BOTTLENECK_PORT || 6379;
+
 logger.info(
-  `REDIS_BOTTLENECK_HOST: ${process.env.REDIS_BOTTLENECK_HOST}\nREDIS_BOTTLENECK_PORT: ${process.env.REDIS_BOTTLENECK_PORT}\nREDIS_URL: ${REDIS_URL}\nREDIS_PORT: ${REDIS_PORT}`,
+  `REDIS_URL: ${REDIS_URL}\nREDIS_PORT: ${REDIS_PORT}`,
 );
 
-logger.info(`redisInfo: ${JSON.stringify(redisInfo)}`);
-
-let password;
-// if (redisInfo.auth?.split(':').length === 2) {
-//   password = redisInfo.auth.split(':')[1];
-// }
-
-const db = redisInfo.pathname?.split('/') || [];
-
-/**
- * @param {string} connectionName - The name for the connection
- * @returns {{REDIS_URL: string, redisOptions: import('ioredis').RedisOptions}}
- */
 export default (connectionName: string): RedisInfo => ({
-  redisUrl: REDIS_URL,
   redisOptions: {
-    password,
     port: Number(REDIS_PORT) || 6379,
     host: REDIS_URL,
-    db: db.length >= 2 ? Number(db[1]) : 0,
+    db: 0,
     connectionName,
   },
 });
 
 interface RedisInfo {
-  redisUrl: string;
   redisOptions: Redis.RedisOptions;
 }
