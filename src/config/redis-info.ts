0b65443bbf5134afcfcfa1c1ad4f91fdd96312ae
Rachelle Rathbone <rachellerathbone@gmail.com>
2021-06-17 16:28:57 +1000

    clean up logs

--- src/config/redis-info.ts
+++ src/config/redis-info.ts
@@ -1,14 +1,7 @@
 import Redis from 'ioredis';
-import bunyan from 'bunyan';
-
-const logger = bunyan.createLogger({ name: 'redis info' });
 
 const REDIS_URL = process.env.REDIS_BOTTLENECK_HOST || '127.0.0.1';
 const REDIS_PORT = process.env.REDIS_BOTTLENECK_PORT || 6379;
-
-logger.info(
-  `REDIS_URL: ${REDIS_URL}\nREDIS_PORT: ${REDIS_PORT}`,
-);
 
 export default (connectionName: string): RedisInfo => ({
   redisOptions: {
