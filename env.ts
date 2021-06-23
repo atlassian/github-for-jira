31581774412803d406e84435f9762f7c40e569c6
Michel Boudreau <michelboudreau@gmail.com>
2021-06-24 09:52:28 +1000

    ARC-128 Dockerizing - Fix for network_mode:host (#502)
    * adding ngrok to docker compose, adding automatic .env replacements
    
    * forgot to regenerate package-lock
    
    * changing deploy on push only from micros branch
    
    * changing port to 8080 instead of 4002
    
    * Removing network_mode:host in docker-compose because it doesn't work with macs
    
    * updating prestart to fail if .env doesn't exist
    
    * updating for better flow and logging

--- env.ts
+++ env.ts
@@ -1,22 +1,35 @@
-import fs from "fs";
-import axios from "axios";
+import axios, { AxiosResponse } from "axios";
+import * as fs from "fs";
 
 const filepath = ".env";
 
 // Check to see if ngrok is up and running
-axios.get("http://localhost:4040/api/tunnels", { responseType: "json" })
-  .then(response => {
-    const tunnel = response.data.tunnels.find(tunnel => tunnel.public_url.startsWith('https'));
-    const ngrokDomain = tunnel.public_url;
-    console.log(`ngrok forwarding ${ngrokDomain} to ${tunnel.config.addr}`);
+(async function main() {
 
-    // Does .env exists?
-    if (fs.existsSync(filepath)) {
-      const contents = fs.readFileSync(filepath, { encoding: "utf-8" });
-      contents.replace(/APP_URL=.*/, `APP_URL=${ngrokDomain}`);
-      contents.replace(/WEBHOOK_PROXY_URL=.*/, `WEBHOOK_PROXY_URL=${ngrokDomain}/github/events`);
-      fs.writeFileSync(filepath, contents);
-    }
-    console.log(`Updated .env file to use ngrok domain ${ngrokDomain}`);
-  }, () => console.log('Ngrok not running, skipping updating .env file.'))
-  .finally(() => process.exit());
+  // Does .env exists?
+  if (!fs.existsSync(filepath)) {
+    console.error(`.env file doesn't exist. Please create it following the steps in the CONTRIBUTING.md file.`);
+    process.exit(1);
+  }
+
+  const results = await Promise.all([
+    axios.get("http://localhost:4040/api/tunnels", { responseType: "json" }).catch(() => ({})),
+    axios.get("http://ngrok:4040/api/tunnels", { responseType: "json" }).catch(() => ({}))
+  ]);
+  const response = results.find((value: AxiosResponse) => value.status === 200) as AxiosResponse;
+  if (response) {
+    const tunnel = response.data.tunnels.find(tunnel => tunnel.public_url.startsWith("https"));
+    const ngrokDomain = tunnel.public_url;
+    console.info(`ngrok forwarding ${ngrokDomain} to ${tunnel.config.addr}`);
+
+    const contents = fs.readFileSync(filepath, { encoding: "utf-8" });
+    contents.replace(/APP_URL=.*/, `APP_URL=${ngrokDomain}`);
+    contents.replace(/WEBHOOK_PROXY_URL=.*/, `WEBHOOK_PROXY_URL=${ngrokDomain}/github/events`);
+    fs.writeFileSync(filepath, contents);
+    console.info(`Updated .env file to use ngrok domain ${ngrokDomain}`);
+  } else {
+    console.info("Ngrok not running, skipping updating .env file.");
+  }
+
+  process.exit();
+})();
