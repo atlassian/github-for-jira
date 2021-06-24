f65992a163dcbc4b8ed4b1850a89fb0f6b9749c7
KAbakumovAtl <kabakumov@atlassian.com>
2021-06-24 16:14:38 +1000

    Added healthcheck endpoint to the worker node. Changed main worker name to WebServer

--- src/worker/app.ts
+++ src/worker/app.ts
@@ -2,21 +2,31 @@
 //        instance, mainly to use the `auth()` method. Probot needs refactored
 //        so that method is more easily accessible.
 
-import { createProbot } from 'probot';
+import {Application, createProbot} from 'probot';
 import { findPrivateKey } from 'probot/lib/private-key';
+import setupDeepcheck from "../deepcheck";
 
-const probot = createProbot({
+export const probot = createProbot({
   id: Number(process.env.APP_ID),
   cert: findPrivateKey(),
 
   // These aren't needed by worker process
   secret: undefined,
-  port: undefined,
+  port: Number(process.env.WORKER_PORT) || 8080,
   webhookPath: undefined,
   webhookProxy: undefined,
 });
 
+const App = async (app: Application): Promise<Application> => {
+  // Deepcheck and Healthchecks endpoints for checking app health
+  setupDeepcheck(app);
+  return app;
+};
+
+// We are always behind a proxy, but we want the source IP
+probot.server.set('trust proxy', true);
+
 // Load an empty app so we can get access to probot's auth handling
 // eslint-disable-next-line @typescript-eslint/no-empty-function
-export default probot.load(() => {});
+export default probot.load(App);
 
