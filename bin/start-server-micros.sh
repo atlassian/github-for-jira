ebdf29d6cfc6cb5de5df590b67fc2c78199981d6
KAbakumovAtl <kabakumov@atlassian.com>
2021-06-25 14:36:53 +1000

    ARC-294 ARC-301 Updated startup script and prod docker file

--- bin/start-server-micros.sh
+++ bin/start-server-micros.sh
@@ -2,17 +2,18 @@
 
 cd "$(dirname "$0")/.."
 
-if [ "$MICROS_GROUP" = "WebServer" ]; then
-  COMMAND="start:main:production"
-fi
 
-if [ "$MICROS_GROUP" = "Worker" ]; then
-  COMMAND="start:worker:production"
-fi
-
-if [ -z "$COMMAND" ]; then
-  echo "Wrong MICROS_GROUP environment parameter: ${MICROS_GROUP}"
-  exit 1
-fi
+case "$MICROS_GROUP" in
+  "WebServer")
+    COMMAND="start:main:production"
+  ;;
+  "Worker")
+    COMMAND="start:worker:production"
+  ;;
+  *)
+    echo "Wrong MICROS_GROUP environment parameter: ${MICROS_GROUP}"
+    exit 1
+  ;;
+esac
 
 npm run "${COMMAND}"
