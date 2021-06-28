3bbe80b95345048d5747967d8903d610898a4773
Rachelle Rathbone <rachellerathbone@gmail.com>
2021-06-28 18:05:14 +1000

    trying to add logging to debug

--- src/config/sentry.ts
+++ src/config/sentry.ts
@@ -1,20 +1,31 @@
 import * as Sentry from '@sentry/node';
+import { Event } from '@sentry/node';
 import { envVars } from './environment-variables';
-import { RewriteFrames } from '@sentry/integrations';
-
+import bunyan from 'bunyan';
 const { sentryServerDSN, microsEnv, microsServiceVersion } = envVars;
+
+export const beforeSendToSentry = (event: Event) => {
+  const logger = bunyan.createLogger({ name: 'sentry' });
+  if (event.request?.data && typeof event.request.data === 'string') {
+    try {
+      logger.info('have request data');
+    } catch (err) {
+      // If for some reason the parsing or sanitization fails, remove the request body to ensure
+      // there is no bad data being sent to Sentry
+      logger.error(
+        `failed to send metrics to sentry: ${JSON.stringify(err, null, 2)}`,
+      );
+    }
+  }
+
+  return event;
+};
 
 export default (): void => {
   Sentry.init({
     dsn: sentryServerDSN,
     environment: microsEnv || 'development',
     release: microsServiceVersion,
-    integrations: [
-      new RewriteFrames({
-        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-        // @ts-ignore
-        root: global.__rootdir__,
-      }),
-    ],
+    beforeSend: beforeSendToSentry,
   });
 };
