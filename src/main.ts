826c2097090dd46fce58364f82e27f09d85f0761
Tom Hombergs <thombergs@atlassian.com>
2021-07-09 12:54:05 +1000

    ARC-10: configuring proxy for outbound calls

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ import App from './configure-robot';
 import bunyan from 'bunyan';
 import { exec } from 'child_process';
 import { initializeSentry } from './config/sentry';
+import './config/proxy';
 
 const isProd = process.env.NODE_ENV === 'production';
 const { redisOptions } = getRedisInfo('probot');
