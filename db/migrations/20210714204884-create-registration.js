b2e35d00ddc01ee251b9214c44f4524af96205aa
Ankit Kaushal <10987643+ankitkaushal01@users.noreply.github.com>
2021-07-20 12:20:24 +0530

    resolve comments

--- db/migrations/20210714204884-create-registration.js
+++ db/migrations/20210714204884-create-registration.js
@@ -1,32 +1,28 @@
-'use strict'
+
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('Registrations', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      githubHost: {
-        allowNull: false,
-        type: Sequelize.STRING
-      },
-      state: {
-        allowNull: false,
-        type: Sequelize.STRING
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    }).then(() => queryInterface.addIndex('Registrations', ['state']))
-  },
-  down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('Registrations').then(() => queryInterface.removeIndex('Registrations', ['state']))
-  }
-}
+  up: (queryInterface, Sequelize) => queryInterface.createTable('Registrations', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: Sequelize.INTEGER,
+    },
+    githubHost: {
+      allowNull: false,
+      type: Sequelize.STRING,
+    },
+    state: {
+      allowNull: false,
+      type: Sequelize.STRING,
+    },
+    createdAt: {
+      allowNull: false,
+      type: Sequelize.DATE,
+    },
+    updatedAt: {
+      allowNull: false,
+      type: Sequelize.DATE,
+    },
+  }).then(() => queryInterface.addIndex('Registrations', ['state'])),
+  down: (queryInterface, Sequelize) => queryInterface.dropTable('Registrations').then(() => queryInterface.removeIndex('Registrations', ['state'])),
+};
