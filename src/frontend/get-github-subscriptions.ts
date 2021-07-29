2aae15c7866122e8355d39a31979c2daeb25e346
Rachelle Rathbone <rachellerathbone@gmail.com>
2021-07-29 20:44:32 +1000

    update manage jira subscriptions page;

--- src/frontend/get-github-subscriptions.ts
+++ src/frontend/get-github-subscriptions.ts
@@ -7,14 +7,15 @@ export default async (req: Request, res: Response, next: NextFunction): Promise<
 	}
 
 	const { github, client, isAdmin } = res.locals;
-
 	const installationId = Number(req.params.installationId);
+
 	try {
 		const { data: { login } } = await github.users.getAuthenticated();
 		// get the installation to see if the user is an admin of it
 		const { data: installation } = await client.apps.getInstallation({ installation_id: installationId });
 		// get all subscriptions from the database for this installation ID
 		const subscriptions = await Subscription.getAllForInstallation(installationId);
+
 		// Only show the page if the logged in user is an admin of this installation
 		if (await isAdmin({
 			org: installation.account.login,
@@ -22,11 +23,13 @@ export default async (req: Request, res: Response, next: NextFunction): Promise<
 			type: installation.target_type
 		})) {
 			const { data: info } = await client.apps.getAuthenticated();
+
 			return res.render("github-subscriptions.hbs", {
 				csrfToken: req.csrfToken(),
 				nonce: res.locals.nonce,
 				installation,
 				info,
+				host: req.session.jiraHost,
 				subscriptions,
 				hasSubscriptions: subscriptions.length > 0
 			});
