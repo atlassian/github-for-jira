c57267b1c890b656a3e6b062071cdbe9508e6942
Tom Hombergs <thombergs@atlassian.com>
2021-08-10 16:56:30 +1000

    ARC-423: Add support for Feature Flags (#584)
    * update env vars for staging app
    
    * ARC-423: integrate LaunchDarkly for feature flags
    
    * ARC-423: fix unit test
    
    * ARC-423: fix moar unit test
    
    * ARC-423: remove log statement
    
    * ARC-423: refactor feature flagging to a slimmer solution
    
    * ARC-423: fix whitespaces
    
    * ARC-423: fix whitespaces
    
    * ARC-423: polishing after review
    
    * ARC-423: removed unnecessary snapshots
    
    * ARC-423: added test case comparing response contents again
    
    Co-authored-by: Michel Boudreau <michelboudreau@gmail.com>

---
+++ test/unit/config/feature-flags.test.ts
@@ -0,0 +1,31 @@
+import LaunchDarkly, { LDClient } from "launchdarkly-node-server-sdk";
+import { mocked } from "ts-jest";
+
+jest.mock("launchdarkly-node-server-sdk");
+
+describe("Feature Flag", () => {
+
+	let featureFlags;
+
+	const mockFeatureFlagValue = async (flagValue: boolean) => {
+		mocked(LaunchDarkly.init).mockReturnValue(({
+			variation: jest.fn().mockResolvedValue(flagValue),
+			waitForInitialization: jest.fn().mockResolvedValue({})
+		} as unknown) as LDClient);
+
+		// We're importing featureFlags only after mocking LaunchDarkly.init(), so
+		// that LaunchDarkly.init() is called on the mock and not on the real thing.
+		featureFlags = await import("../../../src/config/feature-flags")
+	}
+
+	it("returns true when LaunchDarkly returns true", async () => {
+		await mockFeatureFlagValue(true)
+		expect(await featureFlags.booleanFlag(featureFlags.BooleanFlags.MAINTENANCE_MODE, true, "https://myjira.atlassian.net")).toBeTruthy();
+	});
+
+	it("returns false when LaunchDarkly returns false", async () => {
+		await mockFeatureFlagValue(false)
+		expect(await featureFlags.booleanFlag(featureFlags.BooleanFlags.MAINTENANCE_MODE, true, "https://myjira.atlassian.net")).toBeFalsy();
+	});
+
+});
