a93fc767b1406a01b011bea0128dbe0c2c2efb89
Rachelle Rathbone <rachellerathbone@gmail.com>
2021-08-13 13:59:27 +1000

    initial setup

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const App = (): JSX.Element => {
+	return (
+		<div>
+			<h1>Hi, from React!</h1>
+		</div>
+	)
+}
---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+// Instantiate the plugin.
+// The `template` property defines the source
+// of a template file that this plugin will use.
+// We will create it later.
+const htmlPlugin = new HtmlWebPackPlugin({
+  template: "./public/index.html",
+});
+
+module.exports = {
+  // Our application entry point.
+  entry: "./src/index.tsx",
+
+  // These rules define how to deal
+  // with files with given extensions.
+  // For example, .tsx files
+  // will be compiled with ts-loader,
+  // a spcific loader for webpack
+  // that knows how to work with TypeScript files.
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+
+  // Telling webpack which extensions
+  // we are interested in.
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+
+  // What file name should be used for the result file,
+  // and where it should be palced.
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+
+  // Use the html plugin.
+  plugins: [htmlPlugin],
+
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 3000
+  }
+};
