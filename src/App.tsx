f0cc9f98cbae56e15b7266ae1339509ac6952ee6
Rachelle Rathbone <rachellerathbone@gmail.com>
2021-08-16 13:32:53 +1000

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Route, Link } from "react-router-dom";
 import { TestPage } from "./client/ui/Test";
 
 export const App = (): JSX.Element => {
 	return (
 		<div>
 			<h1>Hi, from React!</h1>
+			<Link to="/test">Test</Link>
 
 			<div>
 				<Route path='/test' component={TestPage} />
