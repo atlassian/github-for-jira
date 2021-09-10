6c11d69ee37a8ac471ae03b8ae7beffe2dc3b2bd
Terry Agapitos <terry@agapitos.com.au>
2021-09-10 11:58:30 +1000

    Arc 618 Resolve PR titles (#710)
    * ARC-616: Extend app descriptor to include remotelinks
    
    * ARC-616: Fix snapshot test
    
    * ARC-616: Change RemoteLinksProvider name
    
    * ARC-615 added references to new permissions
    
    * ARC-615 - added schema and transformer for github code scanning alerts
    
    * ARC-613: Update README and app.yml to include permissions on Security events for code scanning alertts
    
    * ARC-616: Add reference in jiraClient for remote links. Yet to add tests
    
    * ARC-615 - added status and description and used correct association type
    
    * ARC-615- fixed formatting
    
    * ARC-615 fix eslint concerns
    
    * ARC-615 - tests for remote links transformer
    
    * ARC-615- fixed conflicts
    
    * ARC-615 - remove dupe
    
    * ARC-615 - added test for manual code scanning alert triggers
    
    * ARC-615 - use alert number for display name, description as description (instead of full desc)
    
    * ARC-615 - refactor tests to not change payload every test
    
    * ARC-615 - format file
    
    * ARC-615 - instructions in contributing
    
    * ARC-618 - added functionality to grab PR title. added test
    
    * ARC-618 - restructured promises
    
    * ARC-618 removed duplicate logger when debugging. fixed bug when fetching PRs. don't fail whole operation if can't fetch PR titles
    
    * ARC-618 - don't fail whole request on unknown ref
    
    * ARC-618 - use proper equality operator, simplify code
    
    Co-authored-by: damoster <damoster576@gmail.com>
    Co-authored-by: Amanda Trigueiro <atrigueiro@atlassian.com>

--- src/github/code-scanning-alert.ts
+++ src/github/code-scanning-alert.ts
@@ -1,15 +1,15 @@
 import transformCodeScanningAlert from "../transforms/code-scanning-alert";
 import { Context } from "probot/lib/context";
+import logger from "../config/logger";
 
 export default async (context: Context, jiraClient): Promise<void> => {
 	const jiraPayload = await transformCodeScanningAlert(context);
 
 	if (!jiraPayload) {
-		context.log({noop: "no_jira_payload_code_scanning_alert"}, "Halting further execution for code scanning alert since jiraPayload is empty");
+		logger.info({noop: "no_jira_payload_code_scanning_alert"}, "Halting further execution for code scanning alert since jiraPayload is empty");
 		return;
 	}
 
-	context.log(`Sending code scanning alert event as Remote Link to Jira: ${jiraClient.baseURL}`)
-
+	logger.info(`Sending code scanning alert event as Remote Link to Jira: ${jiraClient.baseURL}`);
 	await jiraClient.remoteLink.submit(jiraPayload);
 };
