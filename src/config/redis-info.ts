1f9dee8189b0e76b36c710009ca52951be8c2fbf
Michel Boudreau <michelboudreau@gmail.com>
2021-09-13 14:14:03 +1000

    adding redisx to resources

--- src/config/redis-info.ts
+++ src/config/redis-info.ts
@@ -2,8 +2,8 @@ import Redis from "ioredis";
 
 export default (connectionName: string): RedisInfo => ({
 	redisOptions: {
-		port: Number(process.env.REDIS_BOTTLENECK_PORT) || 6379,
-		host: process.env.REDIS_BOTTLENECK_HOST || "127.0.0.1",
+		port: Number(process.env.REDIS_CACHE_PORT) || 6379,
+		host: process.env.REDIS_CACHE_HOST || "127.0.0.1",
 		db: 0,
 		connectionName
 	}
