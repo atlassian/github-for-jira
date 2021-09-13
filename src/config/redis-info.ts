7aa0f0bfc16567429d76720ce600086d3bbf944f
Michel Boudreau <michelboudreau@gmail.com>
2021-09-14 08:56:58 +1000

    enabling TLS for redis

--- src/config/redis-info.ts
+++ src/config/redis-info.ts
@@ -1,8 +1,10 @@
 import Redis from "ioredis";
+import isProd from "../jira/util/isProd";
 
 export default (connectionName: string): Redis.RedisOptions => ({
 	port: Number(process.env.REDISX_CACHE_PORT) || 6379,
 	host: process.env.REDISX_CACHE_HOST || "127.0.0.1",
 	db: 0,
+	tls: isProd() ? { checkServerIdentity: () => undefined } : undefined,
 	connectionName
 });
