ceb2de81bfff9ad1e428e00f45e90883c49ed4e9
Tom Hombergs <thombergs@atlassian.com>
2021-09-17 16:40:25 +1000

    explicitly disabling throttling, delaying on all 403s (#754)

--- src/config/github-api.ts
+++ src/config/github-api.ts
@@ -5,5 +5,8 @@ import { Options } from "probot/lib/github";
 
 export default (options: Partial<Options> = {}): GitHubAPI => {
 	options.logger = options.logger || getLogger("config.github-api");
+	options.throttle = {
+		enabled: false,
+	}
 	return enhanceOctokit(GitHubAPI(options as Options));
 }
