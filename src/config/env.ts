0df2d8e7643150841b2688ff0caef2f68fd2ab02
Michel Boudreau <michelboudreau@gmail.com>
2021-09-23 00:24:53 +1000

    Changing external proxy for whitelist proxy

--- src/config/env.ts
+++ src/config/env.ts
@@ -26,10 +26,9 @@ if (env.error && nodeEnv !== EnvironmentEnum.production) {
 	throw env.error;
 }
 
-const getProxyFromEnvironment = (): string => {
-	const proxyHost = process.env.EXTERNAL_ONLY_PROXY_HOST;
-	const proxyPort = process.env.EXTERNAL_ONLY_PROXY_PORT;
-	return proxyHost && proxyPort ? `http://${proxyHost}:${proxyPort}` : undefined;
+const getProxyFromEnvironment = (): string | undefined => {
+	const proxy = process.env.WHITELIST_PROXY;
+	return proxy ? `http://${proxy}` : undefined;
 };
 
 // TODO: Make envvars dynamic
