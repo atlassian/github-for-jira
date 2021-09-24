b0a155dbc77ff27909aa9665749621372d4d6b5f
Rachelle <rachellerathbone@gmail.com>
2021-09-24 19:39:01 +1000

    changes now off main base

--- static/js/github-configuration.js
+++ static/js/github-configuration.js
@@ -1,40 +1,72 @@
 /* globals $ */
-$('.install-link').click(function (event) {
-  event.preventDefault()
+$(".install-link").click(function (event) {
+	event.preventDefault();
 
-  $.post('/github/configuration', {
-    installationId: $(event.target).data('installation-id'),
-    _csrf: document.getElementById('_csrf').value,
-    clientKey: document.getElementById('clientKey').value
-  }, function (data) {
-    if (data.err) {
-      return console.log(data.err)
-    }
-    window.close()
-  })
-})
+	$.post(
+		"/github/configuration",
+		{
+			installationId: $(event.target).data("installation-id"),
+			_csrf: document.getElementById("_csrf").value,
+			clientKey: document.getElementById("clientKey").value,
+		},
+		function (data) {
+			if (data.err) {
+				return console.log(data.err);
+			}
+			window.close();
+		}
+	);
+});
 
-$('.delete-link').click(function (event) {
-  event.preventDefault()
+$(".delete-link").click(function (event) {
+	event.preventDefault();
 
-  $.post('/github/subscription', {
-    installationId: $(event.target).data('installation-id'),
-    jiraHost: $(event.target).data('jira-host'),
-    _csrf: document.getElementById('_csrf').value
-  }, function (data) {
-    if (data.err) {
-      return console.log(data.err)
-    }
-    window.close()
-  })
-})
+	$.post(
+		"/github/subscription",
+		{
+			installationId: $(event.target).data("installation-id"),
+			jiraHost: $(event.target).data("jira-host"),
+			_csrf: document.getElementById("_csrf").value,
+		},
+		function (data) {
+			if (data.err) {
+				return console.log(data.err);
+			}
+			window.close();
+		}
+	);
+});
 
-$('.logout-link').click(function (event) {
-  event.preventDefault();
+$(".logout-link").click(function (event) {
+	event.preventDefault();
 
-	window.open('https://github.com/logout','_blank');
+	window.open("https://github.com/logout", "_blank");
 
-	window.setTimeout(function() {
-		this.close()
-	}, 100)
-})
+	window.setTimeout(function () {
+		this.close();
+	}, 100);
+});
+
+$(".sync-connection-link").click(function (event) {
+	event.preventDefault();
+	const installationId = $(event.target).data("installation-id");
+	const jiraHost = $(event.target).data("jira-host");
+	const csrfToken = document.getElementById("_csrf").value;
+
+	$.ajax({
+		type: "POST",
+		url: `/jira/sync`,
+		data: {
+			installationId,
+			jiraHost,
+			syncType: "full",
+			_csrf: csrfToken,
+		},
+		success: function (data) {
+			window.close();
+		},
+		error: function (error) {
+			console.log(error);
+		},
+	});
+});
