316884ddf76a70fdf99cff8c93db812fc8ea68f1
Rachelle <rachellerathbone@gmail.com>
2021-09-28 16:24:52 +1000

    update histogram metrics

--- src/util/webhooks.ts
+++ src/util/webhooks.ts
@@ -8,37 +8,58 @@ export const calculateProcessingTimeInSeconds = (
 	webhookName: string,
 	contextLogger: any,
 	status?: number
-): number | undefined => {
+): number | undefined | void => {
 	const currentTime = getCurrentTime();
 
-	// only send logs if time of webhookReceived occurred before the currentTime
-	// and if webhookReceivedTime is not null/undefined
-	if (webhookReceivedTime < currentTime) {
-		const timeToProcessWebhookEvent = getCurrentTime() - webhookReceivedTime;
+	try {
+		// only send logs if time of webhookReceived occurred before the currentTime
+		// and if webhookReceivedTime is not null/undefined
+		if (webhookReceivedTime < currentTime) {
+			const timeToProcessWebhookEvent = getCurrentTime() - webhookReceivedTime;
 
-		contextLogger.info(
-			"timeToProcessWebhookEvent: ",
-			timeToProcessWebhookEvent
+			contextLogger.info(
+				"timeToProcessWebhookEvent: ",
+				timeToProcessWebhookEvent
+			);
+
+			contextLogger.info(
+				{ webhookName },
+				`Webhook processed in ${timeToProcessWebhookEvent} milliseconds`
+			);
+
+			const tags = {
+				name: webhookName,
+				status: status?.toString() || "none",
+			};
+
+			// send metrics without gsd_histogram tag so we still have .count, .p99, .median etc
+			statsd.histogram(
+				metricWebhooks.webhookEvent,
+				timeToProcessWebhookEvent,
+				tags
+			);
+
+			const histogramBuckets =
+				"1000_10000_30000_60000_120000_300000_600000_3000000";
+
+			tags["gsd_histogram"] = histogramBuckets;
+
+			// send metrics with gsd_histogram so it will be treated as a histogram-type metric
+			// https://hello.atlassian.net/wiki/spaces/OBSERVABILITY/pages/797646144/Reference+-+Understanding+histogram-type+metrics#So%2C-how-do-I-get-started%3F
+			statsd.histogram(
+				metricWebhooks.webhookEvent,
+				timeToProcessWebhookEvent,
+				tags
+			);
+
+			return timeToProcessWebhookEvent;
+		} else {
+			return undefined;
+		}
+	} catch (err) {
+		contextLogger?.error(
+			{ err },
+			"Failed to send webhook processing time metrics."
 		);
-
-		contextLogger.info(
-			{ webhookName },
-			`Webhook processed in ${timeToProcessWebhookEvent} milliseconds`
-		);
-
-		const tags = {
-			name: webhookName,
-			status: status?.toString() || "none",
-		};
-
-		statsd.histogram(
-			metricWebhooks.webhookEvent,
-			timeToProcessWebhookEvent,
-			tags
-		);
-
-		return timeToProcessWebhookEvent;
-	} else {
-		return undefined;
 	}
 };
