1ea3e0857a73a3fdfe3a1a04255ad2fa57564d2d
Rachelle <rachellerathbone@gmail.com>
2021-10-08 17:28:40 +1100

    disable btn - need to clean up

--- src/frontend/post-github-setup.ts
+++ src/frontend/post-github-setup.ts
@@ -9,23 +9,25 @@ Renders https://jira.github.com/github/setup and prompts user to enter a Jira si
 User is either prompted to login into GitHub, or if already logged in, is redirected to Jira Marketplace.
 */
 export default async (req: Request, res: Response): Promise<void> => {
-	const { jiraSubdomain, jiraDomain } = req.body;
+	const { jiraSubdomain, jiraSubdomainModal, jiraDomain } = req.body;
+
+	const subdomain = jiraSubdomain || jiraSubdomainModal;
 
 	req.log.info(
-		`Received github setup page request for jira ${jiraSubdomain}.atlassian.net`
-		// `Received github setup page request for jira ${jiraSubdomain}.${jiraDomain}`
+		`Received github setup page request for jira ${subdomain}.atlassian.net`
+		// `Received github setup page request for jira ${subdomain}.${jiraDomain}`
 	);
 
-	req.log.info("HERE: ", res)
+	req.log.info("HERE: ", res);
 
 	// TODO UPDATE CORRECTLY AFTER JADE UPDATES
-	if (!validJiraDomains(jiraSubdomain, "atlassian.net")) {
+	if (!validJiraDomains(subdomain, "atlassian.net")) {
 		res.status(400);
 
 		if (await booleanFlag(BooleanFlags.NEW_SETUP_PAGE, true)) {
 			return res.render("github-setup.hbs", {
 				error: "The entered Jira Cloud Site is not valid",
-				jiraSubdomain,
+				subdomain,
 				nonce: res.locals.nonce,
 				jiraDomainOptions: jiraDomainOptions("atlassian.net"),
 				csrfToken: req.csrfToken(),
@@ -33,7 +35,7 @@ export default async (req: Request, res: Response): Promise<void> => {
 		} else {
 			return res.render("github-setup-OLD.hbs", {
 				error: "The entered Jira Cloud Site is not valid",
-				jiraSubdomain,
+				subdomain,
 				nonce: res.locals.nonce,
 				jiraDomainOptions: jiraDomainOptions(jiraDomain),
 				csrfToken: req.csrfToken(),
@@ -41,7 +43,7 @@ export default async (req: Request, res: Response): Promise<void> => {
 		}
 	}
 
-	req.session.jiraHost = `https://${jiraSubdomain}.atlassian.net`;
+	req.session.jiraHost = `https://${subdomain}.atlassian.net`;
 	// req.session.jiraHost = `https://${jiraSubdomain}.${jiraDomain}`;
 
 	res.redirect(
--- static/js/github-setup.js
+++ static/js/github-setup.js
@@ -13,3 +13,35 @@ if (cancelNewJiraSiteBtn != null) {
 		newJiraSiteModal.style.display = "none";
 	};
 }
+
+$(document).ready(() => {
+	$(".githubSetup__form__input").on("input", () => {
+		const jiraSubdomain = $("#jiraSubdomain").val();
+
+		if (jiraSubdomain.length > 0) {
+			$("#jiraSubdomainSubmitBtn").prop("disabled", false);
+			$("#jiraSubdomainSubmitBtn").attr("aria-disabled", "false");
+		}
+
+		if (jiraSubdomain.length === 0) {
+			$("#jiraSubdomainSubmitBtn").prop("disabled", true);
+			$("#jiraSubdomainSubmitBtn").attr("aria-disabled", "true");
+		}
+	});
+});
+
+$(document).ready(() => {
+	$(".githubSetup__form__input__modal").on("input", () => {
+		const jiraSubdomainModal = $("#jiraSubdomainModal").val();
+
+		if (jiraSubdomainModal.length > 0) {
+			$("#jiraSubdomainModalSubmitBtn").prop("disabled", false);
+			$("#jiraSubdomainModalSubmitBtn").attr("aria-disabled", "false");
+		}
+
+		if (jiraSubdomainModal.length === 0) {
+			$("#jiraSubdomainModalSubmitBtn").prop("disabled", true);
+			$("#jiraSubdomainModalSubmitBtn").attr("aria-disabled", "true");
+		}
+	});
+});
