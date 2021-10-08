19062019e4dc1a986001a9272e7f95fb64fdbd15
Rachelle <rachellerathbone@gmail.com>
2021-10-08 15:15:26 +1100

    address pr comments

--- src/frontend/app.ts
+++ src/frontend/app.ts
@@ -299,12 +299,9 @@ export default (octokitApp: App): Express => {
 		};
 
 		const errorStatusCode = errorCodes[err.message] || 500;
-
 		const tags = [`status: ${errorStatusCode}`];
 
 		statsd.increment(metricError.githubErrorRendered, tags);
-
-		const jiraHost = req.session.jiraHost ? req.session.jiraHost : "";
 
 		const newErrorPgFlagIsOn = await booleanFlag(BooleanFlags.NEW_GITHUB_ERROR_PAGE, false, jiraHost);
 		const errorPageVersion = newErrorPgFlagIsOn ? "github-error.hbs" : "github-error-OLD.hbs"
