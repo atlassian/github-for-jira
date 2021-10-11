0716fa1a2592c7785d439673e7f51928f945cd6b
Rachelle <rachellerathbone@gmail.com>
2021-10-12 10:13:12 +1100

    merge main

--- src/util/getUrl.ts
+++ src/util/getUrl.ts
@@ -1,7 +1,12 @@
+
 import Logger from "bunyan";
 import url from "url";
 
-export const getGitHubConfigurationUrl = (githubHost, jwt, jiraHost): string =>
+export const getGitHubConfigurationUrl = (
+	githubHost,
+	jwt,
+	jiraHost
+): string =>
 	`https://${githubHost}/github/configuration?jwt=${jwt}&xdm_e=${jiraHost}`;
 
 export const getJiraMarketplaceUrl = (jiraHost: string): string =>
@@ -16,19 +21,13 @@ export const getJiraHostFromRedirectUrl = (url: string): string => {
 	}
 };
 
-export const getJiraHostFromRedirectUrlNew = (
-	urlOrPath: string,
-	log: Logger
-): string => {
+export const getJiraHostFromRedirectUrlNew = (urlOrPath: string, log: Logger): string => {
 	if (!urlOrPath) {
 		return "empty";
 	}
 	try {
 		const { host, query } = url.parse(urlOrPath, true);
-		return (
-			(query?.xdm_e ? url.parse(query.xdm_e.toString(), false).host : host) ||
-			"unknown"
-		);
+		return (query?.xdm_e ? url.parse(query.xdm_e.toString(), false).host : host) || "unknown";
 	} catch (err) {
 		log.error(err, "Cannot detect jiraHost from redirect URL");
 		return "error";
