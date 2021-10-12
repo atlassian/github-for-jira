1668bdffadb7aeb22048fe25ddb9223483b221de
thombergs <tom.hombergs@gmail.com>
2021-10-12 18:17:24 +1100

    ARC-655: first attempt at a structured backfilling algorithm.

---
+++ src/backfill/api.ts
@@ -0,0 +1,208 @@
+/**
+ * A processing step in a job. A job consists of multiple steps.
+ *
+ * A step does not hold any information about what to do exactly. The StepProcessor will decide based on the JobState
+ * what exactly to do.
+ */
+export interface Step<JOB_ID> {
+	jobId: JOB_ID;
+}
+
+/**
+ * Processes a single step of a job.
+ *
+ * In practice, a processor will load data from a data source, transform it into another shape, and send it to
+ * a data sink.
+ */
+export interface StepProcessor<JOB_STATE> {
+
+	/**
+	 * Processes the next step in the job. Can make use of the JobState to determine what exactly it needs to do
+	 * as the next step.
+	 */
+	process(jobState: JOB_STATE): StepResult;
+
+	/**
+	 * This method is called when the processing of the step has failed for some reason and the same processing
+	 * shouldn't be retried.
+	 *
+	 * An implementation of this function must increment some kind of cursor in the job state so that the next step of the
+	 * job doesn't try to do the exact same thing again (and fail again). By moving a cursor, the step is basically skipped.
+	 */
+	skip();
+}
+
+export interface StepResult {
+
+	/**
+	 * True if the processing step was successful. False if the processing was not successful. In this case, the error
+	 * field should be populated with information about the error.
+	 */
+	success: boolean;
+
+	/**
+	 * Set to true if the whole job is finished (i.e. if this was the last step).
+	 */
+	jobFinished: boolean;
+
+	/**
+	 * Information about the state of the rate limit for the job after successful or unsuccessful processing of the step.
+	 */
+	rateLimit?: RateLimitState;
+
+	/**
+	 * Information about the error, if an error has occurred during processing.
+	 */
+	error?: {
+		message: string;
+		isRetryable: boolean;
+		isFatal: boolean;
+	}
+}
+
+/**
+ * A factory that can decide what processor should process this step based on some state in the database.
+ */
+export interface StepPrioritizer<JOB_ID, JOB_STATE> {
+
+	/**
+	 * Decides which StepProcessor should process the step. Can make a decision based on the JobState and RateLimitState.
+	 *
+	 * If the job entails the processing items of different types, for example, this method can prioritize between them.
+	 * It can also prioritize based on recency (i.e. which item type has longest not been updated), or on the state of
+	 * a rate limit.
+	 */
+	getStepProcessor(step: Step<JOB_ID>, jobState: JOB_STATE, rateLimitState?: RateLimitState): StepProcessor<JOB_STATE>;
+}
+
+
+export class RateLimitState {
+
+	/**
+	 * The rate limit budget that is left. If 0, the rate limit is hit.
+	 */
+	budgetLeft: number;
+
+	/**
+	 * The date at which the rate limit will refresh (i.e. the budget will increase).
+	 */
+	refreshDate: Date;
+}
+
+/**
+ * Interface to a database that stores state about the Job.
+ */
+export interface JobStore<JOB_ID, JOB_STATE> {
+
+	/**
+	 * Returns the jobState for the given job.
+	 */
+	getJobState(jobId: JOB_ID): JOB_STATE;
+
+	/**
+	 * Increments the number of failed attempts for the current step of the given job in the database.
+	 */
+	setFailedAttemptsCount(jobId: JOB_ID, count: number);
+
+	/**
+	 * Retrieves the number of failed attempts for the current step of the given job from the database.
+	 */
+	getFailedAttemptsCount(jobId: JOB_ID);
+
+	/**
+	 * Updates the rate limit state for the given job in the database.
+	 */
+	updateRateLimitState(jobId: JOB_ID, rateLimitState: RateLimitState);
+
+	/**
+	 * Retrieves the current rate limit state for the given job in the database.
+	 * @param jobId
+	 */
+	getRateLimitState(jobId: JOB_ID): RateLimitState | undefined;
+}
+
+/**
+ * Strategy that decides if a step should be retried and how long a retry should be delayed. Retrying means that the
+ * next step of the job should try to do exactly the same as the previous step, because it's expected to work after retrying a few times.
+ */
+export interface RetryStrategy {
+
+	/**
+	 * Decides based on the already failed attempts if a step should be retried and how long the retry should be delayed.
+	 */
+	getRetry(failedAttempts: number): Retry;
+}
+
+export interface Retry {
+	shouldRetry: boolean;
+	retryAfterSeconds?: number;
+}
+
+/**
+ * Strategy that decides how long to delay based on the state of a rate limit.
+ */
+export interface RateLimitStrategy {
+
+	/**
+	 * Calculates number of seconds the next step of the job should wait to not trigger the rate limit.
+	 */
+	getDelayInSeconds(rateLimitState?: RateLimitState): number;
+}
+
+/**
+ * Calculates a maximum delay of 15 minutes so that it works with restriction of the maximum 15 minute delay in SQS
+ * queues.
+ */
+export class SqsRateLimitStrategy implements RateLimitStrategy {
+
+	public getDelayInSeconds(rateLimitState?: RateLimitState): number {
+
+		// No rate limit information -> no delay.
+		if (!rateLimitState) {
+			return 0;
+		}
+
+		// We still have budget left -> no delay.
+		if (rateLimitState.budgetLeft > 0) {
+			return 0;
+		}
+
+		const now = new Date();
+
+		// If the refresh date is in the past, we just assume that the rate limit has refreshed already,
+		// so no delay is needed.
+		if (rateLimitState.refreshDate.getTime() < now.getTime()) {
+			return 0;
+		}
+
+		// Otherwise, calculate the delay from the rate limit refresh date.
+		const delay = (now.getTime() - rateLimitState.refreshDate.getTime()) / 1000;
+
+		return Math.min(delay, 15 * 60);
+	}
+
+}
+
+/**
+ * A RetryStrategy that simply retries a fix number of times.
+ */
+export class BackoffRetryStrategy implements RetryStrategy {
+
+	private readonly retries: number;
+	private readonly initialDelayInSeconds: number;
+	private readonly backoffMultiplier: number;
+
+	constructor(retries: number, backoffMultiplier: number, initialDelayInSeconds: number) {
+		this.retries = retries;
+		this.backoffMultiplier = backoffMultiplier;
+		this.initialDelayInSeconds = initialDelayInSeconds;
+	}
+
+	getRetry(failedAttempts: number): Retry {
+		return {
+			shouldRetry: failedAttempts <= this.retries,
+			retryAfterSeconds: this.initialDelayInSeconds * Math.pow(this.backoffMultiplier, failedAttempts)
+		};
+	}
+
+}
---
+++ src/backfill/backfiller.ts
@@ -0,0 +1,152 @@
+import {
+	JobStore,
+	StepResult,
+	StepPrioritizer,
+	RateLimitState,
+	RateLimitStrategy, Step, RetryStrategy, StepProcessor
+} from "./api";
+import Logger from "bunyan";
+
+/**
+ * Information about the next action that should be taken after a step has been processed.
+ */
+export interface NextAction<JOB_ID> {
+
+	/**
+	 * Identifier for the job.
+	 */
+	jobId: JOB_ID;
+
+	/**
+	 * If this is true, the job isn't finished, yet. Go ahead and schedule the next step of the job.
+	 * If this is false, the job is finished.
+	 */
+	scheduleNextStep: boolean;
+
+	/**
+	 * Number of seconds to delay the next step of the job to not run into rate limits (or to wait
+	 * until a rate limit is refreshed).
+	 */
+	delayInSeconds?: number;
+}
+
+export class Backfiller<JOB_ID, JOB_STATE> {
+
+	private readonly prioritizer: StepPrioritizer<JOB_ID, JOB_STATE>;
+	private readonly jobStore: JobStore<JOB_ID, JOB_STATE>;
+	private readonly rateLimitStrategy: RateLimitStrategy;
+	private readonly retryStrategy: RetryStrategy;
+	private readonly logger: Logger;
+
+	constructor(
+		prioritizer: StepPrioritizer<JOB_ID, JOB_STATE>,
+		jobStore: JobStore<JOB_ID, JOB_STATE>,
+		rateLimitStrategy: RateLimitStrategy,
+		retryStrategy: RetryStrategy,
+		logger: Logger
+	) {
+		this.prioritizer = prioritizer;
+		this.jobStore = jobStore;
+		this.rateLimitStrategy = rateLimitStrategy;
+		this.retryStrategy = retryStrategy;
+		this.logger = logger;
+	}
+
+	/**
+	 * Processes the next step of the given job. This method should be called again and again for the same job until
+	 * the return NextAction.scheduleNextStep is false. If NextAction.delayInSeconds is set, don't call the method
+	 * for the same job again before this number of seconds has passed to honor rate limits.
+	 */
+	public processStep(step: Step<JOB_ID>): NextAction<JOB_ID> {
+
+		const currentRateLimitState = this.jobStore.getRateLimitState(step.jobId);
+
+		// If the rate limit is hit, delay the next step of the job accordingly.
+		if (this.rateLimitStrategy.getDelayInSeconds(currentRateLimitState) > 0
+		) {
+			return this.continueJobWithRateLimit(step, currentRateLimitState);
+		}
+
+		// find the right processor and process the next step of the job
+		const jobState = this.jobStore.getJobState(step.jobId);
+		const stepProcessor = this.prioritizer.getStepProcessor(step, jobState, currentRateLimitState);
+		const stepResult = stepProcessor.process(jobState);
+
+		// store the new rate limit state in the database
+		this.updateRateLimitState(stepResult, step);
+
+		// the job is finished, don't schedule another step
+		if (stepResult.jobFinished) {
+			return this.stopJob(step);
+		}
+
+		// in case of success, we want to schedule the job back on the queue
+		if (stepResult.success) {
+			return this.continueJobWithRateLimit(step, stepResult.rateLimit);
+		}
+
+		// in case of an error we want to throw an error to let the queueing system know that we want to retry
+		if (!stepResult.error || stepResult.error?.isRetryable) {
+			this.logger.error(`Retrying step ${JSON.stringify(step)} due to error: ${stepResult.error?.message}`);
+			return this.retryStep(step, stepProcessor, stepResult);
+		} else if (stepResult.error.isFatal) {
+			this.logger.error(`Stopping job ${step.jobId} due to error: ${stepResult.error?.message}`);
+			return this.stopJob(step);
+		} else {
+			this.logger.error(`Skipping step ${JSON.stringify(step)} due to error: ${stepResult.error?.message}`);
+			return stepProcessor.skip();
+		}
+	}
+
+	/**
+	 * If the RetryStrategy determines the step should be retried, increment the failed attempts counter and return an
+	 * action to continue the job.
+	 *
+	 * If the retry limit has been reached, skip the step.
+	 */
+	private
+
+	retryStep(step: Step<JOB_ID>, stepProcessor: StepProcessor<JOB_STATE>, stepResult: StepResult): NextAction<JOB_ID> {
+		const failedAttempts = this.jobStore.getFailedAttemptsCount(step.jobId);
+
+		const retry = this.retryStrategy.getRetry(failedAttempts);
+
+		if (retry.shouldRetry
+		) {
+			// Retry the same step again, honoring the retry delay.
+			this.jobStore.setFailedAttemptsCount(step.jobId, failedAttempts + 1);
+			return this.continueJobWithDelay(step, retry.retryAfterSeconds);
+		} else {
+			// Don't retry. Reset the failed attempts and skip the current step.
+			this.jobStore.setFailedAttemptsCount(step.jobId, 0);
+			stepProcessor.skip();
+		}
+
+		return this.continueJobWithRateLimit(step, stepResult.rateLimit);
+	}
+
+	private continueJobWithDelay(step: Step<JOB_ID>, delayInSeconds ?: number): NextAction<JOB_ID> {
+		return {
+			jobId: step.jobId,
+			scheduleNextStep: true,
+			delayInSeconds: delayInSeconds
+		};
+	}
+
+	private continueJobWithRateLimit(step: Step<JOB_ID>, rateLimitState ?: RateLimitState): NextAction<JOB_ID> {
+		return this.continueJobWithDelay(step, this.rateLimitStrategy.getDelayInSeconds(rateLimitState));
+	}
+
+	private stopJob(step: Step<JOB_ID>): NextAction<JOB_ID> {
+		return {
+			jobId: step.jobId,
+			scheduleNextStep: false,
+		};
+	}
+
+	private updateRateLimitState(stepResult: StepResult, step: Step<JOB_ID>) {
+		if (stepResult.rateLimit) {
+			this.jobStore.updateRateLimitState(step.jobId, stepResult.rateLimit);
+		}
+	}
+}
