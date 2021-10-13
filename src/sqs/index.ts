aa320f343fe9ea8f73483f0049efc139e4b0a8d7
KAbakumovAtl <kabakumov@atlassian.com>
2021-10-13 17:58:50 +1100

    Base sqs processing code

---
+++ src/sqs/index.ts
@@ -0,0 +1,136 @@
+import AWS, {AWSError} from "aws-sdk";
+import Logger from "bunyan"
+import {getLogger} from "../config/logger"
+import {DeleteMessageRequest, Message, ReceiveMessageResult} from "aws-sdk/clients/sqs";
+import { uuidv4 } from "uuid";
+
+const logger = getLogger("sqs")
+
+/**
+ * Message processing context, which will be passed to message handler to handle the received message
+ */
+export type Context<MessagePayload> = {
+
+	/**
+	 * Message payload
+	 */
+	payload: MessagePayload;
+
+	/**
+	 * Oritinal SQS Mesage
+	 */
+	message: Message;
+
+	/**
+	 * Context logger, which has parameters for the processing context (like message id, execution id, etc)
+	 */
+	log: Logger;
+}
+
+/**
+ * Handler for the queue messages
+ */
+export interface MessageHandler<MessagePayload> {
+	handle(context: Context<MessagePayload>);
+}
+
+export class SqsQueue<MessagePayload> {
+	queueUrl: string;
+	queueName: string;
+	queueRegion: string;
+	sqs: AWS.SQS;
+
+	constructor(queueName: string, queueUrl: string, queueRegion: string) {
+		this.queueUrl = queueUrl;
+		this.queueName = queueName;
+		this.queueRegion = queueRegion;
+		this.sqs = new AWS.SQS({apiVersion: "2012-11-05", region: queueRegion});
+	}
+
+	sendMessage(payload: MessagePayload, log?: Logger) {
+		const params = {
+			MessageBody: JSON.stringify(payload),
+			QueueUrl: this.queueUrl
+		};
+		this.sqs.sendMessage(params, (err, data) => {
+			if (err) {
+				( log || logger).error(err, "Error sending SQS message");
+			} else {
+				( log || logger).info(`Successfully added message to sqs queue messageId: ${data.MessageId}`);
+			}
+		});
+	}
+
+	deleteMessage(message: Message) {
+
+		if(!message.ReceiptHandle) {
+			logger.error({ message }, "Unable to delete message, ReceiptHandle parameter is missing");
+			return;
+		}
+
+		const deleteParams: DeleteMessageRequest = {
+			QueueUrl: this.queueUrl,
+			ReceiptHandle: message.ReceiptHandle || ""
+		};
+		this.sqs.deleteMessage(deleteParams, (err, data) => {
+			if (err) {
+				logger.error({err, data}, "Error deleting message from the queue");
+			} else {
+				logger.debug({data}, "Successfully deleted message from queue");
+			}
+		});
+	}
+
+	executeMessage(message: Message, messageHandler: MessageHandler<MessagePayload>) {
+		const payload = message.Body ? JSON.parse(message.Body) : {};
+
+		const log = logger.child({id: message.MessageId, executionId: uuidv4()})
+
+		const context: Context<MessagePayload> = {message, payload, log }
+
+		log.info({messagePayload: payload}, "Sqs message received");
+
+		try {
+			messageHandler.handle(context)
+			this.deleteMessage(message)
+		} catch (err) {
+			log.error(err, "error executing sqs message")
+		}
+	}
+
+	async handleSqsResponse(err: AWSError, data: ReceiveMessageResult, messageHandler) {
+		if (err) {
+			logger.error({err}, `Error receiving message from SQS queue, queueName ${this.queueName}`);
+		} else {
+			if (!data.Messages) {
+				logger.debug("Nothing to process");
+				return;
+			}
+
+			await Promise.all(data.Messages.map(message => {
+				return new Promise<void>((resolve) => {
+					this.executeMessage(message, messageHandler)
+					resolve()
+				})
+			})
+			)
+		}
+	}
+
+	listen(messageHandler) {
+		// Setup the receiveMessage parameters
+		const params = {
+			QueueUrl: this.queueUrl,
+			MaxNumberOfMessages: 1,
+			VisibilityTimeout: 0,
+			WaitTimeSeconds: 5
+		};
+		this.sqs.receiveMessage(params, async (err, data) => {
+			try {
+				await this.handleSqsResponse(err, data, messageHandler)
+			} finally {
+				this.listen(messageHandler)
+			}
+		});
+	}
+}
