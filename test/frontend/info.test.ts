eb2213d4ca3f24f89b6a8d4573f067f97e65b528
yvonnevictoria <28718897+yvonnevictoria@users.noreply.github.com>
2021-10-14 17:24:29 +1100

    arc-384 update info.test

--- test/frontend/info.test.ts
+++ test/frontend/info.test.ts
@@ -9,7 +9,7 @@ describe("/info", () => {
 		app.use("/info", info);
 	});
 
-	it("should return 200 and info", async () => {
+	it("should return 200 with relevant git commit link and date", async () => {
 		process.env.COMMIT_SHA = "34b6df6ca06f4ba9516a6e943b90bdb148b3f0e8";
 		process.env.COMMIT_DATE = "Wed Oct 13 16:32:22 2021 +1100";
 
