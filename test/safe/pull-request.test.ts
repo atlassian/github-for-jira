93a0aa2c2f6808e70a721168dde3fc2f98639e6a
tpurohit <tpurohit@atlassian.com>
2021-10-21 12:30:29 +1100

    Initial take on more meaning full integration tests

--- test/safe/pull-request.test.ts
+++ test/safe/pull-request.test.ts
@@ -1,6 +1,20 @@
+import { mocked } from 'ts-jest/utils';
 /* eslint-disable @typescript-eslint/no-var-requires */
 import { createWebhookApp } from "../utils/probot";
 import { Application } from "probot";
+import { Installation, Subscription } from "../../src/models";
+import nock from "nock";
+
+jest.mock("../../src/models");
+// jest.mock("../../src/jira/client");
+// jest.mock("../../src/jira/util", () => {
+// 	return jest.fn().mockImplementation(
+// 		() => {
+// 			return {
+// 				unfurl: jest.fn(() => "[TEST-123] body of the test pull request.\n\n[TEST-123]: https://test-atlassian-instance.net/browse/TEST-123")
+// 			}
+// 		})
+// });
 
 describe("GitHub Actions", () => {
 	let app: Application;
@@ -8,8 +22,34 @@ describe("GitHub Actions", () => {
 		app = await createWebhookApp();
 	});
 
+	afterEach(() => {
+		nock.cleanAll();
+		jest.restoreAllMocks(); 
+	});	
+
 	describe("pull_request", () => {
-		it("should update the Jira issue with the linked GitHub pull_request", async () => {
+		beforeEach(() => {
+			mocked(Subscription.getAllForInstallation).mockResolvedValue([
+				{
+					jiraHost: process.env.ATLASSIAN_URL,
+					gitHubInstallationId: 1234,
+					enabled: true
+				}] as any);
+			mocked(Subscription.getSingleInstallation).mockResolvedValue(
+				{
+					id: 1,
+					jiraHost: process.env.ATLASSIAN_URL
+				} as any);
+			mocked(Installation.getForHost).mockResolvedValue(
+				{
+					jiraHost: process.env.ATLASSIAN_URL,
+					sharedSecret: process.env.ATLASSIAN_SECRET,
+					enabled: true
+				} as any
+			);
+		});
+
+		it("should have reviewers on pull request action", async () => {
 			const fixture = require("../fixtures/pull-request-basic.json");
 
 			githubNock
@@ -18,102 +58,196 @@ describe("GitHub Actions", () => {
 					login: "test-pull-request-author-login",
 					avatar_url: "test-pull-request-author-avatar",
 					html_url: "test-pull-request-author-url"
-				});
+				})
+				.get("/repos/test-repo-owner/test-repo-name/pulls/1/reviews")
+				.reply(200, [
+					{
+						id: 80,
+						node_id: "MDE3OlB1bGxSZXF1ZXN0UmV2aWV3ODA=",
+						user: {
+							login: "test-pull-request-reviewer-login",
+							id: 1,
+							node_id: "MDQ6VXNlcjE=",
+							avatar_url: "test-pull-request-reviewer-avatar",
+							gravatar_id: "",
+							url: "https://api.github.com/users/reviewer",
+							html_url: "https://github.com/reviewer",
+							followers_url: "https://api.github.com/users/reviewer/followers",
+							following_url: "https://api.github.com/users/reviewer/following{/other_user}",
+							gists_url: "https://api.github.com/users/reviewer/gists{/gist_id}",
+							starred_url: "https://api.github.com/users/reviewer/starred{/owner}{/repo}",
+							subscriptions_url: "https://api.github.com/users/reviewer/subscriptions",
+							organizations_url: "https://api.github.com/users/reviewer/orgs",
+							repos_url: "https://api.github.com/users/reviewer/repos",
+							events_url: "https://api.github.com/users/reviewer/events{/privacy}",
+							received_events_url: "https://api.github.com/users/reviewer/received_events",
+							type: "User",
+							site_admin: false
+						},
+						body: "Here is the body for the review.",
+						state: "APPROVED",
+						html_url: "https://github.com/test-repo-owner/test-repo-name/pull/1#pullrequestreview-80",
+						pull_request_url: "https://api.github.com/repos/test-repo-owner/test-repo-name/pulls/1",
+						_links: {
+							html: {
+								href: "https://github.com/test-repo-owner/test-repo-name/pull/1#pullrequestreview-80"
+							},
+							pull_request: {
+								href: "https://api.github.com/repos/test-repo-owner/test-repo-name/pulls/1"
+							}
+						},
+						submitted_at: "2019-11-17T17:43:43Z",
+						commit_id: "ecdd80bb57125d7ba9641ffaa4d7d2c19d3f3091",
+						author_association: "COLLABORATOR"
+					}
+				])
+				.patch("/repos/test-repo-owner/test-repo-name/issues/1", {
+					body: "[TEST-123] body of the test pull request.\n\n[TEST-123]: https://test-atlassian-instance.net/browse/TEST-123",
+					id: "test-pull-request-id"
+				}).reply(200);
 
-			jiraNock.get("/rest/api/latest/issue/TEST-123?fields=summary")
+			jiraNock
+				.get("/rest/api/latest/issue/TEST-123?fields=summary")
 				.reply(200, {
 					key: "TEST-123",
 					fields: {
 						summary: "Example Issue"
 					}
-				});
-
-			githubNock
-				.patch("/repos/test-repo-owner/test-repo-name/issues/1", {
-					body: "[TEST-123] body of the test pull request.\n\n[TEST-123]: https://test-atlassian-instance.net/browse/TEST-123",
-					id: "test-pull-request-id"
 				})
-				.reply(200);
-
-			jiraNock.post("/rest/devinfo/0.10/bulk", {
-				preventTransitions: false,
-				repositories: [
-					{
-						name: "example/test-repo-name",
-						url: "test-repo-url",
-						id: "test-repo-id",
-						branches: [
-							{
-								createPullRequestUrl: "test-pull-request-head-url/pull/new/TEST-321-test-pull-request-head-ref",
-								lastCommit: {
-									author: {
-										name: "test-pull-request-author-login"
+				.post("/rest/devinfo/0.10/bulk", {
+					preventTransitions: false,
+					repositories: [
+						{
+							url: "test-pull-request-base-url",
+							branches: [
+								{
+									createPullRequestUrl: "test-pull-request-head-url/pull/new/TEST-321-test-pull-request-head-ref",
+									lastCommit: {
+										author: {
+											avatar: "https://github.com/ghost.png",
+											name: "Deleted User",
+											email: "deleted@noreply.user.github.com",
+											url: "https://github.com/ghost"
+										},
+										authorTimestamp: "test-pull-request-update-time",
+										displayId: "test-p",
+										fileCount: 0,
+										hash: "test-pull-request-sha",
+										id: "test-pull-request-sha",
+										issueKeys: ["TEST-123", "TEST-321"],
+										message: "n/a",
+										updateSequenceId: 12345678,
+										url: "test-pull-request-head-url/commit/test-pull-request-sha"
 									},
-									authorTimestamp: "test-pull-request-update-time",
-									displayId: "test-p",
-									fileCount: 0,
-									hash: "test-pull-request-sha",
-									id: "test-pull-request-sha",
+									id: "TEST-321-test-pull-request-head-ref",
 									issueKeys: ["TEST-123", "TEST-321"],
-									message: "n/a",
-									updateSequenceId: 12345678,
-									url: "test-pull-request-head-url/commit/test-pull-request-sha"
-								},
-								id: "TEST-321-test-pull-request-head-ref",
-								issueKeys: ["TEST-123", "TEST-321"],
-								name: "TEST-321-test-pull-request-head-ref",
-								url: "test-pull-request-head-url/tree/TEST-321-test-pull-request-head-ref",
-								updateSequenceId: 12345678
-							}
-						],
-						pullRequests: [
-							{
-								author: {
-									name: "test-pull-request-author-login",
-									avatar: "test-pull-request-author-avatar",
-									url: "test-pull-request-author-url"
-								},
-								commentCount: "test-pull-request-comment-count",
-								destinationBranch: "test-pull-request-base-url/tree/test-pull-request-base-ref",
-								displayId: "#1",
-								id: 1,
-								reviewers: [],
-								issueKeys: ["TEST-123", "TEST-321"],
-								lastUpdate: "test-pull-request-update-time",
-								sourceBranch: "TEST-321-test-pull-request-head-ref",
-								sourceBranchUrl: "test-pull-request-head-url/tree/TEST-321-test-pull-request-head-ref",
-								status: "OPEN",
-								title: "[TEST-123] Test pull request.",
-								timestamp: "test-pull-request-update-time",
-								url: "test-pull-request-url",
-								updateSequenceId: 12345678
-							}
-						],
-						updateSequenceId: 12345678
-					}
-				],
-				properties: {
-					installationId: 1234
-				}
-			}).reply(200);
+									name: "TEST-321-test-pull-request-head-ref",
+									url: "test-pull-request-head-url/tree/TEST-321-test-pull-request-head-ref",
+									updateSequenceId: 12345678
+								}
+							],
+							pullRequests: [
+								{
+									author: {
+										avatar: "test-pull-request-author-avatar",
+										name: "test-pull-request-author-login",
+										email: "test-pull-request-author-login@noreply.user.github.com",
+										url: "test-pull-request-author-url"
+									},
+									commentCount: "test-pull-request-comment-count",
+									destinationBranch: "test-pull-request-base-url/tree/test-pull-request-base-ref",
+									displayId: "#1",
+									id: 1,
+									issueKeys: ["TEST-123", "TEST-321"],
+									lastUpdate: "test-pull-request-update-time",
+									reviewers: [
+										{
+											avatar: "test-pull-request-reviewer-avatar",
+											name: "test-pull-request-reviewer-login",
+											email: "test-pull-request-reviewer-login@noreply.user.github.com",
+											url: "https://github.com/reviewer",
+											approvalStatus: "APPROVED"
+										}
+									],
+									sourceBranch: "TEST-321-test-pull-request-head-ref",
+									sourceBranchUrl: "test-pull-request-head-url/tree/TEST-321-test-pull-request-head-ref",
+									status: "OPEN",
+									timestamp: "test-pull-request-update-time",
+									title: "[TEST-123] Test pull request.",
+									url: "test-pull-request-url",
+									updateSequenceId: 12345678
+								}
+							],
+							updateSequenceId: 12345678
+						}
+					],
+					properties: { installationId: 1234 }
+				}).reply(200);
 
 			Date.now = jest.fn(() => 12345678);
 
 			await expect(app.receive(fixture)).toResolve();
+			expect(githubNock.pendingMocks()).toEqual([]);
+			expect(jiraNock.pendingMocks()).toEqual([]);
 		});
 
 		it("should not update the Jira issue if the source repo of a pull_request was deleted", async () => {
 			const fixture = require("../fixtures/pull-request-null-repo.json");
 
-			githubNock.get("/users/test-pull-request-user-login").reply(200, {
-				login: "test-pull-request-author-login",
-				avatar_url: "test-pull-request-author-avatar",
-				html_url: "test-pull-request-author-url"
-			});
+			githubNock
+				.get("/users/test-pull-request-user-login").reply(200, {
+					login: "test-pull-request-author-login",
+					avatar_url: "test-pull-request-author-avatar",
+					html_url: "test-pull-request-author-url"
+				})
+				.get("/repos/test-repo-owner/test-repo-name/pulls/1/reviews")
+				.reply(200, [
+					{
+						id: 80,
+						node_id: "MDE3OlB1bGxSZXF1ZXN0UmV2aWV3ODA=",
+						user: {
+							login: "test-pull-request-reviewer-login",
+							id: 1,
+							node_id: "MDQ6VXNlcjE=",
+							avatar_url: "test-pull-request-reviewer-avatar",
+							gravatar_id: "",
+							url: "https://api.github.com/users/reviewer",
+							html_url: "https://github.com/reviewer",
+							followers_url: "https://api.github.com/users/reviewer/followers",
+							following_url: "https://api.github.com/users/reviewer/following{/other_user}",
+							gists_url: "https://api.github.com/users/reviewer/gists{/gist_id}",
+							starred_url: "https://api.github.com/users/reviewer/starred{/owner}{/repo}",
+							subscriptions_url: "https://api.github.com/users/reviewer/subscriptions",
+							organizations_url: "https://api.github.com/users/reviewer/orgs",
+							repos_url: "https://api.github.com/users/reviewer/repos",
+							events_url: "https://api.github.com/users/reviewer/events{/privacy}",
+							received_events_url: "https://api.github.com/users/reviewer/received_events",
+							type: "User",
+							site_admin: false
+						},
+						body: "Here is the body for the review.",
+						state: "APPROVED",
+						html_url: "https://github.com/test-repo-owner/test-repo-name/pull/1#pullrequestreview-80",
+						pull_request_url: "https://api.github.com/repos/test-repo-owner/test-repo-name/pulls/1",
+						_links: {
+							html: {
+								href: "https://github.com/test-repo-owner/test-repo-name/pull/1#pullrequestreview-80"
+							},
+							pull_request: {
+								href: "https://api.github.com/repos/test-repo-owner/test-repo-name/pulls/1"
+							}
+						},
+						submitted_at: "2019-11-17T17:43:43Z",
+						commit_id: "ecdd80bb57125d7ba9641ffaa4d7d2c19d3f3091",
+						author_association: "COLLABORATOR"
+					}
+				]);
 
 			Date.now = jest.fn(() => 12345678);
 
 			await expect(app.receive(fixture)).toResolve();
+			// expect(githubNock.pendingMocks()).toEqual([]);
+			// expect(jiraNock.pendingMocks()).toEqual([]);
 		});
 
 		it("should delete the reference to a pull request when issue keys are removed from the title", async () => {
@@ -465,145 +599,6 @@ describe("GitHub Actions", () => {
 
 				await expect(app.receive(fixture[2])).toResolve();
 			});
-		});
-
-		it("should have reviewers on pull request action", async () => {
-			const fixture = require("../fixtures/pull-request-basic.json");
-
-			githubNock
-				.get("/users/test-pull-request-user-login")
-				.reply(200, {
-					login: "test-pull-request-author-login",
-					avatar_url: "test-pull-request-author-avatar",
-					html_url: "test-pull-request-author-url"
-				})
-				.get("/repos/test-repo-owner/test-repo-name/pulls/1/reviews")
-				.reply(200, [
-					{
-						id: 80,
-						node_id: "MDE3OlB1bGxSZXF1ZXN0UmV2aWV3ODA=",
-						user: {
-							login: "test-pull-request-reviewer-login",
-							id: 1,
-							node_id: "MDQ6VXNlcjE=",
-							avatar_url: "test-pull-request-reviewer-avatar",
-							gravatar_id: "",
-							url: "https://api.github.com/users/reviewer",
-							html_url: "https://github.com/reviewer",
-							followers_url: "https://api.github.com/users/reviewer/followers",
-							following_url: "https://api.github.com/users/reviewer/following{/other_user}",
-							gists_url: "https://api.github.com/users/reviewer/gists{/gist_id}",
-							starred_url: "https://api.github.com/users/reviewer/starred{/owner}{/repo}",
-							subscriptions_url: "https://api.github.com/users/reviewer/subscriptions",
-							organizations_url: "https://api.github.com/users/reviewer/orgs",
-							repos_url: "https://api.github.com/users/reviewer/repos",
-							events_url: "https://api.github.com/users/reviewer/events{/privacy}",
-							received_events_url: "https://api.github.com/users/reviewer/received_events",
-							type: "User",
-							site_admin: false
-						},
-						body: "Here is the body for the review.",
-						state: "APPROVED",
-						html_url: "https://github.com/test-repo-owner/test-repo-name/pull/1#pullrequestreview-80",
-						pull_request_url: "https://api.github.com/repos/test-repo-owner/test-repo-name/pulls/1",
-						_links: {
-							html: {
-								href: "https://github.com/test-repo-owner/test-repo-name/pull/1#pullrequestreview-80"
-							},
-							pull_request: {
-								href: "https://api.github.com/repos/test-repo-owner/test-repo-name/pulls/1"
-							}
-						},
-						submitted_at: "2019-11-17T17:43:43Z",
-						commit_id: "ecdd80bb57125d7ba9641ffaa4d7d2c19d3f3091",
-						author_association: "COLLABORATOR"
-					}
-				]);
-
-			jiraNock
-				.get("/rest/api/latest/issue/TEST-123?fields=summary")
-				.reply(200, {
-					key: "TEST-123",
-					fields: {
-						summary: "Example Issue"
-					}
-				});
-
-			githubNock.patch("/repos/test-repo-owner/test-repo-name/issues/1", {
-				body: "[TEST-123] body of the test pull request.\n\n[TEST-123]: https://test-atlassian-instance.net/browse/TEST-123",
-				id: "test-pull-request-id"
-			}).reply(200);
-
-			jiraNock.post("/rest/devinfo/0.10/bulk", {
-				preventTransitions: false,
-				repositories: [
-					{
-						name: "example/test-repo-name",
-						url: "test-repo-url",
-						id: "test-repo-id",
-						branches: [
-							{
-								createPullRequestUrl: "test-pull-request-head-url/pull/new/TEST-321-test-pull-request-head-ref",
-								lastCommit: {
-									author: {
-										name: "test-pull-request-author-login"
-									},
-									authorTimestamp: "test-pull-request-update-time",
-									displayId: "test-p",
-									fileCount: 0,
-									hash: "test-pull-request-sha",
-									id: "test-pull-request-sha",
-									issueKeys: ["TEST-123", "TEST-321"],
-									message: "n/a",
-									updateSequenceId: 12345678,
-									url: "test-pull-request-head-url/commit/test-pull-request-sha"
-								},
-								id: "TEST-321-test-pull-request-head-ref",
-								issueKeys: ["TEST-123", "TEST-321"],
-								name: "TEST-321-test-pull-request-head-ref",
-								url: "test-pull-request-head-url/tree/TEST-321-test-pull-request-head-ref",
-								updateSequenceId: 12345678
-							}
-						],
-						pullRequests: [
-							{
-								author: {
-									name: "test-pull-request-author-login",
-									avatar: "test-pull-request-author-avatar",
-									url: "test-pull-request-author-url"
-								},
-								commentCount: "test-pull-request-comment-count",
-								destinationBranch: "test-pull-request-base-url/tree/test-pull-request-base-ref",
-								displayId: "#1",
-								id: 1,
-								reviewers: [{
-									name: "test-pull-request-reviewer-login",
-									approvalStatus: "APPROVED",
-									url: "https://github.com/reviewer",
-									avatar: "test-pull-request-reviewer-avatar"
-								}],
-								issueKeys: ["TEST-123", "TEST-321"],
-								lastUpdate: "test-pull-request-update-time",
-								sourceBranch: "TEST-321-test-pull-request-head-ref",
-								sourceBranchUrl: "test-pull-request-head-url/tree/TEST-321-test-pull-request-head-ref",
-								status: "OPEN",
-								title: "[TEST-123] Test pull request.",
-								timestamp: "test-pull-request-update-time",
-								url: "test-pull-request-url",
-								updateSequenceId: 12345678
-							}
-						],
-						updateSequenceId: 12345678
-					}
-				],
-				properties: {
-					installationId: 1234
-				}
-			}).reply(200);
-
-			Date.now = jest.fn(() => 12345678);
-
-			await expect(app.receive(fixture)).toResolve();
 		});
 	});
 });
