60784eaae278011d68952b912f66bb30f883f41b
Michel Boudreau <michelboudreau@gmail.com>
2021-11-15 14:59:00 +1100

    Using direct connection instead of pgbouncer

--- db/migrations/20211150173000-create-test-table-2.js
+++ db/migrations/20211150173000-create-test-table-2.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const tableName = "MichelTestTable";
+const tableName = "MichelTestTable2";
 module.exports = {
 	up: async (queryInterface, Sequelize) => {
 		await queryInterface.createTable(tableName, {
