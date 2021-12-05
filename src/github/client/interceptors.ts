d7552afb1665939788ca1b4257eada8b40d3173c
thombergs <tom.hombergs@gmail.com>
2021-12-06 09:41:42 +1100

    fix some small rate limiting error handling

--- src/github/client/interceptors.ts
+++ src/github/client/interceptors.ts
@@ -11,6 +11,7 @@ const extractPath = (someUrl = ""): string =>
 	url.parse(someUrl).pathname || "";
 
 const RESPONSE_TIME_HISTOGRAM_BUCKETS =	"100_1000_2000_3000_5000_10000_30000_60000";
+const ONE_HOUR_IN_SECONDS = 60 * 60;
 
 /**
  * Enrich the config object to include the time that the request started.
@@ -84,22 +85,16 @@ export const handleFailedRequest = (logger: Logger) =>
 			const errorMessage = `Error executing Axios Request ` + error.message;
 
 			const rateLimitResetHeaderValue:string = response.headers?.["x-ratelimit-reset"];
-			const rateLimitRemainingHeaderValue:string = response.headers?.["x-ratelimit-reset"];
-			if(response.headers?.["x-ratelimit-remaining"] == "0" && rateLimitResetHeaderValue) {
+			const rateLimitRemainingHeaderValue:string = response.headers?.["x-ratelimit-remaining"];
+			if(status === 403 && rateLimitRemainingHeaderValue == "0") {
 				logger.warn({ err: error }, "Rate limiting error");
-				const rateLimitReset: number = parseInt(rateLimitResetHeaderValue);
+				const rateLimitReset: number = parseInt(rateLimitResetHeaderValue) || Date.now() / 1000 + ONE_HOUR_IN_SECONDS;
 				return Promise.reject(new RateLimitingError(rateLimitReset, 0, error, status));
 			}
 
 			if (status === 403 && response.data.message.includes("has an IP allow list enabled")) {
 				logger.error({ err: error }, "Blocked by GitHub allowlist");
 				return Promise.reject(new BlockedIpError(error, status));
-			}
-
-			if (status === 403) {
-				const rateLimitRemaining: number = parseInt(rateLimitRemainingHeaderValue);
-				logger.warn({ err: error }, "Github returned 403 without ratelimit header");
-				return Promise.reject(new RateLimitingError(Date.now() / 1000 + 60 * 60, rateLimitRemaining, error, status));
 			}
 
 			const isWarning = status && (status >= 300 && status < 500 && status !== 400);
