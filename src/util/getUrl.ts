a7241f29bf9b0e45ed15817b9c40e6ee898a1edb
Michel Boudreau <michelboudreau@gmail.com>
2021-12-06 16:47:30 +1100

    removing deprecated function to get jiraHost

--- src/util/getUrl.ts
+++ src/util/getUrl.ts
@@ -1,5 +1,3 @@
-import Logger from "bunyan";
-import url from "url";
 import envVars from "../config/env";
 
 export const getGitHubConfigurationUrl = (
@@ -9,16 +7,3 @@ export const getGitHubConfigurationUrl = (
 
 export const getJiraMarketplaceUrl = (jiraHost: string): string =>
 	`${jiraHost}/plugins/servlet/ac/com.atlassian.jira.emcee/discover#!/discover/app/com.github.integration.production`;
-
-export const getJiraHostFromRedirectUrl = (urlOrPath: string, log: Logger): string => {
-	if (!urlOrPath) {
-		return "empty";
-	}
-	try {
-		const { host, query } = url.parse(urlOrPath, true);
-		return (query?.xdm_e ? url.parse(query.xdm_e.toString(), false).host : host) || "unknown";
-	} catch (err) {
-		log.error(err, "Cannot detect jiraHost from redirect URL");
-		return "error";
-	}
-};
