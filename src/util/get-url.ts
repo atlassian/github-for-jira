1812a510dceab3b09997816c6ff0e041aa7c9764
Michel Boudreau <michelboudreau@gmail.com>
2021-12-14 10:33:08 +1100

    Change name of util url function to better describe it (#970)
    * changing name of util url function to better describe it
    
    * changing test name

--- src/util/get-url.ts
+++ src/util/get-url.ts
@@ -1,6 +1,6 @@
 import envVars from "../config/env";
 
-export const getGitHubConfigurationUrl = (jiraHost: string): string =>
+export const getJiraAppUrl = (jiraHost: string): string =>
 	jiraHost?.length ? `${jiraHost}/plugins/servlet/ac/com.github.integration.${envVars.INSTANCE_NAME}/github-post-install-page` : "";
 
 export const getJiraMarketplaceUrl = (jiraHost: string): string =>
