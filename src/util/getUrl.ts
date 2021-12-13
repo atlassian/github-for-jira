16c6ab8a157bb8a322db4833cc6c5e040fa75b2b
Michel Boudreau <michelboudreau@gmail.com>
2021-12-13 13:50:00 +1100

    fixing up URL for github configuration url

--- src/util/getUrl.ts
+++ src/util/getUrl.ts
@@ -3,7 +3,7 @@ import envVars from "../config/env";
 export const getGitHubConfigurationUrl = (
 	jiraHost
 ): string =>
-	`https://${jiraHost}/plugins/servlet/ac/com.github.integration.${envVars.INSTANCE_NAME}/github-post-install-page`;
+	`${jiraHost}/plugins/servlet/ac/com.github.integration.${envVars.INSTANCE_NAME}/github-post-install-page`;
 
 export const getJiraMarketplaceUrl = (jiraHost: string): string =>
 	`${jiraHost}/plugins/servlet/ac/com.atlassian.jira.emcee/discover#!/discover/app/com.github.integration.production`;
