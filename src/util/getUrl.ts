32abb30c6b05dd967fe6e71f4478e6e8f106a729
Michel Boudreau <michelboudreau@gmail.com>
2021-12-13 15:30:13 +1100

    adding tests to getUrl

--- src/util/getUrl.ts
+++
@@ -1,9 +0,0 @@
-import envVars from "../config/env";
-
-export const getGitHubConfigurationUrl = (
-	jiraHost
-): string =>
-	`${jiraHost}/plugins/servlet/ac/com.github.integration.${envVars.INSTANCE_NAME}/github-post-install-page`;
-
-export const getJiraMarketplaceUrl = (jiraHost: string): string =>
-	`${jiraHost}/plugins/servlet/ac/com.atlassian.jira.emcee/discover#!/discover/app/com.github.integration.production`;
