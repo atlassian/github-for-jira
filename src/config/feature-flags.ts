cd5543674b3c7caeedb08168cb7107343367ea68
Konstantin <kabakumov@atlassian.com>
2022-01-05 10:37:34 +1100

    Feature flags cleanup use-sqs-for-discovery-queue and  use-new-github-client-to-count-repos (#1009)

--- src/config/feature-flags.ts
+++ src/config/feature-flags.ts
@@ -23,9 +23,7 @@ export enum BooleanFlags {
 	TRACE_LOGGING = "trace-logging",
 	SUPPORT_BRANCH_AND_MERGE_WORKFLOWS_FOR_BUILDS = "support-branch-and-merge-workflows-for-builds",
 	USE_NEW_GITHUB_CLIENT_FOR_PUSH = "use-new-github-client-for-push",
-	USE_NEW_GITHUB_CLIENT_TO_COUNT_REPOS = "use-new-github-client-to-count-repos",
 	REPO_SYNC_STATE_AS_SOURCE = "repo-sync-state-as-source",
-	USE_SQS_FOR_DISCOVERY_QUEUE = "use-sqs-for-discovery-queue",
 	USE_SQS_FOR_DEPLOYMENT = "use-sqs-for-deployment",
 	ASSOCIATE_PR_TO_ISSUES_IN_BODY = "associate-pr-to-issues-in-body"
 }
