fefcecdf20e29a49be6c8db5a23ad78bd42acc7f
Michel Boudreau <michelboudreau@gmail.com>
2022-01-17 15:48:41 +1100

    ARC-880 blocked IP per organization (#1034)
    * latest work, not done
    
    * latest work on fixing blocked ip orgs from github configuration page
    
    * small fixes
    
    * fixing up another API call that would be blocked by IP allowlisting that I didn't find before, adding test
    
    * small fix to only uninstall deleted orgs, not blocked ones
    
    * fixing test

--- test/frontend/util/get-jira-configuration-helpers.test.ts
+++ test/frontend/util/get-jira-configuration-helpers.test.ts
@@ -1,167 +1,21 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-import { getFailedConnections, getInstallation } from "../../../src/frontend/get-jira-configuration";
-import { GitHubAPI } from "probot";
+/* eslint-disable @typescript-eslint/no-var-requires,@typescript-eslint/no-explicit-any */
 import { RepoSyncState, Subscription } from "../../../src/models";
 import SubscriptionClass from "../../../src/models/subscription";
+import { getInstallations } from "../../../src/frontend/get-jira-configuration";
+import GithubApi from "../../../src/config/github-api";
+import { GitHubAPI } from "probot";
 
-describe("getFailedConnections", () => {
-	const noSubscriptions = require("../../fixtures/get-jira-configuration/no-subscriptions.json");
-	const singleSubscription = require("../../fixtures/get-jira-configuration/single-subscription.json");
-	const multipleSubscriptions = require("../../fixtures/get-jira-configuration/multiple-subscriptions.json");
-	const subscriptionWithNoRepos = require("../../fixtures/get-jira-configuration/subscription-with-no-repos.json");
 
-	beforeEach(async () => {
-		await RepoSyncState.create({
-			subscriptionId: 12345678,
-			repoId: 1,
-			repoName: "test-repo-name",
-			repoOwner: "integrations",
-			repoFullName: "integrations/test-repo-name",
-			repoUrl: "test-repo-url",
-		});
-	});
-
-	afterEach(async () => {
-		await RepoSyncState.destroy({ truncate: true });
-	});
-
-	it("should return no failed connections if there are no installations", async () => {
-		const noInstallations = require("../../fixtures/get-jira-configuration/no-installations");
-		const result = await getFailedConnections(noInstallations, noSubscriptions);
-		expect(result).toHaveLength(0);
-		expect(result).toEqual([]);
-	});
-
-	it("should return no failed connections if no connections fail", async () => {
-		const singleSuccessfulInstallation = require("../../fixtures/get-jira-configuration/single-successful-installation");
-		const result = await getFailedConnections(singleSuccessfulInstallation, singleSubscription);
-		expect(result).toHaveLength(0);
-		expect(result).toEqual([]);
-	});
-
-	it("should return a single failed connection if 1 connection fails", async () => {
-		const singleFailedInstallation = require("../../fixtures/get-jira-configuration/single-failed-installation.json");
-		const result = await getFailedConnections(singleFailedInstallation, singleSubscription);
-		expect(result).toHaveLength(1);
-		expect(result).toEqual([{ deleted: true, id: 12345678, orgName: "integrations" }]);
-	});
-
-	it("should return a single failed connection if 1 connection fails and 1 succeeds", async () => {
-		const singleFailedAndSingleSuccessdulInstallation = require("../../fixtures/get-jira-configuration/single-successful-and-single-failed-installations");
-		const result = await getFailedConnections(singleFailedAndSingleSuccessdulInstallation, singleSubscription);
-		expect(result).toHaveLength(1);
-		expect(result).toEqual([{ deleted: true, id: 12345678, orgName: "integrations" }]);
-	});
-
-	it("should return a multiple failed connections if there is more than 1 failed connection", async () => {
-		const multipleFailedInstallations = require("../../fixtures/get-jira-configuration/multiple-failed-installations");
-		await RepoSyncState.create({
-			subscriptionId: 23456789,
-			repoId: 2,
-			repoName: "test-repo-name",
-			repoOwner: "integrations2",
-			repoFullName: "integrations/test-repo-name",
-			repoUrl: "test-repo-url",
-		});
-		const result = await getFailedConnections(multipleFailedInstallations, multipleSubscriptions);
-		expect(result).toHaveLength(2);
-		expect(result).toEqual([
-			{ deleted: true, id: 12345678, orgName: "integrations" },
-			{ deleted: true, id: 23456789, orgName: "integrations2" }
-		]);
-	});
-
-	it("should return a multiple failed connections if there are multiple successful and failed connections", async () => {
-		const multipleFailedAndSuccessfulInstallations = require("../../fixtures/get-jira-configuration/muliple-successful-and-multiple-failed-installations");
-
-		await RepoSyncState.create({
-			subscriptionId: 23456789,
-			repoId: 2,
-			repoName: "test-repo-name",
-			repoOwner: "integrations2",
-			repoFullName: "integrations/test-repo-name",
-			repoUrl: "test-repo-url",
-		});
-		const result = await getFailedConnections(multipleFailedAndSuccessfulInstallations, multipleSubscriptions);
-		expect(result).toHaveLength(2);
-		expect(result).toEqual([
-			{ deleted: true, id: 12345678, orgName: "integrations" },
-			{ deleted: true, id: 23456789, orgName: "integrations2" }
-		]);
-	});
-
-	it("should return 'undefined' for the orgName of subscriptions with no repos", async () => {
-		const singleFailedInstallation = require("../../fixtures/get-jira-configuration/single-failed-installation");
-		const result = await getFailedConnections(singleFailedInstallation, subscriptionWithNoRepos);
-		expect(result).toHaveLength(1);
-		expect(result).toEqual([{ deleted: true, id: 12345678, orgName: undefined }]);
-	});
-});
-
-describe("getInstallation", () => {
-	let subscription: SubscriptionClass;
-	const gitHubInstallationId = 1234;
-	let match;
+describe("getInstallations", () => {
+	let sub: SubscriptionClass;
+	const client = GithubApi();
 
 	beforeEach(async () => {
-		match = {
-			syncStatus: "IN PROGRESS",
-			syncWarning: null,
-			totalNumberOfRepos: 2,
-			numberOfSyncedRepos: 1,
-			jiraHost
-		};
-
-		subscription = await Subscription.create({
-			gitHubInstallationId,
+		sub = await Subscription.create({
+			gitHubInstallationId: 12345678,
 			jiraHost,
-			syncStatus: "ACTIVE"
+			jiraClientKey: "myClientKey"
 		});
-
-		await RepoSyncState.create({
-			subscriptionId: subscription.id,
-			repoId: 1,
-			repoName: "github-for-jira",
-			repoOwner: "atlassian",
-			repoFullName: "atlassian/github-for-jira",
-			repoUrl: "github.com/atlassian/github-for-jira",
-			pullStatus: "complete",
-			branchStatus: "complete",
-			commitStatus: "complete",
-			branchCursor: "foo",
-			commitCursor: "bar",
-			pullCursor: "blarg"
-		});
-
-		await RepoSyncState.create({
-			subscriptionId: subscription.id,
-			repoId: 2,
-			repoName: "github-for-jira",
-			repoOwner: "atlassian",
-			repoFullName: "atlassian/github-for-jira",
-			repoUrl: "github.com/atlassian/github-for-jira",
-			branchStatus: "failed"
-		});
-
-		githubNock
-			.get(`/app/installations/${gitHubInstallationId}`)
-			.reply(200, {
-				"id": gitHubInstallationId,
-				"account": {
-					"login": "atlassian",
-					"id": 1,
-					"node_id": "MDQ6VXNlcjE=",
-					"avatar_url": "https://github.com/images/error/octocat_happy.gif",
-					"url": "https://api.github.com/users/octocat",
-					"html_url": "https://github.com/octocat",
-					"type": "User",
-					"site_admin": false
-				},
-				"html_url": "https://github.com/organizations/github/settings/installations/1",
-				"app_id": 1,
-				"target_id": 1,
-				"target_type": "Organization"
-			});
 	});
 
 	afterEach(async () => {
@@ -169,9 +23,162 @@ describe("getInstallation", () => {
 		await RepoSyncState.destroy({ truncate: true });
 	});
 
-	it("Should get installation from Repo Sync State table", async () => {
-		const result = await getInstallation(GitHubAPI(), subscription);
-		expect(result).toMatchObject(match);
+	it("should return no success or failed connections if no subscriptions given", async () => {
+		expect(await getInstallations(client, [])).toEqual({
+			fulfilled: [],
+			rejected: [],
+			total: 0
+		});
+	});
+
+	it("should return successful connections when installation exists", async () => {
+		githubNock
+			.get(`/app/installations/${sub.gitHubInstallationId}`)
+			.reply(200, require("../../fixtures/get-jira-configuration/single-installation.json"));
+
+		expect(await getInstallations(GitHubAPI(), [sub])).toMatchObject({
+			fulfilled: [{
+				id: sub.gitHubInstallationId,
+				syncStatus: null,
+				syncWarning: null,
+				totalNumberOfRepos: 0,
+				numberOfSyncedRepos: 0,
+				jiraHost
+			}],
+			rejected: []
+		});
+	});
+
+	it("should return a single failed connection if 1 connection fails", async () => {
+		githubNock
+			.get(`/app/installations/${sub.gitHubInstallationId}`)
+			.reply(404, require("../../fixtures/get-jira-configuration/failed-installation.json"));
+
+		expect(await getInstallations(GitHubAPI(), [sub])).toMatchObject({
+			fulfilled: [],
+			rejected: [{
+				error: {
+					status: 404,
+					documentation_url: "https://docs.github.com/rest/reference/apps#get-an-installation-for-the-authenticated-app"
+				},
+				id: sub.gitHubInstallationId,
+				deleted: true
+			}]
+		});
+	});
+
+	it("should return one successful connection if installation exists and one failed one if installation doesn't exist", async () => {
+		const failedSub: SubscriptionClass = await Subscription.create({
+			gitHubInstallationId: 123,
+			jiraHost,
+			jiraClientKey: "myClientKey"
+		});
+
+		githubNock
+			.get(`/app/installations/${sub.gitHubInstallationId}`)
+			.reply(200, require("../../fixtures/get-jira-configuration/single-installation.json"));
+
+		githubNock
+			.get(`/app/installations/${failedSub.gitHubInstallationId}`)
+			.reply(404, require("../../fixtures/get-jira-configuration/failed-installation.json"));
+
+		expect(await getInstallations(GitHubAPI(), [sub, failedSub])).toMatchObject({
+			fulfilled: [{
+				id: sub.gitHubInstallationId,
+				syncStatus: null,
+				syncWarning: null,
+				totalNumberOfRepos: 0,
+				numberOfSyncedRepos: 0,
+				jiraHost
+			}],
+			rejected: [{
+				error: {
+					status: 404,
+					documentation_url: "https://docs.github.com/rest/reference/apps#get-an-installation-for-the-authenticated-app"
+				},
+				id: failedSub.gitHubInstallationId,
+				deleted: true
+			}]
+		});
+	});
+
+	it("should return a multiple failed connections if no installations exists", async () => {
+		const failedSub: SubscriptionClass = await Subscription.create({
+			gitHubInstallationId: 123,
+			jiraHost,
+			jiraClientKey: "myClientKey"
+		});
+
+		githubNock
+			.get(`/app/installations/${sub.gitHubInstallationId}`)
+			.reply(404, require("../../fixtures/get-jira-configuration/failed-installation.json"));
+
+		githubNock
+			.get(`/app/installations/${failedSub.gitHubInstallationId}`)
+			.reply(404, require("../../fixtures/get-jira-configuration/failed-installation.json"));
+
+		expect(await getInstallations(GitHubAPI(), [sub, failedSub])).toMatchObject({
+			fulfilled: [],
+			rejected: [
+				{
+					error: {
+						status: 404,
+						documentation_url: "https://docs.github.com/rest/reference/apps#get-an-installation-for-the-authenticated-app"
+					},
+					id: sub.gitHubInstallationId,
+					deleted: true
+				},
+				{
+					error: {
+						status: 404,
+						documentation_url: "https://docs.github.com/rest/reference/apps#get-an-installation-for-the-authenticated-app"
+					},
+					id: failedSub.gitHubInstallationId,
+					deleted: true
+				}
+			]
+		});
+	});
+
+	it("should return successful connection with correct number of repos and sync status", async () => {
+		await RepoSyncState.create({
+			subscriptionId: sub.id,
+			repoId: 1,
+			repoName: "github-for-jira",
+			repoOwner: "atlassian",
+			repoFullName: "atlassian/github-for-jira",
+			repoUrl: "github.com/atlassian/github-for-jira",
+			pullStatus: "complete",
+			commitStatus: "complete",
+			branchStatus: "complete"
+		});
+
+		await RepoSyncState.create({
+			subscriptionId: sub.id,
+			repoId: 1,
+			repoName: "github-for-jira",
+			repoOwner: "atlassian",
+			repoFullName: "atlassian/github-for-jira",
+			repoUrl: "github.com/atlassian/github-for-jira",
+			pullStatus: "pending",
+			commitStatus: "complete",
+			branchStatus: "complete"
+		});
+
+		githubNock
+			.get(`/app/installations/${sub.gitHubInstallationId}`)
+			.reply(200, require("../../fixtures/get-jira-configuration/single-installation.json"));
+
+		expect(await getInstallations(GitHubAPI(), [sub])).toMatchObject({
+			fulfilled: [{
+				id: sub.gitHubInstallationId,
+				syncStatus: null,
+				syncWarning: null,
+				totalNumberOfRepos: 2,
+				numberOfSyncedRepos: 1,
+				jiraHost
+			}],
+			rejected: []
+		});
 	});
 });
-
