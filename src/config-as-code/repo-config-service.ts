d15337bb99f5880e3992a79e796d2ad7226c38a0
Josh Kay <joshk@atlassian.com>
2022-02-02 10:25:07 +1300

    ARC-1047 - fixed config path - added additional comments

--- src/config-as-code/repo-config-service.ts
+++ src/config-as-code/repo-config-service.ts
@@ -6,12 +6,14 @@ import { RepoConfig } from "./repo-config";
 import RepoConfigDatabaseModel from "./repo-config-database-model";
 
 const logger = getLogger("services.config-as-code");
-const CONFIG_PATH = "./jira/config.yml";
+const CONFIG_PATH = ".jira/config.yml";
 
+/**
+ * Fetches contents from CONFIG_PATH using guthub api, transforms its from base64 to ascii and returns the transformed string.
+ */
 const getRepoConfigFromGitHub = async (githubInstallationId: number, owner: string, repo: string): Promise<string | null> => {
 	const client = new GitHubClient(getCloudInstallationId(githubInstallationId), logger);
 	const response = await client.getRepositoryContent(owner, repo, CONFIG_PATH);
-
 	if (response == null) {
 		return null;
 	}
@@ -25,23 +27,31 @@ const getRepoConfigFromGitHub = async (githubInstallationId: number, owner: stri
 	return contentString;
 }
 
+/**
+ * Converts incoming YAML string to JSON (RepoConfig)
+ */
 const convertYamlToRepoConfig = (input: string): RepoConfig => {
-	return YAML.parse(input)
+	const config: RepoConfig = YAML.parse(input);
+	return config;
 }
 
-// save the json blob to the DB with installationId and RepoId
+/**
+ * Saves a JSON blob to the RepoConfig database with installationId and RepoId
+ */
 const saveRepoConfigToDB = async (githubInstallationId: number, repoId: number, config: RepoConfig): Promise<RepoConfig | null> => {
 	logger.debug({githubInstallationId, repoId, config}, "saving repo config to Database");
 	return await RepoConfigDatabaseModel.saveOrUpdate(githubInstallationId, repoId, config)
 }
 
-// Attempt to find ./jira/config.yaml on main branch, parse it from yaml to JSON, then commit it to database.
+/**
+ * Attempts to find ./jira/config.yaml on main branch, parse it from YAML to JSON, then commit it to RepoConfig database.
+ */
 export const processRepoConfig = async (githubInstallationId: number, owner: string, repo: string, repoId: number): Promise<RepoConfig | null> => {
 	const buffer = await getRepoConfigFromGitHub(githubInstallationId, owner, repo);
 	if (buffer) {
 		const config: RepoConfig = convertYamlToRepoConfig(buffer);
 		return await saveRepoConfigToDB(githubInstallationId, repoId, config);
 	}
-	logger.info({ githubInstallationId, repo, repoId, owner }, "could not find repo config")
+	logger.info({ githubInstallationId, repo, repoId, owner }, "could not find repo config or config is empty - expected .jira/config.yml")
 	return null;
 }
