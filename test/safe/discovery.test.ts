ee5ad460b7bb8c9c03ff3d847827e8ca116b7e1a
thombergs <tom.hombergs@gmail.com>
2022-02-02 17:35:18 +1100

    add tests for "config discovery"

--- test/safe/discovery.test.ts
+++ test/safe/discovery.test.ts
@@ -1,11 +1,14 @@
-import {Installation, RepoSyncState, Subscription} from "../../src/models";
-import {start, stop} from "../../src/worker/startup";
+import { Installation, RepoSyncState, Subscription } from "../../src/models";
+import { start, stop } from "../../src/worker/startup";
 import sqsQueues from "../../src/sqs/queues";
-import {createWebhookApp} from "../utils/probot";
+import { createWebhookApp } from "../utils/probot";
 import app from "../../src/worker/app";
-import {discovery} from "../../src/sync/discovery";
-import {getLogger} from "../../src/config/logger";
+import { discovery } from "../../src/sync/discovery";
+import { getLogger } from "../../src/config/logger";
 import waitUntil from "../utils/waitUntil";
+import RepoConfigDatabaseModel from "../../src/config-as-code/repo-config-database-model";
+import { mocked } from "ts-jest/utils";
+import { booleanFlag } from "../../src/config/feature-flags";
 
 jest.mock("../../src/config/feature-flags");
 
@@ -30,21 +33,24 @@ describe("Discovery Queue Test", () => {
 	});
 
 	afterEach(async () => {
-		await Installation.destroy({truncate: true})
-		await Subscription.destroy({truncate: true})
+		await Installation.destroy({ truncate: true })
+		await Subscription.destroy({ truncate: true })
 	});
 
 	let originalBackfillQueueSendMessageFunction;
+	let originalDiscoveryQueueSendMessageFunction;
 
 	beforeAll(async () => {
 		//Start worker node for queues processing
 		await start();
 		originalBackfillQueueSendMessageFunction = sqsQueues.backfill.sendMessage;
+		originalDiscoveryQueueSendMessageFunction = sqsQueues.discovery.sendMessage;
 		sqsQueues.backfill.sendMessage = jest.fn();
 	});
 
 	afterAll(async () => {
 		sqsQueues.backfill.sendMessage = originalBackfillQueueSendMessageFunction;
+		sqsQueues.discovery.sendMessage = originalDiscoveryQueueSendMessageFunction;
 		//Stop worker node
 		await stop();
 		await sqsQueues.discovery.waitUntilListenerStopped();
@@ -65,6 +71,21 @@ describe("Discovery Queue Test", () => {
 			.reply(200, require("../fixtures/list-repositories.json"));
 	}
 
+	const mockRepoContentResponses = () => {
+
+		githubNock
+			.post("/app/installations/1234/access_tokens")
+			.optionally() // TODO: need to remove optionally and make it explicit
+			.reply(200, {
+				token: "token",
+				expires_at: new Date().getTime() + 1_000_000
+			});
+
+		githubNock.get("/repos/octocat/Hello-World/contents/.jira/config.yml")
+			// eslint-disable-next-line @typescript-eslint/no-var-requires
+			.reply(200, require("../fixtures/get-repo-content.json"));
+
+	}
 
 	async function verify2RepositoriesInTheStateAndBackfillMessageSent() {
 
@@ -85,12 +106,17 @@ describe("Discovery Queue Test", () => {
 
 	}
 
+	async function verifyRepoConfig() {
+		const repoConfig = await RepoConfigDatabaseModel.getForRepo(TEST_INSTALLATION_ID, 1296269);
+		expect(repoConfig).toBeTruthy();
+	}
+
 	it("Discovery sqs queue processes the message", async () => {
 
 
 		mockGitHubReposResponses();
 
-		await sqsQueues.discovery.sendMessage({installationId: TEST_INSTALLATION_ID, jiraHost});
+		await sqsQueues.discovery.sendMessage({ installationId: TEST_INSTALLATION_ID, jiraHost });
 
 		await waitUntil(async () => {
 
@@ -103,9 +129,39 @@ describe("Discovery Queue Test", () => {
 
 		mockGitHubReposResponses();
 
-		await discovery(app)({installationId: TEST_INSTALLATION_ID, jiraHost}, getLogger("test"))
+		await discovery(app)({ installationId: TEST_INSTALLATION_ID, jiraHost }, getLogger("test"))
 
 		await verify2RepositoriesInTheStateAndBackfillMessageSent();
+	});
+
+	it("Discovery queue listener DOESN'T schedule repo config discovery messages when feature flag is disabled", async () => {
+		mocked(booleanFlag).mockResolvedValue(false);
+		sqsQueues.discovery.sendMessage = jest.fn();
+		mockGitHubReposResponses();
+		await discovery(app)({ installationId: TEST_INSTALLATION_ID, jiraHost }, getLogger("test"))
+		expect(sqsQueues.discovery.sendMessage).not.toBeCalled();
+	});
+
+	it("Discovery queue listener DOES schedule repo config discovery messages when feature flag is enabled", async () => {
+		mocked(booleanFlag).mockResolvedValue(true);
+		sqsQueues.discovery.sendMessage = jest.fn();
+		mockGitHubReposResponses();
+		await discovery(app)({ installationId: TEST_INSTALLATION_ID, jiraHost }, getLogger("test"))
+		expect(sqsQueues.discovery.sendMessage).toBeCalledTimes(2);
+	});
+
+	it("Discovery queue listener gets repo config from GitHub", async () => {
+		mockRepoContentResponses();
+		await discovery(app)({
+			installationId: TEST_INSTALLATION_ID,
+			jiraHost,
+			repo: {
+				id: 1296269,
+				name: "Hello-World",
+				owner: "octocat"
+			}
+		}, getLogger("test"))
+		await verifyRepoConfig();
 	});
 
 
