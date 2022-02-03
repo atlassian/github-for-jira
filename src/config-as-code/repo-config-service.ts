ad004c98ecc2d09df360e7c341203635c839f5cb
joshkay10 <josh.kay@outlook.com>
2022-02-03 17:31:45 +1300

    Innovation project - config as code - Arc 1047 (#1068)
    * wip - added config as code service to read github contents -  convert yaml to json - save config to db
    
    * ARC-1047 - tidied up config as a service error handling and types
    
    * tidied up types and removedtry blocks
    
    * ARC-1047 added tests for config as code service - excluding getRepoConfigFromGitHub due to mocking issues
    
    * remove debug testing
    
    * removed unwanted commits
    
    * ARC-1047 - renamed function for clarity
    
    * ARC-1047 - reversed testenvironmentmapping logic to be more readable
    
    * ARC-1047 - function rename
    
    Co-authored-by: Josh Kay <joshk@atlassian.com>

--- src/config-as-code/repo-config-service.ts
+++ src/config-as-code/repo-config-service.ts
@@ -7,19 +7,43 @@ import RepoConfigDatabaseModel from "./repo-config-database-model";
 
 const logger = getLogger("services.config-as-code");
 const CONFIG_PATH = ".jira/config.yml";
+const MAX_ENVIROMENT_TESTS = 5;
+const MAX_FILE_SIZE_BYTES = 1024;
+
+/**
+ * Tests if filesize is greater than maximum - MAX_FILE_SIZE_BYTES
+ */
+export const isFileTooBig = (fileSize: number): boolean => {
+	return fileSize > MAX_FILE_SIZE_BYTES;
+}
+
+/**
+ * Iterates through environment tests and returns true if any environemnt contains too many tests
+ */
+export const hasTooManyEnvironmentMappingTests = (config: RepoConfig): boolean => {
+	const environmentMapping = config.deployments.environmentMapping;
+	return Object.keys(environmentMapping).some(key => {
+		return environmentMapping[key].length > MAX_ENVIROMENT_TESTS
+	});
+}
 
 /**
  * Fetches contents from CONFIG_PATH using guthub api, transforms its from base64 to ascii and returns the transformed string.
  */
-const getRepoConfigFromGitHub = async (githubInstallationId: number, owner: string, repo: string): Promise<string | null> => {
+export const getRepoConfigFromGitHub = async (githubInstallationId: number, owner: string, repo: string): Promise<string | null> => {
 	const client = new GitHubClient(getCloudInstallationId(githubInstallationId), logger);
 	const response = await client.getRepositoryContent(owner, repo, CONFIG_PATH);
+	
 	if (response == null) {
 		return null;
 	}
 
 	if (response.data === undefined || Array.isArray(response.data) || response.data.content === undefined) {
 		return null;
+	}
+
+	if(!isFileTooBig(response.data.size)) {
+		throw new Error(`file size is too large, max file size: ${MAX_FILE_SIZE_BYTES} bytes`)
 	}
 
 	const contentString = Buffer.from(response.data.content, "base64").toString("ascii");
@@ -30,16 +54,33 @@ const getRepoConfigFromGitHub = async (githubInstallationId: number, owner: stri
 /**
  * Converts incoming YAML string to JSON (RepoConfig)
  */
-const convertYamlToRepoConfig = (input: string): RepoConfig => {
+export const convertYamlToRepoConfig = (input: string): RepoConfig => {
+
 	const config: RepoConfig = YAML.parse(input);
-	return config;
+
+	// Trim the input data to only include the required attributes
+	const output = {
+		deployments: {
+			environmentMapping: {
+				development: config.deployments.environmentMapping.development,
+				testing: config.deployments.environmentMapping.testing,
+				staging: config.deployments.environmentMapping.staging,
+				production: config.deployments.environmentMapping.production,
+			}
+		}
+	}
+
+	if(hasTooManyEnvironmentMappingTests(output)) {
+		throw new Error(`Too many enviroment mapping tests, maximum test per environemnt: ${MAX_ENVIROMENT_TESTS}`)
+	}
+	return output;
 }
 
 /**
  * Saves a JSON blob to the RepoConfig database with installationId and RepoId
  */
-const saveRepoConfigToDB = async (githubInstallationId: number, repoId: number, config: RepoConfig): Promise<RepoConfig | null> => {
-	logger.info({ githubInstallationId, repoId, config }, "saving repo config to Database");
+export const saveRepoConfigToDB = async (githubInstallationId: number, repoId: number, config: RepoConfig): Promise<RepoConfig | null> => {
+	logger.info({githubInstallationId, repoId, config}, "saving repo config to Database");
 	return await RepoConfigDatabaseModel.saveOrUpdate(githubInstallationId, repoId, config)
 }
 
