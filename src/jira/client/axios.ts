3cc4a4c4711604fc4be4ea052c703ef646cc91ca
Michel Boudreau <michelboudreau@gmail.com>
2022-02-09 09:18:19 +1100

    ARC-1059 url params revert and JWT token fix (#1067)
    * Revert "Revert url params (#1065)"
    
    This reverts commit 0f75712e79a2cf2007b943fae4baccec09440f88.
    
    * fixing JWT token generation for Jira API calls
    
    * removing accidental paste
    
    * changing order of interceptors because they're in reverse
    
    * adding tests to axios middlewares, changing URL templating syntax to be more specific
    
    * fix metrics test
    
    * adding more tests to JWT token generation

--- src/jira/client/axios.ts
+++ src/jira/client/axios.ts
@@ -5,70 +5,22 @@ import url from "url";
 import statsd from "../../config/statsd";
 import { getLogger } from "../../config/logger";
 import { metricHttpRequest } from "../../config/metric-names";
-import { createQueryStringHash, encodeSymmetric } from "atlassian-jwt";
-import {urlParamsMiddleware} from "../../util/axios/common-middleware";
-
-const instance = process.env.INSTANCE_NAME;
-const iss = `com.github.integration${instance ? `.${instance}` : ""}`;
-
-// TODO: type hack to fix custom implementation of URL templating vars
-declare module "axios" {
-	interface AxiosRequestConfig {
-		urlParams?: Record<string, string>;
-	}
-}
-
-/**
- * Middleware to create a custom JWT for a request.
- *
- * @param {string} secret - The key to use to sign the JWT
- */
-function getAuthMiddleware(secret: string) {
-	return (
-		/**
-		 * @param {import("axios").AxiosRequestConfig} config - The config for the outgoing request.
-		 * @returns {import("axios").AxiosRequestConfig} Updated axios config with authentication token.
-		 */
-		(config) => {
-			const { query, pathname } = url.parse(config.url, true);
-
-			const jwtToken = encodeSymmetric(
-				{
-					...getExpirationInSeconds(),
-					iss,
-					qsh: createQueryStringHash({
-						method: config.method,
-						pathname: pathname || undefined,
-						query
-					})
-				},
-				secret
-			);
-
-			return {
-				...config,
-				headers: {
-					...config.headers,
-					Authorization: `JWT ${jwtToken}`
-				}
-			};
-		}
-	);
-}
+import { urlParamsMiddleware } from "../../util/axios/url-params-middleware";
+import { jiraAuthMiddleware } from "../../util/axios/jira-auth-middleware";
 
 /**
  * Wrapper for AxiosError, which includes error status
  */
 export class JiraClientError extends Error {
 	status?: number;
-	cause: AxiosError
+	cause: AxiosError;
 
-	constructor(message: string, cause: AxiosError, status?:number) {
+	constructor(message: string, cause: AxiosError, status?: number) {
 		super(message);
-		this.status = status
+		this.status = status;
 
 		//Remove config from the cause to prevent large payloads from being logged
-		this.cause = {...cause, config: {}};
+		this.cause = { ...cause, config: {} };
 	}
 }
 
@@ -92,80 +44,57 @@ export const getJiraErrorMessages = (status: number) => {
 /**
  * Middleware to enhance failed requests in Jira.
  */
-function getErrorMiddleware(logger: Logger) {
-	return (
-		/**
-		 * Potentially enrich the promise's rejection.
-		 *
-		 * @param {import("axios").AxiosError} error - The error response from Axios
-		 * @returns {Promise<Error>} The rejected promise
-		 */
-		(error: AxiosError): Promise<Error> => {
+const getErrorMiddleware = (logger: Logger) =>
+	/**
+	 * Potentially enrich the promise's rejection.
+	 *
+	 * @param {import("axios").AxiosError} error - The error response from Axios
+	 * @returns {Promise<Error>} The rejected promise
+	 */
+	(error: AxiosError): Promise<Error> => {
 
-			const status = error?.response?.status;
+		const status = error?.response?.status;
 
-			const errorMessage = "Error executing Axios Request " + (status ? getJiraErrorMessages(status) : error.message || "");
+		const errorMessage = "Error executing Axios Request " + (status ? getJiraErrorMessages(status) : error.message || "");
 
-			const isWarning = status && (status >= 300 && status < 500 && status !== 400);
+		const isWarning = status && (status >= 300 && status < 500 && status !== 400);
 
-			// Log appropriate level depending on status - WARN: 300-499, ERROR: everything else
-			// Log exception only if it is error, because AxiosError contains the request payload
-			if (isWarning) {
-				logger.warn(errorMessage)
-			} else {
-				logger.error({err: error}, errorMessage)
-			}
+		// Log appropriate level depending on status - WARN: 300-499, ERROR: everything else
+		// Log exception only if it is error, because AxiosError contains the request payload
+		if (isWarning) {
+			logger.warn(errorMessage);
+		} else {
+			logger.error({ err: error }, errorMessage);
+		}
 
-			return Promise.reject(new JiraClientError(errorMessage, error, status));
-		});
-}
+		return Promise.reject(new JiraClientError(errorMessage, error, status));
+	};
 
 /**
  * Middleware to enhance successful requests in Jira.
  *
  * @param {import("probot").Logger} logger - The probot logger instance
  */
-function getSuccessMiddleware(logger: Logger) {
-	return (
-		/**
-		 * DEBUG log the response info from Jira
-		 *
-		 * @param {import("axios").AxiosResponse} response - The response from axios
-		 * @returns {import("axios").AxiosResponse} The axios response
-		 */
-		(response) => {
-			logger.debug(
-				{
-					params: response.config.urlParams
-				},
-				`Jira request: ${response.config.method.toUpperCase()} ${
-					response.config.originalUrl
-				} - ${response.status} ${response.statusText}
+const getSuccessMiddleware = (logger: Logger) =>
+	/**
+	 * DEBUG log the response info from Jira
+	 *
+	 * @param {import("axios").AxiosResponse} response - The response from axios
+	 * @returns {import("axios").AxiosResponse} The axios response
+	 */
+	(response) => {
+		logger.debug(
+			{
+				params: response.config.urlParams
+			},
+			`Jira request: ${response.config.method.toUpperCase()} ${
+				response.config.originalUrl
+			} - ${response.status} ${response.statusText}
 				Response data: ${JSON.stringify(response.data)}`
-			);
+		);
 
-			return response;
-		}
-	);
-}
-
-
-/*
- * The Atlassian API uses JSON Web Tokens (JWT) for authentication along with
- * Query String Hashing (QSH) to prevent URL tampering. IAT, or issued-at-time,
- * is a Unix-style timestamp of when the token was issued. EXP, or expiration
- * time, is a Unix-style timestamp of when the token expires and must be no
- * more than three minutes after the IAT. Since our tokens are per-request and
- * short-lived, we use a timeout of 30 seconds.
- */
-function getExpirationInSeconds() {
-	const nowInSeconds = Math.floor(Date.now() / 1000);
-
-	return {
-		iat: nowInSeconds,
-		exp: nowInSeconds + 30
+		return response;
 	};
-}
 
 /**
  * Enrich the config object to include the time that the request started.
@@ -183,7 +112,7 @@ const setRequestStartTime = (config) => {
  *
  */
 export const extractPath = (someUrl = ""): string =>
-	url.parse(someUrl).pathname || "";
+	decodeURIComponent(url.parse(someUrl).pathname || "");
 
 /**
  * Submit statsd metrics on successful requests.
@@ -192,7 +121,7 @@ export const extractPath = (someUrl = ""): string =>
  * @returns {import("axios").AxiosResponse} The response object.
  */
 const instrumentRequest = (response) => {
-	if(!response) {
+	if (!response) {
 		return;
 	}
 	const requestDurationMs = Number(
@@ -241,14 +170,23 @@ export default (
 		timeout: Number(process.env.JIRA_TIMEOUT) || 30000
 	});
 
+	// *** IMPORTANT: Interceptors are executed in reverse order. ***
+	// the last one specified is the first to executed.
+
 	instance.interceptors.request.use(setRequestStartTime);
+
+	// This has to be the before any middleware that might change the URL
+	// to generate the JWT token for Jira API correctly
+	instance.interceptors.request.use(jiraAuthMiddleware(secret, instance));
+
+	// URL params need to be at the bottom, after auth middle,
+	// to generate the correct path before creating a JWT token based on it.
+	instance.interceptors.request.use(urlParamsMiddleware);
+
 	instance.interceptors.response.use(
 		instrumentRequest,
 		instrumentFailedRequest()
 	);
-
-	instance.interceptors.request.use(getAuthMiddleware(secret));
-	instance.interceptors.request.use(urlParamsMiddleware);
 
 	instance.interceptors.response.use(
 		getSuccessMiddleware(logger),
