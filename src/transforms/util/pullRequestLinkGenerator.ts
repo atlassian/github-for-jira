2487d66f85af3aab4534eca8e2ab696db9894fa2
Josh Kay <joshk@atlassian.com>
2022-02-14 09:41:04 +1300

    ARC-778 - refactor with default keys value and ensure urlencoding on all variables

--- src/transforms/util/pullRequestLinkGenerator.ts
+++ src/transforms/util/pullRequestLinkGenerator.ts
@@ -2,16 +2,9 @@
  * Generates a create pull request link for GH in the format [URL]/compare/[name]?title=[...keys]-[name]
  * This format allows the use of title query param to set the PR name
  */
-export const generateCreatePullRequestUrl = (baseUrl: string, name: string, issueKeys?: string[]) => {
-	// Remove duplicate keys
-	const uniqueIssueKeys: string[] = [...new Set(issueKeys)];
-	let title: string | undefined = uniqueIssueKeys?.join(" ");
+export const generateCreatePullRequestUrl = (baseUrl: string, name: string, issueKeys: string[] = []) => {
+	const keys = issueKeys.length ? `${issueKeys.join(" ")} - ` : "";
+	const title = encodeURIComponent(keys + name);
 
-	// Only prepend issue keys if they exist
-	if (title) {
-		title = encodeURIComponent(`${title} - ${name}`);
-	} else {
-		title = encodeURIComponent(`${name}`);
-	}
 	return  `${baseUrl}/compare/${name}?title=${title}`;
 }
