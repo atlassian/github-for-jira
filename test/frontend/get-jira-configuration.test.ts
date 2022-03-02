7560cc6ef7885e816b0e38e9f156c5cc83e0f2c9
Amanda Trigueiro <atrigueiro@atlassian.com>
2022-03-02 11:16:52 +1100

    fix test

--- test/frontend/get-jira-configuration.test.ts
+++ test/frontend/get-jira-configuration.test.ts
@@ -3,7 +3,7 @@ import { JiraConfigurationGet } from "../../src/routes/jira/configuration/jira-c
 import { Installation, RepoSyncState, Subscription } from "../../src/models";
 import SubscriptionClass from "../../src/models/subscription";
 
-jest.mock("../../src/frontend/analytics-client");
+jest.mock("../../src/util/analytics-client");
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { sendAnalytics } = require("../../src/util/analytics-client");
 
