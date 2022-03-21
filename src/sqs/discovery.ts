8971aa63061e2262422bce1fe099883b09146b05
Josh Kay <joshk@atlassian.com>
2022-03-22 11:18:26 +1300

    removed feature flag use-new-github-client-for-discovery

--- src/sqs/discovery.ts
+++ src/sqs/discovery.ts
@@ -1,7 +1,5 @@
 import { MessageHandler } from "./index";
-import app from "../worker/app";
-import { discovery, discoveryOctoKit } from "../sync/discovery";
-import { booleanFlag, BooleanFlags } from "../config/feature-flags";
+import { discovery } from "../sync/discovery";
 
 export type DiscoveryMessagePayload = {
 	installationId: number,
@@ -9,14 +7,9 @@ export type DiscoveryMessagePayload = {
 }
 
 export const discoveryQueueMessageHandler : MessageHandler<DiscoveryMessagePayload> = async (context) => {
-	const useNewGHClient = await booleanFlag(BooleanFlags.USE_NEW_GITHUB_CLIENT_FOR_DISCOVERY, false, context?.payload?.jiraHost);
 	context.log = context.log.child({
 		jiraHost: context?.payload?.jiraHost,
 		installationId: context?.payload?.installationId
 	})
-	if(useNewGHClient) {
-		await discovery(context.payload, context.log);
-		return;
-	}
-	await discoveryOctoKit(app)({ data: context.payload }, context.log);
+	await discovery(context.payload, context.log);
 };
