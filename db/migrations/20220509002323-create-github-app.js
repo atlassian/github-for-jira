ad24743c985a5af3a0a896dc4f3ef232bd40804c
Harminder <hsingh5@atlassian.com>
2022-05-09 17:31:03 +1000

    Added script file to create GithubApps table

---
+++ db/migrations/20220509002323-create-github-app.js
@@ -0,0 +1,47 @@
+'use strict';
+
+module.exports = {
+	up: async (queryInterface, Sequelize) => {
+		return queryInterface.createTable('GithubApps', {
+			id: {
+				allowNull: false,
+				autoIncrement: true,
+				primaryKey: true,
+				type: Sequelize.INTEGER
+			},
+			githubBaseUrl: {
+				allowNull: false,
+				type: Sequelize.STRING
+			},
+			githubApiBaseUrl: {
+				allowNull: false,
+				type: Sequelize.STRING
+			},
+			githubServerType: {
+				allowNull: false,
+				type: Sequelize.ENUM("cloud", "ghe"),
+
+			},
+			githubClientId: {
+				allowNull: false,
+				type: Sequelize.STRING
+			},
+			secrets: {
+				allowNull: false,
+				type: Sequelize.BLOB
+			},
+			createdAt: {
+				allowNull: false,
+				type: Sequelize.DATE
+			},
+			updatedAt: {
+				allowNull: false,
+				type: Sequelize.DATE
+			}
+		})
+	},
+
+	down: async (queryInterface, Sequelize) => {
+		return queryInterface.dropTable('GithubApps')
+	}
+};
