5d87bb6a80fd3f0f4910d1832adb1ea87ff5e52a
Harminder <hsingh5@atlassian.com>
2022-05-10 11:33:03 +1000

    made some fields non-nulllable

--- db/migrations/20220509002323-create-github-app.js
+++ db/migrations/20220509002323-create-github-app.js
@@ -1,43 +1,47 @@
 'use strict';
 
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    return queryInterface.createTable('GithubApps', {
-		id: {
-		  allowNull: false,
-		  autoIncrement: true,
-		  primaryKey: true,
-		  type: Sequelize.INTEGER
-		},
-		githubBaseUrl: {
-		  type: Sequelize.STRING
-		},
-		githubApiBaseUrl: {
-		  type: Sequelize.STRING
-		},
-		githubServerType: {
-			type: Sequelize.ENUM("cloud", "ghe"),
-			allowNull: false
+	up: async (queryInterface, Sequelize) => {
+		return queryInterface.createTable('GithubApps', {
+			id: {
+				allowNull: false,
+				autoIncrement: true,
+				primaryKey: true,
+				type: Sequelize.INTEGER
+			},
+			githubBaseUrl: {
+				allowNull: false,
+				type: Sequelize.STRING
+			},
+			githubApiBaseUrl: {
+				allowNull: false,
+				type: Sequelize.STRING
+			},
+			githubServerType: {
+				allowNull: false,
+				type: Sequelize.ENUM("cloud", "ghe")
 
-		  },
-		githubClientId: {
-			type: Sequelize.STRING
-		},
-		secrets: {
-			type: Sequelize.BLOB
-		},
-		createdAt: {
-		  allowNull: false,
-		  type: Sequelize.DATE
-		},
-		updatedAt: {
-		  allowNull: false,
-		  type: Sequelize.DATE
-		}
-	  })
-  },
+			},
+			githubClientId: {
+				allowNull: false,
+				type: Sequelize.STRING
+			},
+			secrets: {
+				allowNull: false,
+				type: Sequelize.BLOB
+			},
+			createdAt: {
+				allowNull: false,
+				type: Sequelize.DATE
+			},
+			updatedAt: {
+				allowNull: false,
+				type: Sequelize.DATE
+			}
+		})
+	},
 
-  down: async (queryInterface, Sequelize) => {
-	return queryInterface.dropTable('GithubApps')
-  }
+	down: async (queryInterface, Sequelize) => {
+		return queryInterface.dropTable('GithubApps')
+	}
 };
