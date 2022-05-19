52ab3a91e5412f9ff3777e8b551bfd25a6754d9e
Rachelle <rachellerathbone@gmail.com>
2022-05-19 13:10:44 +1000

    wip

--- src/models/git-hub-server-app.ts
+++ src/models/git-hub-server-app.ts
@@ -1,7 +1,63 @@
 import { Model, DataTypes } from "sequelize";
 import { encrypted, sequelize } from "models/sequelize";
 
-class GitHubServerApp extends Model {}
+interface Secrets {
+	githubClientSecret: string;
+	webhookSecret: string;
+	privateKey: string;
+}
+
+interface GitHubServerAppPayload {
+	uuid: string;
+	githubBaseUrl: string;
+	githubClientId: string;
+	secrets: Secrets;
+}
+
+export class GitHubServerApp extends Model {
+	id: number;
+	uuid: string;
+	githubBaseUrl: string;
+	githubClientId: string;
+	secrets: string;
+	githubClientSecret: string;
+	webhookSecret: string;
+	privateKey: string;
+
+	/**
+	 * Get GitHubServerApp
+	 *
+	 * @param {{gitHubServerAppId: number}} gitHubServerAppId
+	 * @returns {GitHubServerApp}
+	 */
+	static async getForGitHubServerAppId(
+		gitHubServerAppId: number
+	): Promise<GitHubServerApp | null> {
+		if (!gitHubServerAppId) {
+			return null;
+		}
+
+		return this.findOne({
+			where: {
+				id: gitHubServerAppId
+			}
+		});
+	}
+
+	/**
+	 * Create a new GitHubServerApp object
+	 *
+	 * @param {{host: string, clientKey: string, secret: string}} payload
+	 * @returns {GitHubServerApp}
+	 */
+	static async install(payload: GitHubServerAppPayload): Promise<GitHubServerApp> {
+		const [gitHubServerApp] = await this.create({
+			...payload
+		});
+
+		return gitHubServerApp;
+	}
+}
 
 GitHubServerApp.init({
 	id: {
@@ -38,3 +94,4 @@ GitHubServerApp.init({
 		allowNull: false
 	})
 }, { sequelize });
+
--- src/util/check-github-app-type.ts
+++ src/util/check-github-app-type.ts
@@ -1,14 +1,17 @@
 // this will need to be updated later to check for database entry
 import { Installation } from "models/installation";
 import { envVars } from "config/env";
+import {GitHubServerApp} from "models/git-hub-server-app";
+import {getLogger} from "config/logger";
+
+const logger = getLogger("mylogger");
 
 export const getGitHubBaseUrl = async (jiraHost: string): Promise<string> => {
 	const installation = await Installation.getForHost(jiraHost);
 	const gitHubAppId = installation?.githubAppId;
+	const gitHubServerApp = gitHubAppId && await GitHubServerApp.getForGitHubServerAppId(gitHubAppId);
 
-	// TODO - if gitHubAppId query GitHubServerApps table to get githubBaseUrl
-	return gitHubAppId ? "http://github.internal.atlassian.com" : envVars.GITHUB_HOSTNAME;
-
+	return gitHubServerApp ? gitHubServerApp?.githubBaseUrl : envVars.GITHUB_HOSTNAME;
 }
 
 export const setGitHubBaseUrl = (gitHubBaseUrl: string): string => {
