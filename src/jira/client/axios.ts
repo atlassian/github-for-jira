8ad1aae93afcc52b33bd55aa994920bf5d53b352
Amanda Trigueiro <atrigueiro@atlassian.com>
2022-05-23 15:36:40 +1000

    Fix logging

--- src/jira/client/axios.ts
+++ src/jira/client/axios.ts
@@ -153,10 +153,10 @@ const instrumentFailedRequest = (instance: AxiosInstance, logger: Logger) => {
 		instrumentRequest(error?.response);
 		if (error.response?.status === 503 || error.response?.status === 404) {
 			try {
-				logger.info("calling status endpoint")
+				logger.info("calling status endpoint");
 				await instance.get("/status");
 			} catch (error) {
-				console.log(`ERROR OBJ IS -====>>> ${error}`)
+				logger.info(`ERROR OBJ IS -====>>> ${error}`);
 				if (error?.status === 503 || error?.status === 404) {
 					logger.info(`503 from Jira: Jira instance ${instance} has been deactivated, is suspended or does not exist. Returning 404 to our application.`);
 					error.response.status = 404;
