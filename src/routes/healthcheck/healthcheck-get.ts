4f399cc7ad488cf06805818b01ef388ec3a7bce8
Josh Kay <joshk@atlassian.com>
2022-05-27 13:12:45 +1200

    ARC-1366 removed debugging data

--- src/routes/healthcheck/healthcheck-get.ts
+++ src/routes/healthcheck/healthcheck-get.ts
@@ -1,11 +1,8 @@
 import { Request, Response } from "express";
 import { getLogger } from "config/logger";
-import { createHashWithSharedSecret } from "~/src/util/encryption";
 
 const logger = getLogger("healthcheck");
 export const HealthcheckGet = (_: Request, res: Response): void => {
 	logger.debug("Successfully called /healthcheck.");
-	// TEMPORARY DISPLAY OF HASH SO I CAN VERiFIY ON STAGE / MICROS STASH
-	const hash = createHashWithSharedSecret('testdata');
-	res.status(200).send("OK " + hash);
+	res.status(200).send("OK");
 };
