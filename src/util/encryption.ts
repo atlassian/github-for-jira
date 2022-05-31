e2f0757906dddcf1662a3d956f06ece7b9ea16c1
Josh Kay <joshk@atlassian.com>
2022-05-31 14:28:53 +1200

    back to non defulat key

--- src/util/encryption.ts
+++ src/util/encryption.ts
@@ -1,8 +1,8 @@
 import { BinaryLike, createHmac } from "crypto";
 import { envVars } from "../config/env";
 
-export const createHashWithSharedSecret = (data: BinaryLike, secret = envVars.GLOBAL_HASH_SECRET): string => {
-	return createHmac("sha256", secret)
+export const createHashWithSharedSecret = (data: BinaryLike): string => {
+	return createHmac("sha256", envVars.GLOBAL_HASH_SECRET)
 		.update(data)
 		.digest("hex");
 }
