c24859fc093afd10a1fefe165dfda71f6ce919d8
Gary Xue <gxue@atlassian.com>
2022-06-01 15:50:48 +1000

    add more test

--- src/middleware/jirahost-middleware.test.ts
+++ src/middleware/jirahost-middleware.test.ts
@@ -2,49 +2,67 @@ import {NextFunction, Request, Response} from "express";
 import {jirahostMiddleware} from "./jirahost-middleware";
 import {verifyJiraJwtMiddleware } from "middleware/jira-jwt-middleware";
 import {postInstallUrl} from "routes/jira/jira-atlassian-connect-get";
+import {TokenType} from "~/src/jira/util/jwt";
 
 jest.mock("middleware/jira-jwt-middleware");
 const mockVerifyJiraJwtMiddleware = (verifyJiraJwtMiddleware as any) as jest.Mock<typeof verifyJiraJwtMiddleware>;
 
 //const APP_URL = 'https://github-for-jira-app.atlassian.com';
 const LEGIT_JIRA_HOST = "https://legit-jira-host.atlassian.net";
+//const TEST_JWT_TOKEN = 'TO_BE_DETERMINED';
 
 describe("jirahostMiddleware", () => {
 
 	let req: Request, res: Response, next: NextFunction;
+	let mockJwtVerificationFn;
 	beforeEach(() => {
 		req = getReq();
 		res = getRes();
 		next = jest.fn();
-		mockVerifyJiraJwtMiddleware.mockImplementation(() => () => async (_, __, n) => {n()});
+		mockJwtVerificationFn = jest.fn();
+		mockVerifyJiraJwtMiddleware.mockReturnValue(mockJwtVerificationFn);
 	});
 
+	describe('jiraHost is provided in a trusted session cookie', ()=>{
+		it('should extract jiraHost from session', ()=>{
+			req.session.jiraHost = LEGIT_JIRA_HOST;
+			jirahostMiddleware(req, res, next);
+			expect(res.locals.jiraHost).toBe(LEGIT_JIRA_HOST);
+			expect(next).toBeCalled();
+		});
+	});
 	describe("jiraHost is provided by jira software", () => {
-		beforeEach(() => {
+		it("should extract jiraHost correctly from xdm_e query", () => {
 			req.path = postInstallUrl;
 			req.method = "GET",
 			req.query = {xdm_e: LEGIT_JIRA_HOST}
-		});
-		it("should extract jiraHost correctly from xdm_e query", () => {
 			jirahostMiddleware(req, res, next);
-			expectJiraHostInResLocals();
+			expect(res.locals.jiraHost).toBe(LEGIT_JIRA_HOST);
+			expect(mockVerifyJiraJwtMiddleware).toBeCalledWith(TokenType.normal);
 		});
 	});
 	describe("jiraHost is provided by app form post", () => {
-		beforeEach(()=>{
+		it("should extract jiraHost correctly from request body", ()=>{
 			req.path = postInstallUrl;
 			req.method = "POST";
 			req.body = {jiraHost: LEGIT_JIRA_HOST}
-		});
-		it("should extract jiraHost correctly from request body", ()=>{
 			jirahostMiddleware(req, res, next);
-			expectJiraHostInResLocals();
+			expect(res.locals.jiraHost).toBe(LEGIT_JIRA_HOST);
+			expect(mockVerifyJiraJwtMiddleware).toBeCalledWith(TokenType.context);
+		});
+	});
+	describe('jiraHost is provided by cookie as temporary method', () => {
+		it("should extract jiraHost correctly from cookie and remove cookie", () => {
+			req.path = '/whatever';
+			req.method = 'GET';
+			req.cookies.jiraHost = LEGIT_JIRA_HOST;
+			jirahostMiddleware(req, res, next);
+			expect(res.locals.jiraHost).toBe(LEGIT_JIRA_HOST);
+			expect(res.clearCookie).toBeCalledWith('jiraHost');
+			expect(mockVerifyJiraJwtMiddleware).toBeCalledWith(TokenType.context);
 		});
 	});
 
-	function expectJiraHostInResLocals() {
-		expect(res.locals.jiraHost).toBe(LEGIT_JIRA_HOST);
-	}
 });
 
 function getReq(): Request {
