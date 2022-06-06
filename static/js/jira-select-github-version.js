d93f015ebc2496ea6f47d7ead66ebff4e09e0cea
Rachelle <rachellerathbone@gmail.com>
2022-06-06 11:07:17 +1000

    complete states for gh version screen

---
+++ static/js/jira-select-github-version.js
@@ -0,0 +1,17 @@
+$('.jiraSelectGitHubVersion__options__card').click(function (event) {
+	event.preventDefault();
+	const selectedCard = event.target.className;
+	const selectedVersion = selectedCard.substring(selectedCard.indexOf(' ') + 1);
+	const className = `.jiraSelectGitHubVersion__options__card.${selectedVersion}`
+
+	if (selectedVersion === "cloud") {
+		$(".jiraSelectGitHubVersion__options__card.server").removeClass("selected");
+	}
+
+	if (selectedVersion === "server") {
+		$(".jiraSelectGitHubVersion__options__card.cloud").removeClass("selected");
+	}
+
+	$(`${className}`).addClass("selected");
+	$(".jiraSelectGitHubVersion__actionBtn").prop("disabled", false).attr("aria-disabled", "false").addClass("aui-button-primary");
+})
