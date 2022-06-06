d5e933cc4d77b68ea79342e95c4c3c64333418f7
Rachelle <rachellerathbone@gmail.com>
2022-06-06 15:39:56 +1000

    remove conditional

--- static/js/jira-select-github-version.js
+++ static/js/jira-select-github-version.js
@@ -2,13 +2,9 @@ $('.jiraSelectGitHubVersion__options__card').click(function (event) {
 	event.preventDefault();
 	const selectedCard = event.target.className;
 	const selectedVersion = selectedCard.includes("cloud") ? "cloud" : "server";
-	$(event.target).closest('.jiraSelectGitHubVersion__options__card').addClass('selected')
+	const className = `.jiraSelectGitHubVersion__options__card.${selectedVersion}`;
 
-	if (selectedVersion === "cloud" && $(".jiraSelectGitHubVersion__options__card.server").hasClass("selected")) {
-		$(".jiraSelectGitHubVersion__options__card.server").removeClass("selected");
-	} else if (selectedVersion === "server" && $(".jiraSelectGitHubVersion__options__card.cloud").hasClass("selected")) {
-		$(".jiraSelectGitHubVersion__options__card.cloud").removeClass("selected");
-	}
-
+	$(".jiraSelectGitHubVersion__options__card").removeClass("selected");
+	$(`${className}`).addClass("selected");
 	$(".jiraSelectGitHubVersion__actionBtn").prop("disabled", false).attr("aria-disabled", "false").addClass("aui-button-primary");
 })
