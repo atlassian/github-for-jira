9930fb014c83393f5d1b091188308bc141ad66d6
Rachelle <rachellerathbone@gmail.com>
2022-06-06 15:35:35 +1000

    update addclass

--- static/js/jira-select-github-version.js
+++ static/js/jira-select-github-version.js
@@ -2,7 +2,7 @@ $('.jiraSelectGitHubVersion__options__card').click(function (event) {
 	event.preventDefault();
 	const selectedCard = event.target.className;
 	const selectedVersion = selectedCard.includes("cloud") ? "cloud" : "server";
-	const className = `.jiraSelectGitHubVersion__options__card.${selectedVersion}`;
+	$(event.target).closest('.jiraSelectGitHubVersion__options__card').addClass('selected')
 
 	if (selectedVersion === "cloud" && $(".jiraSelectGitHubVersion__options__card.server").hasClass("selected")) {
 		$(".jiraSelectGitHubVersion__options__card.server").removeClass("selected");
@@ -10,6 +10,5 @@ $('.jiraSelectGitHubVersion__options__card').click(function (event) {
 		$(".jiraSelectGitHubVersion__options__card.cloud").removeClass("selected");
 	}
 
-	$(`${className}`).addClass("selected");
 	$(".jiraSelectGitHubVersion__actionBtn").prop("disabled", false).attr("aria-disabled", "false").addClass("aui-button-primary");
 })
