e9f9f518bfaf3301a8c574f48a4f65ddfc67e44e
Josh Kay <joshk@atlassian.com>
2022-06-08 10:17:42 +1200

    ff-remove-get-install fixed final test

--- src/routes/jira/configuration/jira-configuration-get.test.ts
+++ src/routes/jira/configuration/jira-configuration-get.test.ts
@@ -66,6 +66,9 @@ describe("Jira Configuration Suite", () => {
 
 	it("should return success message after page is rendered", async () => {
 		const response = mockResponse();
+		githubNock
+			.get(`/app/installations/15`)
+			.reply(200, singleInstallation);
 
 		await JiraConfigurationGet(mockRequest(), response, jest.fn());
 		const data = response.render.mock.calls[0][1];
