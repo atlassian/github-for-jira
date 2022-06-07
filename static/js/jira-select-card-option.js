77efac529743038a22fa0b9fde8a648a08f05ed2
Rachelle <rachellerathbone@gmail.com>
2022-06-07 11:44:30 +1000

    remove data type

--- static/js/jira-select-card-option.js
+++ static/js/jira-select-card-option.js
@@ -1,8 +1,6 @@
 $(".optionsCard").click(function (event) {
 	event.preventDefault();
-	const selectedVersion = $(event.target).data("type");
-
 	$(".optionsCard").removeClass("selected");
-	$(`.optionsCard.${selectedVersion}`).addClass("selected");
+	$(event.currentTarget).addClass("selected");
 	$(".optionBtn").prop("disabled", false).attr("aria-disabled", "false").addClass("aui-button-primary");
 });
