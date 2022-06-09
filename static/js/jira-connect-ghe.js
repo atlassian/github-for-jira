a1797f4a01bbd3bacf668b6304cce93b8ee198cc
Kayub Maharjan <kay.krazzie@gmail.com>
2022-06-10 08:59:33 +1000

    - some replaced by includes for protocal check

--- static/js/jira-connect-ghe.js
+++ static/js/jira-connect-ghe.js
@@ -11,7 +11,7 @@ const checkValidUrl = inputURL => {
 	try {
 		const { protocol, hostname } = new URL(inputURL);
 
-		if (!ALLOWED_PROTOCOLS.some(allowedProtocol => protocol === allowedProtocol)) {
+		if (!ALLOWED_PROTOCOLS.includes(protocol)) {
 			return false;
 		}
 		if (!ALLOWED_DOMAIN_EXTENSIONS.some(extension => hostname.endsWith(extension))) {
