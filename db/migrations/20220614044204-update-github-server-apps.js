f25585a8d113b2e0524f0b289c30d00d007f3d06
Rachelle <rachellerathbone@gmail.com>
2022-06-14 15:04:06 +1000

    wip

---
+++ db/migrations/20220614044204-update-github-server-apps.js
@@ -0,0 +1,16 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  }
+};
