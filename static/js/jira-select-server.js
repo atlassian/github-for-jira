5a6b512eca1327607d87b40e79d7818a1103f027
Kayub Maharjan <kay.krazzie@gmail.com>
2022-06-21 09:46:10 +1000

    - Review changes: Added a TODO

--- static/js/jira-select-server.js
+++ static/js/jira-select-server.js
@@ -4,14 +4,15 @@ $('.install-link').click(function (event) {
   const csrfToken = document.getElementById('_csrf').value;
   const clientKey = document.getElementById('clientKey').value;
 
+  // TODO: Revisit later once all the screens are hooked up
   $.post('/github/configuration', {
     installationId,
     _csrf: csrfToken,
     clientKey
   }, function (data) {
     if (data.err) {
-      return console.log(data.err)
+      return console.log(data.err);
     }
-    window.close()
+    window.close();
   });
 });
