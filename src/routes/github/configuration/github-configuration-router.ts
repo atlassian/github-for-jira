4c47f264f1e42e2e2da6ef3b3150b6a27fb0a559
Rachelle <rachellerathbone@gmail.com>
2022-06-21 08:43:43 +1000

    part 1 of pr

--- src/routes/github/configuration/github-configuration-router.ts
+++ src/routes/github/configuration/github-configuration-router.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { GithubConfigurationGet } from "./github-configuration-get";
 import { GithubConfigurationPost } from "./github-configuration-post";
+import { GithubServerAppMiddleware } from "middleware/github-server-app-middleware";
 
 export const GithubConfigurationRouter = Router();
 
 GithubConfigurationRouter.route("/")
+	.all(GithubServerAppMiddleware)
 	.get(GithubConfigurationGet)
 	.post(GithubConfigurationPost);
