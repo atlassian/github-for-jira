41abbe9518e3dd58c4671c2364b9a6336d7daeec
Boris Gvozdev <bgvozdev@atlassian.com>
2022-06-20 21:10:10 +1000

    testing

--- src/middleware/cookiesession-middleware.ts
+++ src/middleware/cookiesession-middleware.ts
@@ -5,8 +5,7 @@ import { createHashWithSharedSecret } from "utils/encryption";
 
 const THIRTY_DAYS_MSEC = 30 * 24 * 60 * 60 * 1000;
 
-// TODO: replace with encryption + Cryptor
-export const cookieSessionMiddleware = cookieSession({
+const middleware = cookieSession({
 	keys: [createHashWithSharedSecret(envVars.STORAGE_SECRET), envVars.GITHUB_CLIENT_SECRET],
 	maxAge: THIRTY_DAYS_MSEC,
 	signed: true,
@@ -14,3 +13,22 @@ export const cookieSessionMiddleware = cookieSession({
 	secure: true,
 	httpOnly: false
 });
+
+// TODO: replace with encryption + Cryptor
+export const cookieSessionMiddleware = (req, resp, next) => {
+	return middleware(req, resp, () => {
+		if (req.session.encryptedData) {
+			req.session = JSON.parse(req.session.encryptedData.substring("blah".length));
+		}
+		try {
+			next();
+		} finally {
+			if (req.session) {
+				const session = req.session;
+				req.session = {
+					encryptedData: "blah" + JSON.stringify(session)
+				};
+			}
+		}
+	});
+};
