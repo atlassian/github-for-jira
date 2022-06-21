10592bc4383d1a498ab581c89cc146a96828f42a
Michel Boudreau <michelboudreau@gmail.com>
2022-06-21 16:51:32 +1000

    removing cypress config as it's not being used

--- cypress.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "cypress";
-
-export default defineConfig({
-  e2e: {
-    setupNodeEvents(_on, _config) {
-      // implement node event listeners here
-    },
-  },
-});
