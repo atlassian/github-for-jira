a5e04fe87b9d5f25411a2264c5742aa9a0b4c080
Boris Gvozdev <bgvozdev@atlassian.com>
2022-06-22 21:00:41 +1000

    ARC-1441: debugging

--- src/util/cryptor-http-client.ts
+++ src/util/cryptor-http-client.ts
@@ -90,13 +90,25 @@ export class CryptorHttpClient {
 		try {
 			const result = (await instance.post(path, data)).data;
 			const finished = new  Date().getTime();
-			statsd.histogram(cryptorMetrics.clientHttpCallDuration, finished - started, {operation});
+			statsd.histogram(cryptorMetrics.clientHttpCallDuration, finished - started, { operation });
 
 			return result;
 		}
-		catch (e) {
-			logger.info({ error: e }, "caught error");
-			throw e;
+		catch (error) {
+			logger.info({ json: error.toJSON() }, "json");
+			if (error.response) {
+				// The request was made and the server responded with a status code
+				// that falls out of the range of 2xx
+				logger.info({ data: error.response.data, status: error.response.status, headers: error.response.headers }, "at 1");
+			} else if (error.request) {
+				// The request was made but no response was received
+				// `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+				// http.ClientRequest in node.js
+				logger.info({ request: error.request }, "at 2");
+			} else {
+				// Something happened in setting up the request that triggered an Error
+				logger.info({ error: error.message }, "at 3");
+			}
 		}
 	}
 }
