220e7d282e0c79dc79862b273afec7c2ae9a9b39
Kayub Maharjan <kay.krazzie@gmail.com>
2022-06-28 09:25:29 +1000

    - Review changes

--- static/js/jira-select-server.js
+++ static/js/jira-select-server.js
@@ -1,18 +1,5 @@
-$('.install-link').click(function (event) {
+$('.select-server').click(function (event) {
   event.preventDefault();
-  const installationId = $(event.target).data('installation-id');
-  const csrfToken = document.getElementById('_csrf').value;
-  const clientKey = document.getElementById('clientKey').value;
 
-  // TODO: Revisit later once all the screens are hooked up
-  $.post('/github/configuration', {
-    installationId,
-    _csrf: csrfToken,
-    clientKey
-  }, function (data) {
-    if (data.err) {
-      return console.log(data.err);
-    }
-    window.close();
-  });
+  // TODO: Need to add the logic for navigation
 });
