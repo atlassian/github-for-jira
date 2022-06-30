21ef5659a7a8a4bd9d33426e62db25459684a808
Gary Xue <gxue@atlassian.com>
2022-06-30 13:48:52 +1000

    add db migration for extra columns

---
+++ db/migrations/20220630002212-add-github-client-secrets-to-github-server-apps.js
@@ -0,0 +1,26 @@
+"use strict";
+
+const tableName = "GitHubServerApps";
+
+module.exports = {
+	up: async (queryInterface, Sequelize) => {
+		await queryInterface.addColumn(tableName, "gitHubClientSecret", {
+			type: Sequelize.STRING,
+			allowNull: false
+		});
+		await queryInterface.addColumn(tableName, "webhookSecret", {
+			type: Sequelize.STRING,
+			allowNull: false
+		});
+		await queryInterface.addColumn(tableName, "privateKey", {
+			type: Sequelize.STRING,
+			allowNull: false
+		});
+	},
+
+	down: async (queryInterface) => {
+		await queryInterface.removeColumn(tableName, "gitHubClientSecret");
+		await queryInterface.removeColumn(tableName, "webhookSecret");
+		await queryInterface.removeColumn(tableName, "privateKey");
+	}
+};
