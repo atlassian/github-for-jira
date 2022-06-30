be2e0855231b45ffcf3f8088209ff939295886b8
Gary Xue <gxue@atlassian.com>
2022-06-30 16:05:28 +1000

    Fixup db migraiton scripts

--- db/migrations/20220630002212-add-github-client-secrets-to-github-server-apps.js
+++ db/migrations/20220630002212-add-github-client-secrets-to-github-server-apps.js
@@ -23,5 +23,15 @@ module.exports = {
 		await queryInterface.removeColumn(tableName, "gitHubClientSecret");
 		await queryInterface.removeColumn(tableName, "webhookSecret");
 		await queryInterface.removeColumn(tableName, "privateKey");
+		/*
+		 * Please note, rollback of following won't work, or will have bugs in the app IF THE APP still use sequalize.encrypt, as data type is different (and allowNull will messup)
+		 * Assuming we are using Cryptor for this table in the future, so above concern need not to be worried.
+		 * If we switch back to sequalize.encrypt, we will need to use another name for the secret vault column, can't reuse this one (secret).
+		 * Having this line merely because to keep the db migration scripts backward/forward compatible, able to roll forward and backwards.
+		 */
+		await queryInterface.addColumn(tableName, "secrets", {
+			type: Sequelize.STRING,
+			allowNull: true
+		});
 	}
 };
