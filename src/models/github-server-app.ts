551728971b5854e44ffcb11e135f657e2b9cf11b
Harminder <hsingh5@atlassian.com>
2022-07-01 15:48:11 +1000

    add appId column in GitHubServerApps table

--- src/models/github-server-app.ts
+++ src/models/github-server-app.ts
@@ -6,6 +6,7 @@ const encrypted = EncryptedField(Sequelize, process.env.STORAGE_SECRET);
 
 interface GitHubServerAppPayload {
 	uuid: string;
+	appId: number;
 	gitHubBaseUrl: string;
 	gitHubClientId: string;
 	gitHubClientSecret: string;
@@ -18,6 +19,7 @@ interface GitHubServerAppPayload {
 export class GitHubServerApp extends Model {
 	id: number;
 	uuid: string;
+	appId: number;
 	gitHubBaseUrl: string;
 	gitHubClientId: string;
 	gitHubClientSecret: string;
@@ -54,6 +56,7 @@ export class GitHubServerApp extends Model {
 	 * @param {{
 	 * 		gitHubClientId: string,
 	 * 		uuid: string,
+	 * 		appId: number;
 	 * 		gitHubBaseUrl: string,
 	 * 		gitHubClientSecret: string,
 	 * 		webhookSecret: string,
@@ -66,6 +69,7 @@ export class GitHubServerApp extends Model {
 	static async install(payload: GitHubServerAppPayload): Promise<GitHubServerApp> {
 		const {
 			uuid,
+			appId,
 			gitHubAppName,
 			gitHubBaseUrl,
 			gitHubClientId,
@@ -81,6 +85,7 @@ export class GitHubServerApp extends Model {
 			},
 			defaults: {
 				uuid,
+				appId,
 				gitHubBaseUrl,
 				gitHubClientSecret,
 				webhookSecret,
@@ -119,6 +124,10 @@ GitHubServerApp.init({
 		type: DataTypes.UUID,
 		defaultValue: DataTypes.UUIDV4,
 		unique: true,
+		allowNull: false
+	},
+	appId: {
+		type: DataTypes.INTEGER,
 		allowNull: false
 	},
 	gitHubBaseUrl: {
