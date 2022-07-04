f258c2e6fd5fc3394735b6e4b1ddde951d2634f2
Kayub Maharjan <kay.krazzie@gmail.com>
2022-07-04 12:30:20 +1000

    - Navigation go-back logic updated - Header in GitHub Pages replaced by navigation partial

--- static/js/navigation.js
+++ static/js/navigation.js
@@ -2,5 +2,15 @@
 $(".go-back").click(function(event) {
 	event.preventDefault();
 	const previousPagePath = $(event.target).data("previous-page-path");
-	AP.navigator.go('addonmodule', { moduleKey: previousPagePath });
+	const closeOnBack = $(event.target).data("close-on-back");
+
+	if (closeOnBack) {
+		window.close();
+	} else {
+		if (previousPagePath) {
+			AP.navigator.go('addonmodule', { moduleKey: previousPagePath });
+		} else {
+			history.back();
+		}
+	}
 });
