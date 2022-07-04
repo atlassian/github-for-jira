4d31ab19c38e20db7f361ebba0145c22d2ac4ee3
Kayub Maharjan <kay.krazzie@gmail.com>
2022-07-04 14:39:13 +1000

    - Review changes

--- static/js/navigation.js
+++ static/js/navigation.js
@@ -1,16 +1,14 @@
 /* globals $, AP */
-$(".go-back").click(function(event) {
-	event.preventDefault();
-	const previousPagePath = $(event.target).data("previous-page-path");
-	const closeOnBack = $(event.target).data("close-on-back");
+$(".go-back").click(function (event) {
+  event.preventDefault();
+  const previousPagePath = $(event.target).data("previous-page-path");
+  const closeOnBack = $(event.target).data("close-on-back");
 
-	if (closeOnBack) {
-		window.close();
-	} else {
-		if (previousPagePath) {
-			AP.navigator.go('addonmodule', { moduleKey: previousPagePath });
-		} else {
-			history.back();
-		}
-	}
+  if (closeOnBack) {
+    window.close();
+  } else if (previousPagePath) {
+    AP.navigator.go("addonmodule", {moduleKey: previousPagePath});
+  } else {
+    history.back();
+  }
 });
