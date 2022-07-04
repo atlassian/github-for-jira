6b513db717834a0df5761164d76193fe97805627
Kayub Maharjan <kay.krazzie@gmail.com>
2022-07-04 17:06:02 +1000

    - Manual app creation form created

---
+++ static/js/jira-manual-app-creation.js
@@ -0,0 +1,34 @@
+$(".jiraManualAppCreation__formGroupIcon").click((event) => {
+  const url = $(event.target).prev().val();
+  // TODO: Enable iFrame Permissions and copy this URL to clipboard
+  console.log("Copy this URL: ", url);
+});
+
+$(".jiraManualAppCreation_formSubmitBtn").click((event) => {
+  event.preventDefault();
+  console.log("Submit this");
+});
+
+
+$(".jiraManualAppCreation__formFileInput")
+  .on("dragenter focus", () => {
+    $(".jiraManualAppCreation__formFileDropArea").addClass("active");
+  }).on("dragleave blur drop", () => {
+    $(".jiraManualAppCreation__formFileDropArea").removeClass("active");
+  }).on("change", (event) => {
+    const fileName = $(event.target).val().split('\\').pop();
+
+    $(".jiraManualAppCreation__formFileUploaded").css('display', 'flex');
+    $(".jiraManualAppCreation__formFileDropArea").hide();
+    $("#jiraManualAppCreation__uploadedFile").text(fileName);
+
+  //  TODO: Validation and/or Upload
+  });
+
+
+$("#jiraManualAppCreation__clearUploadedFile").click(() => {
+  $("#jiraManualAppCreation__uploadedFile").text("");
+  $(".jiraManualAppCreation__formFileInput").val(null);
+  $(".jiraManualAppCreation__formFileDropArea").show();
+  $(".jiraManualAppCreation__formFileUploaded").hide();
+});
