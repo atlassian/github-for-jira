0803c0cf4c6a55bb57aead1ae99815266b7b1486
Rachelle <rachellerathbone@gmail.com>
2022-07-04 12:29:44 +1000

    wip

--- src/routes/jira/server/jira-app-creation-get.ts
+++ src/routes/jira/server/jira-app-creation-get.ts
@@ -6,13 +6,13 @@ export const JiraAppCreationGet = async (
 	next: NextFunction
 ): Promise<void> => {
 	try {
-		req.log.info("Received Jira app creation page request");
+		req.log.debug("Received Jira app creation page request");
 
 		res.render("jira-select-app-creation.hbs", {
 			previousPagePath: "github-server-url-page"
 		});
 
-		req.log.info("Jira app creation page rendered successfully.");
+		req.log.debug("Jira app creation page rendered successfully.");
 	} catch (error) {
 		return next(new Error(`Failed to render Jira app creation page: ${error}`));
 	}
