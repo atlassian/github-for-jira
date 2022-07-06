f8d2ae9710c636b82e6f8ab0bc3c561acfa2cc53
Gary Xue <gxue@atlassian.com>
2022-07-06 11:38:35 +1000

    restructure test

--- src/models/installation.test.ts
+++ src/models/installation.test.ts
@@ -4,59 +4,56 @@ import { v4 as UUID } from "uuid";
 import { getHashedKey } from "models/sequelize";
 
 describe("Installation", () => {
-	describe("Encryption and decryption with cryptor", () => {
-		describe("operate via existing 'sharedSecret' property", () => {
-			it("should auto set and encrypted the new 'encryptedSharedSecret' when install", async () => {
-				const clientKey = UUID();
-				await Installation.install({
-					host: "whatever.abc",
-					clientKey,
-					sharedSecret: "some-plain-shared-secret"
-				});
-				const encryptedSharedSecretInDB = await findEncryptedSharedSecretBy({ clientKey });
-				expect(encryptedSharedSecretInDB).toEqual("encrypted:some-plain-shared-secret");
-			});
-			it("should auto set and encrypted the new 'encryptedSharedSecret' when create", async () => {
-				const clientKey = UUID();
-				await Installation.create({
-					host: "whatever.abc",
-					clientKey: getHashedKey(clientKey),
-					sharedSecret: "some-plain-shared-secret"
-				});
-				const encryptedSharedSecretInDB = await findEncryptedSharedSecretBy({ clientKey });
-				expect(encryptedSharedSecretInDB).toEqual("encrypted:some-plain-shared-secret");
-			});
-			it("can decrypted the new safeSharedSecret column successfully", async () => {
-				const clientKey = UUID();
-				await insertNewInstallation({ clientKey });
-				const installation = await Installation.findOne({ where: { clientKey } });
-				expect(installation.encryptedSharedSecret).toBe("encrypted:some-plain-text");
-				expect(await installation.decrypt("encryptedSharedSecret")).toBe("some-plain-text");
-			});
+	describe("Decryption with cryptor", () => {
+		it("can decrypted the new safeSharedSecret column successfully", async () => {
+			const clientKey = UUID();
+			await insertNewInstallation({ clientKey });
+			const installation = await Installation.findOne({ where: { clientKey } });
+			expect(installation.encryptedSharedSecret).toBe("encrypted:some-plain-text");
+			expect(await installation.decrypt("encryptedSharedSecret")).toBe("some-plain-text");
 		});
-		describe("operate via new 'encryptedSharedSecret' property", () => {
-			it("should auto set and encrypted the new 'encryptedSharedSecret' when manually build", async () => {
-				const clientKey = UUID();
-				await Installation.build({
-					host: "whatever.abc",
-					clientKey: getHashedKey(clientKey), //TODO: indicate in-consistent usage of the model
-					sharedSecret: "secret-A",
-					encryptedSharedSecret: "secret-B"
-				}).save();
-				const encryptedSharedSecretInDB = await findEncryptedSharedSecretBy({ clientKey });
-				expect(encryptedSharedSecretInDB).toEqual("encrypted:secret-B");
+	});
+	describe("Encryption with cryptor", () => {
+		it("should auto set and encrypted the new 'encryptedSharedSecret' when install", async () => {
+			const clientKey = UUID();
+			await Installation.install({
+				host: "whatever.abc",
+				clientKey,
+				sharedSecret: "some-plain-shared-secret-install"
 			});
-			it("can decrypted the new safeSharedSecret column successfully when manually build", async () => {
-				const clientKey = UUID();
-				await Installation.build({
-					host: "whatever.abc",
-					clientKey: getHashedKey(clientKey),
-					sharedSecret: "secret-A",
-					encryptedSharedSecret: "secret-B"
-				}).save();
-				const encryptedSharedSecretInDB = await findEncryptedSharedSecretBy({ clientKey });
-				expect(encryptedSharedSecretInDB).toEqual("encrypted:secret-B");
+			const encryptedSharedSecretInDB = await findEncryptedSharedSecretBy({ clientKey });
+			expect(encryptedSharedSecretInDB).toEqual("encrypted:some-plain-shared-secret-install");
+		});
+		it("should auto set and encrypted the new 'encryptedSharedSecret' when create", async () => {
+			const clientKey = UUID();
+			await Installation.create({
+				host: "whatever.abc",
+				clientKey: getHashedKey(clientKey),
+				sharedSecret: "some-plain-shared-secret-create"
 			});
+			const encryptedSharedSecretInDB = await findEncryptedSharedSecretBy({ clientKey });
+			expect(encryptedSharedSecretInDB).toEqual("encrypted:some-plain-shared-secret-create");
+		});
+		it("should auto set and encrypted the new 'encryptedSharedSecret' when build", async () => {
+			const clientKey = UUID();
+			await Installation.build({
+				host: "whatever.abc",
+				clientKey: getHashedKey(clientKey),
+				sharedSecret: "some-plain-shared-secret-build"
+			}).save();
+			const encryptedSharedSecretInDB = await findEncryptedSharedSecretBy({ clientKey });
+			expect(encryptedSharedSecretInDB).toEqual("encrypted:some-plain-shared-secret-build");
+		});
+		it("should use the specific set encryptedSharedSecret when both set", async () => {
+			const clientKey = UUID();
+			await Installation.create({
+				host: "whatever.abc",
+				clientKey: getHashedKey(clientKey), //TODO: indicate in-consistent usage of the model
+				sharedSecret: "secret-A",
+				encryptedSharedSecret: "secret-B"
+			});
+			const encryptedSharedSecretInDB = await findEncryptedSharedSecretBy({ clientKey });
+			expect(encryptedSharedSecretInDB).toEqual("encrypted:secret-B");
 		});
 	});
 	const insertNewInstallation = async ({ clientKey }) => {
