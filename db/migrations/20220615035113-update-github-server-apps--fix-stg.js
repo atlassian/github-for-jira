80333df118fb49eb7607f3faf348ae3a997982f0
Gary Xue <gxue@atlassian.com>
2022-07-11 10:58:49 +1000

    temp fix stg by adding back db columns.

---
+++ db/migrations/20220615035113-update-github-server-apps--fix-stg.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const tableName = "GitHubServerApps";
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.addColumn('Installations', 'secrets', Sequelize.BLOB)
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.removeColumn('Installations', 'secrets')
+  }
+};
