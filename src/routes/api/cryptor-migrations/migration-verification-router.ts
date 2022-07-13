213ad93f78d08cd9781cba10fce4c9548b852cc4
Gary Xue <gxue@atlassian.com>
2022-07-13 17:02:50 +1000

    add logging

--- src/routes/api/cryptor-migrations/migration-verification-router.ts
+++ src/routes/api/cryptor-migrations/migration-verification-router.ts
@@ -5,21 +5,41 @@ export const CryptorMigrationInstallationVerificationPost = async (req: Request,
 
 	const jiraHost: string[] = req.body?.jiraHost;
 
-	if (!jiraHost) return res.status(200).end({ ok: false, reason: "invalid jiraHost param in body" });
+	req.log.info("---verifying----- 1");
+	if (!jiraHost) {
+		req.log.info("---verifying----- 2");
+		res.status(200).json({ ok: false, reason: "invalid jiraHost param in body" });
+		return;
+	}
 
 	const inst: Installation = await Installation.findOne({ where: { jiraHost } });
-	if (!inst) return res.status(200).json({ ok: false, reason: "Could not find installation for jiraHost" });
+	if (!inst) {
+		req.log.info("---verifying----- 3");
+		res.status(200).json({ ok: false, reason: "Could not find installation for jiraHost" });
+		return;
+	}
 
-	if (!inst.encryptedSharedSecret) return res.status(200).json({ ok: false, reason: "Empty encryptedSharedSecret" });
+	if (!inst.encryptedSharedSecret) {
+		req.log.info("---verifying----- 4");
+		res.status(200).json({ ok: false, reason: "Empty encryptedSharedSecret" });
+		return;
+	}
 
 	try {
 		if (inst.sharedSecret === (await inst.decrypt("encryptedSharedSecret"))) {
-			return res.status(200).json({ ok: true });
+			req.log.info("---verifying----- 5");
+			res.status(200).json({ ok: true });
+			return;
 		} else {
-			return res.status(200).json({ ok: false, reason: "different values to sharedSecret" });
+			req.log.info("---verifying----- 6");
+			res.status(200).json({ ok: false, reason: "different values to sharedSecret" });
+			return;
 		}
 	} catch (e) {
-		return res.status(200).json({ ok: false, reason: e.message });
+		req.log.info("---verifying----- 7");
+		res.status(200).json({ ok: false, reason: e.message });
+		return;
 	}
 
+	req.log.info("---verifying----- 8");
 };
