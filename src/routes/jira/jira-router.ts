7a083a4647c807f997af056588ae542cfafbacfc
Kayub Maharjan <kay.krazzie@gmail.com>
2022-07-13 15:19:18 +1000

    - Review changes

--- src/routes/jira/jira-router.ts
+++ src/routes/jira/jira-router.ts
@@ -4,14 +4,17 @@ import { JiraSyncPost } from "./jira-sync-post";
 import { JiraAtlassianConnectGet } from "./jira-atlassian-connect-get";
 import { JiraEventsRouter } from "./events/jira-events-router";
 import { JiraSelectVersionRouter } from "./server/jira-select-version-router";
-import { JiraContextJwtTokenMiddleware } from "middleware/jira-jwt-middleware";
+import { JiraContextJwtTokenMiddleware, JiraJwtTokenMiddleware } from "middleware/jira-jwt-middleware";
 import { JiraServerUrlRouter } from "routes/jira/server/jira-server-url-router";
 import { JiraAppCreationRouter } from "./server/jira-app-creation-router";
-import { JiraGheServersRouter } from "routes/jira/server/jira-ghe-servers-router";
+import { csrfMiddleware } from "middleware/csrf-middleware";
+import { JiraGheServers } from "routes/jira/server/jira-ghe-servers";
 
 export const JiraRouter = Router();
 
 JiraRouter.get("/atlassian-connect.json", JiraAtlassianConnectGet);
+JiraRouter.get("/ghe-servers", csrfMiddleware, JiraJwtTokenMiddleware, JiraGheServers);
+
 JiraRouter.use("/configuration", JiraConfigurationRouter);
 // TODO - add csrf middleware
 JiraRouter.post("/sync", JiraContextJwtTokenMiddleware, JiraSyncPost);
@@ -19,4 +22,3 @@ JiraRouter.use("/events", JiraEventsRouter);
 JiraRouter.use("/select-version", JiraSelectVersionRouter);
 JiraRouter.use("/server-url", JiraServerUrlRouter);
 JiraRouter.use("/app-creation", JiraAppCreationRouter);
-JiraRouter.use("/ghe-servers", JiraGheServersRouter);
--- src/routes/jira/server/jira-ghe-servers-router.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { JiraJwtTokenMiddleware } from "middleware/jira-jwt-middleware";
-import { csrfMiddleware } from "middleware/csrf-middleware";
-import { JiraGheServers } from "routes/jira/server/jira-ghe-servers";
-
-export const JiraGheServersRouter = Router();
-
-JiraGheServersRouter.route("/")
-	.get(csrfMiddleware, JiraJwtTokenMiddleware, JiraGheServers);
