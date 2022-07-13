72f6b57b7529a3049006ae642d20d8c08b85db56
Gary Xue <gxue@atlassian.com>
2022-07-13 18:34:56 +1000

    remove unused logging

--- src/routes/api/cryptor-migrations/migration-verification-router.ts
+++ src/routes/api/cryptor-migrations/migration-verification-router.ts
@@ -7,41 +7,33 @@ export const CryptorMigrationInstallationVerificationPost = async (req: Request,
 
 	req.log = req.log.child({ operation: "migrate-installations-verification" });
 
-	req.log.info("---verifying----- 1");
 	if (!jiraHost) {
-		req.log.info("---verifying----- 2");
 		res.status(200).json({ ok: false, reason: "invalid jiraHost param in body" });
 		return;
 	}
 
 	const inst: Installation = await Installation.findOne({ where: { jiraHost } });
 	if (!inst) {
-		req.log.info("---verifying----- 3");
 		res.status(200).json({ ok: false, reason: "Could not find installation for jiraHost" });
 		return;
 	}
 
 	if (!inst.encryptedSharedSecret) {
-		req.log.info("---verifying----- 4");
 		res.status(200).json({ ok: false, reason: "Empty encryptedSharedSecret" });
 		return;
 	}
 
 	try {
 		if (inst.sharedSecret === (await inst.decrypt("encryptedSharedSecret"))) {
-			req.log.info("---verifying----- 5");
 			res.status(200).json({ ok: true });
 			return;
 		} else {
-			req.log.info("---verifying----- 6");
 			res.status(200).json({ ok: false, reason: "different values to sharedSecret" });
 			return;
 		}
 	} catch (e) {
-		req.log.info("---verifying----- 7");
 		res.status(200).json({ ok: false, reason: e.message });
 		return;
 	}
 
-	req.log.info("---verifying----- 8");
 };
