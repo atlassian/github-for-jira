bfa8d5514ea604215682b9fc8dd967f7b6be4ca7
Kayub Maharjan <kay.krazzie@gmail.com>
2022-07-14 10:27:38 +1000

    - New method added for manual app creation page

--- src/routes/jira/jira-router.ts
+++ src/routes/jira/jira-router.ts
@@ -4,13 +4,17 @@ import { JiraSyncPost } from "./jira-sync-post";
 import { JiraAtlassianConnectGet } from "./jira-atlassian-connect-get";
 import { JiraEventsRouter } from "./events/jira-events-router";
 import { JiraSelectVersionRouter } from "./server/jira-select-version-router";
-import { JiraContextJwtTokenMiddleware } from "middleware/jira-jwt-middleware";
+import { JiraContextJwtTokenMiddleware, JiraJwtTokenMiddleware } from "middleware/jira-jwt-middleware";
 import { JiraServerUrlRouter } from "routes/jira/server/jira-server-url-router";
 import { JiraAppCreationRouter } from "./server/jira-app-creation-router";
+import { csrfMiddleware } from "middleware/csrf-middleware";
+import { JiraManualAppCreationGet } from "routes/jira/server/jira-manual-app-creation-get";
 
 export const JiraRouter = Router();
 
 JiraRouter.get("/atlassian-connect.json", JiraAtlassianConnectGet);
+JiraRouter.get("/manual-app-creation", csrfMiddleware, JiraJwtTokenMiddleware, JiraManualAppCreationGet);
+
 JiraRouter.use("/configuration", JiraConfigurationRouter);
 // TODO - add csrf middleware
 JiraRouter.post("/sync", JiraContextJwtTokenMiddleware, JiraSyncPost);
---
+++ src/routes/jira/server/jira-manual-app-creation-get.ts
@@ -0,0 +1,26 @@
+import { NextFunction, Request, Response } from "express";
+import { GitHubServerApp } from "models/github-server-app";
+
+export const JiraManualAppCreationGet = async (
+	req: Request,
+	res: Response,
+	next: NextFunction
+): Promise<void> => {
+	try {
+		req.log.debug("Received Jira app creation page request");
+
+		const gitHubServerAppId = req.query.ghsaId as string;
+		const app = await GitHubServerApp.getForGitHubServerAppId(parseInt(gitHubServerAppId));
+
+		req.log.debug("Received query Parameter------------>", app);
+
+		res.render("jira-manual-app-creation.hbs", {
+			previousPagePath: "github-app-creation-page",
+			app
+		});
+
+		req.log.debug("Jira manual app creation page rendered successfully.");
+	} catch (error) {
+		return next(new Error(`Failed to render Jira app creation page: ${error}`));
+	}
+};
