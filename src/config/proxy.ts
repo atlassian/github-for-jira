ca2af59a56831b8244e0f728b2b13307cb128f5b
Tom Hombergs <tom.hombergs@gmail.com>
2022-07-15 14:31:25 +1000

    remove proxy for testing

--- src/config/proxy.ts
+++ src/config/proxy.ts
@@ -1,13 +1,10 @@
 import { envVars }  from "./env";
-import { bootstrap } from "global-agent";
 import { getLogger } from "./logger";
 
 const logger = getLogger("config.proxy");
 
 if (envVars.PROXY) {
-	logger.info(`configuring proxy: ${envVars.PROXY} for outbound calls`);
-	process.env.GLOBAL_AGENT_HTTP_PROXY = envVars.PROXY;
-	bootstrap();
+	logger.info("configuring no proxy for outbound calls");
 } else {
 	logger.info("configuring no proxy for outbound calls");
 }
