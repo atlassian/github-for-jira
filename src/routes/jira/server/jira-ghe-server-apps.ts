930f0a2e810ba05715be4c75a0eb4f3c2c04680d
Kayub Maharjan <kay.krazzie@gmail.com>
2022-07-18 14:17:54 +1000

    - GithubApp name pulled

--- src/routes/jira/server/jira-ghe-server-apps.ts
+++ src/routes/jira/server/jira-ghe-server-apps.ts
@@ -1,6 +1,5 @@
 import { NextFunction, Request, Response } from "express";
 import { GitHubServerApp } from "models/github-server-app";
-import { Subscription } from "models/subscription";
 
 export const JiraGheServerApps = async (
 	req: Request,
@@ -10,21 +9,15 @@ export const JiraGheServerApps = async (
 	try {
 		req.log.debug("Received Jira GHE server apps page request");
 
-		const { jiraHost, installation: { id: installationId } } = res.locals;
+		const { id: installationId } = res.locals.installation;
 		const githubServerBaseUrl = req?.query.serverUrl as string;
 
 		if (!githubServerBaseUrl) {
 			throw new Error("No server URL passed!");
 		}
 
-		const servers = await GitHubServerApp.getAllForGitHubBaseUrl(githubServerBaseUrl, installationId);
-		const serverIds = servers ? servers.map(server => server.id) : [];
-		const allSubscriptions = await Subscription.getAllForHost(jiraHost);
-
-		const subscriptions = allSubscriptions.filter(subscription => subscription.gitHubAppId && serverIds.includes(subscription.gitHubAppId));
-		const serverApps = subscriptions.map(subscription => ({
-			displayName: subscription.gitHubInstallationId
-		}));
+		const servers = await GitHubServerApp.getAllForGitHubBaseUrl(githubServerBaseUrl, installationId) || [];
+		const serverApps = servers.map(server => ({ displayName: server.gitHubAppName }));
 
 		if (serverApps.length) {
 			res.render("jira-select-github-cloud-app.hbs", {
