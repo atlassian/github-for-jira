8c68eb639d26a1ba5ddd281e6d1ff3dcfc7138a5
joshkay10 <josh.kay@outlook.com>
2022-07-21 12:25:50 +1200

    remove ff use-new-github-client-for-pr-title (#1393)
    Co-authored-by: Josh Kay <joshk@atlassian.com>
    Co-authored-by: Tom Hombergs <tom.hombergs@gmail.com>

--- src/config/feature-flags.ts
+++ src/config/feature-flags.ts
@@ -26,7 +26,6 @@ export enum BooleanFlags {
 	USE_NEW_GITHUB_CLIENT_FOR_GET_REPO = "use-new-github-client-for-get-repo",
 	USE_NEW_GITHUB_CLIENT_FOR_INSTALLATION_API = "use-new-github-client-for-installation-api",
 	USE_NEW_GITHUB_CLIENT_FOR_REDIRECT = "use-new-github-client-for-redirect",
-	USE_NEW_GITHUB_CLIENT_FOR_PR_TITLE = "use-new-github-client-for-pr-title",
 	RETRY_ALL_ERRORS = "retry-all-errors",
 	GHE_SERVER = "ghe_server",
 	USE_REST_API_FOR_DISCOVERY = "use-rest-api-for-discovery",
