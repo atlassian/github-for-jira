e0f04378a20f431020177c2054e78620ed1592f5
Josh Kay <joshk@atlassian.com>
2022-07-26 09:20:08 +1200

    renamed githubapi with probot prefix for clarity

--- src/config/github-api.ts
+++ src/config/github-api.ts
@@ -1,11 +1,11 @@
-import { GitHubAPI } from "probot";
+import { GitHubAPI as ProbotGitHubAPI } from "probot";
 import { getLogger } from "./logger";
 import { Options } from "probot/lib/github";
 
-export const GithubAPI = (options: Partial<Options> = {}): GitHubAPI => {
+export const GithubAPI = (options: Partial<Options> = {}): ProbotGitHubAPI => {
 	options.logger = options.logger || getLogger("config.github-api");
 	options.throttle = {
 		enabled: false
 	};
-	return GitHubAPI(options as Options);
+	return ProbotGitHubAPI(options as Options);
 };
