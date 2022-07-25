d33db057f7b2fa054a743326fe19f95e43a37c05
Kayub Maharjan <kay.krazzie@gmail.com>
2022-07-25 14:24:54 +1000

    -Callback, Setup and Webhook URLs configured in both create/edit Manual app form

--- src/routes/jira/connect/enterprise/app/jira-connect-enterprise-app-create-or-edit.ts
+++ src/routes/jira/connect/enterprise/app/jira-connect-enterprise-app-create-or-edit.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import { GitHubServerApp } from "models/github-server-app";
+import { v4 as newUUID } from "uuid";
 
 export const JiraConnectEnterpriseAppCreateOrEdit = async (
 	req: Request,
@@ -8,15 +9,26 @@ export const JiraConnectEnterpriseAppCreateOrEdit = async (
 ): Promise<void> => {
 	try {
 		req.log.debug("Received Jira edit app page request");
+		let config;
+		const uuid = req.params.uuid as string ;
 
-		const uuid = req.params.uuid as string;
-		const app = uuid ? await GitHubServerApp.findForUuid(uuid) : null;
+		if (uuid) {
+			const app = await GitHubServerApp.findForUuid(uuid);
+			config = {
+				previousPagePath: "github-app-creation-page",
+				app,
+				serverUrl: app?.gitHubBaseUrl,
+				uuid
+			};
+		} else {
+			config = {
+				previousPagePath: "github-app-creation-page",
+				serverUrl: req.params.serverUrl,
+				uuid: newUUID()
+			};
+		}
 
-		res.render("jira-manual-app-creation.hbs", {
-			previousPagePath: "github-app-creation-page",
-			app
-		});
-
+		res.render("jira-manual-app-creation.hbs", config);
 		req.log.debug("Jira edit app page rendered successfully.");
 	} catch (error) {
 		return next(new Error(`Failed to render Jira edit app page: ${error}`));
