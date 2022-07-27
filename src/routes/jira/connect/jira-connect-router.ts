4e12ce36c0f82638d6a8e68973d79dfc3f9ebf35
kAy <krazziekay@users.noreply.github.com>
2022-07-27 16:21:30 +1000

    ARC-1519-part-4 - Improve Jira Routing for connect/app creation (#1419)
    * - Created new route jira/connect for GHE server url and GHE server list
    
    * - snapshots updated

--- src/routes/jira/connect/jira-connect-router.ts
+++ src/routes/jira/connect/jira-connect-router.ts
@@ -2,8 +2,11 @@ import { Router } from "express";
 import { JiraJwtTokenMiddleware } from "middleware/jira-jwt-middleware";
 import { csrfMiddleware } from "middleware/csrf-middleware";
 import { JiraConnectGet } from "./jira-connect-get";
+import { JiraConnectEnterpriseRouter } from "./enterprise/jira-connect-enterprise-router";
 
 export const JiraConnectRouter = Router();
 
 JiraConnectRouter.route("/")
 	.get(csrfMiddleware, JiraJwtTokenMiddleware, JiraConnectGet);
+
+JiraConnectRouter.use("/enterprise", JiraConnectEnterpriseRouter);
--- src/routes/jira/jira-router.ts
+++ src/routes/jira/jira-router.ts
@@ -4,9 +4,7 @@ import { JiraSyncPost } from "./jira-sync-post";
 import { JiraAtlassianConnectGet } from "./atlassian-connect/jira-atlassian-connect-get";
 import { JiraEventsRouter } from "./events/jira-events-router";
 import { JiraContextJwtTokenMiddleware, JiraJwtTokenMiddleware } from "middleware/jira-jwt-middleware";
-import { JiraServerUrlRouter } from "routes/jira/server/jira-server-url-router";
 import { JiraAppCreationRouter } from "./server/jira-app-creation-router";
-import { JiraGheServerRouter } from "routes/jira/server/jira-ghe-server-router";
 import { csrfMiddleware } from "middleware/csrf-middleware";
 import { JiraGet } from "routes/jira/jira-get";
 import { JiraEditAppGet } from "routes/jira/server/jira-edit-app-get";
@@ -18,20 +16,17 @@ export const JiraRouter = Router();
 // TODO: The params `installationId` needs to be replaced by `subscriptionId`
 JiraRouter.delete("/subscription/:installationId", JiraContextJwtTokenMiddleware, JiraDelete);
 
-// TODO: Need to cleanup the URLs and Routers
-
 JiraRouter.use("/connect", JiraConnectRouter);
 
 JiraRouter.get("/atlassian-connect.json", JiraAtlassianConnectGet);
-JiraRouter.get("/edit-app/:id", csrfMiddleware, JiraJwtTokenMiddleware, JiraEditAppGet);
 
+// TODO: Need to cleanup the URLs and Routers
+JiraRouter.get("/edit-app/:id", csrfMiddleware, JiraJwtTokenMiddleware, JiraEditAppGet);
 // TODO - add csrf middleware
 JiraRouter.post("/sync", JiraContextJwtTokenMiddleware, JiraSyncPost);
 JiraRouter.use("/events", JiraEventsRouter);
-JiraRouter.use("/server-url", JiraServerUrlRouter);
 JiraRouter.use("/app-creation", JiraAppCreationRouter);
 
-JiraRouter.use("/ghe-servers", csrfMiddleware, JiraJwtTokenMiddleware, JiraGheServerRouter);
 
 JiraRouter.get("/", csrfMiddleware, JiraJwtTokenMiddleware, JiraGet);
 
