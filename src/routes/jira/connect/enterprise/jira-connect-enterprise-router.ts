7c61c65f74cfe3215e655c9f0fa0f469961d37c1
Kayub Maharjan <kay.krazzie@gmail.com>
2022-07-27 16:27:55 +1000

    - Minor correction

--- src/routes/jira/connect/enterprise/jira-connect-enterprise-router.ts
+++ src/routes/jira/connect/enterprise/jira-connect-enterprise-router.ts
@@ -1,7 +1,11 @@
 import { Router } from "express";
-import { JiraContextJwtTokenMiddleware } from "middleware/jira-jwt-middleware";
+import { JiraJwtTokenMiddleware, JiraContextJwtTokenMiddleware } from "middleware/jira-jwt-middleware";
+import { csrfMiddleware } from "middleware/csrf-middleware";
+import { JiraConnectEnterpriseGet } from "./jira-connect-enterprise-get";
 import { JiraConnectEnterprisePost } from "routes/jira/connect/enterprise/jira-connect-enterprise-post";
 
 export const JiraConnectEnterpriseRouter = Router();
+
+JiraConnectEnterpriseRouter.route("/").get(csrfMiddleware, JiraJwtTokenMiddleware, JiraConnectEnterpriseGet);
 
 JiraConnectEnterpriseRouter.post("/", JiraContextJwtTokenMiddleware, JiraConnectEnterprisePost);
