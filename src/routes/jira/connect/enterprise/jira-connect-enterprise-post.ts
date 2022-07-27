1883c1029304103be560c2a4b71fb86350a58c84
kAy <krazziekay@users.noreply.github.com>
2022-07-27 16:29:56 +1000

    ARC-1519-part-5 - Improve Jira Routing for connect/app creation (#1420)
    * - Created new route jira/connect/enterprise
    
    * - Route updated in jira-server-url.js
    
    * - Minor correction

---
+++ src/routes/jira/connect/enterprise/jira-connect-enterprise-post.ts
@@ -0,0 +1,75 @@
+import { Request, Response } from "express";
+import axios from "axios";
+import { GitHubServerApp } from "models/github-server-app";
+import { isValidUrl } from "utils/is-valid-url";
+import { statsd } from "config/statsd";
+import { metricError } from "config/metric-names";
+
+interface MessageAndCode {
+	errorCode: string;
+	message: string;
+}
+
+interface GheServerUrlErrors {
+	[key: string | number]: MessageAndCode;
+}
+
+const GHE_ERROR_UNREACHABLE = {
+	errorCode: "GHE_ERROR_ENOTFOUND",
+	message: "Request to URL failed"
+};
+
+export const gheServerUrlErrors: GheServerUrlErrors = {
+	invalidUrl: {
+		errorCode: "GHE_ERROR_INVALID_URL",
+		message: "Invalid URL"
+	},
+	ENOTFOUND: GHE_ERROR_UNREACHABLE,
+	403: GHE_ERROR_UNREACHABLE,
+	502: {
+		errorCode: "GHE_SERVER_BAD_GATEWAY",
+		message: "Bad gateway"
+	},
+	default: {
+		errorCode: "GHE_ERROR_DEFAULT",
+		message: "Something went wrong"
+	}
+};
+
+export const JiraConnectEnterprisePost = async (
+	req: Request,
+	res: Response
+): Promise<void> => {
+	const { gheServerURL } = req.body;
+	const { id: installationId } = res.locals.installation;
+
+	req.log.debug(`Verifying provided GHE server url ${gheServerURL} is a valid URL`);
+	const isGheUrlValid = isValidUrl(gheServerURL);
+
+	if (!isGheUrlValid) {
+		const { errorCode, message } = gheServerUrlErrors.invalidUrl;
+		res.status(200).send({ success: false, errors: [{ code: errorCode, message }] });
+		req.log.error(`The entered URL is not valid. ${gheServerURL} is not a valid url`);
+		return;
+	}
+
+	try {
+		const gitHubServerApps = await GitHubServerApp.getAllForGitHubBaseUrl(gheServerURL, installationId);
+
+		if (gitHubServerApps?.length) {
+			req.log.debug(`GitHub apps found for url: ${gheServerURL}. Redirecting to Jira list apps page.`);
+			res.status(200).send({ success: true, appExists: true });
+			return;
+		}
+
+		req.log.debug(`No existing GitHub apps found for url: ${gheServerURL}. Making request to provided url.`);
+		await axios.get(gheServerURL);
+		res.status(200).send({ success: true, appExists: false });
+	} catch (err) {
+		req.log.error({ err, gheServerURL }, `Something went wrong`);
+		const codeOrStatus = err.code || err.response.status;
+		const { errorCode, message } = gheServerUrlErrors[codeOrStatus] || gheServerUrlErrors.default;
+		res.status(200).send({ success: false, errors: [{ code: errorCode, message }] });
+		statsd.increment(metricError.gheServerUrlError, { errorCode, status: err.response.status	 });
+	}
+};
