47f0a70e07cd87593c0a37362f588a319c509374
Kayub Maharjan <kay.krazzie@gmail.com>
2022-07-27 17:43:35 +1000

    - New routes added for PUT and POST for JiraConnectEnterpriseApp

--- src/routes/jira/connect/enterprise/app/jira-connect-enterprise-app-router.ts
+++ src/routes/jira/connect/enterprise/app/jira-connect-enterprise-app-router.ts
@@ -1,9 +1,14 @@
 import { Router } from "express";
 import { JiraContextJwtTokenMiddleware } from "middleware/jira-jwt-middleware";
 import { csrfMiddleware } from "middleware/csrf-middleware";
+import { JiraConnectEnterpriseAppPost } from "routes/jira/connect/enterprise/app/jira-connect-enterprise-app-post";
+import { JiraConnectEnterpriseAppPut } from "routes/jira/connect/enterprise/app/jira-connect-enterprise-app-put";
 import { JiraConnectEnterpriseAppDelete } from "routes/jira/connect/enterprise/app/jira-connect-enterprise-app-delete";
 
 export const JiraConnectEnterpriseAppRouter = Router();
 
+JiraConnectEnterpriseAppRouter.post("/", csrfMiddleware, JiraContextJwtTokenMiddleware, JiraConnectEnterpriseAppPost);
+
 JiraConnectEnterpriseAppRouter.route("/:uuid")
+	.put(csrfMiddleware, JiraContextJwtTokenMiddleware, JiraConnectEnterpriseAppPut)
 	.delete(csrfMiddleware, JiraContextJwtTokenMiddleware, JiraConnectEnterpriseAppDelete);
