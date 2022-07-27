d3c52db5ad6148e52f7195c86cd58727ae94a6d3
kAy <krazziekay@users.noreply.github.com>
2022-07-27 16:47:46 +1000

    ARC-1519-part-6 - Improve Jira Routing for connect/app creation (#1421)
    * - New route for github server app added
    
    * - Routes corrected in atlassian connect
    
    * - snapshots updated
    
    * - snapshots updated

--- src/routes/jira/connect/enterprise/jira-connect-enterprise-router.ts
+++ src/routes/jira/connect/enterprise/jira-connect-enterprise-router.ts
@@ -3,9 +3,12 @@ import { JiraJwtTokenMiddleware, JiraContextJwtTokenMiddleware } from "middlewar
 import { csrfMiddleware } from "middleware/csrf-middleware";
 import { JiraConnectEnterpriseGet } from "./jira-connect-enterprise-get";
 import { JiraConnectEnterprisePost } from "routes/jira/connect/enterprise/jira-connect-enterprise-post";
+import { JiraConnectEnterpriseServerAppGet } from "routes/jira/connect/enterprise/server_app/jira-connect-enterprise-server-app-get";
 
 export const JiraConnectEnterpriseRouter = Router();
 
+JiraConnectEnterpriseRouter.post("/", JiraContextJwtTokenMiddleware, JiraConnectEnterprisePost);
+
 JiraConnectEnterpriseRouter.route("/").get(csrfMiddleware, JiraJwtTokenMiddleware, JiraConnectEnterpriseGet);
 
-JiraConnectEnterpriseRouter.post("/", JiraContextJwtTokenMiddleware, JiraConnectEnterprisePost);
+JiraConnectEnterpriseRouter.get("/:serverUrl/app", csrfMiddleware, JiraJwtTokenMiddleware, JiraConnectEnterpriseServerAppGet);
