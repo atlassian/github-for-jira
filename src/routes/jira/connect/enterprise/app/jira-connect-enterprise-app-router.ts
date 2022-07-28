f7c795a42914084edc1d060243fbd6ef7536f3d1
kAy <krazziekay@users.noreply.github.com>
2022-07-28 11:04:10 +1000

    ARC-1519-part-9 - Improve Jira Routing for connect/app creation  (#1424)
    * - New routes added for PUT and POST for JiraConnectEnterpriseApp
    
    * - Minor comment added

--- src/routes/jira/connect/enterprise/app/jira-connect-enterprise-app-router.ts
+++ src/routes/jira/connect/enterprise/app/jira-connect-enterprise-app-router.ts
@@ -2,10 +2,15 @@ import { Router } from "express";
 import { JiraContextJwtTokenMiddleware, JiraJwtTokenMiddleware } from "middleware/jira-jwt-middleware";
 import { csrfMiddleware } from "middleware/csrf-middleware";
 import { JiraConnectEnterpriseAppDelete } from "routes/jira/connect/enterprise/app/jira-connect-enterprise-app-delete";
+import { JiraConnectEnterpriseAppPut } from "routes/jira/connect/enterprise/app/jira-connect-enterprise-app-put";
+import { JiraConnectEnterpriseAppPost } from "routes/jira/connect/enterprise/app/jira-connect-enterprise-app-post";
 import { JiraConnectEnterpriseAppCreateOrEdit } from "routes/jira/connect/enterprise/app/jira-connect-enterprise-app-create-or-edit";
 
 export const JiraConnectEnterpriseAppRouter = Router();
 
+JiraConnectEnterpriseAppRouter.post("/", csrfMiddleware, JiraContextJwtTokenMiddleware, JiraConnectEnterpriseAppPost);
+
 JiraConnectEnterpriseAppRouter.route("/:uuid")
 	.get(csrfMiddleware, JiraJwtTokenMiddleware, JiraConnectEnterpriseAppCreateOrEdit)
+	.put(csrfMiddleware, JiraContextJwtTokenMiddleware, JiraConnectEnterpriseAppPut)
 	.delete(csrfMiddleware, JiraContextJwtTokenMiddleware, JiraConnectEnterpriseAppDelete);
