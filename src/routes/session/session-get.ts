47298c8fe763be3e33aa731a8f3fe755e61fe4e7
Kayub Maharjan <kay.krazzie@gmail.com>
2022-07-29 11:43:15 +1000

    - Review changes

--- src/routes/session/session-get.ts
+++ src/routes/session/session-get.ts
@@ -5,11 +5,14 @@ export const SessionEnterpriseGet = (req: Request, res: Response, next: NextFunc
 		return next(new Error("Missing redirect url for session enterprise. Needs to be in format `/session/enterprise/:redirectUrl`"));
 	}
 
+	// TODO: This title is different for different scenarios, need to add the different scenarios
+	const titleForLoading = "Redirecting to your GitHub Enterprise Server instance";
+
 	res.render("gitHub-session.hbs", {
 		APP_URL: process.env.APP_URL,
 		redirectUrl: new URL(req.params[0], process.env.APP_URL).href,
 		nonce: res.locals.nonce,
-		isGitHubCloud: false
+		titleForLoading
 	});
 };
 
@@ -22,6 +25,6 @@ export const SessionGet = (req: Request, res: Response, next: NextFunction) => {
 		APP_URL: process.env.APP_URL,
 		redirectUrl: new URL(req.params[0], process.env.APP_URL).href,
 		nonce: res.locals.nonce,
-		isGitHubCloud: true
+		titleForLoading: "Redirecting to your GitHub Cloud instance"
 	});
 };
