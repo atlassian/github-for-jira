eb06c6f5de7a4dd3f8a2ca525c8dbb759ad6e10f
Kayub Maharjan <kay.krazzie@gmail.com>
2022-08-02 17:16:01 +1000

    - Review changes

--- src/routes/github/github-redirect-get.ts
+++ src/routes/github/github-redirect-get.ts
@@ -1,12 +1,11 @@
 import { Request, Response } from "express";
 
 export const GithubRedirectGet = async (
-	req: Request,
+	_req: Request,
 	res: Response
 ): Promise<void> => {
 
 	res.render("jira-redirect.hbs", {
-		title: "Redirecting",
-		baseUrl: req.query?.baseUrl
+		title: "Redirecting"
 	});
 };
