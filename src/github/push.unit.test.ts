0a706841fe4955cb18860b4e1b4298fbd9b259b5
Gary Xue <gxue@atlassian.com>
2022-08-04 20:38:28 +1000

    fix naming

--- src/github/push.unit.test.ts
+++ src/github/push.unit.test.ts
@@ -6,11 +6,11 @@ import { enqueuePush } from "../transforms/push";
 
 jest.mock("../transforms/push");
 
-const GHES__GITHUB_INSTALLATION_ID = 1234;
-const GHES__GITHUB_APP_ID = 111;
-const GHES__GITHUB_UUID = "xxx-xxx-xxx-xxx";
-const GHES__GITHUB_APP_APP_ID = 1;
-const GHES__GITHUB_APP_CLIENT_ID = "client-id";
+const GHES_GITHUB_INSTALLATION_ID = 1234;
+const GHES_GITHUB_APP_ID = 111;
+const GHES_GITHUB_UUID = "xxx-xxx-xxx-xxx";
+const GHES_GITHUB_APP_APP_ID = 1;
+const GHES_GITHUB_APP_CLIENT_ID = "client-id";
 
 describe("PushWebhookHandler", ()=>{
 	let jiraClient: any;
@@ -21,18 +21,18 @@ describe("PushWebhookHandler", ()=>{
 	});
 	describe("GitHub Cloud", ()=>{
 		it("should NOT push GitHubAppConfig", async ()=>{
-			await pushWebhookHandler(getWebhookContext({ cloud: true }), jiraClient, util, GHES__GITHUB_INSTALLATION_ID);
+			await pushWebhookHandler(getWebhookContext({ cloud: true }), jiraClient, util, GHES_GITHUB_INSTALLATION_ID);
 			expect(enqueuePush).toBeCalledWith(expect.anything(), expect.anything(), undefined);
 		});
 	});
 	describe("GitHub Enterprise Serer", ()=>{
 		it("should push GitHubAppConfig", async ()=>{
-			await pushWebhookHandler(getWebhookContext({ cloud: false }), jiraClient, util, GHES__GITHUB_INSTALLATION_ID);
+			await pushWebhookHandler(getWebhookContext({ cloud: false }), jiraClient, util, GHES_GITHUB_INSTALLATION_ID);
 			expect(enqueuePush).toBeCalledWith(expect.anything(), expect.anything(), expect.objectContaining({
-				uuid: GHES__GITHUB_UUID,
-				gitHubAppId: GHES__GITHUB_APP_ID,
-				appId: GHES__GITHUB_APP_APP_ID,
-				clientId: GHES__GITHUB_APP_CLIENT_ID,
+				uuid: GHES_GITHUB_UUID,
+				gitHubAppId: GHES_GITHUB_APP_ID,
+				appId: GHES_GITHUB_APP_APP_ID,
+				clientId: GHES_GITHUB_APP_CLIENT_ID,
 				gitHubBaseUrl: gheUrl
 			}));
 		});
@@ -40,7 +40,7 @@ describe("PushWebhookHandler", ()=>{
 	const getWebhookContext = ({ cloud }: {cloud: boolean}) => {
 		const payload: GitHubPushData = {
 			installation: {
-				id: GHES__GITHUB_INSTALLATION_ID,
+				id: GHES_GITHUB_INSTALLATION_ID,
 				node_id: 123
 			},
 			webhookId: "aaa-bbb-ccc",
@@ -58,10 +58,10 @@ describe("PushWebhookHandler", ()=>{
 			payload,
 			...(cloud ? undefined : {
 				gitHubAppConfig: {
-					uuid: GHES__GITHUB_UUID,
-					gitHubAppId: GHES__GITHUB_APP_ID,
-					appId: GHES__GITHUB_APP_APP_ID,
-					clientId: GHES__GITHUB_APP_CLIENT_ID,
+					uuid: GHES_GITHUB_UUID,
+					gitHubAppId: GHES_GITHUB_APP_ID,
+					appId: GHES_GITHUB_APP_APP_ID,
+					clientId: GHES_GITHUB_APP_CLIENT_ID,
 					gitHubBaseUrl: gheUrl
 				}
 			})
