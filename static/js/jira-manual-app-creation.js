0ac71c81ef1d04be3d19652bce7b9f4441954980
Kayub Maharjan <kay.krazzie@gmail.com>
2022-08-10 09:51:23 +1000

    - File input reader corrected

--- static/js/jira-manual-app-creation.js
+++ static/js/jira-manual-app-creation.js
@@ -28,7 +28,7 @@ AJS.$("#jiraManualAppCreation__form").on("aui-valid-submit", (event) => {
   // Reading the content of the file
   const file = $("#privateKeyFile")[0].files[0];
   const reader = new FileReader();
-  reader.readAsDataURL(file);
+  reader.readAsText(file);
 
   reader.onload = () => {
     data.privateKey = reader.result;
