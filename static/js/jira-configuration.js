3fc245d27a3be83174f15147e9e2005bf2a0c2d0
Rachelle Rathbone <37155488+rachellerathbone@users.noreply.github.com>
2022-08-09 16:06:11 +1000

    ARC-1563 delete app and navigate to form on edit (#1475)
    * clean branch
    
    * address pr comments
    
    * address pr comments

--- static/js/jira-configuration.js
+++ static/js/jira-configuration.js
@@ -17,7 +17,7 @@ function openChildWindow(url) {
 $(".add-organization-link").click(function(event) {
 	event.preventDefault();
 	const queryParameter = $(this).data("gh-cloud") ? "" : "?ghRedirect=to";
-	window.AP.context.getToken(function(token) {
+	AP.context.getToken(function(token) {
 		const child = openChildWindow("/session/github/configuration" + queryParameter);
 		child.window.jiraHost = jiraHost;
 		child.window.jwt = token;
@@ -60,9 +60,9 @@ $(".delete-connection-link").click(function(event) {
 			data: {
 				installationId: $(event.target).data("installation-id"),
 				jwt: token,
-				jiraHost: jiraHost
+				jiraHost
 			},
-			success: function(data) {
+			success: function() {
 				AP.navigator.reload();
 			}
 		});
@@ -170,4 +170,43 @@ window.onclick = function(event) {
 		restartBackfillModal.style.display = "none";
 	}
 };
+
+$(".jiraConfiguration__deleteGitHubApp").click(function(event) {
+	event.preventDefault();
+	const uuid = $(event.target).data("app-uuid");
+
+	AP.context.getToken(function(token) {
+		$.ajax({
+			type: "DELETE",
+			url: `/jira/connect/enterprise/app/${uuid}`,
+			data: {
+				uuid,
+				jwt: token,
+				jiraHost
+			},
+			success: function(data) {
+				if (data.success) {
+					AP.navigator.reload();
+				}
+			},
+			error: function (error) {
+				// TODO - we should render an error here when the app fails to delete
+			},
+		});
+	});
+});
+
+$(".jiraConfiguration__editGitHubApp").click(function(event) {
+	event.preventDefault();
+	const uuid = $(event.target).data("app-uuid");
+
+	AP.navigator.go(
+		'addonmodule',
+		{
+			moduleKey: "github-edit-app-page",
+			customData: { uuid }
+		}
+	);
+});
+
 
