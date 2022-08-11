59bb1278c1d568919e9d65580f8c5aeef043c32e
Gary Xue <gxue@atlassian.com>
2022-08-11 14:10:03 +1000

    text change

--- src/sync/sync-utils.ts
+++ src/sync/sync-utils.ts
@@ -71,7 +71,7 @@ const getGitHubAppConfig = async (subscription: Subscription, logger: Logger): P
 	const gitHubServerApp = await GitHubServerApp.findByPk(gitHubAppId);
 	if (!gitHubServerApp) {
 		logger.error("Cannot find gitHubServerApp by pk", { gitHubAppId: gitHubAppId, subscriptionId: subscription.id });
-		throw new Error("Cannot find gitHubServerApp record by pk during findOrStartSync");
+		throw new Error("Error duing find and start sync. Reason: Cannot find ghes record from subscription.");
 	}
 	return ghesGitHubAppConfig(gitHubServerApp);
 
