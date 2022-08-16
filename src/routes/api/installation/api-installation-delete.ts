8eab5413bfe2183e1bb87ad8a342e143d4d8aec2
Rachelle <rachellerathbone@gmail.com>
2022-08-16 16:12:40 +1000

    update api installation delete

--- src/routes/api/installation/api-installation-delete.ts
+++ src/routes/api/installation/api-installation-delete.ts
@@ -3,10 +3,11 @@ import { Subscription } from "models/subscription";
 import { getJiraClient } from "~/src/jira/client/jira-client";
 
 export const ApiInstallationDelete = async (req: Request, res: Response): Promise<void> => {
-	const githubInstallationId = req.params.installationId;
+	const installationId = Number(req.params.installationId);
+	const { gitHubAppConfig } = res.locals;
 	const jiraHost = req.params.jiraHost;
 
-	if (!jiraHost || !githubInstallationId) {
+	if (!jiraHost || !installationId) {
 		const msg = "Missing Jira Host or Installation ID";
 		req.log.warn({ req, res }, msg);
 		res.status(400).send(msg);
@@ -15,7 +16,8 @@ export const ApiInstallationDelete = async (req: Request, res: Response): Promis
 
 	const subscription = await Subscription.getSingleInstallation(
 		jiraHost,
-		Number(githubInstallationId)
+		Number(installationId),
+		gitHubAppConfig?.gitHubAppId
 	);
 
 	if (!subscription) {
@@ -24,11 +26,13 @@ export const ApiInstallationDelete = async (req: Request, res: Response): Promis
 		return;
 	}
 
+	const gitHubInstallationId = subscription.gitHubInstallationId;
+
 	try {
-		const jiraClient = await getJiraClient(jiraHost, Number(githubInstallationId), req.log);
-		req.log.info({ jiraHost, githubInstallationId }, `Deleting DevInfo`);
-		await jiraClient.devinfo.installation.delete(githubInstallationId);
-		res.status(200).send(`DevInfo deleted for jiraHost: ${jiraHost} githubInstallationId: ${githubInstallationId}`);
+		const jiraClient = await getJiraClient(jiraHost, gitHubInstallationId, req.log);
+		req.log.info({ jiraHost, gitHubInstallationId }, `Deleting DevInfo`);
+		await jiraClient.devinfo.installation.delete(gitHubInstallationId);
+		res.status(200).send(`DevInfo deleted for jiraHost: ${jiraHost} githubInstallationId: ${gitHubInstallationId}`);
 	} catch (err) {
 		res.status(500).json(err);
 	}
