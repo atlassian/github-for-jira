99ad643efe565d6da4c2dcfba39cdb4e894059bb
Kayub Maharjan <kay.krazzie@gmail.com>
2022-08-19 14:34:09 +1000

    - Test case fixes

--- src/models/subscription.test.ts
+++ src/models/subscription.test.ts
@@ -32,38 +32,38 @@ describe("Subscription", () => {
 				const records = await Subscription.getAllForHost(jiraHost);
 				expect(records.length).toBe(3);
 				expect(records[0]).toEqual(expect.objectContaining({
-					gitHubInstallationId: GITHUHB_INSTALLATION_ID,
+					gitHubInstallationId: GITHUB_INSTALLATION_ID,
 					jiraHost,
 					jiraClientKey: "myClientKey"
 				}));
 				expect(records[1]).toEqual(expect.objectContaining({
-					gitHubInstallationId: GITHUHB_INSTALLATION_ID,
-					gitHubAppId: GHEH_GITHUB_SERVER_APP_PK_ID_1,
+					gitHubInstallationId: GITHUB_INSTALLATION_ID,
+					gitHubAppId: GHES_GITHUB_SERVER_APP_PK_ID_1,
 					jiraHost,
 					jiraClientKey: "myClientKey_ghe_1"
 				}));
 				expect(records[2]).toEqual(expect.objectContaining({
-					gitHubInstallationId: GITHUHB_INSTALLATION_ID,
-					gitHubAppId: GHEH_GITHUB_SERVER_APP_PK_ID_2,
+					gitHubInstallationId: GITHUB_INSTALLATION_ID,
+					gitHubAppId: GHES_GITHUB_SERVER_APP_PK_ID_2,
 					jiraHost,
 					jiraClientKey: "myClientKey_ghe_2"
 				}));
 			});
 			it("should get all server records when jiraHost is passed with gitHubAppid", async () => {
-				const record1 = await Subscription.getAllForHost(jiraHost, GHEH_GITHUB_SERVER_APP_PK_ID_1);
+				const record1 = await Subscription.getAllForHost(jiraHost, GHES_GITHUB_SERVER_APP_PK_ID_1);
 				expect(record1.length).toBe(1);
 				expect(record1[0]).toEqual(expect.objectContaining({
-					gitHubInstallationId: GITHUHB_INSTALLATION_ID,
-					gitHubAppId: GHEH_GITHUB_SERVER_APP_PK_ID_1,
+					gitHubInstallationId: GITHUB_INSTALLATION_ID,
+					gitHubAppId: GHES_GITHUB_SERVER_APP_PK_ID_1,
 					jiraHost,
 					jiraClientKey: "myClientKey_ghe_1"
 				}));
 
-				const record2 = await Subscription.getAllForHost(jiraHost, GHEH_GITHUB_SERVER_APP_PK_ID_2);
+				const record2 = await Subscription.getAllForHost(jiraHost, GHES_GITHUB_SERVER_APP_PK_ID_2);
 				expect(record2.length).toBe(1);
 				expect(record2[0]).toEqual(expect.objectContaining({
-					gitHubInstallationId: GITHUHB_INSTALLATION_ID,
-					gitHubAppId: GHEH_GITHUB_SERVER_APP_PK_ID_2,
+					gitHubInstallationId: GITHUB_INSTALLATION_ID,
+					gitHubAppId: GHES_GITHUB_SERVER_APP_PK_ID_2,
 					jiraHost,
 					jiraClientKey: "myClientKey_ghe_2"
 				}));
