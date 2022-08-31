1a46ce6564a5cdc3141fa5a99433fee04c4126ea
Kayub Maharjan <kay.krazzie@gmail.com>
2022-08-31 15:21:18 +1000

    - WIP

--- static/js/github-create-branch.js
+++ static/js/github-create-branch.js
@@ -1,5 +1,18 @@
-$('.gitHubCreateBranch__option').click(function (event) {
-	event.preventDefault();
-	$('.gitHubCreateBranch__option').removeClass('gitHubCreateBranch__selected');
-	$(event.target).addClass('gitHubCreateBranch__selected');
+
+$(document).ready(() => {
+	setupSelectFields();
+
+	$('.gitHubCreateBranch__option').click(function (event) {
+		event.preventDefault();
+		$('.gitHubCreateBranch__option').removeClass('gitHubCreateBranch__selected');
+		$(event.target).addClass('gitHubCreateBranch__selected');
+	});
 });
+
+const setupSelectFields = () => {
+	const options = {
+		formatNoMatches: () => "<a href='#'>Can't find the repository you're looking for?</a>"
+	};
+	AJS.$("#gitHubRepos").auiSelect2(options);
+	AJS.$("#gitHubBranches").auiSelect2(options);
+};
