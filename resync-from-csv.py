654bebea3416773171dbac023faae14e79dc1a02
Josh Kay <joshk@atlassian.com>
2022-09-02 10:59:15 +1200

    added github for jira as the base for python backfill script

--- resync-from-csv.py
+++ resync-from-csv.py
@@ -1,20 +1,20 @@
 #!/usr/bin/env python
 
 """
-Script to trigger secret rotation for installations in dss-secrets-store.
+Script to trigger Backfill from CSV of installation IDs.
 
-Takes an installations.csv with cloud_id,workspace_uuid rows and triggers rotation for each installation.
+Takes an installations.csv with installation_id.
 Keeps track of processed entries in a separate file, so that script can be stopped
 and resumed without having to change the input file.
 
 Input file format:
 
-    cloud_id,workspace_uuid
-    59fd52f3-b8d7-44f7-9441-01ab70fab45d,{20f0af8c-1b1a-462b-80d7-36818edaaa84}
+    installation_id
+    12345
 
 Running the script:
 
-    $ ./rotate_installations.py --env staging --sleep 1 --input installations.csv --output output.csv
+    $ ./resync-from-csv.py --env staging --sleep 1 --input installations.csv --output output.csv
 
 First time setup:
 
@@ -75,60 +75,68 @@ class SLAuth(AuthBase):
 
 def create_slauth(env: str) -> SLAuth:
     return SLAuth(
-        audience=f'dss-secrets-store',
-        group=f'micros-sv--dss-secrets-store-dl-admins',
+        audience=f'github-for-jira',
+        group=f'micros-sv--github-for-jira-dl-admins',
         environment=env
     )
 
 
 class Environment(NamedTuple):
-    secrets_url: str
+    github_for_jira_url: str
     secrets_auth: object
 
 
 def create_environment(env: str) -> Environment:
-    if env == 'dev':
+    if env == 'dev'
         return Environment(
-            secrets_url='https://dss-secrets-store.ap-southeast-2.dev.atl-paas.net',
-            secrets_auth=create_slauth(env))
+            github_for_jira_url='https://github-for-jira.ap-southwest-2.dev.atl-paas.net',
+            github_for_jira_auth=create_slauth(env))
     elif env == 'staging':
         return Environment(
-            secrets_url='https://dss-secrets-store.us-east-1.staging.atl-paas.net',
-            secrets_auth=create_slauth(env))
+            github_for_jira_url='https://github-for-jira.us-west-1.staging.atl-paas.net',
+            github_for_jira_auth=create_slauth(env))
     elif env == 'prod':
         return Environment(
-            secrets_url='https://dss-secrets-store.prod.atl-paas.net',
-            secrets_auth=create_slauth(env))
+            github_for_jira_url='https://github-for-jira.us-west-1.prod.atl-paas.net',
+            github_for_jira_auth=create_slauth(env))
     else:
         raise ValueError(f'Invalid environment {env}')
 
-
+#  PROBS REMOVE THIS
 @dataclass(frozen=True)
 class Installation:
-    cloud: str
-    workspace: str
+    installationId: str
 
     @staticmethod
     def from_dict(d):
-        return Installation(d['cloud_id'], d['workspace_uuid'])
-
+        return Installation(d['installation_id'])
+# #############################
 
 def process_installation(env: Environment, installation: Installation) -> bool:
-    url = '{}/api/internal/installations/rotate/cloudid/{}/bitbucket/workspaceuuid/{}'.format(
-        env.secrets_url, installation.cloud, installation.workspace)
+    url = '{}/api/resync'.format(env.github_for_jira_url)
 
     LOG.debug('Starting rotation of %s with url: %s', installation, url)
-    response = session.post(url, auth=env.secrets_auth)
+    response = session.post(url,
+      auth=env.github_for_jira_auth,
+      data={
+        "installationIds": "[1111,2222]", ## PASS INSTALLATION ID HERE
+        "syncType": "full",
+        "statusTypes": ["FAILED", "PENDING", "ACTIVE", "COMPLETE"],
+        "targetTasks": ["pull"] # Could pass this through as a arg????
+      })
+
+      atlas slauth curl -a github-for-jira -g micros-sv--github-for-jira-dl-admins -- \
+      -v https://github-for-jira.us-west-1.staging.atl-paas.net/api/resync \
+      -H "Content-Type: application/json" \
+      -d '{"": "full", "": [1111,2222], "statusTypes": ["FAILED", "PENDING", "ACTIVE", "COMPLETE"]}'
+
+
     if response.ok:
-        LOG.info('Rotation of %s was successful (%s).',
+        LOG.info('Sync of %s successfulyl started: (%s).',
                  installation, response.status_code)
         return 'success'
-    elif response.status_code == 404:
-        LOG.info('Rotation of %s returned not found, skipping (%s).',
-                 installation, response.status_code)
-        return 'not_found'
     else:
-        LOG.error('Rotation of %s failed (%s): %s',
+        LOG.error('Sync of %s failed (%s): %s',
                   installation, response.status_code, response.text)
         return 'error'
 
@@ -136,10 +144,10 @@ def process_installation(env: Environment, installation: Installation) -> bool:
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--env', choices=('dev', 'staging', 'prod'), required=True)
+    parser.add_argument('--batch-size', default=10, type=float, help='How many installations to process each iteration')
     parser.add_argument('--input', type=argparse.FileType('r'), required=True)
     parser.add_argument('--output', type=argparse.FileType('a+'), required=True)
-    parser.add_argument('--sleep', type=float,
-                        help='How long to wait between requests in seconds', required=True)
+    parser.add_argument('--sleep', type=float, help='How long to wait between requests in seconds', required=True)
     parser.add_argument('--level', default='INFO', choices=('INFO', 'ERROR', 'WARNING', 'DEBUG', 'CRITICAL'))
     args = parser.parse_args()
 
@@ -151,14 +159,15 @@ def main():
 
     processed = set()
     output_file.seek(0)
+    ############################ TODO
     output_reader = csv.DictReader(output_file, fieldnames=['cloud_id', 'workspace_uuid'])
     for row in output_reader:
         processed.add(Installation.from_dict(row))
-
+    ############################ TODO
     output_writer = csv.DictWriter(output_file, fieldnames=['cloud_id', 'workspace_uuid', 'status'])
-
+    ############################ TODO
     input_reader = csv.DictReader(input_file, fieldnames=['cloud_id', 'workspace_uuid'])
-
+    ############################ TODO
     for row in input_reader:
         installation = Installation.from_dict(row)
         if installation.cloud == 'cloud_id' and installation.workspace == 'workspace_uuid':
@@ -169,13 +178,15 @@ def main():
             # Skip already processed
             continue
 
+# INSTALLATION PROCESS IS DONE HERE
+#  todo pass a fake installation id here as a test to do it without a csv
         status = process_installation(env, installation)
         if status == 'error':
             LOG.error('Stopping due to error. To skip a particular installation, add a row to output file')
             sys.exit(1)
 
         processed.add(installation)
-        output_writer.writerow({'cloud_id': installation.cloud, 'workspace_uuid': installation.workspace, 'status': status})
+        output_writer.writerow({'installation_id': installation.installationId, 'status': status})
 
         time.sleep(args.sleep)
 
