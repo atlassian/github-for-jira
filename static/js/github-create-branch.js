0266f19ffa553fa0f1ed9ed4268fc522f0533361
Kayub Maharjan <kay.krazzie@gmail.com>
2022-09-02 09:22:21 +1000

    - Review changes

--- static/js/github-create-branch.js
+++ static/js/github-create-branch.js
@@ -2,12 +2,6 @@ $(document).ready(() => {
   $("#ghServers").auiSelect2();
   $("#ghRepo").auiSelect2();
   $("#ghParentBranch").auiSelect2();
-
-  $(".gitHubCreateBranch__option").on("click", event => {
-    event.preventDefault();
-    $(".gitHubCreateBranch__option").removeClass("gitHubCreateBranch__selected");
-    $(event.target).addClass("gitHubCreateBranch__selected");
-  });
 });
 
 
