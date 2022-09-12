0547184c63751b82ade46b47cd61643d640350d4
Kayub Maharjan <kay.krazzie@gmail.com>
2022-09-12 13:59:56 +1000

    - Debounce method added in view - Key event added for the select2 input

--- static/js/github-create-branch.js
+++ static/js/github-create-branch.js
@@ -1,11 +1,29 @@
 $(document).ready(() => {
-  $("#ghServers").auiSelect2();
-  $("#ghRepo").auiSelect2();
-  $("#ghParentBranch").auiSelect2();
-});
+  $("#ghServers").auiSelect2({
+    dropdownCssClass: "aui-select2-dropdown-server"
+  });
+  $("#ghRepo").auiSelect2({
+    dropdownCssClass: "aui-select2-dropdown-repo"
+  });
+  $("#ghParentBranch").auiSelect2({
+    dropdownCssClass: "aui-select2-dropdown-parent-branch"
+  });
+}).on('keyup', '.select2-input', debounce(event => {
+  const parentContainer = $(event.target).parent().parent();
+  const userInput = event.target.value;
 
-$('#cancelBtn').click(function (event) {
+  if (userInput) {
+    if (parentContainer.hasClass("aui-select2-dropdown-server")) {
+      // TODO: Query to search for the server instance based on the input
+    } else if (parentContainer.hasClass("aui-select2-dropdown-repo")) {
+      // TODO: Query to search for the repo based on the input(ARC-1600)
+    } else {
+      // TODO: Query to search for the parent branch based on the input
+    }
+  }
+}));
+
+  $('#cancelBtn').click(function (event) {
 	event.preventDefault();
-
 	window.close();
 });
