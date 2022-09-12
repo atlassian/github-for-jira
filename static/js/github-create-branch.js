11d12adcf2f5b3ab0f5fb4d0f380114b6cf83b96
joshkay10 <josh.kay@outlook.com>
2022-09-12 14:52:24 +1000

    ARC-1612 First cut for submit create branch (#1568)
    * WIP first cut for submit create branch
    
    * disply server error message on UI
    
    * removed comments and moved disable submit to new func
    
    * allowed jirahos undefined for userclient and fxied string splint on onwer repo
    
    * fix 401 test that no longer checks for jirahost
    
    Co-authored-by: Josh Kay <joshk@atlassian.com>
    Co-authored-by: Harminder <hsingh5@atlassian.com>
    Co-authored-by: Harminder84 <harminder.singh84@gmail.com>

--- static/js/github-create-branch.js
+++ static/js/github-create-branch.js
@@ -1,11 +1,47 @@
+const createBranchPost = () => {
+	const url = "/github/create-branch";
+	// Todo improve this select2 get and split once we have real data coming in
+	const repoWithOwner = $("#ghRepo").select2('data').text.split('/');
+	const data = {
+		owner: repoWithOwner[0],
+		repo: repoWithOwner[1],
+		sourceBranchName: $("#ghParentBranch").select2('data').text,
+		newBranchName: $('#branchNameText').val(),
+		_csrf: $('#_csrf').val(),
+	};
+	toggleSubmitDisabled(true);
+
+	$.post(url, data)
+		.done(() => {
+			// On success, we close the tab so the user returns to original screen
+			window.close();
+		})
+		.fail((err) => {
+			toggleSubmitDisabled(false);
+			$(".gitHubCreateBranch__serverError").show();
+			$(".errorMessageBox__message")
+				.empty()
+				.append("Please make sure all the details you entered are correct.")
+		});
+}
+
+const toggleSubmitDisabled = (bool) => {
+	$("#createBranchBtn").prop("disabled", bool);
+	$("#createBranchBtn").attr("aria-disabled", String(bool));
+}
+
 $(document).ready(() => {
   $("#ghServers").auiSelect2();
   $("#ghRepo").auiSelect2();
   $("#ghParentBranch").auiSelect2();
+
+	$("#createBranchForm").on("aui-valid-submit", (event) => {
+		event.preventDefault();
+		createBranchPost();
+	});
 });
 
 $('#cancelBtn').click(function (event) {
 	event.preventDefault();
-
 	window.close();
 });
