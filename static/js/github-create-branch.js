eb344394b967f53963e65f45aa8063c7e82ca872
Kayub Maharjan <kay.krazzie@gmail.com>
2022-09-13 19:32:45 +1000

    - WIP

--- static/js/github-create-branch.js
+++ static/js/github-create-branch.js
@@ -1,3 +1,55 @@
+// Holds all the searched repos
+let cachedRepos = [];
+
+$(document).ready(() => {
+	// Fetching the list of default repos
+	const defaultRepos = $(".default-repos").map((_, option) => ({
+		id: $(option).html(),
+		text: $(option).html()
+	})).toArray();
+
+  $("#ghServers").auiSelect2();
+
+  $("#ghRepo").auiSelect2({
+		placeholder: "Select a repository",
+		data: defaultRepos,
+		_ajaxQuery: Select2.query.ajax({
+			dataType: "json",
+			quietMillis: 500,
+			url: "/github/repository",
+			data: term => ({
+				repoName: term
+			}),
+			results: function(response) {
+				const { repositories } = response;
+				cachedRepos = cachedRepos.concat(repositories.map(repository => ({
+					id: repository.repo.nameWithOwner,
+					text: repository.repo.nameWithOwner
+				})));
+				return  {
+					results: cachedRepos
+				}
+			}
+		}),
+		query: function(options) {
+			const userInput = options.term;
+			cachedRepos = defaultRepos.filter(repo => repo.id.toUpperCase().indexOf(userInput.toUpperCase()) >= 0);
+			if (userInput.length) {
+				this._ajaxQuery.call(this, options);
+			}
+			options.callback({ results: cachedRepos });
+		}
+	});
+
+  $("#ghParentBranch").auiSelect2();
+
+  $("#createBranchForm").on("aui-valid-submit", (event) => {
+		event.preventDefault();
+		createBranchPost();
+	});
+});
+
+
 const createBranchPost = () => {
 	const url = "/github/create-branch";
 	// Todo improve this select2 get and split once we have real data coming in
@@ -29,53 +81,6 @@ const toggleSubmitDisabled = (bool) => {
 	$("#createBranchBtn").prop("disabled", bool);
 	$("#createBranchBtn").attr("aria-disabled", String(bool));
 }
-
-$(document).ready(() => {
-	// Fetching the list of default repos
-	const defaultRepos = $(".default-repos").map((_, option) => ({
-		id: $(option).html(),
-		text: $(option).html()
-	})).toArray();
-
-  $("#ghServers").auiSelect2();
-
-  $("#ghRepo").auiSelect2({
-		placeholder: "Select a repository",
-		data: defaultRepos,
-		_ajaxQuery: Select2.query.ajax({
-			dataType: "json",
-			quietMillis: 500,
-			url: "/github/repository",
-			data: term => ({
-				repoName: term
-			}),
-			results: (response) => {
-				const { repositories } = response;
-				return  {
-					results: repositories.map(repository => ({
-						id: repository.repo.nameWithOwner,
-						text: repository.repo.nameWithOwner
-					}))
-				}
-			},
-		}),
-		query: function(options) {
-			const userInput = options.term;
-			let filteredRepos = defaultRepos.filter(repo => repo.id.toUpperCase().indexOf(userInput.toUpperCase()) >= 0);
-			if (userInput.length) {
-				this._ajaxQuery.call(this, options);
-			}
-			options.callback({ results: filteredRepos });
-		}
-	});
-
-  $("#ghParentBranch").auiSelect2();
-
-  $("#createBranchForm").on("aui-valid-submit", (event) => {
-		event.preventDefault();
-		createBranchPost();
-	});
-});
 
 $('#cancelBtn').click(function (event) {
 	event.preventDefault();
