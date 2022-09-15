3578daa647855bcb2a153a85136f6a2c69a60ced
Kayub Maharjan <kay.krazzie@gmail.com>
2022-09-15 13:33:23 +1000

    - Fixing conflicts

--- static/js/github-create-branch.js
+++ static/js/github-create-branch.js
@@ -53,20 +53,110 @@ $(document).ready(() => {
       toggleLoaderInInput($(".ghRepo-dropdown"), false);
     });
 
-	$("#ghParentBranch").auiSelect2({
-		placeholder: "Select a branch",
-		data: []
-	});
+  $("#ghParentBranch").auiSelect2({
+    placeholder: "Select a branch",
+    data: []
+  });
 
-	$("#ghRepo").on("change", () => {
-		loadBranches();
-	});
+  $("#ghRepo").on("change", () => {
+    loadBranches();
+  });
 
   $("#createBranchForm").on("aui-valid-submit", (event) => {
     event.preventDefault();
     createBranchPost();
   });
+
+  $('#cancelBtn').click(function (event) {
+    event.preventDefault();
+    window.close();
+  });
+
 });
+
+const loadBranches = () => {
+  clearBranches();
+  toggleSubmitDisabled(true);
+  hideErrorMessage();
+  const repo = getRepoDetails();
+  $.ajax({
+    type: "GET",
+    url: `/github/create-branch/owners/${repo.owner}/repos/${repo.name}/branches`,
+    success: (data) => {
+      $("#ghParentBranch").auiSelect2({
+        data: () => {
+          data.repository.refs.edges.forEach((item) => {
+            item.id = item.node.name;
+          });
+          return {
+            text: item => item.node.name,
+            results: data.repository.refs.edges
+          }
+        },
+        formatSelection: item => item.node.name,
+        formatResult: item => item.node.name
+      });
+      $("#ghParentBranch").select2("val", data.repository.defaultBranchRef.name);
+      toggleSubmitDisabled(false);
+    },
+    error: (error) => {
+      showErrorMessage("Failed to fetch branches");
+      toggleSubmitDisabled(false);
+    }
+  });
+};
+
+const createBranchPost = () => {
+  const url = "/github/create-branch";
+  const repo = getRepoDetails();
+  const data = {
+    owner: repo.owner,
+    repo: repo.name,
+    sourceBranchName: $("#ghParentBranch").select2('val'),
+    newBranchName: $('#branchNameText').val(),
+    _csrf: $('#_csrf').val(),
+  };
+  toggleSubmitDisabled(true);
+  hideErrorMessage();
+
+  $.post(url, data)
+    .done(() => {
+      // On success, we close the tab so the user returns to original screen
+      window.close();
+    })
+    .fail((err) => {
+      toggleSubmitDisabled(false);
+      showErrorMessage("Please make sure all the details you entered are correct.")
+    });
+}
+
+const toggleSubmitDisabled = (bool) => {
+  $("#createBranchBtn").prop("disabled", bool);
+  $("#createBranchBtn").attr("aria-disabled", String(bool));
+}
+
+const getRepoDetails = () => {
+  const repoWithOwner = $("#ghRepo").select2('val').split('/');
+  return {
+    owner: repoWithOwner[0],
+    name: repoWithOwner[1],
+  }
+};
+
+const showErrorMessage = (msg) => {
+  $(".gitHubCreateBranch__serverError").show();
+  $(".errorMessageBox__message")
+    .empty()
+    .append(msg);
+};
+
+const hideErrorMessage = () => {
+  $(".gitHubCreateBranch__serverError").hide();
+};
+
+const clearBranches = () => {
+  $("#ghParentBranch").auiSelect2({ data: [] });
+};
 
 const toggleLoaderInInput = (inputDOM, state) => {
   const container = inputDOM.find("div.select2-search");
@@ -79,96 +169,3 @@ const toggleLoaderInInput = (inputDOM, state) => {
     container.find(loader).remove();
   }
 };
-
-const createBranchPost = () => {
-  const url = "/github/create-branch";
-  // Todo improve this select2 get and split once we have real data coming in
-  const repoWithOwner = $("#ghRepo").select2("data").text.split("/");
-  const data = {
-    owner: repoWithOwner[0],
-    repo: repoWithOwner[1],
-    sourceBranchName: $("#ghParentBranch").select2("data").text,
-    newBranchName: $("#branchNameText").val(),
-    _csrf: $("#_csrf").val(),
-  };
-  toggleSubmitDisabled(true);
-
-  $.post(url, data)
-    .done(() => {
-      // On success, we close the tab so the user returns to original screen
-      window.close();
-    })
-    .fail((err) => {
-      toggleSubmitDisabled(false);
-      $(".gitHubCreateBranch__serverError").show();
-      $(".errorMessageBox__message")
-        .empty()
-        .append("Please make sure all the details you entered are correct.")
-    });
-}
-
-const loadBranches = () => {
-	clearBranches();
-	toggleSubmitDisabled(true);
-	hideErrorMessage();
-	const repo = getRepoDetails();
-	$.ajax({
-		type: "GET",
-		url: `/github/create-branch/owners/${repo.owner}/repos/${repo.name}/branches`,
-		success: (data) => {
-			$("#ghParentBranch").auiSelect2({
-				data: () => {
-					data.repository.refs.edges.forEach((item) => {
-						item.id = item.node.name;
-					});
-					return {
-						text: item => item.node.name,
-						results: data.repository.refs.edges
-					}
-				},
-				formatSelection: item => item.node.name,
-				formatResult: item => item.node.name
-			});
-			$("#ghParentBranch").select2("val", data.repository.defaultBranchRef.name);
-			toggleSubmitDisabled(false);
-		},
-		error: (error) => {
-			console.log(error);
-			showErrorMessage("Failed to fetch branches");
-			toggleSubmitDisabled(false);
-		}
-	});
-};
-
-const getRepoDetails = () => {
-	const repoWithOwner = $("#ghRepo").select2('val').split('/');
-	return {
-		owner: repoWithOwner[0],
-		name: repoWithOwner[1],
-	}
-};
-
-const showErrorMessage = (msg) => {
-	$(".gitHubCreateBranch__serverError").show();
-	$(".errorMessageBox__message")
-		.empty()
-		.append(msg);
-};
-
-const hideErrorMessage = (msg) => {
-	$(".gitHubCreateBranch__serverError").hide();
-};
-
-const clearBranches = () => {
-	$("#ghParentBranch").auiSelect2({ data: [] });
-};
-
-const toggleSubmitDisabled = (bool) => {
-	$("#createBranchBtn").prop("disabled", bool);
-	$("#createBranchBtn").attr("aria-disabled", String(bool));
-};
-
-$('#cancelBtn').click(function (event) {
-	event.preventDefault();
-	window.close();
-});
