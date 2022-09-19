b12955158466103e3af477d477da5de84e789a5f
Josh Kay <joshk@atlassian.com>
2022-09-19 16:19:39 +1200

    remove prRef from log message

--- src/github/branch.ts
+++ src/github/branch.ts
@@ -77,7 +77,8 @@ export const deleteBranchWebhookHandler = async (context: WebhookContext, jiraCl
 		return;
 	}
 
-	context.log.info(`Deleting branch for repo ${context.payload.repository?.id} with ref ${context.payload.ref}`);
+
+	context.log.info({ prRef: context.payload.ref }, `Deleting branch for repo ${context.payload.repository?.id}`);
 
 	const jiraResponse = await jiraClient.devinfo.branch.delete(
 		`${payload.repository?.id}`,
