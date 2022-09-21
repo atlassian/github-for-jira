108c3e78e3020a8341689f3599aaf0944ebb73e4
Harminder84 <harminder.singh84@gmail.com>
2022-09-22 03:00:41 +0530

    ARC-1606 Display user details (#1617)
    * Initial checkin
    
    * fix test
    
    Co-authored-by: Harminder <hsingh5@atlassian.com>

--- src/routes/github/create-branch/github-create-branch-router.ts
+++ src/routes/github/create-branch/github-create-branch-router.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { GithubCreateBranchGet } from "routes/github/create-branch/github-create-branch-get";
 import { GithubCreateBranchPost } from "routes/github/create-branch/github-create-branch-post";
 import { GithubBranchesGet } from "~/src/routes/github/create-branch/github-branches-get";
+import { GithubRemoveSession } from "~/src/routes/github/create-branch/github-remove-session";
 
 export const GithubCreateBranchRouter = Router();
 
@@ -10,3 +11,5 @@ GithubCreateBranchRouter.route("/")
 	.post(GithubCreateBranchPost);
 
 GithubCreateBranchRouter.get("/owners/:owner/repos/:repo/branches", GithubBranchesGet);
+
+GithubCreateBranchRouter.get("/change-github-login", GithubRemoveSession);
