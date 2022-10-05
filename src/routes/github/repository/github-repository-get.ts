a5202aa93c2c6854261cc6b861e622a88ba6c05a
Kayub Maharjan <kay.krazzie@gmail.com>
2022-10-05 12:13:56 +1100

    - Cleanup

--- src/routes/github/repository/github-repository-get.ts
+++ src/routes/github/repository/github-repository-get.ts
@@ -2,12 +2,7 @@ import { Request, Response } from "express";
 import { createAppClient } from "utils/get-github-client-config";
 
 export const GitHubRepositoryGet = async (req: Request, res: Response): Promise<void> => {
-	const { githubToken } = res.locals;
-
-	// TODO: remove later
-	const jiraHostForTest = "https://kmaharjan4.atlassian.net";
-	const gitHubAppIdForTest = undefined;
-
+	const { githubToken, jiraHost, gitHubAppConfig } = res.locals;
 	const { repoName } = req.query;
 
 	if (!githubToken) {
@@ -21,7 +16,7 @@ export const GitHubRepositoryGet = async (req: Request, res: Response): Promise<
 	}
 
 	try {
-		const gitHubAppClient = await createAppClient(req.log, jiraHostForTest, gitHubAppIdForTest);
+		const gitHubAppClient = await createAppClient(req.log, jiraHost, gitHubAppConfig.gitHubAppId);
 		const installations = await gitHubAppClient.getInstallations();
 		const orgString = installations.data?.map(installation => ` org:${installation.account.login}`).join(" ");
 		const searchedRepos = await gitHubAppClient.searchUserRepositoriesRest(`${repoName} ${orgString}`);
