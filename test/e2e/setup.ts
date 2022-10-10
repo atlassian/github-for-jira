5249d65ef0f73225b61705d72a2d7d769654937f
Michel Boudreau <michelboudreau@gmail.com>
2022-10-10 21:12:49 +1100

    trying screenshot in case of issues

--- test/e2e/setup.ts
+++ test/e2e/setup.ts
@@ -2,7 +2,7 @@ import { chromium } from "@playwright/test";
 import { jiraLogin } from "test/e2e/utils/jira";
 // import { githubAppUpdateURLs, githubLogin } from "test/e2e/utils/github";
 import { clearState, stateExists } from "test/e2e/e2e-utils";
-import { testData } from "test/e2e/constants";
+import { SCREENSHOT_PATH, testData } from "test/e2e/constants";
 
 export default async function setup() {
 	const browser = await chromium.launch();
@@ -11,8 +11,9 @@ export default async function setup() {
 	clearState();
 
 	// login and save state before tests
+	const page = await browser.newPage();
 	await Promise.all([
-		jiraLogin(await browser.newPage(), "admin", true)
+		jiraLogin(page, "admin", true).finally(async () => await page.screenshot({ path: `${SCREENSHOT_PATH}/jira-login.jpg` }))
 		// githubLogin(await browser.newPage(), "admin", true).then(githubAppUpdateURLs)
 	]);
 
@@ -20,7 +21,7 @@ export default async function setup() {
 	await browser.close();
 
 	// Check to make sure state exists before continuing
-	if (!stateExists(testData.jira.roles.admin) /*|| !stateExists(testData.github.roles.admin)*/) {
+	if (!stateExists(testData.jira.roles.admin) || !stateExists(testData.github.roles.admin)) {
 		throw "Missing state";
 	}
 }
