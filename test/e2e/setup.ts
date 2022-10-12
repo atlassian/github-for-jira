f9c6a1f5d3596e5d2e6c21a6e963e3e8356ad184
Michel Boudreau <michelboudreau@gmail.com>
2022-10-12 16:27:28 +1100

    fixing tests, adding log output, archival

--- test/e2e/setup.ts
+++ test/e2e/setup.ts
@@ -2,7 +2,7 @@ import { chromium } from "@playwright/test";
 import { jiraLogin } from "test/e2e/utils/jira";
 // import { githubAppUpdateURLs, githubLogin } from "test/e2e/utils/github";
 import { clearState, stateExists } from "test/e2e/e2e-utils";
-import { SCREENSHOT_PATH, testData } from "test/e2e/constants";
+import { testData } from "test/e2e/constants";
 
 export default async function setup() {
 	const browser = await chromium.launch();
@@ -13,7 +13,7 @@ export default async function setup() {
 	// login and save state before tests
 	const page = await browser.newPage();
 	await Promise.all([
-		jiraLogin(page, "admin", true).finally(async () => await page.screenshot({ path: `${SCREENSHOT_PATH}/jira-login.jpg` }))
+		jiraLogin(page, "admin", true)/*.finally(async () => await page.screenshot({ path: `${SCREENSHOT_PATH}/jira-login.jpg` }))*/
 		// githubLogin(await browser.newPage(), "admin", true).then(githubAppUpdateURLs)
 	]);
 
--- test/e2e/utils/jira.ts
+++ test/e2e/utils/jira.ts
@@ -50,7 +50,7 @@ export const jiraAppInstall = async (page: Page): Promise<Page> => {
 	await page.click("#upm-upload-dialog .aui-button-primary");
 	await page.click(`#upm-plugin-status-dialog .confirm`);
 	const iframe = await page.frameLocator("#ak-main-content iframe");
-	await (await iframe.locator(".jiraConfiguration")).waitFor({ timeout: 10000 });
+	await (await iframe.locator(".jiraConfiguration")).waitFor();
 	return page;
 };
 
