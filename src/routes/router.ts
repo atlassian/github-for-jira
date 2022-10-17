59c598994eefb253c967404d790fa85672625370
Gary Xue <105693507+gxueatlassian@users.noreply.github.com>
2022-10-17 14:04:14 +1100

    Arc 1662 increase payload limit for enterprise (#1672)
    ARC-1662: increase payload limit to 30mb according to github doc

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -25,7 +25,9 @@ RootRouter.use(Sentry.Handlers.requestHandler());
 
 // Parse URL-encoded bodies for Jira configuration requests
 RootRouter.use(urlencoded({ extended: false }));
-RootRouter.use(json());
+RootRouter.use(json({
+	limit: "30mb" //set limit according to github doc https://docs.github.com/en/developers/webhooks-and-events/webhooks/webhook-events-and-payloads#webhook-payload-object-common-properties
+}));
 RootRouter.use(cookieParser());
 
 // Add pertinent information to logger for all subsequent routes
