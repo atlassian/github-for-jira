592340bd67d8a475473c398dd36bb5f1a905a95a
Gary Xue <gxue@atlassian.com>
2022-10-19 14:20:14 +1100

    ARC-1770-add-outbound-proxy-to-oauth-router

--- src/routes/github/github-oauth-router.ts
+++ src/routes/github/github-oauth-router.ts
@@ -5,8 +5,9 @@ import axios from "axios";
 import { getLogger } from "config/logger";
 import { envVars } from "config/env";
 import { Errors } from "config/errors";
-import { getGitHubApiUrl } from "~/src/util/get-github-client-config";
+import { getGitHubApiUrl, createAnonymousClient } from "~/src/util/get-github-client-config";
 import { createHashWithSharedSecret } from "utils/encryption";
+import { BooleanFlags, booleanFlag } from "config/feature-flags";
 
 const logger = getLogger("github-oauth");
 const appUrl = envVars.APP_URL;
@@ -84,26 +85,35 @@ const GithubOAuthCallbackGet = async (req: Request, res: Response, next: NextFun
 
 	logger.info(`${createHashWithSharedSecret(gitHubClientSecret)} is used`);
 
-	try {
-		const response = await axios.get(
-			`${hostname}/login/oauth/access_token`,
-			{
-				params: {
-					client_id: clientId,
-					client_secret: gitHubClientSecret,
-					code,
-					state
-				},
-				headers: {
-					accept: "application/json",
-					"content-type": "application/json"
-				},
-				responseType: "json"
-			}
-		);
 
-		// Saving it to session be used later
-		req.session.githubToken = response.data.access_token;
+	try {
+
+		if(await booleanFlag(BooleanFlags.USE_OUTBOUND_PROXY_FOR_OUATH_ROUTER, false, jiraHost)) {
+			const gitHubAnonymousClient = await createAnonymousClient(hostname, jiraHost, logger);
+			const accessToken = await gitHubAnonymousClient.exchangeGitHubToken({
+				clientId, clientSecret: gitHubClientSecret, code, state
+			});
+			req.session.githubToken = accessToken;
+		} else {
+			const response = await axios.get(
+				`${hostname}/login/oauth/access_token`,
+				{
+					params: {
+						client_id: clientId,
+						client_secret: gitHubClientSecret,
+						code,
+						state
+					},
+					headers: {
+						accept: "application/json",
+						"content-type": "application/json"
+					},
+					responseType: "json"
+				}
+			);
+			// Saving it to session be used later
+			req.session.githubToken = response.data.access_token;
+		}
 
 		// Saving UUID for each GitHubServerApp
 		req.session.gitHubUuid = uuid;
