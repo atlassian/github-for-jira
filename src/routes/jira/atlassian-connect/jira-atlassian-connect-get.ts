85fdfd7204cd581a6e059465d0a20cb8d0faa8d3
Josh Kay <joshk@atlassian.com>
2022-10-20 10:39:22 +1300

    fixed snapshots

--- src/routes/jira/atlassian-connect/jira-atlassian-connect-get.ts
+++ src/routes/jira/atlassian-connect/jira-atlassian-connect-get.ts
@@ -167,8 +167,7 @@ const addCreateBranchAction = async (modules) => {
 	if (await booleanFlag(BooleanFlags.CREATE_BRANCH, false)) {
 		modules.jiraDevelopmentTool.actions = {
 			createBranch: {
-				templateUrl: `${envVars.APP_URL}/create-branch-options?issueKey={issue.key}&issueSummary={issue.summary}&jiraHost={tenant.info}`
-				// `/plugins/servlet/ac/${key}/create-branch-options?ac.issueKey={issue.key}&ac.issueSummary={issue.summary}&ac.jiraHost={tenant.host}`
+				templateUrl: `${envVars.APP_URL}/create-branch-options?issueKey={issue.key}&issueSummary={issue.summary}&jiraHost={tenant.host}`
 			}
 		};
 	}
