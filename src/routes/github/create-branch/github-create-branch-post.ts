088819b463932a541ee23640383f171709b4b35f
Kayub Maharjan <kay.krazzie@gmail.com>
2022-10-24 10:18:40 +1100

    - Review changes - function renamed, err status fixed

--- src/routes/github/create-branch/github-create-branch-post.ts
+++ src/routes/github/create-branch/github-create-branch-post.ts
@@ -36,7 +36,7 @@ const getGitHubConfigurationLink = async (
 	return url + subscription?.gitHubInstallationId;
 };
 
-const errorMessages = (statusCode: number, url?: string): string => {
+const getErrorMessages = (statusCode: number, url?: string): string => {
 	switch (statusCode) {
 		case 403: {
 			return `We couldn’t create this branch, possibly because this GitHub repository hasn't been configured to your Jira site. <a href="${url}" target="_blank">Allow access to this repository.</a>`;
@@ -70,7 +70,7 @@ export const GithubCreateBranchPost = async (req: Request, res: Response): Promi
 	}
 
 	if (!owner || !repo || !sourceBranchName || !newBranchName) {
-		res.status(400).json(errorMessages(400));
+		res.status(400).json(getErrorMessages(400));
 		return;
 	}
 	const gitHubUserClient = await createUserClient(githubToken, jiraHost, req.log, gitHubAppConfig.gitHubAppId);
@@ -97,10 +97,10 @@ export const GithubCreateBranchPost = async (req: Request, res: Response): Promi
 		if (err.status === 403) {
 			const user = await gitHubUserClient.getUser();
 			const gitHubConfigurationLink = await getGitHubConfigurationLink(user.data.login, jiraHost, gitHubAppConfig.hostname, owner, repo);
-			res.status(err.status).json(errorMessages(err?.status || 500, gitHubConfigurationLink));
+			res.status(err.status).json(getErrorMessages(err?.status, gitHubConfigurationLink));
 
 		} else {
-			res.status(err.status).json(errorMessages(err?.status || 500));
+			res.status(err.status).json(getErrorMessages(err?.status));
 		}
 		sendTrackEventAnalytics(AnalyticsTrackEventsEnum.CreateBranchErrorTrackEventName, jiraHost);
 		statsd.increment(metricCreateBranch.failed, {
