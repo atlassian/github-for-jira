b1ffda9dfee1d24ec5abc91e15c81cbad3938ccf
Michel Boudreau <michelboudreau@gmail.com>
2022-10-28 12:06:33 -0300

    fixing test for issue parser

--- src/github/push.test.ts
+++ src/github/push.test.ts
@@ -65,7 +65,7 @@ describe("PushWebhookHandler", ()=>{
 			repository: {} as GitHubRepository, //force it as not required in test
 			commits: [{
 				id: "commit-1",
-				message: "ARC-0001 some commit message",
+				message: "ARC-1000 some commit message",
 				added: [],
 				modified: [],
 				removed: []
