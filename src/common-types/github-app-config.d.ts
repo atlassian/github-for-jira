b02d54cf3ef5b332dd9595f0a2cbd6ee49980cd8
Gary Xue <gxue@atlassian.com>
2022-11-10 10:12:15 +1100

    ARC-1823 add stronger typings to github app config

---
+++ src/common-types/github-app-config.d.ts
@@ -0,0 +1,9 @@
+type GitHubAppConfig = {
+	gitHubAppId: number | undefined, // undefined for cloud
+	uuid: string | undefined, //undefined for cloud
+	appId: number,
+	clientId: string,
+	gitHubBaseUrl: string,
+	gitHubApiUrl: string,
+}
+
