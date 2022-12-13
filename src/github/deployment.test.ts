d71031761846ffb2ab04e81715d3cba3195fc0f0
Amanda Trigueiro <atrigueiro@atlassian.com>
2022-12-13 14:56:26 +1100

    wip - fix tests

--- src/github/deployment.test.ts
+++ src/github/deployment.test.ts
@@ -1,13 +1,9 @@
 import { deploymentWebhookHandler } from "./deployment";
 import { WebhookContext } from "routes/github/webhook/webhook-context";
 import { getLogger } from "config/logger";
-import { waitUntil } from "test/utils/wait-until";
 import { GITHUB_CLOUD_BASEURL, GITHUB_CLOUD_API_BASEURL } from "utils/get-github-client-config";
 import { envVars } from "config/env";
 import { sqsQueues } from "../sqs/queues";
-import { WebhookApp } from "test/utils/create-webhook-app";
-
-import deploymentStatusBasic from "fixtures/deployment_status-basic.json";
 
 jest.mock("../sqs/queues");
 
@@ -20,13 +16,10 @@ const GHES_GITHUB_APP_CLIENT_ID = "client-id";
 describe("DeploymentWebhookHandler", () => {
 	let jiraClient: any;
 	let util: any;
-	let app: WebhookApp;
-
 	beforeEach(() => {
 		jiraClient = { baseURL: jiraHost };
 		util = null;
 	});
-
 	describe("GitHub Cloud", () => {
 		it("should be called with cloud GitHubAppConfig", async () => {
 			await deploymentWebhookHandler(getWebhookContext({ cloud: true }), jiraClient, util, GITHUB_INSTALLATION_ID);
@@ -41,71 +34,7 @@ describe("DeploymentWebhookHandler", () => {
 				}
 			}));
 		});
-
-		it("should queue and process a deployment event", async () => {
-			const sha = deploymentStatusBasic.payload.deployment.sha;
-
-			githubUserTokenNock(1234);
-
-			githubNock.get(`/repos/test-repo-owner/test-repo-name/commits/${sha}`)
-				.reply(200, {
-					commit: {
-						author: {
-							name: "test-branch-author-name",
-							email: "test-branch-author-name@github.com",
-							date: "test-branch-author-date"
-						},
-						message: "[TEST-123] test-commit-message"
-					},
-					html_url: `test-repo-url/commits/${sha}`
-				});
-
-			jiraNock.post("/rest/deployments/0.1/bulk", {
-				deployments:
-					[
-						{
-							schemaVersion: "1.0",
-							deploymentSequenceNumber: 1234,
-							updateSequenceNumber: 123456,
-							issueKeys:
-								[
-									"TEST-123"
-								],
-							displayName: "deploy",
-							url: "test-repo-url/commit/885bee1-commit-id-1c458/checks",
-							description: "deploy",
-							lastUpdated: "2021-06-28T12:15:18.000Z",
-							state: "successful",
-							pipeline:
-								{
-									id: "deploy",
-									displayName: "deploy",
-									url: "test-repo-url/commit/885bee1-commit-id-1c458/checks"
-								},
-							environment:
-								{
-									id: "Production",
-									displayName: "Production",
-									type: "production"
-								}
-						}
-					],
-				properties:
-					{
-						gitHubInstallationId: 1234,
-						repositoryId: "test-repo-id"
-					}
-			}).reply(200);
-
-			await expect(app.receive(deploymentStatusBasic as any)).toResolve();
-
-			await waitUntil(async () => {
-				expect(githubNock).toBeDone();
-				expect(jiraNock).toBeDone();
-			});
-		});
 	});
-
 	describe("GitHub Enterprise Server", () => {
 		it("should be called with GHES GitHubAppConfig", async () => {
 			await deploymentWebhookHandler(getWebhookContext({ cloud: false }), jiraClient, util, GITHUB_INSTALLATION_ID);
@@ -121,7 +50,6 @@ describe("DeploymentWebhookHandler", () => {
 			}));
 		});
 	});
-
 	const getWebhookContext = ({ cloud }: {cloud: boolean}) => {
 		return new WebhookContext({
 			id: "1",
