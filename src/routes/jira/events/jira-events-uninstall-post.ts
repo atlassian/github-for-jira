5a176fc613e68b5cdb69844acfd714467e254b42
Michel Boudreau <michelboudreau@gmail.com>
2022-12-19 21:54:47 -0400

    ARC-1873 Making the uninstall non-blocking on app property which might not be set (#1804)
    making the uninstall non-blocking on app property which might not be set

--- src/routes/jira/events/jira-events-uninstall-post.ts
+++ src/routes/jira/events/jira-events-uninstall-post.ts
@@ -19,7 +19,8 @@ export const JiraEventsUninstallPost = async (req: Request, res: Response): Prom
 
 	const jiraClient = await getJiraClient(installation.jiraHost, undefined, undefined, req.log);
 
-	await jiraClient.appProperties.delete();
+	// Don't wait for promise as it might fail if the property is not set
+	jiraClient.appProperties.delete();
 	await installation.uninstall();
 
 	req.log.info("App uninstalled on Jira.");
