a8c34fef234309ec3af400e7d11132c6e385e7f9
Gary Xue <gxue@atlassian.com>
2023-01-31 10:36:20 +1100

    Update regex for client key extraction

--- src/routes/api/client-key/client-key-regex.test.ts
+++ src/routes/api/client-key/client-key-regex.test.ts
@@ -1,9 +1,11 @@
 import { extractClientKey } from "./client-key-regex";
 
 const sampleUuid = "yyyyyyyy-0569-4f33-8413-cccccccc";
+const sampleJiraSite = "sample-site.atlassian.net";
 const possibleJiraXmlInfo = [
 	[`blah\n<key>jira:${sampleUuid}</key>\nblah`, `jira:${sampleUuid}`],
-	[`blah\n<key>${sampleUuid}</key>\nblah`, sampleUuid]
+	[`blah\n<key>${sampleUuid}</key>\nblah`, sampleUuid],
+	[`blah\n<key>${sampleJiraSite}</key>\nblah`, sampleJiraSite]
 ];
 
 describe("Extract client key from xml", ()=>{
