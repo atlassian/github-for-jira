13579d45f7b476a4bffa61a068afec6752f21e47
Josh Kay <jkay@atlassian.com>
2023-01-31 14:49:50 +1300

    moved out of before each test

--- test/e2e/create-branch.e2e.ts
+++ test/e2e/create-branch.e2e.ts
@@ -1,24 +1,29 @@
-import { expect, Page, test } from "@playwright/test";
+import { expect, test } from "@playwright/test";
+// import { expect, Page, test } from "@playwright/test";
 import { jiraAddProject, jiraCreateIssue, jiraLogin, jiraRemoveProject } from "test/e2e/utils/jira";
 import { testData } from "test/e2e/constants";
 
 test.describe("Create branch", () => {
-	let page: Page;
+	// let page: Page;
 	const data = testData.jira;
 
-	test.beforeEach(async ({ page: newPage }) => {
-		page = newPage;
-		await jiraLogin(page, "admin");
-		await jiraAddProject(page);
-		await jiraCreateIssue(page);
-	});
+	// test.beforeEach(async ({ page: newPage }) => {
+	// 	page = newPage;
+	// 	await jiraLogin(page, "admin");
+	// 	await jiraAddProject(page);
+	// 	await jiraCreateIssue(page);
+	// });
 
 	test.use({
 		storageState: testData.jira.roles.admin.state
 	});
 
 	test.describe("cloud", () => {
-		test("When there are no GitHub connections", async () => {
+		test("When there are no GitHub connections", async ({ page }) => {
+
+			await jiraLogin(page, "admin");
+			await jiraAddProject(page);
+			await jiraCreateIssue(page);
 			await page.goto(data.urls.testProjectIssue);
 			await (page.locator("a[data-testid='development-summary-common.ui.summary-item.link-formatted-button']")).click();
 			const poppedUpPage = await page.waitForEvent("popup");
