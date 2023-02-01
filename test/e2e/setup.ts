a100a429896e4f0ee82486dba164879bb6a251fa
Josh Kay <jkay@atlassian.com>
2023-02-01 14:27:47 +1300

    moved install to setupscript

--- test/e2e/setup.ts
+++ test/e2e/setup.ts
@@ -1,22 +1,29 @@
 import { chromium } from "@playwright/test";
-import { jiraLogin } from "test/e2e/utils/jira";
+import { jiraAppInstall, jiraLogin } from "test/e2e/utils/jira";
 // import { githubAppUpdateURLs, githubLogin } from "test/e2e/utils/github";
 import { clearState, stateExists } from "test/e2e/e2e-utils";
 import { testData } from "test/e2e/constants";
 import { ngrokBypass } from "test/e2e/utils/ngrok";
 
 export default async function setup() {
-	const browser = await chromium.launch();
+	// eslint-disable-next-line no-console
+	console.log("START SET SCRIPT");
 
+	const browser = await chromium.launch();
+	const page = await browser.newPage();
 	// Remove old state before starting
 	clearState();
-
 	// login and save state before tests
+	// eslint-disable-next-line no-console
+	console.log("START APP LOGIN");
 	await Promise.all([
-		ngrokBypass(await browser.newPage()).then(async (page) => jiraLogin(page, "admin", true))
+		ngrokBypass(page).then(async (page) => jiraLogin(page, "admin", true))
 		// githubLogin(await browser.newPage(), "admin", true).then(githubAppUpdateURLs)
 	]);
 
+	// eslint-disable-next-line no-console
+	console.log("START APP INSTALL");
+	await jiraAppInstall(page);
 	// Close the browser
 	await browser.close();
 
@@ -24,4 +31,7 @@ export default async function setup() {
 	if (!stateExists(testData.jira.roles.admin) || !stateExists(testData.github.roles.admin)) {
 		throw "Missing state";
 	}
+	// eslint-disable-next-line no-console
+	console.log("END SET SCRIPT");
+
 }
