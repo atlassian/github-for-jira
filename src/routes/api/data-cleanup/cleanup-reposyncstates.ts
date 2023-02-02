5c1dd45359bc3cfd75cca66c0ec446dce4e99030
Gary Xue <gxue@atlassian.com>
2023-02-02 12:11:22 +1100

    ARC-1422 clean up orphan repo sync state data

---
+++ src/routes/api/data-cleanup/cleanup-reposyncstates.ts
@@ -0,0 +1,20 @@
+import { Request, Response } from "express";
+import { getLogger } from "config/logger";
+import { RepoSyncState } from "models/reposyncstate";
+import { Subscription } from "models/subscription";
+import { Op } from "sequelize";
+
+const DEFAULT_BATCH_SIZE = 1000;
+
+const log = getLogger("RecoverClientKeyPost");
+
+const getCleanupSql = (size: number) => {
+	return `delete from "RepoSyncStates" where not exists (select null from "Subscriptions" where "RepoSyncStates"."subscriptionId" = "Subscriptions"."id") order by "RepoSyncStates"."updatedAt" limit ${size}`;
+}
+
+export const RepoSyncStateCleanUpOrphanDataPost = async (req: Request, res: Response): Promise<void> => {
+	const batchSize = Number(req.query.batchSize) || DEFAULT_BATCH_SIZE;
+	const result = await RepoSyncState.sequelize?.query(getCleanupSql(batchSize));
+	log.info({ count: result }, "Orphan RepoSyncStates data deleted");
+	res.status(204).end("Done. Count: " + result);
+};
