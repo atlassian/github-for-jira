b98ad16b4e4f44d8929c441dbb65f54a7b309f6d
Gary Xue <gxue@atlassian.com>
2023-02-02 12:21:56 +1100

    Remove @types/xml2json

--- src/routes/api/client-key/client-key-regex.ts
+++ src/routes/api/client-key/client-key-regex.ts
@@ -2,7 +2,7 @@ import { toJson } from "xml2json";
 export const extractClientKey = (text: string): string | undefined  => {
 	try {
 		const json = toJson(text, { object: true });
-		return (json.consumer as { [key: string]: string }).key;
+		return json?.consumer?.key;
 	} catch (e) {
 		return undefined;
 	}
