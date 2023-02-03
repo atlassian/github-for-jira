1f10da77b79b467ea120b12774b2f20c838e7aa1
Gary Xue <gxue@atlassian.com>
2023-02-03 16:04:26 +1100

    Remove data copy api

--- src/routes/api/data-cleanup/data-cleanup-router.ts
+++ src/routes/api/data-cleanup/data-cleanup-router.ts
@@ -1,9 +1,7 @@
 import { Router } from "express";
 import { RepoSyncStateCleanUpOrphanDataPost } from "./cleanup-reposyncstates";
-import { DataTableCopyPost } from "./data-table-copy";
 
 export const DataCleanupRouter = Router();
 DataCleanupRouter.delete("/repo-sync-states", RepoSyncStateCleanUpOrphanDataPost);
-DataCleanupRouter.post("/copy-data-table", DataTableCopyPost);
 
 
