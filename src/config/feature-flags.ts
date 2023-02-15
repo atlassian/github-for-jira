95e4df5923d52fe5abf213db9479c9b3d8cac60a
Harminder <hsingh5@atlassian.com>
2023-02-15 16:07:24 +1100

    inital checkin

--- src/config/feature-flags.ts
+++ src/config/feature-flags.ts
@@ -18,7 +18,6 @@ export enum BooleanFlags {
 	SEND_PR_COMMENTS_TO_JIRA = "send-pr-comments-to-jira_zy5ib",
 	ISSUEKEY_REGEX_CHAR_LIMIT = "issuekey-regex-char-limit",
 	USE_SHARED_PR_TRANSFORM = "use-shared-pr-transform",
-	NEW_JWT_VALIDATION = "new-jwt-validation",
 	RELAX_GHE_URLS_CHECK = "relax-ghe-url-check",
 	RENEW_GITHUB_TOKEN = "renew-github-token"
 }
--- src/routes/jira/jira-router.ts
+++ src/routes/jira/jira-router.ts
@@ -9,13 +9,12 @@ import { JiraDelete } from "routes/jira/jira-delete";
 import { JiraConnectRouter } from "routes/jira/connect/jira-connect-router";
 import { body } from "express-validator";
 import { returnOnValidationError } from "routes/api/api-utils";
-import { JiraContextJwtTokenMiddleware, JiraJwtTokenMiddleware } from "~/src/middleware/jira-jwt-middleware";
 import { jiraSymmetricJwtMiddleware } from "~/src/middleware/jira-symmetric-jwt-middleware";
 
 export const JiraRouter = Router();
 
 // TODO: The params `installationId` needs to be replaced by `subscriptionId`
-JiraRouter.delete("/subscription/:installationId", JiraContextJwtTokenMiddleware, jiraSymmetricJwtMiddleware, JiraDelete);
+JiraRouter.delete("/subscription/:installationId", jiraSymmetricJwtMiddleware, JiraDelete);
 
 JiraRouter.get("/atlassian-connect.json", JiraAtlassianConnectGet);
 
@@ -24,13 +23,12 @@ JiraRouter.use("/connect", JiraConnectRouter);
 JiraRouter.post("/sync",
 	body("commitsFromDate").optional().isISO8601(),
 	returnOnValidationError,
-	JiraContextJwtTokenMiddleware,
 	jiraSymmetricJwtMiddleware,
 	JiraSyncPost);
 
 JiraRouter.use("/events", JiraEventsRouter);
 
-JiraRouter.get("/", csrfMiddleware, JiraJwtTokenMiddleware, jiraSymmetricJwtMiddleware, JiraGet);
+JiraRouter.get("/", csrfMiddleware, jiraSymmetricJwtMiddleware, JiraGet);
 
 /********************************************************************************************************************
  * TODO: remove this later, keeping this for now cause its out in `Prod`
