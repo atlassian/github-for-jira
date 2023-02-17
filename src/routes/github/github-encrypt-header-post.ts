3e30a40e430e838418eaaaed93aa020ac97c19ba
Boris Gvozdev <bgvozdev@atlassian.com>
2023-02-17 11:54:57 +1100

    ARC-1976: temp endpoint to encrypt a header

--- src/routes/github/github-encrypt-header-post.ts
+++ src/routes/github/github-encrypt-header-post.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import { EncryptionClient, EncryptionSecretKeyEnum } from "utils/encryption-client";
 import crypto from "crypto";
+import { stringFlag, StringFlags } from "config/feature-flags";
 
 const getHashedKey = (clientKey: string): string => {
 	const keyHash = crypto.createHash("sha256");
@@ -9,24 +10,51 @@ const getHashedKey = (clientKey: string): string => {
 };
 
 export const GithubEncryptHeaderPost = async (req: Request, res: Response): Promise<void> => {
+	req.log.info("Encryption was requested");
 
-	if (typeof req.headers.tdauthtoken === "string") {
-		const plainValue: string = req.headers.tdauthtoken || "";
+	const headerToEncrypt = (await stringFlag(StringFlags.HEADERS_TO_ENCRYPT, "", res.locals.jiraHost) || "").toLowerCase().trim();
 
-		const encryptedValue = await EncryptionClient.encrypt(EncryptionSecretKeyEnum.GITHUB_SERVER_PREAUTH_HEADER_VALUE, plainValue, {
-			jiraHost: res.locals.jiraHost
-		});
-
-		res.json({
-			encryptedValue,
-			plainValueSha: getHashedKey(plainValue)
-		}).sendStatus(200);
-
-	} else {
-		res.json({
+	if (headerToEncrypt.length === 0) {
+		req.log.warn("Not allowed");
+		res.status(401).json({
 			error: {
-				message: "Header was not found!"
+				message: "Not allowed"
 			}
-		}).sendStatus(400);
+		});
+		return;
 	}
+
+	if (!req.headers[headerToEncrypt]) {
+		req.log.info("Header wasn't provided: " + headerToEncrypt);
+		res.status(400).json({
+			error: {
+				message: "Header not found: " + headerToEncrypt
+			}
+		});
+		return;
+	}
+
+	const plainValue = req.headers[headerToEncrypt] || "";
+
+	if (Array.isArray(plainValue)) {
+		req.log.info("Array was provided: " + headerToEncrypt);
+		res.status(400).json({
+			error: {
+				message: "Just one header was expected: " + headerToEncrypt
+			}
+		});
+		return;
+	}
+
+	const encryptedValue = await EncryptionClient.encrypt(EncryptionSecretKeyEnum.GITHUB_SERVER_PREAUTH_HEADER_VALUE, plainValue, {
+		jiraHost: res.locals.jiraHost
+	});
+
+	res.json({
+		encryptedValue: encryptedValue,
+		plainValueSha256: getHashedKey(plainValue)
+	});
+
+	req.log.info("Encrypted header was returned sent back!");
+
 };
