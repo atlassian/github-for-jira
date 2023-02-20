f990ec2d7b6b5c35d71026dfa4887b835a01b0ac
Gary Xue <gxue@atlassian.com>
2023-02-20 14:05:13 +1100

    ARC-1994 clean up issuekey-regex-char-limit

--- src/config/feature-flags.ts
+++ src/config/feature-flags.ts
@@ -16,7 +16,6 @@ export enum BooleanFlags {
 	VERBOSE_LOGGING = "verbose-logging",
 	USE_REST_API_FOR_DISCOVERY = "use-rest-api-for-discovery",
 	SEND_PR_COMMENTS_TO_JIRA = "send-pr-comments-to-jira_zy5ib",
-	ISSUEKEY_REGEX_CHAR_LIMIT = "issuekey-regex-char-limit",
 	USE_SHARED_PR_TRANSFORM = "use-shared-pr-transform",
 	NEW_JWT_VALIDATION = "new-jwt-validation",
 	RELAX_GHE_URLS_CHECK = "relax-ghe-url-check",
