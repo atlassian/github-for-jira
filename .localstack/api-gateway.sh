0c984a7c61b128cdc1277db5fd505294ffaad574
Michel Boudreau <michelboudreau@gmail.com>
2023-02-22 14:14:33 +1100

    finally got the devloop working

--- .localstack/api-gateway.sh
+++ .localstack/api-gateway.sh
@@ -1,3 +1,25 @@
 #!/usr/bin/env bash
 
-# awslocal apigateway create-rest-api --name gateway
+# Installing utility to convert yaml file to json
+if ! [ -x "$(command -v yq)" ]; then
+  echo "yq utility missing, installing..."
+  curl -o /usr/bin/yq -L https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64 && chmod +x /usr/bin/yq
+fi
+
+echo "Converting api gateway yaml to json..."
+yq -o=json '.' /api-gateway.yml > /api-gateway.json
+echo "Replacing api gateway variables..."
+sed -i 's/{{WebhooksQueueRegion}}/us-west-1/' /api-gateway.json
+sed -i 's/{{AccountNumber}}/000000000000/' /api-gateway.json
+sed -i 's/{{WebhooksQueueName}}/webhooks/' /api-gateway.json
+sed -i 's/credentials: "{{RoleARN}}"//' /api-gateway.json
+sed -i 's/{{RoleARN}}/arn:aws:iam::000000000000:role\/fake-role/' /api-gateway.json
+sed -i 's/https:\/\/{{ServerHost}}/http:\/\/app:8080/' /api-gateway.json
+sed -i 's/{{ServerHost}}/app:8080/' /api-gateway.json
+#echo "api gateway json contents:"
+#cat /api-gateway.json
+
+# Create REST API with static API ID of 'gateway'
+awslocal apigateway create-rest-api --name gateway --tags '{"_custom_id_":"gateway"}'
+# Import openapi definition to the gateway API
+awslocal apigateway put-rest-api --rest-api-id gateway --body 'file:///api-gateway.json'
