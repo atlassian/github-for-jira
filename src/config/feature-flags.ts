0460c712ceadc42739c2fdb41d79e79c2606c4b0
Josh Kay <jkay@atlassian.com>
2023-02-28 09:41:17 +1300

    conflict resolve

--- src/config/feature-flags.ts
+++ src/config/feature-flags.ts
@@ -17,6 +17,7 @@ export enum BooleanFlags {
 	USE_REST_API_FOR_DISCOVERY = "use-rest-api-for-discovery",
 	SEND_PR_COMMENTS_TO_JIRA = "send-pr-comments-to-jira_zy5ib",
 	USE_SHARED_PR_TRANSFORM = "use-shared-pr-transform",
+	USE_BACKFILL_ALGORITHM_INCREMENTAL = "backfill-algorithm-incremental",
 	RENEW_GITHUB_TOKEN = "renew-github-token",
 	REPO_CREATED_EVENT = "repo-created-event"
 }
