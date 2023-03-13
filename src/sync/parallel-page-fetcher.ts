258ade35e007bec881cc0e860ef8123d14870bdf
Boris Gvozdev <bgvozdev@atlassian.com>
2023-03-13 15:22:39 +1100

    NONE: refactor parallel fetching into common function

---
+++ src/sync/parallel-page-fetcher.ts
@@ -0,0 +1,45 @@
+import { TaskPayload } from "~/src/sync/sync.types";
+
+const mergeJiraPayload = (jiraPayload1?: any, jiraPayload2?: any) => {
+	if (!jiraPayload1 && !jiraPayload2) {
+		return undefined;
+	}
+
+	// TODO: add more
+	const pullRequests = [...(jiraPayload1?.pullRequests || []), ...(jiraPayload2?.pullRequests || [])];
+	const builds = [...(jiraPayload1?.builds || []), ...(jiraPayload2?.builds || [])];
+
+	if (pullRequests.length == 0 && builds.length == 0) {
+		return undefined;
+	}
+
+	return {
+		...(jiraPayload1 || jiraPayload2),
+		... (pullRequests.length > 0 ? { pullRequests } : {}),
+		... (builds.length > 0 ? { builds } : {})
+	};
+};
+
+export const fetchNextPagesInParallel = async (
+	pagesToFetch: number,
+	nextPageCursor: number,
+	singlePageFetchFactory: (pageCursor: number) => Promise<TaskPayload>
+): Promise<TaskPayload> => {
+	const tasks = Array.from(
+		{
+			length: pagesToFetch
+		},
+		(_, index) => singlePageFetchFactory(nextPageCursor + index)
+	);
+	const fetchedData = await Promise.all(tasks);
+	return fetchedData.reduce((prev, curr) => {
+		return {
+			edges: [...(prev.edges || []), ...(curr.edges || [])],
+			jiraPayload:
+				mergeJiraPayload(prev.jiraPayload, curr.jiraPayload)
+		};
+	}, {
+		edges: [],
+		jiraPayload: undefined
+	});
+};
