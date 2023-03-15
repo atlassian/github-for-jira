e3cf2d05435f500ba5783662e76fc953465c561c
Kayub Maharjan <kmaharjan4@atlassian.com>
2023-03-15 12:57:06 +1100

    - Fixes

--- static/js/jira-select-card-option.js
+++ static/js/jira-select-card-option.js
@@ -68,7 +68,6 @@ $(document).ready(function() {
 				const child = openChildWindow("/github-manifest");
 				child.window.jiraHost = jiraHost;
 				child.window.jwt = token;
-				child.window.gheHost = gitHubServerBaseUrl;
 			});
 		}
 	});
