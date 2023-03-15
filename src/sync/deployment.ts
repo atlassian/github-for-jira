324ac28e0f03691434c722b91e685a137bfa98b9
Gary Xue <gxue@atlassian.com>
2023-03-15 15:06:07 +1100

    ARC-2033 Add filter on deployments checking edges node's created date

--- src/sync/deployment.ts
+++ src/sync/deployment.ts
@@ -1,12 +1,15 @@
 import { Repository } from "models/subscription";
 import { WebhookPayloadDeploymentStatus } from "@octokit/webhooks";
 import { GitHubInstallationClient } from "../github/client/github-installation-client";
+import { getDeploymentsResponse, DeploymentQueryNode } from "../github/client/github-queries";
 import Logger from "bunyan";
 import { transformDeployment } from "../transforms/transform-deployment";
 import { BackfillMessagePayload } from "~/src/sqs/sqs.types";
+import { booleanFlag, BooleanFlags } from "config/feature-flags";
+
 
 const fetchDeployments = async (gitHubInstallationClient: GitHubInstallationClient, repository: Repository, cursor?: string | number, perPage?: number) => {
-	const deploymentData = await gitHubInstallationClient.getDeploymentsPage(repository.owner.login, repository.name, perPage, cursor);
+	const deploymentData: getDeploymentsResponse = await gitHubInstallationClient.getDeploymentsPage(repository.owner.login, repository.name, perPage, cursor);
 	const edges = deploymentData.repository.deployments.edges || [];
 	const deployments = edges?.map(({ node: item }) => item) || [];
 
@@ -52,6 +55,16 @@ export const getDeploymentTask = async (logger: Logger, gitHubInstallationClient
 	logger.debug("Syncing Deployments: started");
 	const { edges, deployments } = await fetchDeployments(gitHubInstallationClient, repository, cursor, perPage);
 
+	if (await booleanFlag(BooleanFlags.USE_BACKFILL_ALGORITHM_INCREMENTAL, jiraHost)) {
+		const fromDate = data?.commitsFromDate ? new Date(data?.commitsFromDate) : undefined;
+		if (isAllEdgesEarlierThanFromDate(edges, fromDate)) {
+			return {
+				edges,
+				jiraPayload: undefined
+			};
+		}
+	}
+
 	if (!deployments?.length) {
 		return {
 			edges,
@@ -72,4 +85,13 @@ export const getDeploymentTask = async (logger: Logger, gitHubInstallationClient
 		edges,
 		jiraPayload
 	};
+};
+
+const isAllEdgesEarlierThanFromDate  = (edges: DeploymentQueryNode[], fromDate: Date | undefined) => {
+	if (!fromDate) return false;
+	const edgeCountEarlierThanFromDate = edges.filter(edge => {
+		const edgeCreatedAt = new Date(edge.node.createdAt);
+		return edgeCreatedAt.getTime() < fromDate.getTime();
+	}).length;
+	return edgeCountEarlierThanFromDate === edges.length;
 };
