cea20087b5113aa2284e9b01c35528fe7ebc1b35
Harminder <hsingh5@atlassian.com>
2023-03-17 09:45:09 +1100

    removed RENEW_GITHUB_TOKEN ff

--- src/routes/github/github-oauth-router.test.ts
+++ src/routes/github/github-oauth-router.test.ts
@@ -7,19 +7,10 @@ import nock from "nock";
 import { envVars } from "config/env";
 import { DatabaseStateCreator } from "test/utils/database-state-creator";
 import { GitHubServerApp } from "models/github-server-app";
-import { when } from "jest-when";
-import { booleanFlag, BooleanFlags } from "config/feature-flags";
 
 jest.mock("config/feature-flags");
 
 describe("github-oauth-router", () => {
-
-	beforeEach(() => {
-		when(booleanFlag).calledWith(
-			BooleanFlags.RENEW_GITHUB_TOKEN,
-			expect.anything()
-		).mockResolvedValue(true);
-	});
 
 	describe("GithubOAuthCallbackGet", () => {
 
