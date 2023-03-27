96804e3e87635c7fd695caf017e48fac5c8b8463
joshkay10 <josh.kay@outlook.com>
2023-03-28 10:51:03 +1300

    avoid processing metrics for deployment queue when it has been rate limited (#2009)
    * avoid processing metrics for deployment queue when it has been rate limited
    
    * avoid processing metrics for deployment queue when it has been rate limited
    
    ---------
    
    Co-authored-by: Josh Kay <jkay@atlassian.com>

--- src/sqs/sqs.types.ts
+++ src/sqs/sqs.types.ts
@@ -150,6 +150,7 @@ export type DeploymentMessagePayload = BaseMessagePayload & {
 	// The original webhook payload from GitHub. We don't need to worry about the SQS size limit because metrics show
 	// that payload size for deployment_status webhooks maxes out at 13KB.
 	webhookPayload: WebhookPayloadDeploymentStatus,
+	rateLimited?: boolean
 }
 
 export type PushQueueMessagePayload = BaseMessagePayload & {
