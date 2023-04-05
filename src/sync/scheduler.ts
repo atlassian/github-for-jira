a3e1f0ae144f526fb5ea4bbc846d5c56c5d9e845
Boris Gvozdev <bgvozdev@atlassian.com>
2023-04-05 16:03:04 +1000

    NONE: optimize scheduler, add more tests

--- src/sync/scheduler.ts
+++ src/sync/scheduler.ts
@@ -5,6 +5,7 @@ import { getTargetTasks } from "~/src/sync/installation";
 import { createInstallationClient } from "utils/get-github-client-config";
 import Logger from "bunyan";
 import { booleanFlag, BooleanFlags } from "config/feature-flags";
+import { Op } from "sequelize";
 
 const getCursorKey = (type: TaskType) => `${type}Cursor`;
 const getStatusKey = (type: TaskType) => `${type}Status`;
@@ -15,11 +16,7 @@ const MAX_NUMBER_OF_SUBTASKS = 100;
 // Coefficient to determine pool size of the selection for the subtasks
 const SUBTASKS_POOL_COEF = 10;
 
-/**
- *
- * @param otherTasks - the array will be shuffled after the call, BEWARE!
- */
-const calculateTasksUsingGitHubRateLimitQuota = async (subscription: Subscription, logger: Logger, mainTask: Task, otherTasks: Task[]) => {
+const estimateNumberOfSubtasks = async (subscription: Subscription, logger: Logger) => {
 	try {
 		const metrics = {
 			trigger: "ratelimit_check_backfill"
@@ -35,17 +32,10 @@ const calculateTasksUsingGitHubRateLimitQuota = async (subscription: Subscriptio
 		const nSubTasks = Math.min(allowedSubtasks, MAX_NUMBER_OF_SUBTASKS);
 
 		logger.info({ nSubTasks, rateLimitData }, `Using subtasks: ${nSubTasks}`);
-
-		if (nSubTasks === 0) {
-			return [mainTask];
-		}
-
-		return [mainTask, ...(
-			otherTasks.slice(0, nSubTasks * SUBTASKS_POOL_COEF).sort(() => Math.random() - 0.5).slice(0, nSubTasks)
-		)];
+		return nSubTasks;
 	} catch (err) {
 		logger.warn({ err }, "Cannot determine rate limit, return only main task");
-		return [mainTask];
+		return 0;
 	}
 };
 
@@ -74,71 +64,132 @@ const mapSyncStateToTasks = (tasks: TaskType[], syncState: RepoSyncState): Task[
 };
 
 /**
- *
- * @param subscription
- * @param targetTasks - the first task is always same/deterministic! The tail is random from not complete ones tasks.
- * 											The caller can call this function after some delay again (e.g. SQS exponential backoff) and
- * 											it is guaranteed they will receive same task as the very first one (a.k.a. "main task")
- * @param logger
+ * Generates WHERE condition for RepoSyncStates table that would guarantee that there's at least one "pending" task
+ * for the record
  */
-export const getNextTasks = async (subscription: Subscription, targetTasks: TaskType[], logger: Logger): Promise<Task[]> => {
-	if (subscription.repositoryStatus !== "complete") {
-		return [{
-			task: "repository",
-			repositoryId: 0,
-			repository: {} as Repository,
-			cursor: subscription.repositoryCursor || undefined
-		}];
-	}
+const getLookupPendingConditionForTasks = (tasks: TaskType[]) => ({
+	[Op.or]: tasks.map(task => {
+		return {
+			[Op.or]: [
+				{
+					[getStatusKey(task)]: null
+				},
+				{
+					[getStatusKey(task)]: "pending"
+				}
+			]
+		};
+	})
+});
 
-	const tasks = getTargetTasks(targetTasks);
-
+/**
+ * Returns records with at least one pending task for the given "tasks"
+ */
+const fetchPendingRepoSyncStates = (subscription: Subscription, tasks: TaskType[], limit: number) =>
 	// Order by "id" to have strongly deterministic behaviour. Relying on something less definite (e.g. repoLastUpdated
 	// that might be changed may result in a erroneous flicking between being the main task and a subtask, thus
 	// slowing down the sync (when it is main, exponential retry is used; when it becomes subtask, other main task resets
 	// retry counter)
-	const repoSyncStates = await RepoSyncState.findAllFromSubscription(subscription, { order: [["id", "DESC"]] });
+	RepoSyncState.findAllFromSubscription(subscription, {
+		order: [["id", "DESC"]],
+		where: getLookupPendingConditionForTasks(tasks),
+		limit
+	});
 
-	const withSideTasks = await booleanFlag(BooleanFlags.USE_SUBTASKS_FOR_BACKFILL, subscription.jiraHost);
-
+/**
+ *
+ * @param repoSyncStatesWithPendingTasks - each record must contain at least one pending task for the given "tasks". The number of records should
+ * 												 be large enough to fill up a pool of size (nSubTasks * SUBTASKS_POOL_COEF)
+ * @param tasks
+ * @param nSubTasks
+ */
+const calculateNextTasksWithSubtasks = (repoSyncStatesWithPendingTasks: RepoSyncState[], tasks: TaskType[], nSubTasks: number) => {
 	let mainTask: Task | undefined = undefined;
 	const otherTasks: Task[] = [];
 
-	for (const syncState of repoSyncStates) {
-		const syncStateTasks = mapSyncStateToTasks(tasks, syncState);
-
-		if (syncStateTasks.length === 0) {
-			continue;
-		}
-
-		if (!withSideTasks) {
-			return [syncStateTasks[0]];
-		}
+	for (const syncStateWithPendingTasks of repoSyncStatesWithPendingTasks) {
+		const pendingTasks = mapSyncStateToTasks(tasks, syncStateWithPendingTasks);
 
 		// The main task always comes first. This is guaranteed by the ordering, when we retrieve the records from Db.
 		// It is very important: the sync will handle errors of the main task only (retries, SQS exponential backoff etc),
 		// we must be sure that on retry the same error will be retried, not some other random task.
 		if (!mainTask) {
-			mainTask = syncStateTasks[0];
-			otherTasks.push(...syncStateTasks.slice((1)));
+			mainTask = pendingTasks[0];
+			otherTasks.push(...pendingTasks.slice(1));
 		} else {
-			otherTasks.push(...syncStateTasks);
+			otherTasks.push(...pendingTasks);
 		}
 
-		// To prevent churn: in case of a large number of repos we want to process those subtasks that soon will become
-		// the main one, otherwise a cursor might become invalid
-		if (otherTasks.length > MAX_NUMBER_OF_SUBTASKS * SUBTASKS_POOL_COEF) {
+		// The pool size must be at least SUBTASKS_POOL_COEF times larger that the number of subtasks to make sure
+		// we are not picking same tasks over and over again, to prevent retrying errors too many times.
+		if (otherTasks.length > nSubTasks * SUBTASKS_POOL_COEF) {
 			break;
 		}
 	}
 
-	if (!mainTask) {
-		return [];
+	return {
+		mainTask,
+		otherTasks: otherTasks.sort(() => Math.random() - 0.5).slice(0, nSubTasks)
+	};
+};
+
+/**
+ *
+ * @param subscription
+ * @param targetTasks - the "mainTask" task is always same/deterministic! The "otherTasks" is random.
+ * 											The caller can call this function after some delay again (e.g. SQS exponential backoff) and
+ * 											expect to receive exactly same main task as during the previous call
+ * @param logger
+ */
+export const getNextTasks = async (subscription: Subscription, targetTasks: TaskType[], logger: Logger): Promise<{
+	mainTask: Task | undefined,
+	otherTasks: Task[]
+}> => {
+	if (subscription.repositoryStatus !== "complete") {
+		return {
+			mainTask: {
+				task: "repository",
+				repositoryId: 0,
+				repository: {} as Repository,
+				cursor: subscription.repositoryCursor || undefined
+			},
+			otherTasks: []
+		};
 	}
 
-	if (otherTasks.length === 0) {
-		return [mainTask];
+	const tasks = getTargetTasks(targetTasks);
+
+	const withSideTasks = await booleanFlag(BooleanFlags.USE_SUBTASKS_FOR_BACKFILL, subscription.jiraHost);
+
+	if (withSideTasks) {
+		const nSubTasks = await estimateNumberOfSubtasks(subscription, logger);
+		if (nSubTasks > 0) {
+			const repoSyncStates = await fetchPendingRepoSyncStates(subscription, tasks, (nSubTasks + 1) * SUBTASKS_POOL_COEF);
+			return calculateNextTasksWithSubtasks(repoSyncStates, tasks, nSubTasks);
+		}
+
+		const pendingRepoSyncState = await fetchPendingRepoSyncStates(subscription, tasks, 1);
+		return {
+			// Both SQL and mapSyncStateToTasks are deterministic => mainTask is deterministic
+			mainTask: mapSyncStateToTasks(tasks, pendingRepoSyncState[0])[0],
+			otherTasks: []
+		};
+
+	} else {
+		const pendingRepoSyncState = await fetchPendingRepoSyncStates(subscription, tasks, 1);
+
+		if (pendingRepoSyncState.length === 0) {
+			return {
+				mainTask: undefined,
+				otherTasks: []
+			};
+
+		} else {
+			return {
+				mainTask: mapSyncStateToTasks(tasks, pendingRepoSyncState[0])[0],
+				otherTasks: []
+			};
+		}
 	}
 
-	return await calculateTasksUsingGitHubRateLimitQuota(subscription, logger, mainTask, otherTasks);
 };
