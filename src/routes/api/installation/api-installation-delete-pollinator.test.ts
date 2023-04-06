f02ec071cd6b4ac581daa6995d30b8d9bb52c7f4
Gary Xue <gxue@atlassian.com>
2023-04-06 10:08:52 +1000

    Update pollinator api installation delete call to remove repo sync states because they can be stale

---
+++ src/routes/api/installation/api-installation-delete-pollinator.test.ts
@@ -0,0 +1,110 @@
+import { when } from "jest-when";
+import { ApiInstallationDeletePollinator, STAGE_POLLINATOR_JIRA_HOST, PROD_POLLINATOR_JIRA_HOST } from "./api-installation-delete-pollinator";
+import { getLogger } from "config/logger";
+import { Subscription } from "models/subscription";
+import { getJiraClient } from "~/src/jira/client/jira-client";
+import { RepoSyncState } from "~/src/models/reposyncstate";
+
+jest.mock("~/src/jira/client/jira-client");
+jest.mock("config/feature-flags");
+
+describe("ApiInstallationDeletePollinator", ()=>{
+	describe("GHES support", ()=>{
+		const GHES_GITHUB_INSTALLATION_ID = 123;
+		const GHES_GITHUB_APP_ID = 456;
+		let mockJiraClient;
+
+		const createSubscription = async (jiraHost: string) => {
+			return await Subscription.install({
+				installationId: GHES_GITHUB_INSTALLATION_ID,
+				host: jiraHost,
+				gitHubAppId: GHES_GITHUB_APP_ID,
+				hashedClientKey: "key"
+			});
+		};
+
+		const createRepoSyncState = async (subscriptionId: number, repoId: number) => {
+			return await RepoSyncState.create({
+				subscriptionId,
+				repoId,
+				repoName: "test-repo-name",
+				repoOwner: "integrations",
+				repoFullName: "test-repo-name",
+				repoUrl: "test-repo-url"
+			});
+		};
+
+
+		beforeEach(async ()=>{
+			mockJiraClient = { devinfo: { installation: { delete: jest.fn() } } };
+		});
+
+		it("should throw error if jiraHost is not within pollinator site", async ()=>{
+			const res = getRes();
+			await ApiInstallationDeletePollinator(getReq({
+				params: {
+					jiraHost: "https://whatever.atlassian.net",
+					installationId: 1,
+					gitHubAppId: undefined
+				},
+				body: {
+					jiraHost
+				}
+			}), res);
+			expect(res.status).toBeCalledWith(400);
+			expect(res.send).toBeCalledWith("Jira Host not a pollinator jira site");
+		});
+
+		it.each([STAGE_POLLINATOR_JIRA_HOST, PROD_POLLINATOR_JIRA_HOST])("should delete repoSyncStates from subcription", async (jiraSiteUrl: string)=>{
+
+			when(jest.mocked(getJiraClient))
+				.calledWith(jiraSiteUrl, GHES_GITHUB_INSTALLATION_ID, GHES_GITHUB_APP_ID, expect.anything())
+				.mockResolvedValue(mockJiraClient);
+
+			const sub = await createSubscription(jiraSiteUrl);
+			await createRepoSyncState(sub.id, 1);
+			expect((await RepoSyncState.findAllFromSubscription(sub)).length).toBe(1);
+
+			const res = getRes();
+			await ApiInstallationDeletePollinator(getReq({
+				params: {
+					jiraHost: jiraSiteUrl,
+					installationId: GHES_GITHUB_INSTALLATION_ID.toString(),
+					gitHubAppId: GHES_GITHUB_APP_ID.toString()
+				},
+				body: {
+					jiraHost
+				}
+			}), res);
+			expect(res.status).toBeCalledWith(200);
+			expect(mockJiraClient.devinfo.installation.delete).toBeCalledWith(GHES_GITHUB_INSTALLATION_ID.toString());
+			expect((await RepoSyncState.findAllFromSubscription(sub)).length).toBe(0);
+		});
+	});
+	const getReq = (opts: any = {}): any => {
+		return {
+			log: getLogger("test"),
+			params: {
+				...opts.params
+			},
+			body: {
+				...opts.body
+			},
+			get: jest.fn()
+		};
+	};
+	const getRes = (opts: any = {}): any => {
+		const ret = {
+			status: jest.fn(),
+			sendStatus: jest.fn(),
+			send: jest.fn(),
+			json: jest.fn(),
+			locals: {
+				...opts
+			}
+		};
+		ret.status.mockReturnValue(ret);
+		return ret;
+	};
+});
+
---
+++ src/routes/api/installation/api-installation-delete-pollinator.ts
@@ -0,0 +1,49 @@
+import { Request, Response } from "express";
+import { Subscription } from "models/subscription";
+import { getJiraClient } from "~/src/jira/client/jira-client";
+import { RepoSyncState } from "~/src/models/reposyncstate";
+
+export const STAGE_POLLINATOR_JIRA_HOST = "https://fusion-arc-pollinator-staging-app.atlassian.net";
+export const PROD_POLLINATOR_JIRA_HOST = "https://fusion-pollinator.atlassian.net";
+
+export const ApiInstallationDeletePollinator = async (req: Request, res: Response): Promise<void> => {
+	const gitHubInstallationId = req.params.installationId;
+	const gitHubAppId = Number(req.params.gitHubAppId) || undefined;
+	const jiraHost = req.params.jiraHost;
+
+	if (!jiraHost || !gitHubInstallationId) {
+		const msg = "Missing Jira Host or Installation ID";
+		req.log.warn({ req, res }, msg);
+		res.status(400).send(msg);
+		return;
+	}
+
+	if (jiraHost !== STAGE_POLLINATOR_JIRA_HOST && jiraHost !== PROD_POLLINATOR_JIRA_HOST) {
+		const msg = "Jira Host not a pollinator jira site";
+		req.log.warn({ req, res }, msg);
+		res.status(400).send(msg);
+		return;
+	}
+
+	const subscription = await Subscription.getSingleInstallation(
+		jiraHost,
+		Number(gitHubInstallationId),
+		gitHubAppId
+	);
+
+	if (!subscription) {
+		req.log.debug("no subscription");
+		res.sendStatus(404);
+		return;
+	}
+
+	try {
+		const jiraClient = await getJiraClient(jiraHost, Number(gitHubInstallationId), gitHubAppId, req.log);
+		req.log.info({ jiraHost, gitHubInstallationId }, `Deleting DevInfo`);
+		await jiraClient.devinfo.installation.delete(gitHubInstallationId);
+		await RepoSyncState.deleteFromSubscription(subscription);
+		res.status(200).send(`DevInfo deleted for jiraHost: ${jiraHost} gitHubInstallationId: ${gitHubInstallationId}`);
+	} catch (err) {
+		res.status(500).json(err);
+	}
+};
