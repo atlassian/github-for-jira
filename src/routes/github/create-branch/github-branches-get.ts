8c9e8363e5125f1c15f1595316b2fdc3eb77de83
Kayub Maharjan <kmaharjan4@atlassian.com>
2023-04-18 15:00:59 +1000

    - Branch name sanitized using isomorphic-dompurify

--- src/routes/github/create-branch/github-branches-get.ts
+++ src/routes/github/create-branch/github-branches-get.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { createUserClient } from "~/src/util/get-github-client-config";
+import DOMPurify from "isomorphic-dompurify";
 
 export const GithubBranchesGet = async (req: Request, res: Response): Promise<void> => {
 	const { githubToken, jiraHost, gitHubAppConfig } = res.locals;
@@ -23,8 +24,11 @@ export const GithubBranchesGet = async (req: Request, res: Response): Promise<vo
 		]);
 
 		res.send({
-			branches: branches.data.filter(branch => branch.name !== repository.data.default_branch),
-			defaultBranch: repository.data.default_branch
+			branches: branches.data.filter(branch => branch.name !== repository.data.default_branch).map(branch => {
+				branch.name = DOMPurify.sanitize(branch.name);
+				return branch;
+			}),
+			defaultBranch: DOMPurify.sanitize(repository.data.default_branch)
 		});
 	} catch (err) {
 		req.log.error({ err }, "Error while fetching branches");
