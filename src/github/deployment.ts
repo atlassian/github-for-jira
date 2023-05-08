5e2a31434f464d6555229872bc5b3114510e8ee7
Gary Xue <105693507+gxueatlassian@users.noreply.github.com>
2023-05-08 10:20:49 +1000

    Arc 2144 check why jira rejected issues (#2099)
    * ARC-2144 debug why there're so many rejected issues.

--- src/github/deployment.ts
+++ src/github/deployment.ts
@@ -69,11 +69,6 @@ export const processDeployment = async (
 	);
 
 	const result: DeploymentsResult = await jiraClient.deployment.submit(jiraPayload);
-	if (result.rejectedDeployments?.length) {
-		logger.warn({
-			rejectedDeployments: result.rejectedDeployments
-		}, "Jira API rejected deployment!");
-	}
 
 	// TODO - remove the rate limited test once valid metrics have been decided
 	!rateLimited && emitWebhookProcessedMetrics(
