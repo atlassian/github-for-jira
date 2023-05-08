ecdfaf470a1eed21142a7d41f0cdd9d494dcfb52
Boris Gvozdev <bgvozdev@atlassian.com>
2023-05-08 10:04:38 +1000

    ARC-2156: add more tests

--- src/routes/jira/connect/enterprise/jira-connect-enterprise-get.frontend.test.ts
+++ src/routes/jira/connect/enterprise/jira-connect-enterprise-get.frontend.test.ts
@@ -43,11 +43,6 @@ describe("jira-connect-enterprise-get.frontend", () => {
 		page = await browser.newPage();
 	});
 
-	beforeEach(async () => {
-		const result = await (new DatabaseStateCreator()).create();
-		installation = result.installation;
-	});
-
 	const generateJwt = async (query: any = {}) => {
 		return encodeSymmetric({
 			qsh: createQueryStringHash({
@@ -59,19 +54,21 @@ describe("jira-connect-enterprise-get.frontend", () => {
 		}, await installation.decrypt("encryptedSharedSecret", getLogger("test")));
 	};
 
-	beforeEach(async () => {
-		await page.goto("http://localhost:3000/jira/connect/enterprise?new=true&jwt=" + await generateJwt({
-			new: "true"
-		}));
-	});
-
-	const expectErrorMessageAndDisabledButton = async (errorMessage: string) => {
+	const expectErrorMessage = async (errorMessage: string) => {
 		await page.waitForFunction(
 			`document.querySelector("body").innerText.includes("${errorMessage}")`,
 			{ timeout: 5000 }
 		);
+	};
+
+	const expectDisabledButton = async () => {
 		const disabledValue = await page.$eval("#gheServerBtn", (element) => element.getAttribute("disabled"));
 		expect(disabledValue).toStrictEqual("disabled");
+	};
+
+	const expectEnabledButton = async () => {
+		const disabledValue = await page.$eval("#gheServerBtn", (element) => element.getAttribute("disabled"));
+		expect(disabledValue || null).toBeNull();
 	};
 
 	const enterApiKeyHeaderName = async (name) => {
@@ -95,116 +92,184 @@ describe("jira-connect-enterprise-get.frontend", () => {
 	const submitForm = async () =>
 		page.click("#gheServerBtnText");
 
-	describe("validates API Key header", () => {
+	const mockAp = async () => {
+		await page.evaluate(
+			(jwt) => {
+				window.AP = window.AP || {};
+				window.AP.context = window.AP.context || {};
+				window.AP.context.getToken = (callback) => {
+					callback(jwt);
+				};
+				window.AP.navigator = window.AP.navigator || {};
+				window.AP.navigator.go = (arg1, arg2) => {
+					window["redirectData"] = {
+						arg1, arg2
+					};
+				};
+			},
+			encodeSymmetric({
+				qsh: "context-qsh",
+				iss: installation.plainClientKey
+			}, await installation.decrypt("encryptedSharedSecret", getLogger("test")))
+		);
+	};
 
-		it("when the value is a known HTTP header", async () => {
-			await enterApiKeyHeaderName("authorization");
+	const waitAndGetRedirectData = async () => {
+		await page.waitForFunction(() => window["redirectData"] !== undefined, { timeout: 5000 });
 
-			await expectErrorMessageAndDisabledButton("authorization is a reserved string and cannot be used.");
+		const redirectData = await page.evaluate(() => {
+			return window["redirectData"];
 		});
+		return redirectData;
+	};
 
-		it("when the value is too long", async () => {
-			await enterApiKeyHeaderName(Array.from({ length: 1200 }, () => "x").join(""));
-
-			await expectErrorMessageAndDisabledButton("Max length is 1,024 characters.");
-		});
-	});
-
-	describe("validates API Key value", () => {
+	describe("no existing ghe", () => {
 		beforeEach(async () => {
+			const result = await (new DatabaseStateCreator()).create();
+			installation = result.installation;
+
 			await page.goto("http://localhost:3000/jira/connect/enterprise?new=true&jwt=" + await generateJwt({
 				new: "true"
 			}));
-			await enterApiKeyHeaderName("foo");
+
+			await mockAp();
 		});
 
-		it("when the value is empty", async () => {
-			await enterApiKeyHeaderValue("   ");
+		describe("validates API Key header", () => {
 
-			await expectErrorMessageAndDisabledButton("Cannot be empty.");
+			it("when the value is a known HTTP header", async () => {
+				await enterApiKeyHeaderName("authorization");
+
+				await expectErrorMessage("authorization is a reserved string and cannot be used.");
+				await expectDisabledButton();
+			});
+
+			it("when the value is too long", async () => {
+				await enterApiKeyHeaderName(Array.from({ length: 1200 }, () => "x").join(""));
+
+				await expectErrorMessage("Max length is 1,024 characters.");
+				await expectDisabledButton();
+			});
 		});
 
-		it("when the value is too long", async () => {
-			await enterApiKeyHeaderValue(Array.from({ length: 12000 }, () => "x").join(""));
+		describe("validates API Key value", () => {
+			beforeEach(async () => {
+				await enterApiKeyHeaderName("foo");
+			});
 
-			await expectErrorMessageAndDisabledButton("Max length is 8,096 characters.");
+			it("when the value is empty", async () => {
+				await enterApiKeyHeaderValue("   ");
+
+				await expectErrorMessage("Cannot be empty.");
+				await expectDisabledButton();
+			});
+
+			it("when the value is too long", async () => {
+				await enterApiKeyHeaderValue(Array.from({ length: 12000 }, () => "x").join(""));
+
+				await expectErrorMessage("Max length is 8,096 characters.");
+				await expectDisabledButton();
+			});
+
+			it("when API header name is not provided", async () => {
+				await enterApiKeyHeaderName("");
+				await enterApiKeyHeaderValue("some-correct-value");
+
+				await expectErrorMessage("Cannot be used without HTTP header name.");
+				await expectDisabledButton();
+			});
 		});
 
-		it("when API header name is not provided", async () => {
-			await enterApiKeyHeaderName("");
-			await enterApiKeyHeaderValue("some-correct-value");
+		describe("submits API Key header/value to server", () => {
+			it("OK without API key", async () => {
+				gheNock.get("/api/v3/rate_limit")
+					.reply(401, {}, { "server": "GitHub.com" });
 
-			await expectErrorMessageAndDisabledButton("Cannot be used without HTTP header name.");
+				await enterServerUrl(gheUrl);
+
+				await submitForm();
+
+				const redirectData = await waitAndGetRedirectData();
+				expect(redirectData).toStrictEqual({
+					arg1: "addonmodule",
+					arg2: {
+						moduleKey: "github-app-creation-page",
+						customData: {
+							connectConfigUuid: expect.anything(),
+							serverUrl: expect.anything(),
+							new: 1
+						}
+					}
+				});
+				expect(await new GheConnectConfigTempStorage().get(redirectData.arg2.customData.connectConfigUuid, installation.id)).toStrictEqual({
+					serverUrl: gheUrl,
+					apiKeyHeaderName: null,
+					encryptedApiKeyValue: null
+				});
+			});
+
+			it("OK with API key", async () => {
+				gheNock.get("/api/v3/rate_limit")
+					.matchHeader("OK-HEADER", "foo")
+					.reply(401, {}, { "server": "GitHub.com" });
+
+				await enterServerUrl(gheUrl);
+				await enterApiKeyHeaderName("OK-HEADER");
+				await enterApiKeyHeaderValue(" foo");
+
+				await page.click("#gheServerBtnText");
+
+				const redirectData = await waitAndGetRedirectData();
+
+				expect(redirectData).toStrictEqual({
+					arg1: "addonmodule",
+					arg2: {
+						moduleKey: "github-app-creation-page",
+						customData: {
+							connectConfigUuid: expect.anything(),
+							serverUrl: expect.anything(),
+							new: 1
+						}
+					}
+				});
+				expect(await new GheConnectConfigTempStorage().get(redirectData.arg2.customData.connectConfigUuid, installation.id)).toStrictEqual({
+					serverUrl: gheUrl,
+					apiKeyHeaderName: "OK-HEADER",
+					encryptedApiKeyValue: "encrypted:foo"
+				});
+			});
+
+			it("Errpr with API key", async () => {
+				gheNock.get("/api/v3/rate_limit")
+					.matchHeader("OK-HEADER", "foo")
+					.reply(200, {});
+
+				await enterServerUrl(gheUrl);
+				await enterApiKeyHeaderName("OK-HEADER");
+				await enterApiKeyHeaderValue(" foo");
+
+				await page.click("#gheServerBtnText");
+
+				await expectErrorMessage("Received OK, but the host is not GitHub Enterprise server.");
+				await expectEnabledButton();
+			});
 		});
 	});
 
-	describe("submits API Key header/value to server", () => {
+	describe("with existing GHE", () => {
 		beforeEach(async () => {
-			await page.evaluate(
-				(jwt) => {
-					window.AP = window.AP || {};
-					window.AP.context = window.AP.context || {};
-					window.AP.context.getToken = (callback) => {
-						callback(jwt);
-					};
-					window.AP.navigator = window.AP.navigator || {};
-					window.AP.navigator.go = (arg1, arg2) => {
-						window["redirectData"] = {
-							arg1, arg2
-						};
-					};
-				},
-				encodeSymmetric({
-					qsh: "context-qsh",
-					iss: installation.plainClientKey
-				}, await installation.decrypt("encryptedSharedSecret", getLogger("test")))
-			);
+			const result = await (new DatabaseStateCreator()).forServer().create();
+			installation = result.installation;
+
+			await page.goto("http://localhost:3000/jira/connect/enterprise?new=true&jwt=" + await generateJwt({
+				new: "true"
+			}));
+
+			await mockAp();
 		});
 
-		const waitAndGetRedirectData = async () => {
-			await page.waitForFunction(() => window["redirectData"] !== undefined, { timeout: 5000 });
-
-			const redirectData = await page.evaluate(() => {
-				return window["redirectData"];
-			});
-			return redirectData;
-		};
-
-		it("OK without API key", async () => {
-			gheNock.get("/api/v3/rate_limit")
-				.reply(401, { }, { "server": "GitHub.com" });
-
+		it("redirects to server apps", async () => {
 			await enterServerUrl(gheUrl);
-
-			await submitForm();
-
-			const redirectData = await waitAndGetRedirectData();
-			expect(redirectData).toStrictEqual({
-				arg1: "addonmodule",
-				arg2: {
-					moduleKey: "github-app-creation-page",
-					customData: {
-						connectConfigUuid: expect.anything(),
-						serverUrl: expect.anything(),
-						new: 1
-					}
-				}
-			});
-			expect(await new GheConnectConfigTempStorage().get(redirectData.arg2.customData.connectConfigUuid, installation.id)).toStrictEqual({
-				serverUrl: gheUrl,
-				apiKeyHeaderName: null,
-				encryptedApiKeyValue: null
-			});
-		});
-
-		it("OK with API key", async () => {
-			gheNock.get("/api/v3/rate_limit")
-				.matchHeader("OK-HEADER", "foo")
-				.reply(401, { }, { "server": "GitHub.com" });
-
-			await enterServerUrl(gheUrl);
-			await enterApiKeyHeaderName("OK-HEADER");
-			await enterApiKeyHeaderValue(" foo");
 
 			await page.click("#gheServerBtnText");
 
@@ -213,18 +278,12 @@ describe("jira-connect-enterprise-get.frontend", () => {
 			expect(redirectData).toStrictEqual({
 				arg1: "addonmodule",
 				arg2: {
-					moduleKey: "github-app-creation-page",
+					moduleKey: "github-list-server-apps-page",
 					customData: {
 						connectConfigUuid: expect.anything(),
-						serverUrl: expect.anything(),
-						new: 1
+						serverUrl: expect.anything()
 					}
 				}
-			});
-			expect(await new GheConnectConfigTempStorage().get(redirectData.arg2.customData.connectConfigUuid, installation.id)).toStrictEqual({
-				serverUrl: gheUrl,
-				apiKeyHeaderName: "OK-HEADER",
-				encryptedApiKeyValue: "encrypted:foo"
 			});
 		});
 	});
