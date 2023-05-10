c1a1b3bf3c9a160daa39ee4e34ad3777b785e1ae
Gary Xue <gxue@atlassian.com>
2023-05-11 09:15:58 +1000

    test gary site

--- src/github/client/github-installation-client.ts
+++ src/github/client/github-installation-client.ts
@@ -243,6 +243,24 @@ export class GitHubInstallationClient extends GitHubClient {
 
 	public listDeployments = async (owner: string, repo: string, environment: string, per_page: number): Promise<AxiosResponse<Octokit.ReposListDeploymentsResponse>> => {
 		try {
+
+			try {
+
+				if (await booleanFlag(BooleanFlags.LOG_CURLV_OUTPUT, this.jiraHost)) {
+					this.logger.warn("===== gary =====");
+					const { headers } = await this.installationAuthenticationHeaders();
+					const { Authorization } = headers as { Authorization: string };
+					const output = await runCurl({
+						fullUrl: `${this.baseUrl}/repos/{owner}/${repo}/deployments`,
+						method: "GET",
+						authorization: Authorization
+					});
+					this.logger.warn({ body: output.body, meta: output.meta }, "=== gary === Curl for list deployments output generated");
+				}
+			} catch (curlE) {
+				this.logger.error({ err: curlE }, "=== gary === Error running curl for list deployments");
+			}
+
 			return await this.get<Octokit.ReposListDeploymentsResponse>(`/repos/{owner}/{repo}/deployments`,
 				{ environment, per_page },
 				{ owner, repo }
