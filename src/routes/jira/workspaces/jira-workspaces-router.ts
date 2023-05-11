2adbb6685f2d0209b1db85c74ee3eeb4b81dcd64
Rachelle <rrathbone@atlassian.com>
2023-05-11 15:36:19 +1000

    fix router

--- src/routes/jira/workspaces/jira-workspaces-router.ts
+++ src/routes/jira/workspaces/jira-workspaces-router.ts
@@ -2,7 +2,6 @@ import { Router } from "express";
 import { JiraWorkspacesGet } from "routes/jira/workspaces/jira-workspaces-get";
 import { jiraSymmetricJwtMiddleware } from "middleware/jira-symmetric-jwt-middleware";
 import { csrfMiddleware } from "middleware/csrf-middleware";
-import { JiraConnectRouter } from "routes/jira/connect/jira-connect-router";
 import {
 	JiraWorkspacesRepositoriesRouter
 } from "routes/jira/workspaces/repositories/jira-workspaces-repositories-router";
@@ -14,4 +13,4 @@ JiraWorkspacesRouter.use(csrfMiddleware, jiraSymmetricJwtMiddleware);
 JiraWorkspacesRouter.route("/search")
 	.get(JiraWorkspacesGet);
 
-JiraConnectRouter.use("/repositories", JiraWorkspacesRepositoriesRouter);
+JiraWorkspacesRouter.use("/repositories", JiraWorkspacesRepositoriesRouter);
