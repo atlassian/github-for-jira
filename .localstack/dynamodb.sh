0403de602e1653eb61830c8307f84262425cc6fb
Gary Xue <gxue@atlassian.com>
2023-05-22 09:24:16 +1000

    fix shs

--- .localstack/dynamodb.sh
+++ .localstack/dynamodb.sh
@@ -1,22 +1,22 @@
 #!/usr/bin/env bash
 
-echo "===== creating dynamo table ${DYNAMO_DEPLOYMENT_HISTORY_TABLE_NAME} ====="
+echo "===== creating dynamo table ${DYNAMO_DEPLOYMENT_HISTORY_CACHE_TABLE_NAME} ====="
 
 awslocal dynamodb create-table \
-    --table-name $DYNAMO_DEPLOYMENT_HISTORY_TABLE_NAME \
+    --table-name $DYNAMO_DEPLOYMENT_HISTORY_CACHE_TABLE_NAME \
     --key-schema \
       AttributeName=Id,KeyType=HASH \
-      AttributeName=StatusCreatedAt,KeyType=RANGE \
+      AttributeName=CreatedAt,KeyType=RANGE \
     --attribute-definitions \
       AttributeName=Id,AttributeType=S \
-      AttributeName=StatusCreatedAt,AttributeType=N \
-    --region $DYNAMO_DEPLOYMENT_HISTORY_TABLE_REGION \
+      AttributeName=CreatedAt,AttributeType=N \
+    --region $DYNAMO_DEPLOYMENT_HISTORY_CACHE_TABLE_REGION \
     --provisioned-throughput \
         ReadCapacityUnits=10,WriteCapacityUnits=5
 
-echo "===== table ${DYNAMO_DEPLOYMENT_HISTORY_TABLE_NAME} created ====="
+echo "===== table ${DYNAMO_DEPLOYMENT_HISTORY_CACHE_TABLE_NAME} created ====="
 echo "===== checking now ====="
 
-awslocal dynamodb list-tables --region $DYNAMO_DEPLOYMENT_HISTORY_TABLE_REGION
+awslocal dynamodb list-tables --region $DYNAMO_DEPLOYMENT_HISTORY_CACHE_TABLE_REGION
 
 echo "===== check finished ====="
