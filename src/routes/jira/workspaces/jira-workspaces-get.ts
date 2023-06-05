5e598e5872ad7d9b2ba66652b41c92e9cc9fec31
Rachelle Rathbone <rrathbone@atlassian.com>
2023-06-05 16:56:39 +1000

    Arc-2115 search connected orgs (#2149)
    * add new actions to app descriptor
    
    * update app descriptor
    
    * wip
    
    * wip
    
    * wip
    
    * wip
    
    * complete tests
    
    * change id
    
    * add router
    
    * update error enum
    
    * fix findByOrgNameAndSubscriptionId
    
    * fix updates
    
    * finish tests
    
    * update action names and add new action;
    
    * remove unnecessary props in response
    
    * update
    
    * remove unused avatarUrl
    
    * updating base branch to include routers and skeleton files
    
    * wip - updating tests
    
    * update route structure
    
    * updating structure to align with base branch
    
    * update router
    
    * fix router
    
    * fix router
    
    * update id type
    
    * update middleware
    
    * update naming
    
    * clean up middleware
    
    * update naming
    
    * update tests
    
    * update comment
    
    * correct middleware
    
    * correct middleware
    
    * correct middleware
    
    * correct middleware
    
    * update action name
    
    * update action name
    
    * clean up tests
    
    * add bearer to jwt middleware
    
    * remove create repo;
    
    * clean up tests
    
    * clean up after merge
    
    * update snapshots
    
    * fix path and add new property
    
    * update tests after adding new property to response obj
    
    * add reposyncstate interface
    
    * clean up error messaging
    
    * fix error msg
    
    * address pr comments
    
    * add jira to route
    
    * address pr comments
    
    * move route
    
    * fix typo;
    
    * update snapshots
    
    * remove csrf from tests
    
    * update logic to handle no search query, paginate reponse and update tests
    
    * move helper functions out
    
    * move helper functions out
    
    * add space
    
    * add space
    
    * update func name
    
    * update func name
    
    * address pr comments
    
    * fix test
    
    * move util functions into test
    
    ---------
    
    Co-authored-by: Gary Xue <105693507+gxueatlassian@users.noreply.github.com>

--- src/routes/jira/workspaces/jira-workspaces-get.ts
+++ src/routes/jira/workspaces/jira-workspaces-get.ts
@@ -1,3 +1,56 @@
-export const JiraWorkspacesGet = () => {
-	// Content included in ARC-2115-search-connected-orgs
+import { Request, Response } from "express";
+import { Subscription } from "models/subscription";
+import { RepoSyncState } from "models/reposyncstate";
+import { Errors } from "config/errors";
+import { paginatedResponse } from "utils/paginate-response";
+
+export type Workspace = {
+	id: string,
+	name: string,
+	canCreateContainer: boolean
+}
+
+const DEFAULT_PAGE_NUMBER = 1; // Current page
+export const DEFAULT_LIMIT = 20; // Number of items per page
+
+const findMatchingOrgs = async (subscriptions: Subscription[], orgName?: string): Promise<Workspace[]> => {
+	const matchingRepos = await Promise.all(subscriptions.map(async (subscription: Subscription) => {
+		return orgName ?
+			await RepoSyncState.findByOrgNameAndSubscriptionId(subscription, orgName) :
+			await RepoSyncState.findOneFromSubscription(subscription);
+	}));
+
+	const matchedOrgs = matchingRepos
+		.filter((org): org is RepoSyncState => org !== null)
+		.map(org => {
+			const { subscriptionId, repoOwner } = org;
+
+			return {
+				id: subscriptionId.toString(),
+				name: repoOwner,
+				canCreateContainer: false
+			};
+		});
+
+	return matchedOrgs;
+};
+
+export const JiraWorkspacesGet = async (req: Request, res: Response): Promise<void> => {
+	req.log.info({ method: req.method, requestUrl: req.originalUrl }, "Request started for GET workspaces");
+
+	const { jiraHost } = res.locals;
+	const subscriptions = await Subscription.getAllForHost(jiraHost);
+
+	if (!subscriptions.length) {
+		req.log.warn({ jiraHost, req, res }, Errors.MISSING_SUBSCRIPTION);
+		res.status(400).send(Errors.MISSING_SUBSCRIPTION);
+		return;
+	}
+
+	const orgName = req.query?.searchQuery as string;
+	const page = Number(req.query?.page) || DEFAULT_PAGE_NUMBER;
+	const limit = Number(req.query?.limit) || DEFAULT_LIMIT;
+	const matchedOrgs = await findMatchingOrgs(subscriptions, orgName);
+
+	res.status(200).json({ success: true, workspaces: paginatedResponse(page, limit, matchedOrgs) });
 };
