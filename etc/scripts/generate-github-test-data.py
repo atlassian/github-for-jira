85b2677e06cc327cf7dd4cb17fdac38bb76fb29e
FIRST_NAME LAST_NAME <jkay@atlassian.com>
2023-06-29 11:18:07 +1200

    added better error handling, saving state for failure incase of error, creates for org instead of user

--- etc/scripts/generate-github-test-data.py
+++ etc/scripts/generate-github-test-data.py
@@ -1,25 +1,10 @@
-# usage
-# Step 1: Generate a Personal Access Token (PAT) from GitHub
-# Step 2: Replace YOUR_PAT_GOES_HERE and USERNAME with your own details
-# Step 3: python3 generate-github-test-data.py
-# it will create repo data locally and you have to manually clean it up for now, TODO
-#
-# For more control.........
-# You can customize the script behavior by providing command-line arguments:
-# --num-repos: Number of repositories to create.
-# --num-branches: Number of branches to create per repository.
-# --num-commits: Number of commits to make per branch.
-# --issue-prefix: Prefix for issue/commit messages.
-
-# python3 generate-github-test-data.py --num-repos 5 --num-branches 3 --num-commits 2
-
-
 import argparse
 import requests
 import os
 import subprocess
 import time
 import random
+import json
 
 # GitHub API base URL
 BASE_URL = 'https://api.github.com'
@@ -27,8 +12,8 @@ BASE_URL = 'https://api.github.com'
 # GitHub access token
 ACCESS_TOKEN = 'YOUR_PAT_GOES_HERE'
 
-# GitHub username
-USERNAME = 'USERNAME'
+# GitHub org name
+ORGANIZATION_NAME = 'ORG_NAME_GOES_HERE'
 
 # Default values
 DEFAULT_NUM_REPOS = 2
@@ -95,7 +80,7 @@ def initialize_repository(repo_name):
     subprocess.run('git add .', shell=True, check=True)
     subprocess.run('git commit -m "Initial commit"', shell=True, check=True)
     subprocess.run('git branch -M main', shell=True, check=True)
-    subprocess.run(f'git remote add origin https://github.com/{USERNAME}/{repo_name}.git', shell=True, check=True)
+    subprocess.run(f'git remote add origin https://github.com/{ORGANIZATION_NAME}/{repo_name}.git', shell=True, check=True)
     subprocess.run('git push -u origin main', shell=True, check=True)
     print(f'Repository "{repo_name}" initialized and initial commit pushed successfully.')
 
@@ -110,10 +95,16 @@ def create_workflow_file():
         with open(workflow_file_path, 'w') as workflow_file:
             workflow_file.write(content)
 
-    os.system(f'git add .github/workflows/{workflow_file_name}')
+    subprocess.run(f'git add .github/workflows/{workflow_file_name}', shell=True, check=True)
 
 def create_pull_request(repo_name, branch_name):
-    create_pull_request_url = f'{BASE_URL}/repos/{USERNAME}/{repo_name}/pulls'
+    create_pull_request_url = f'{BASE_URL}/repos/{ORGANIZATION_NAME}/{repo_name}/pulls'
+    pull_request_data = {
+        'title': f'{args.issue_prefix}-{random.randint(100, 999)}: Pull request',
+        'body': f'{args.issue_prefix}-{random.randint(100, 999)}: Pull request body',
+        'head': branch_name,
+        'base': 'main'
+    }
     pull_request_response = make_api_request('POST', create_pull_request_url, data=pull_request_data)
     print(f'Pull request created from "{branch_name}" to main successfully.')
 
@@ -121,7 +112,7 @@ def create_pull_request(repo_name, branch_name):
     should_merge = random.choice([True, False])
     if should_merge:
         pull_request_number = pull_request_response['number']
-        merge_pull_request_url = f'{BASE_URL}/repos/{USERNAME}/{repo_name}/pulls/{pull_request_number}/merge'
+        merge_pull_request_url = f'{BASE_URL}/repos/{ORGANIZATION_NAME}/{repo_name}/pulls/{pull_request_number}/merge'
         make_api_request('PUT', merge_pull_request_url)
         print(f'Pull request from "{branch_name}" to main merged successfully.')
     else:
@@ -130,57 +121,76 @@ def create_pull_request(repo_name, branch_name):
 # Generate a unique repository name based on epoch time
 base_repo_name = f'repo-{int(time.time())}'
 
+# Create a state file path
+state_file = 'state.json'
+
 try:
-    for repo_index in range(args.num_repos):
+    if os.path.isfile(state_file):
+        # If the state file exists, load the last successful state
+        with open(state_file, 'r') as f:
+            state = json.load(f)
+    else:
+        # If the state file doesn't exist, initialize the state
+        state = {
+            'repo_index': 0,
+            'branch_index': 0,
+            'commit_index': 0
+        }
+
+    for repo_index in range(state['repo_index'], args.num_repos):
         repo_name = f'{base_repo_name}-{repo_index}'
 
-        # Step 1: Create a new repository
-        create_repo_url = f'{BASE_URL}/user/repos'
-        data = {
-            'name': repo_name,
-            'private': False
-        }
-        make_api_request('POST', create_repo_url, data=data)
+        if repo_index == state['repo_index']:
+            # If resuming from a previous state, skip repositories already created
+            print(f'Skipping repository "{repo_name}" (already created).')
+            state['repo_index'] += 1
+            continue
+
+        # Step 1: Create the repository
+        create_repo_url = f'{BASE_URL}/orgs/{ORGANIZATION_NAME}/repos'
+        create_repo_data = {'name': repo_name}
+        make_api_request('POST', create_repo_url, data=create_repo_data)
         print(f'Repository "{repo_name}" created successfully.')
 
-        # Step 2: Initialize the repository, add README, create workflows, and push initial commit
         initialize_repository(repo_name)
 
-        # Step 3: Create branches and commits
-        for branch_index in range(args.num_branches):
+        for branch_index in range(state['branch_index'], args.num_branches):
+            branch_name = f'branch-{branch_index}-{int(time.time())}'
 
-            # Generate a unique branch name with the issue-key prefix and ranfom number
-            branch_name = f'{args.issue_prefix}-{random.randint(100, 999)}'
-            # Create a new branch
+            if branch_index == state['branch_index']:
+                # If resuming from a previous state, skip branches already created
+                print(f'Skipping branch "{branch_name}" (already created).')
+                state['branch_index'] += 1
+                continue
+
+            # Step 4: Create a new branch
             create_branch(branch_name)
 
-            for commit_index in range(args.num_commits):
-                # Generate a unique file name based on epoch time and random number
-                file_name = f'file-{int(time.time())}-{random.randint(100, 999)}.txt'
+            for commit_index in range(state['commit_index'], args.num_commits):
+                file_name = f'file-{commit_index}.txt'
+                commit_message = f'{args.issue_prefix}-{random.randint(100, 999)}: Commit message'
 
-                # Generate a commit message based on issue prefix and random number
-                commit_message = f'{args.issue_prefix}-{random.randint(100, 999)}'
+                if commit_index == state['commit_index']:
+                    # If resuming from a previous state, skip commits already made
+                    print(f'Skipping commit "{commit_message}" (already made).')
+                    state['commit_index'] += 1
+                    continue
 
-                # Create and write to the file
-                with open(file_name, 'w') as f:
-                    f.write(f'Commit {commit_index + 1} on repository {repo_name}')
-
-                # Commit and push changes
+                # Step 6: Make a commit and push the changes
                 commit_and_push_changes(branch_name, file_name, commit_message)
 
-                # Remove the file
-                os.remove(file_name)
+                # Step 7: Create a pull request
+                create_pull_request(repo_name, branch_name)
 
-            pull_request_data = {
-                'title': f'Pull request from {branch_name} to main',
-                'head': branch_name,
-                'base': 'main'
-            }
+                # Update the state file after each successful commit
+                with open(state_file, 'w') as f:
+                    json.dump(state, f)
 
-            create_pull_request(repo_name, branch_name)
-
-        # Back out of repo
-        os.chdir('..')
+    print('Script completed successfully.')
 
 except Exception as e:
-    print(f'An error occurred: {str(e)}')
+    print('An error occurred:', str(e))
+
+    # Save the state to resume from the last successful state
+    with open(state_file, 'w') as f:
+        json.dump(state, f)
