eda440eef192729b44a2fd3b46adec2d0ca4d280
Rachelle Rathbone <rachellerathbone@gmail.com>
2023-06-30 11:41:44 +1000

    update associate repo to handle server ids passed in body

--- src/routes/jira/workspaces/repositories/jira-workspaces-repositories-associate.test.ts
+++ src/routes/jira/workspaces/repositories/jira-workspaces-repositories-associate.test.ts
@@ -99,6 +99,47 @@ describe("Workspaces Associate Repository", () => {
 			});
 	});
 
+	it("Should return repo payload when a server ID is passed in the request", async () => {
+		app = express();
+		app.use((req, _, next) => {
+			req.log = getLogger("test");
+			req.csrfToken = jest.fn();
+			next();
+		});
+		app.use(getFrontendApp());
+
+		const repo1 = await RepoSyncState.create({
+			subscriptionId: sub.id,
+			repoId: 1,
+			repoName: "my-repo",
+			repoOwner: "atlassian",
+			repoFullName: "atlassian/my-repo",
+			repoUrl: "http://github.internal.atlassian.com/atlassian/my-repo"
+		});
+
+		Date.now = jest.fn(() => 1487076708000);
+
+		const associateRepoRes = {
+			id: "676974687562696e7465726e616c61746c61737369616e636f6d-1",
+			name: repo1.repoFullName,
+			url: repo1.repoUrl,
+			updateSequenceId: 1487076708000
+		};
+
+		await supertest(app)
+			.post("/jira/workspaces/repositories/associate")
+			.set({
+				authorization: `JWT ${await generateJwt()}`
+			})
+			.send({
+				id: "676974687562696e7465726e616c61746c61737369616e636f6d-1"
+			})
+			.expect(res => {
+				expect(res.status).toBe(200);
+				expect(res.text).toContain(JSON.stringify(associateRepoRes));
+			});
+	});
+
 	it("Should return empty object when no repo is found", async () => {
 		app = express();
 		app.use((req, _, next) => {
