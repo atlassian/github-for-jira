ce5955612403087dc2f9e4741871060d30620aa9
Kayub Maharjan <kmaharjan4@atlassian.com>
2023-07-03 12:22:24 +1000

    - Minor revert - spa-proxy added

---
+++ src/spa-proxy.ts
@@ -0,0 +1,30 @@
+import { Server } from "http";
+import { Express } from "express";
+import httpProxy from "http-proxy";
+import { isNodeDev } from "utils/is-node-env";
+
+/**
+ * This is only for Dev environment,
+ * You need to run spa separately by `yarn start`, which will be running at port `3000`,
+ * That server is being proxied within this file, which makes developing in spa quicker and easier
+ */
+const proxy = httpProxy.createProxyServer({
+	target: {
+		host: "localhost",
+		port: 5173,
+		path: "/spa"
+	},
+	ws: true
+});
+
+export const proxyLocalUIForDev = (app: Express) => {
+	if (isNodeDev()) {
+		app.use("/spa", (req, res) => proxy.web(req, res));
+	}
+};
+
+export const proxyLocalWSForDev = (server: Server) => {
+	if (isNodeDev()) {
+		server.on("upgrade", (req, socket, head) => proxy.ws(req, socket, head));
+	}
+};
