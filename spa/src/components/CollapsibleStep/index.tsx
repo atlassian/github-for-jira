d0c2055cd0350b65175c21c38c52dc8f504c6692
Kayub Maharjan <kmaharjan4@atlassian.com>
2023-07-06 13:36:12 +1000

    - Collapsible component created

---
+++ spa/src/components/CollapsibleStep/index.tsx
@@ -0,0 +1,90 @@
+import { useState } from "react";
+import { token } from "@atlaskit/tokens";
+import styled from "@emotion/styled";
+import CheckIcon from "@atlaskit/icon/glyph/check";
+
+const Container = styled.div`
+	border: ${token("space.025")} solid ${token("color.border")};
+	border-radius: ${token("space.050")};
+	padding: ${token("space.400")};
+	margin: ${token("space.400")} 0;
+`;
+const Header = styled.div`
+	display: flex;
+	line-height: ${token("space.400")};
+	align-items: center;
+`;
+const StepNumber = styled.span`
+	background: ${token("color.background.accent.gray.subtlest")};
+	height: ${token("space.400")};
+	width: ${token("space.400")};
+	text-align: center;
+	border-radius: 50%;
+`;
+const CompletedIcon = styled.span`
+	height: ${token("space.400")};
+	width: ${token("space.400")};
+	display: flex;
+	justify-content: end;
+	align-items: center;
+`;
+const StepTitle = styled.span`
+	cursor: pointer;
+	font-weight: 600;
+	margin: 0 0 0 ${token("space.200")};
+`;
+const Content = styled.div`
+	margin: ${token("space.100")} 0 0 ${token("space.600")};
+`;
+
+const CollapsibleStep = ({
+	title,
+	step,
+	expanded = false,
+	completed = false,
+	canExpand,
+	children,
+}: {
+	title: string,
+	step: string,
+	canExpand: boolean,
+	expanded?: boolean,
+	completed?: boolean,
+	children: JSX.Element,
+}) => {
+	const [ isExpanded, setIsExpanded ] = useState(expanded);
+	const [ isCompleted ] = useState(completed);
+	const clickedTitle = () => {
+		if (canExpand) {
+			setIsExpanded(!isExpanded);
+		}
+	};
+
+	return (
+		<Container
+			style={{
+				background: isExpanded ? `transparent` : `${token("elevation.surface.sunken")}`
+			}}
+		>
+			<Header>
+				{
+					isCompleted ? (
+						<CompletedIcon>
+							<CheckIcon label="completed" primaryColor={token("color.icon.success")} />
+						</CompletedIcon>
+					) : <StepNumber>{step}</StepNumber>
+				}
+				<StepTitle onClick={clickedTitle}>{title}</StepTitle>
+			</Header>
+			{
+				isExpanded && (
+					<Content data-testid="collapsible-content">
+						{children}
+					</Content>
+				)
+			}
+		</Container>
+	);
+};
+
+export default CollapsibleStep;
--- spa/src/pages/ConfigSteps/index.tsx
+++ spa/src/pages/ConfigSteps/index.tsx
@@ -1,15 +1,45 @@
 import SyncHeader from "../../components/SyncHeader";
 import { Wrapper } from "../../common/Wrapper";
+import styled from "@emotion/styled";
+import CollapsibleStep from "../../components/CollapsibleStep";
 
-/**
- * This is the second page with steps,
- * TODO: Remove the dummy content and add actual ones later
- */
+const ConfigContainer = styled.div`
+	max-width: 580px;
+	margin: 0 auto;
+`;
+
 const ConfigSteps = () => {
 	return (
 		<Wrapper>
 			<SyncHeader />
-			TODO: Config steps
+			<ConfigContainer>
+				<CollapsibleStep
+					step="1"
+					title="Log in and authorize"
+					canExpand={true}
+					expanded={true}
+				>
+					<div>
+						Content insideContent inside
+						Content insideContent insideContent inside
+						Content insideContent inside
+						Content insideContent insideContent inside					Content insideContent inside
+						Content insideContent insideContent inside					Content insideContent inside
+						Content insideContent insideContent inside					Content insideContent inside
+						Content insideContent insideContent inside					Content insideContent inside
+						Content insideContent insideContent inside
+					</div>
+				</CollapsibleStep>
+				<CollapsibleStep
+					step="2"
+					title="Connect your GitHub organization to Jira"
+					canExpand={false}
+					expanded={false}
+				>
+					<div>Content inside</div>
+				</CollapsibleStep>
+
+			</ConfigContainer>
 		</Wrapper>
 	);
 };
