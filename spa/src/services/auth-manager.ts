3d7100b452dbf5dcba16c180dc824d43dbbccae2
Gary Xue <gxue@atlassian.com>
2023-07-07 14:36:47 +1000

    clear listener

--- spa/src/services/auth-manager.ts
+++ spa/src/services/auth-manager.ts
@@ -3,13 +3,16 @@ import { createRestApi } from "../api/rest-api";
 const restApi = createRestApi();
 
 export {
+	startOAuth
 };
 
 async function startOAuth(onDone: () => void) {
-	window.addEventListener("message", (evt: MessageEvent) => {
+	const onSuccess = (evt: MessageEvent) => {
 		restApi.storeGitHubToken({ gitHubAccessToken: evt?.data?.gitHubAccessToken });
+		window.removeEventListener("message", onSuccess);
 		onDone();
-	});
+	};
+	window.addEventListener("message", onSuccess);
 	const oauthUrl = await restApi.generateOAuthUrl();
 	window.open(oauthUrl, "_blank");
 };
