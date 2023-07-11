baf13ace30e98f8b8b577dc8811bc89676d2891f
Kayub Maharjan <kmaharjan4@atlassian.com>
2023-07-11 15:14:51 +1000

    - Test cases added

--- spa/src/pages/ConfigSteps/test.tsx
+++ spa/src/pages/ConfigSteps/test.tsx
@@ -3,18 +3,34 @@ import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { BrowserRouter } from "react-router-dom";
 import ConfigSteps from "./index";
-import OauthManager from "../../oauth-manager";
+
+const Authenticated = {
+	checkValidity: jest.fn().mockReturnValue(Promise.resolve(true)),
+	authenticateInGitHub: jest.fn().mockReturnValue(Promise),
+	setTokens: jest.fn(),
+	getUserDetails: jest.fn().mockReturnValue({ username: "kay", email: "kay"}),
+	clear: jest.fn(),
+};
+const UnAuthenticated = {
+	checkValidity: jest.fn().mockReturnValue(Promise.resolve(false)),
+	authenticateInGitHub: jest.fn().mockReturnValue(Promise),
+	setTokens: jest.fn(),
+	getUserDetails: jest.fn().mockReturnValue({ username: "", email: ""}),
+	clear: jest.fn(),
+};
 
 // Mocking the global variable
 (global as any).AP = {
 	getLocation: jest.fn(),
 	context: {
-		getContext: jest.fn()
+		getContext: jest.fn(),
+		getToken: jest.fn()
 	}
 };
-(global as any).OAuthManagerInstance = OauthManager();
+(global as any).OAuthManagerInstance = UnAuthenticated;
+window.open = jest.fn();
 
-test("Connect GitHub Screen - Initial Loading of the page", () => {
+test("Connect GitHub Screen - Initial Loading of the page when not authenticated", () => {
 	render(
 		<BrowserRouter>
 			<ConfigSteps />
@@ -28,7 +44,7 @@ test("Connect GitHub Screen - Initial Loading of the page", () => {
 	expect(screen.queryByRole("button")).toHaveTextContent("Authorize in GitHub");
 });
 
-test("Connect GitHub Screen - Checking the GitHub Enterprise flow", async () => {
+test("Connect GitHub Screen - Checking the GitHub Enterprise flow when not authenticated", async () => {
 	render(
 		<BrowserRouter>
 			<ConfigSteps />
@@ -39,4 +55,53 @@ test("Connect GitHub Screen - Checking the GitHub Enterprise flow", async () => 
 	await userEvent.click(screen.getByText("Authorize in GitHub"));
 
 	expect(AP.getLocation).toHaveBeenCalled();
+});
+
+test("Connect GitHub Screen - Checking the GitHub Cloud flow when not authenticated", async () => {
+	render(
+		<BrowserRouter>
+			<ConfigSteps />
+		</BrowserRouter>
+	);
+
+	await userEvent.click(screen.getByText("GitHub Cloud"));
+	await userEvent.click(screen.getByText("Authorize in GitHub"));
+
+	expect(OAuthManagerInstance.authenticateInGitHub).toHaveBeenCalled();
+});
+
+test("Connect GitHub Screen - Checking the GitHub Cloud flow when authenticated", () => {
+	(global as any).OAuthManagerInstance = Authenticated;
+
+	render(
+		<BrowserRouter>
+			<ConfigSteps />
+		</BrowserRouter>
+	);
+
+	expect(screen.queryByText("GitHub Cloud")).not.toBeInTheDocument();
+	expect(screen.queryByText("GitHub Enterprise Server")).not.toBeInTheDocument();
+	expect(screen.queryByText("Authorize in GitHub")).not.toBeInTheDocument();
+	expect(screen.queryByText("Log in and authorize")).toBeInTheDocument();
+});
+
+test("Connect GitHub Screen - Changing GitHub login when authenticated", async () => {
+	(global as any).OAuthManagerInstance = Authenticated;
+
+	render(
+		<BrowserRouter>
+			<ConfigSteps />
+		</BrowserRouter>
+	);
+
+	await userEvent.click(screen.getByText("Log in and authorize"));
+
+	expect(screen.queryByText("Change GitHub login")).toBeInTheDocument();
+	await userEvent.click(screen.getByText("Change GitHub login"));
+
+	expect(window.open).toHaveBeenCalled();
+
+	expect(screen.queryByText("GitHub Cloud")).toBeInTheDocument();
+	expect(screen.queryByText("GitHub Enterprise Server")).toBeInTheDocument();
+	expect(screen.queryByText("Authorize in GitHub")).toBeInTheDocument();
 });
