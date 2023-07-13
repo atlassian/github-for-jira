3971e929e965e5594994560596a7f1482419fe43
Kayub Maharjan <kmaharjan4@atlassian.com>
2023-07-13 10:17:46 +1000

    - Minor fixes

--- spa/src/pages/ConfigSteps/index.tsx
+++ spa/src/pages/ConfigSteps/index.tsx
@@ -147,7 +147,7 @@ const ConfigSteps = () => {
 				getOrganizations();
 			}
 		});
-	}, [isLoggedIn, loggedInUser]);
+	}, [isLoggedIn]);
 
 	const authorize = async () => {
 		switch (selectedOption) {
