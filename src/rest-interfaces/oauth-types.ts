a10ff5a80bfbb20e318ed3c4d2fa03325d764e96
Kayub Maharjan <kmaharjan4@atlassian.com>
2023-07-13 14:06:21 +1000

    - Review changes

--- src/rest-interfaces/oauth-types.ts
+++ src/rest-interfaces/oauth-types.ts
@@ -1,5 +1,3 @@
-import { Octokit } from "@octokit/rest";
-
 export type GetRedirectUrlResponse = {
 	redirectUrl: string;
 	state: string;
@@ -16,5 +14,20 @@ export type UsersGetAuthenticatedResponse = {
 };
 
 export type OrganizationsResponse = {
-	orgs: Array<Octokit.AppsListInstallationsForAuthenticatedUserResponseInstallationsItem>;
+	orgs: Array<GitHubInstallationType>;
 }
+
+export type GitHubInstallationType = {
+	account: GitHubInstallationAccountType;
+	app_id: number;
+	id: number;
+};
+
+type GitHubInstallationAccountType = {
+	avatar_url: string;
+	id: number;
+	login: string;
+	url: string;
+	site_admin?: boolean;
+	type?: string;
+};
