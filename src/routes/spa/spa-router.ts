9ac77d0004b84047fa6723388785c51a83f4c827
Gary Xue <gxue@atlassian.com>
2023-07-26 16:17:48 +1000

    fix

--- src/routes/spa/spa-router.ts
+++ src/routes/spa/spa-router.ts
@@ -1,6 +1,6 @@
 import { Router, static as Static } from "express";
 import path from "path";
-import fs from "fs";
+import fs from "fs/promises";
 import { envVars } from "config/env";
 
 export const SpaRouter = Router();
@@ -11,9 +11,10 @@ const rootPath = process.cwd();
 SpaRouter.use("/static", Static(path.join(rootPath, 'spa/build/static')));
 
 //Because it is Single Page App, for all routes to /spa/screen1 , /spa/screen1/step2 should render the spa index.html anyway
-const ENV_KEY = "##SPA_APP_ENV##";
-const indexHtmlContent = fs.readFileSync(path.join(process.cwd(), "spa/build/index.html"), "utf-8").replace(ENV_KEY, envVars.MICROS_ENVTYPE || "");
-
-SpaRouter.use("/*", function SpaIndexHtml(_, res) {
+let indexHtmlContent: string = "";
+SpaRouter.use("/*", async function SpaIndexHtml(_, res) {
+	if (!indexHtmlContent) {
+		indexHtmlContent = (await fs.readFile(path.join(process.cwd(), "spa/build/index.html"), "utf-8")).replace("##SPA_APP_ENV##", envVars.MICROS_ENVTYPE || "");
+	}
 	res.status(200).send(indexHtmlContent);
 });
