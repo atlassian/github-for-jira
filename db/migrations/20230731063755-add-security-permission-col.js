41bc2c1f68a15419844e82ff94dcab91b95bc646
Harminder <hsingh5@atlassian.com>
2023-08-01 16:03:43 +1000

    initial checkin

---
+++ db/migrations/20230731063755-add-security-permission-col.js
@@ -0,0 +1,15 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.addColumn("Subscriptions", "isSecurityPermissionsAccepted", {
+      type: Sequelize.BOOLEAN,
+      allowNull: false,
+      defaultValue: false
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.removeColumn("Subscriptions", "isSecurityPermissionsAccepted");
+  }
+};
