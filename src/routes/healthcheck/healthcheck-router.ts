c6ae83ffd0320a121e595cec9c52e193dfc747f5
bgvozdev <20631664+bgvozdev@users.noreply.github.com>
2023-08-03 14:18:23 +1000

    DISTURBED: add healtcheck endpoint to troubleshoot GHEs webhooks (#2309)
    * DISTURBED: add healtcheck endpoint to troubleshoot GHEs webhooks
    
    * NONE: add poco for POST healthcheck
    
    * DISTURBED: more changes in poco
    
    * NONE: update poco

--- src/routes/healthcheck/healthcheck-router.ts
+++ src/routes/healthcheck/healthcheck-router.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { DeepcheckGet } from "./deepcheck-get";
-import { HealthcheckGet } from "./healthcheck-get";
+import { HealthcheckGetPost } from "./healthcheck-get-post";
 
 export const HealthcheckRouter = Router();
 
@@ -14,4 +14,6 @@ HealthcheckRouter.get("/deepcheck", DeepcheckGet);
 /**
  * healthcheck endpoint to check that the app started properly
  */
-HealthcheckRouter.get("/healthcheck", HealthcheckGet);
+HealthcheckRouter.get("/healthcheck", HealthcheckGetPost);
+// To troubleshoot connectivity between GHEs and the app
+HealthcheckRouter.post("/healthcheck/:uuid", HealthcheckGetPost);
