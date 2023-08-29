feea93f793787218d8a5f0e40d889cafc3e1eb88
Kayub Maharjan <kmaharjan4@atlassian.com>
2023-08-29 13:41:36 +1000

    - New component added

---
+++ spa/src/pages/Connections/index.tsx
@@ -0,0 +1,21 @@
+import { useEffect } from "react";
+import SyncHeader from "../../components/SyncHeader";
+import { Wrapper } from "../../common/Wrapper";
+
+const Connections = () => {
+
+	useEffect(() => {
+
+	}, []);
+
+	return (
+		<Wrapper>
+			<SyncHeader/>
+			<div>
+				List of connections should go here!
+			</div>
+		</Wrapper>
+	);
+};
+
+export default Connections;
