6232b8c6f81a1c49e0b0a8c587ae93a0edaee2c0
Kayub Maharjan <kmaharjan4@atlassian.com>
2023-08-29 22:48:18 +1000

    - WIP

---
+++ spa/src/api/subscriptions/service.ts
@@ -0,0 +1,16 @@
+import { Subscription } from "../../../../src/models/subscription";
+import { GitHubServerApp } from "../../../../src/models/github-server-app";
+
+export const getAllSubscriptions = async (jiraHost: string, installationId: number) => {
+
+	const subscriptions = await Subscription.getAllForHost(jiraHost);
+	const ghEnterpriseServers: GitHubServerApp[] = await GitHubServerApp.findForInstallationId(installationId) || [];
+
+	// Separating the subscriptions for GH cloud and GHE servers
+	const ghCloudSubscriptions = subscriptions.filter(subscription => !subscription.gitHubAppId);
+
+	return {
+		ghCloudSubscriptions,
+		ghEnterpriseServers
+	};
+};
--- src/rest/routes/subscriptions/index.tsx
+++ src/rest/routes/subscriptions/index.tsx
@@ -1,11 +1,16 @@
 import { Router, Request, Response } from "express";
 import { errorWrapper } from "../../helper";
+import { getAllSubscriptions } from "~/spa/src/api/subscriptions/service";
 
 export const SubscriptionsRouter = Router();
 
 SubscriptionsRouter.get("/", errorWrapper("SubscriptionsGet", async (_req: Request, res: Response) => {
+	const { jiraHost, installation } = res.locals;
+	const { ghCloudSubscriptions, ghEnterpriseServers } = await getAllSubscriptions(jiraHost, installation.id);
+
 	res.status(200).json({
-		success: true
+		ghCloudSubscriptions,
+		ghEnterpriseServers
 	});
 }));
 
