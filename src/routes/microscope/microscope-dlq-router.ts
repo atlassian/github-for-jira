52d5f7ea412a7ec86ecdfaa9f2e9c89e132aef53
FIRST_NAME LAST_NAME <jkay@atlassian.com>
2023-08-31 11:41:05 +1200

    slapped together hlf baked microscope endpoint to test feasibility

--- src/routes/microscope/microscope-dlq-router.ts
+++ src/routes/microscope/microscope-dlq-router.ts
@@ -1,8 +1,17 @@
-import { Router } from "express";
+import { Request, Response, Router } from "express";
 import {
 	deleteMessage, deleteMessages,
 	microscopeDlqHealthcheck, queryQueueAttributes, queryQueueMessages, queryQueues, requeueMessage, requeueMessages
 } from "routes/microscope/operations/microscope-dlq-operations";
+import { createHashWithSharedSecret } from "utils/encryption";
+
+export const debugIt = async (req: Request, res: Response): Promise<void> => {
+	// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+	// @ts-ignore
+	const jiraHost: string = req.query?.jiraHost || "";
+	const hashedValue = createHashWithSharedSecret(jiraHost);
+	res.send(hashedValue);
+};
 
 export const MicroscopeDlqRouter = Router();
 
@@ -14,3 +23,4 @@ MicroscopeDlqRouter.post("/queue/:queueName/message", requeueMessage);
 MicroscopeDlqRouter.delete("/queue/:queueName/message", deleteMessage);
 MicroscopeDlqRouter.post("/queue/:queueName/requeue", requeueMessages);
 MicroscopeDlqRouter.delete("/queue/:queueName/messages", deleteMessages);
+MicroscopeDlqRouter.delete("/debugTenant/:jirahost", debugIt);
