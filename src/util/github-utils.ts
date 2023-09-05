e5a967328192d1bf5bf6db84ee373cbd4433b24b
Gary Xue <gxue@atlassian.com>
2023-09-05 14:28:32 +1000

    fix

--- src/util/github-utils.ts
+++ src/util/github-utils.ts
@@ -2,7 +2,6 @@ import { GitHubUserClient } from "~/src/github/client/github-user-client";
 import { GitHubAppClient } from "~/src/github/client/github-app-client";
 import Logger from "bunyan";
 import { booleanFlag, BooleanFlags } from "config/feature-flags";
-import { GithubClientNotFoundError, GithubClientInvalidPermissionsError } from "~/src/github/client/github-client-errors";
 
 export const isUserAdminOfOrganization = async (githubClient: GitHubUserClient, jiraHost: string, gitHubAppClient: GitHubAppClient, orgName: string, username: string, orgType: string, logger: Logger): Promise<boolean> => {
 	if (orgType === "User") {
@@ -18,15 +17,7 @@ export const isUserAdminOfOrganization = async (githubClient: GitHubUserClient, 
 			return role === "admin" && state === "active";
 		}
 	} catch (err) {
-		if (err instanceof GithubClientNotFoundError) {
-			logger.warn({ err, orgName, username }, `could not determine admin status of user in org: GithubClientNotFoundError`);
-			return false;
-		} else if (err instanceof GithubClientInvalidPermissionsError) {
-			logger.warn({ err, orgName, username }, `could not determine admin status of user in org: GithubClientInvalidPermissionsError`);
-			return false;
-		} else {
-			logger.error({ err }, "Fail checking permission using GitHub App Client, fallback to user client");
-		}
+		logger.error({ err }, "Fail checking permission using GitHub App Client, fallback to user client");
 	}
 
 	try {
