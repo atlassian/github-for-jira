c1d5fe7479453531161d44a2790faa8166f4ab4b
yvonnevictoria <28718897+yvonnevictoria@users.noreply.github.com>
2023-09-06 11:27:31 +1000

    ARC-2435 Remove unused mocks

--- src/github/issue-comment.test.ts
+++ src/github/issue-comment.test.ts
@@ -50,33 +50,6 @@ describe("Issue Comment Webhook", () => {
 						}
 					});
 
-				jiraNock
-					.post("/rest/api/3/issue/TEST-123/comment", {
-						body: "Test example comment with linked Jira issue: [TEST-123] - some-comment-url",
-						properties: [
-							{
-								key: "gitHubId",
-								value: {
-									gitHubId: `${ISSUE_ID}`
-								}
-							}
-						]
-					})
-					.reply(201, undefined, {
-						"Accept": "application/json",
-						"Content-Type": "application/json"
-					});
-
-				// Mocks for updating GitHub with a linked Jira ticket
-				jiraNock
-					.get("/rest/api/latest/issue/TEST-123?fields=summary")
-					.reply(200, {
-						key: "TEST-123",
-						fields: {
-							summary: "Example Issue"
-						}
-					});
-
 				await expect(app.receive(issueCommentBasic as any)).toResolve();
 			});
 
