dce29edb9b6b28fe6efb6dc3f72ae8161ba9bd52
Kayub Maharjan <kmaharjan4@atlassian.com>
2023-09-06 18:21:44 +1000

    - WIP

--- test/e2e/utils/ngrok.ts
+++ test/e2e/utils/ngrok.ts
@@ -2,6 +2,8 @@ import { Page } from "@playwright/test";
 import { testData } from "test/e2e/constants";
 
 export const ngrokBypass = async (page: Page): Promise<Page> => {
+	// eslint-disable-next-line no-console
+	console.log("what is ", testData.appUrl);
 	await page.goto(`${testData.appUrl}`);
 	const button = await page.waitForSelector("#ngrok button", { timeout: 5000 }).catch(() => undefined);
 	if (button) {
