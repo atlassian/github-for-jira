91776c3fc13e933421d2bedf856d90cebecc8f05
Boris Gvozdev <bgvozdev@atlassian.com>
2023-09-07 11:56:03 +1000

    NONE: stop calling cryptor in healthchecks once it is ready

--- src/routes/healthcheck/healthcheck-get-post.ts
+++ src/routes/healthcheck/healthcheck-get-post.ts
@@ -2,6 +2,13 @@ import { Request, Response } from "express";
 import { getLogger } from "config/logger";
 import { EncryptionClient } from "utils/encryption-client";
 
+enum CryptorState {
+	UNKNOWN,
+	READY,
+	READY_FULL_STOP // one extra state just for the sake of logging
+}
+let cryptorState = CryptorState.UNKNOWN;
+
 const logger = getLogger("healthcheck");
 export const HealthcheckGetPost = async (req: Request, res: Response): Promise<void> => {
 
@@ -9,10 +16,23 @@ export const HealthcheckGetPost = async (req: Request, res: Response): Promise<v
 		if (req.params["uuid"]) {
 			logger.info({ uuid: req.params["uuid"] }, "healthcheck call from GHEs");
 		}
-		await EncryptionClient.healthcheck();
-		await EncryptionClient.deepcheck();
+		if (req.query["reset_cryptor_check_state"]) {
+			cryptorState = CryptorState.UNKNOWN;
+		}
+
+		if (cryptorState === CryptorState.UNKNOWN) {
+			logger.info("Checking cryptor");
+			await EncryptionClient.healthcheck();
+			await EncryptionClient.deepcheck();
+			logger.info("Cryptor is ready");
+			cryptorState = CryptorState.READY;
+
+		} else if (cryptorState === CryptorState.READY) {
+			logger.info("Cryptor is already ready, no further checks");
+			cryptorState = CryptorState.READY_FULL_STOP;
+		}
 	} catch (err) {
-		logger.debug(err, "Cryptor deepcheck failed.");
+		logger.warn(err, "Cryptor deepcheck failed.");
 		res.status(500).send("NOT OK");
 		return;
 	}
