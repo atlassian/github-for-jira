02641d1170c8285c95d83bd0f39de8541e3b1427
Kayub Maharjan <kmaharjan4@atlassian.com>
2023-09-08 22:06:47 +1000

    - Added jssha to encrypt jiraHost

--- spa/src/feature-flagged-app.tsx
+++ spa/src/feature-flagged-app.tsx
@@ -1,5 +1,6 @@
 import "@atlaskit/css-reset";
 import { withLDProvider } from "launchdarkly-react-client-sdk";
+import createHashWithSharedSecret from "./services/encryptor";
 import App from "./app";
 
 // TODO: Find out how these values are being set for current app and use similar approach
@@ -8,8 +9,7 @@ const LD_SDK_KEY_DEV = "XXXX";
 // Getting the jiraHost name from the iframe URL
 const getJiraHost = (): string => {
 	const jiraHostFromUrl = new URLSearchParams(location.search).get("xdm_e");
-	// TODO: figure out how to hash the jiraHost
-	return jiraHostFromUrl ? jiraHostFromUrl.toString() : "global";
+	return jiraHostFromUrl ? createHashWithSharedSecret(jiraHostFromUrl.toString()) : "global";
 };
 
 const FeatureFlaggedApp = withLDProvider({
---
+++ spa/src/services/encryptor/index.ts
@@ -0,0 +1,20 @@
+import jsSHA from "jssha";
+
+// TODO: Find out how to get the values for the env variables
+const GLOBAL_HASH_SECRET = "";
+const removeNonAlphaNumericCharacters = (str: string): string => {
+	return str.replace(/[^\p{L}\p{N}]+/ug, ""); // Tests all unicode characters and only keeps Letters and Numbers
+};
+const createHashWithSharedSecret = (data: string) => {
+	const cleanedData = removeNonAlphaNumericCharacters(data);
+
+	return new jsSHA("SHA-256", "TEXT", {
+		hmacKey: {
+			value: GLOBAL_HASH_SECRET ,
+			format: "TEXT"
+		}
+	}).update(cleanedData)
+		.getHash("HEX");
+};
+
+export default createHashWithSharedSecret;
