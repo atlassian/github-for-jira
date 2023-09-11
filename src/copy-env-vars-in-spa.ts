ed625a788aca6bc4e1612ee4d41e99f78bf6f1a1
Kayub Maharjan <kmaharjan4@atlassian.com>
2023-09-11 13:41:13 +1000

    - Cleanup

--- src/copy-env-vars-in-spa.ts
+++ src/copy-env-vars-in-spa.ts
@@ -3,6 +3,7 @@
  * This method simply copies the defined env variables to the React app in /spa
  */
 import fs from "fs";
+import { getLogger } from "config/logger";
 
 const ENV_VARS_TO_BE_COPIED = [
 	{ LAUNCHDARKLY_CLIENT_KEY: process.env.LAUNCHDARKLY_CLIENT_KEY },
@@ -16,7 +17,12 @@ const copyEnvVarsInSpa = () => {
 		envVars += `REACT_APP_${Object.keys(variable)[0]}=${Object.values(variable)[0]} \n`;
 	});
 
-	fs.writeFileSync("spa/.env", envVars);
+	try {
+		fs.writeFileSync("spa/.env", envVars);
+		getLogger("spa-environment").info("Created env file for SPA");
+	} catch (e) {
+		getLogger("spa-environment").warn("Couldn't create env file for SPA");
+	}
 };
 
 export default copyEnvVarsInSpa;
