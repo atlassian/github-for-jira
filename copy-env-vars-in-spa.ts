d05fb9ccb38e7066a5c3c592994e74b8860a7ea8
Kayub Maharjan <kmaharjan4@atlassian.com>
2023-09-11 17:35:21 +1000

    - Testing

--- copy-env-vars-in-spa.ts
+++ copy-env-vars-in-spa.ts
@@ -1,9 +1,14 @@
+/**
+ * Please make sure you have these environment variables defined in you .env,
+ * otherwise it won't be copied to spa
+ */
 import fs from "fs";
 
 const ENV_VARS_TO_BE_COPIED = [
 	{ LAUNCHDARKLY_CLIENT_KEY: process.env.LAUNCHDARKLY_CLIENT_KEY },
 	{ GLOBAL_HASH_SECRET: process.env.GLOBAL_HASH_SECRET }
 ];
+
 let envVars = "";
 ENV_VARS_TO_BE_COPIED.forEach(variable => {
 	envVars += `REACT_APP_${Object.keys(variable)[0]}=${Object.values(variable)[0]} \n`;
