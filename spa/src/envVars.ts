5e37414af81218adf35fe7f4ba64ad892f49caa1
Kayub Maharjan <kmaharjan4@atlassian.com>
2023-09-11 17:15:46 +1000

    - Cleanup

--- spa/src/envVars.ts
+++ spa/src/envVars.ts
@@ -1,5 +1,6 @@
 import { envVarsType } from "rest-interfaces";
 
+// TODO: need to add the env variables from the node app somehow
 const envVars: envVarsType = {
 	LAUNCHDARKLY_CLIENT_KEY: process.env.REACT_APP_LAUNCHDARKLY_CLIENT_KEY || "",
 	GLOBAL_HASH_SECRET: process.env.REACT_APP_GLOBAL_HASH_SECRET || ""
