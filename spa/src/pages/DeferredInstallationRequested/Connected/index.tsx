ad7721b33f95db30f338338fdb5e68206ef69ccb
Kayub Maharjan <kmaharjan4@atlassian.com>
2023-09-29 12:05:15 +1000

    - Cleanup

--- spa/src/pages/DeferredInstallationRequested/Connected/index.tsx
+++ spa/src/pages/DeferredInstallationRequested/Connected/index.tsx
@@ -2,19 +2,14 @@ import GithubConnectedHeader from "../../../components/GithubConnectedHeader";
 import Step from "../../../components/Step";
 import { Wrapper } from "../../../common/Wrapper";
 
-const DeferredInstallationConnected = () => {
-
-	return <>
-		<Wrapper>
-			<GithubConnectedHeader />
-			<Step title="Connection is complete">
-				<p>
-					Let your Jira admin know that GitHub’s ready to use and your<br />
-					team can use issue keys to link work to Jira.
-				</p>
-			</Step>
-		</Wrapper>
-	</>;
-};
+const DeferredInstallationConnected = () => <Wrapper>
+	<GithubConnectedHeader />
+	<Step title="Connection is complete">
+		<p>
+			Let your Jira admin know that GitHub’s ready to use and your<br />
+			team can use issue keys to link work to Jira.
+		</p>
+	</Step>
+</Wrapper>;
 
 export default DeferredInstallationConnected;
