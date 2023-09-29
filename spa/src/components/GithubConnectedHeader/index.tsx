f31ae7272e8fdfc2476bde5707de03c9825459b4
Kayub Maharjan <kmaharjan4@atlassian.com>
2023-09-29 16:30:11 +1000

    - Minor CSS change

--- spa/src/components/GithubConnectedHeader/index.tsx
+++ spa/src/components/GithubConnectedHeader/index.tsx
@@ -5,21 +5,12 @@ import { css } from "@emotion/react";
 const headerWrapperStyle = css`
 	text-align: center;
 `;
-
 const logoContainerStyle = css`
 	display: inline-flex;
 	align-items: center;
 `;
-
 const logoImgStyle = css`
-	height: ${token("space.800")};
-	padding: ${token("space.100")};
-`;
-
-const syncLogoImg = css`
-	height: ${token("space.500")};
-	padding: 0;
-	margin: 0 -30px;
+	height: 96px;
 `;
 const titleStyle = css`
 	margin: ${token("space.400")} ${token("space.0")} ${token("space.300")};
@@ -33,23 +24,10 @@ const GithubConnectedHeader = () => {
 			<div css={logoContainerStyle}>
 				<img
 					css={logoImgStyle}
-					className="logo"
-					src="/public/assets/jira-logo.svg"
-					alt=""
-				/>
-				<img
-					css={syncLogoImg}
-					className="sync-logo"
-					src="/public/assets/connected.svg"
-					alt=""
-				/>
-				<img
-					css={logoImgStyle}
-					className="logo"
 					src={
 						colorMode === "dark"
-							? "/public/assets/github-logo-dark-theme.svg"
-							: "/public/assets/github-logo.svg"
+							? "/public/assets/jira-github-connected-dark-theme.svg"
+							: "/public/assets/jira-github-connected.svg"
 					}
 					alt=""
 				/>
