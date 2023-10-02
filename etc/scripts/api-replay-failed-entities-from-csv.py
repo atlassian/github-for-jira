d18ee850377463af347d31246014e582896b6e8e
Harminder <hsingh5@atlassian.com>
2023-10-02 14:27:25 +1100

    added client script

--- etc/scripts/api-replay-failed-entities-from-csv.py
+++ etc/scripts/api-replay-failed-entities-from-csv.py
@@ -6,12 +6,15 @@ import requests
 import subprocess
 import sys
 import time
-import json
+import jsonpickle
 from requests import Request
 from requests.auth import AuthBase
 from typing import NamedTuple
+from dataclasses import dataclass
 
 LOG = logging.getLogger(__name__)
+
+session = requests.Session()
 
 def configure_logging(level='DEBUG'):
     formatter = logging.Formatter(
@@ -73,10 +76,68 @@ def create_environment(env: str) -> Environment:
           github_for_jira_auth=create_slauth(env))
   else:
       raise ValueError(f'Invalid environment {env}')
+  
+@dataclass(frozen=True)
+class FailedEntity:
+    gitHubInstallationId: int
+    hashedJiraHost: str
+    identifiers: list
+
+    @staticmethod
+    def from_dict(d):
+        return FailedEntity(d['gitHubInstallationId'], d['jiraHost'], d['rejectedEntities{}.key.vulnerabilityId'].split("\n"))
+
+class ReplayEntity:
+    def __init__(self, gitHubInstallationId, hashedJiraHost, identifier):
+        self.gitHubInstallationId = gitHubInstallationId
+        self.hashedJiraHost = hashedJiraHost
+        self.identifier = identifier
 
 
+def process_replayEntities(env: Environment, replayEntities: list) -> bool:
+    url = '{}/api/replay-rejected-entities-from-data-depot'.format(env.github_for_jira_url)
+
+    response = session.post(url,
+      auth=env.github_for_jira_auth,
+      json={
+        "replayEntities": jsonpickle.encode(replayEntities)
+      },
+      headers = {"Content-Type": "application/json"})
+
+    LOG.info("Response status %s" , response.status_code)
+    LOG.info("Response text %s" , response.text)
+    
 def main():
-    print("main function....")
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--env', choices=('dev', 'staging', 'prod'), required=True)
+    parser.add_argument('--batchsize', default=100, type=int, help='How many jiraHosts to process each iteration')
+    parser.add_argument('--input', type=argparse.FileType('r'), required=True)
+    parser.add_argument('--sleep', type=int, help='How long to wait between requests in seconds', required=True)
+    parser.add_argument('--level', default='INFO', choices=('INFO', 'ERROR', 'WARNING', 'DEBUG', 'CRITICAL'))
+    args = parser.parse_args()
+
+    configure_logging(level=args.level)
+
+    env = create_environment(args.env)
+    input_file = args.input
+
+    input_reader = csv.DictReader(input_file)
+
+    replayEntities = []
+    for row in input_reader:
+        failedEntity = FailedEntity.from_dict(row)
+        for identifier in failedEntity.identifiers:
+            replayEntities.append(ReplayEntity(failedEntity.gitHubInstallationId, failedEntity.hashedJiraHost, identifier))
+    
+
+    for i in range(0, len(replayEntities), args.batchsize):
+        replayEntitiesBatch = replayEntities[i:i+args.batchsize]
+        print("processing batch ", args.batchsize);
+        status = process_replayEntities(env, replayEntitiesBatch)
+        if status == 'error':
+            LOG.error('Stopping due to error. To skip a particular jiraHost, add a row to output file')
+            sys.exit(1)
+        time.sleep(args.sleep)
 
 if __name__ == '__main__':
     main()
