a20cf39583bf20defbc87a8f2412baa5b285a377
Harminder <hsingh5@atlassian.com>
2023-10-03 16:03:49 +1100

    adding unit test cases

--- src/routes/api/api-replay-failed-entities-from-data-depot.test.ts
+++ src/routes/api/api-replay-failed-entities-from-data-depot.test.ts
@@ -1,12 +1,16 @@
 
-import express, {  Application, NextFunction, Request, Response } from "express";
+import express, { Application, NextFunction, Request, Response } from "express";
 import { getLogger } from "~/src/config/logger";
 import { ApiRouter } from "./api-router";
 import supertest from "supertest";
+import { Subscription, SyncStatus } from "~/src/models/subscription";
+import { Installation } from "~/src/models/installation";
 
 describe("api-replay-failed-entities-from-data-depot", () => {
 
 	let app: Application;
+	//let subscription: Subscription;
+	const gitHubInstallationId = 1234;
 
 	const createApp = () => {
 		const app = express();
@@ -17,6 +21,23 @@ describe("api-replay-failed-entities-from-data-depot", () => {
 		app.use("/api", ApiRouter);
 		return app;
 	};
+
+	beforeEach(async () => {
+		await Installation.create({
+			gitHubInstallationId,
+			jiraHost,
+			encryptedSharedSecret: "secret",
+			clientKey: "client-key"
+		});
+
+		await Subscription.create({
+			gitHubInstallationId,
+			jiraHost,
+			jiraClientKey: "client-key",
+			syncStatus: SyncStatus.PENDING
+		});
+
+	});
 
 	it("should return 400 if slauth header is missing", async () => {
 		app = createApp();
@@ -33,12 +54,29 @@ describe("api-replay-failed-entities-from-data-depot", () => {
 
 		await supertest(app)
 			.post(`/api/replay-rejected-entities-from-data-depot`)
-			.send([])
+			.send({ replayEntities: [] })
 			.set("X-Slauth-Mechanism", "asap")
 			.then((res) => {
-				expect(res.text).toContain("Replay entities are empty.");
+				expect(res.text).toContain("Replay entities are empty");
 			});
 	});
 
+	it("should log error message if subscription is not found", async () => {
+		app = createApp();
+
+		await supertest(app)
+			.post(`/api/replay-rejected-entities-from-data-depot`)
+			.send({
+				replayEntities: [{
+					"gitHubInstallationId": 123,
+					"hashedJiraHost": "hashedJiraHost",
+					"identifier": "d-1234567-1"
+				}]
+			})
+			.set("X-Slauth-Mechanism", "asap")
+			.then((res) => {
+				expect(res.text).toContain("No subscription found");
+			});
+	});
 
 });
