a04b40b2204d4a9858e21d6d924dd26523f6ac0f
Kamakshee Samant <ksamant@atlassian.com>
2023-10-16 14:10:34 +1100

    fix: persistance alert msg in 5ku

--- spa/src/pages/ConfigSteps/index.tsx
+++ spa/src/pages/ConfigSteps/index.tsx
@@ -220,7 +220,7 @@ const ConfigSteps = () => {
 
 	const reLogin = async () => {
 		// Clearing the errors
-		showError(undefined);
+		clearAlerts();
 		OauthManager.clear();
 		// This resets the token validity check in the parent component and resets the UI
 		setIsLoggedIn(false);
@@ -229,10 +229,10 @@ const ConfigSteps = () => {
 			onWinClosed: () => {},
 			onPopupBlocked
 		}).catch(e => {
-				const errorObj = modifyError(e, { }, { onClearGitHubToken: () => {}, onRelogin: () => {}, onPopupBlocked });
-				analyticsClient.sendTrackEvent({ actionSubject: "finishOAuthFlow", action: "fail"}, { errorCode: errorObj.errorCode, step: "initiate-oauth"});
-				reportError(new Error("Reset oauth flow on relogin", { cause: e }), { path: "reLogin" });
-			});
+			const errorObj = modifyError(e, { }, { onClearGitHubToken: () => {}, onRelogin: () => {}, onPopupBlocked });
+			analyticsClient.sendTrackEvent({ actionSubject: "finishOAuthFlow", action: "fail"}, { errorCode: errorObj.errorCode, step: "initiate-oauth"});
+			reportError(new Error("Reset oauth flow on relogin", { cause: e }), { path: "reLogin" });
+		});
 	};
 
 	const clearAlerts = () =>{
