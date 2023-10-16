a8dbd671cf36dca67b886df7277c881fe865899e
kamaksheeAtl <142195272+kamaksheeAtl@users.noreply.github.com>
2023-10-16 14:26:45 +1100

    fix: persistance alert msg in 5ku (#2503)
    * fix: persistance alert msg in 5ku
    
    * fix: persistance alert msg in 5ku

--- spa/src/pages/ConfigSteps/index.tsx
+++ spa/src/pages/ConfigSteps/index.tsx
@@ -220,7 +220,7 @@ const ConfigSteps = () => {
 
 	const reLogin = async () => {
 		// Clearing the errors
-		showError(undefined);
+		clearAlerts();
 		OauthManager.clear();
 		// This resets the token validity check in the parent component and resets the UI
 		setIsLoggedIn(false);
@@ -229,17 +229,22 @@ const ConfigSteps = () => {
 			onWinClosed: () => {},
 			onPopupBlocked
 		}).catch(e => {
-				const errorObj = modifyError(e, { }, { onClearGitHubToken: () => {}, onRelogin: () => {}, onPopupBlocked });
-				analyticsClient.sendTrackEvent({ actionSubject: "finishOAuthFlow", action: "fail"}, { errorCode: errorObj.errorCode, step: "initiate-oauth"});
-				reportError(new Error("Reset oauth flow on relogin", { cause: e }), { path: "reLogin" });
-			});
+			const errorObj = modifyError(e, { }, { onClearGitHubToken: () => {}, onRelogin: () => {}, onPopupBlocked });
+			analyticsClient.sendTrackEvent({ actionSubject: "finishOAuthFlow", action: "fail"}, { errorCode: errorObj.errorCode, step: "initiate-oauth"});
+			reportError(new Error("Reset oauth flow on relogin", { cause: e }), { path: "reLogin" });
+		});
+	};
+
+	const clearAlerts = () =>{
+		showError(undefined);
+		setPopupBlocked(false);
 	};
 
 	const clearLogin = () => {
 		setIsLoggedIn(false);
 		setLoaderForLogin(false);
 		setLoggedInUser("");
-		showError(undefined);
+		clearAlerts();
 	};
 
 	const doCreateConnection = async (gitHubInstallationId: number, mode: "auto" | "manual", orgLogin: string) => {
@@ -309,6 +314,7 @@ const ConfigSteps = () => {
 			if (event.origin !== originalUrl) return;
 			if (event.data?.type === "oauth-callback" && event.data?.code) {
 				const response: boolean | AxiosError = await OAuthManager.finishOAuthFlow(event.data?.code, event.data?.state);
+				clearAlerts();
 				setLoaderForLogin(false);
 				if (response instanceof AxiosError) {
 					showError(modifyError(response, {}, { onClearGitHubToken: clearGitHubToken, onRelogin: reLogin, onPopupBlocked }));
