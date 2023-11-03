dc1b62e244662c83d0fbdbd8e71c7dc76109f173
Gary Xue <gxue@atlassian.com>
2023-11-03 15:49:43 +1100

    fix

--- spa/src/pages/ConfigSteps/empty-state-flow.test.tsx
+++ spa/src/pages/ConfigSteps/empty-state-flow.test.tsx
@@ -1,20 +1,20 @@
-import { act, render, screen, fireEvent, waitFor } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
+import { act, render, screen, waitFor } from "@testing-library/react";
 import ConfigSteps from "./index";
 import Connected from "../Connected";
 import { MemoryRouter, Route, Routes } from 'react-router-dom'
 import nock from "nock";
 
+import * as testUtils from "../../utils/test-helper";
+
 describe("Empty state user flow", () => {
 
-	let mockWinOpenFunc: jest.Mock;
 	beforeEach(() => {
-		setupGlobalAP();
+		testUtils.setupGlobalAP();
 		nockApiResponse();
 	});
 
 
-	it.only("should open new tab to log user into GitHub", async () => {
+	it("should open new tab to log user into GitHub", async () => {
 
 		await act(async () => {
 			render(
@@ -30,23 +30,23 @@ describe("Empty state user flow", () => {
 		});
 
 		//Ensure user is not logged in.
-		await waitFor(() => expectUserNotLogin());
+		await waitFor(() => testUtils.expectUserNotLogin());
 
 		//Start OAuth flow
-		await clickButton("Next");
-		await waitFor(() => expect(mockWinOpenFunc).toHaveBeenLastCalledWith("some-redirect-url", "_blank"));
+		await testUtils.clickButton("Next");
+		await waitFor(() => testUtils.expectPopupWith("some-redirect-url", "_blank"));
 
 		try {
 		//Finish Oauth flow
-		await postMessage({ type: "oauth-callback", code: "some-code", state: "some-state" });
+		await testUtils.postMessage({ type: "oauth-callback", code: "some-code", state: "some-state" });
 		//Ensure logged in
-		await waitFor(() => expectUserLoginSuccess("some-login"));
+		await waitFor(() => testUtils.expectUserLoginSuccess("some-login"));
 
 		//Because empty state, check app auto popup new github app install page
-		await waitFor(() => expect(mockWinOpenFunc).toHaveBeenLastCalledWith("some-app-install-url", "_blank"));
+		await waitFor(() => testUtils.expectPopupWith("some-app-install-url", "_blank"));
 
 		//Now trigger app installed on github
-		await postMessage({ type: "install-callback", gitHubInstallationId: 1111 });
+		await testUtils.postMessage({ type: "install-callback", gitHubInstallationId: 1111 });
 
 		//Should navigate to success screen
 		await waitFor(() => expect(screen.queryByText("Check your backfill status")).toBeInTheDocument());
@@ -95,49 +95,5 @@ describe("Empty state user flow", () => {
 		});
 	}
 
-	async function clickButton(buttonText: string) {
-		await act(async() => {
-			await userEvent.click(screen.getByText(buttonText));
-		});
-	}
-
-	function expectUserNotLogin() {
-		expect(screen.queryByText("Select your GitHub product", { exact: false })).toBeInTheDocument();
-		expect(screen.queryByText("Logged in as", { exact: false })).not.toBeInTheDocument();
-	}
-
-	function expectUserLoginSuccess(login: string) {
-		expect(screen.queryByText("Select your GitHub product")).not.toBeInTheDocument();
-		expect(screen.queryByText("Change GitHub login")).toBeInTheDocument();
-		expect(screen.queryByText("Logged in as", { exact: false })).toBeInTheDocument();
-		expect(screen.queryByText(login)).toBeInTheDocument();
-	}
-
-	function setupGlobalAP() {
-		(global as any).AP = {
-			getLocation: jest.fn(),
-			context: {
-				getContext: jest.fn(),
-				getToken: jest.fn().mockImplementation((cb: (token: string) => void) => cb("some-token"))
-			},
-			navigator: {
-				go: jest.fn(),
-				reload: jest.fn()
-			}
-		};
-		(global as any).open = mockWinOpenFunc = jest.fn().mockReturnValue({
-			closed: false
-		});
-	}
-
-	async function postMessage(data: any) {
-		await act(async () => {
-			fireEvent(window, new MessageEvent("message", {
-				bubbles: true,
-				origin: window.location.origin,
-				data
-			}));
-		});
-	}
 
 });
