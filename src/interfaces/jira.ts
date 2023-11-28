3d3521feedb9fcf80d9330b6a9a4480a942a5ace
Gary Xue <105693507+gxueatlassian@users.noreply.github.com>
2023-11-28 16:07:48 +1100

    ARC-2643 allow multi workflow entries in audit log during backfill (#2579)

--- src/interfaces/jira.ts
+++ src/interfaces/jira.ts
@@ -254,6 +254,6 @@ export interface JiraSubmitOptions {
 	preventTransitions: boolean;
 	operationType: JiraOperationType;
 	auditLogsource: AuditLogSourceType;
-	entityAction?: AuditEntityType;
-	subscriptionId?: number;
+	entityAction: AuditEntityType;
+	subscriptionId: number;
 }
