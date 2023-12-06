65014060e3a9133dd8fa687f5ee124db66cc960c
Kayub Maharjan <kmaharjan4@atlassian.com>
2023-12-06 17:14:26 +1100

    - Correction in API

--- spa/src/api/subscriptions/index.ts
+++ spa/src/api/subscriptions/index.ts
@@ -2,6 +2,6 @@ import { axiosRest, axiosRestWithGitHubToken } from "../axiosInstance";
 
 export default {
 	getSubscriptions: () => axiosRest.get("/rest/subscriptions"),
-	deleteSubscription: (installationId: number) =>
-		axiosRestWithGitHubToken.delete("/rest/app/cloud/subscription/:installationId", { params: { installationId } })
+	deleteSubscription: (subscriptionId: number) =>
+		axiosRestWithGitHubToken.delete("/rest/app/cloud/subscription/:subscriptionId", { params: { subscriptionId } })
 };
