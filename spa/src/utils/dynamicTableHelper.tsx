c6020bb991d4c98be8129529eb304f5706f40f2c
sopliu <123719396+sopliu@users.noreply.github.com>
2023-12-08 11:45:44 +1100

    [INNO] ARC-2720-Adding links in spa backfill (#2606)
    * ARC-2720 Add the edit button to SPA backfill page
    
    * NONE: Amend table head keys for SPA backfill
    
    * ARC-2720 Add the settings dropdown with configure link to backfill SPA
    
    * WIP Create and add link to respositories page
    
    * WIP Add back btn, heading and search bar to repo page
    
    * ARC-2720 Add link to repos on backfill SPA
    
    * NONE Removed unused repositories page and respective route
    
    * ARC-2720 Fixed redirect for repos to existing repos page

--- spa/src/utils/dynamicTableHelper.tsx
+++ spa/src/utils/dynamicTableHelper.tsx
@@ -6,6 +6,10 @@ import Lozenge from "@atlaskit/lozenge";
 import { SuccessfulConnection } from "../rest-interfaces";
 import { ThemeAppearance } from "@atlaskit/lozenge/dist/types/Lozenge";
 import { css } from "@emotion/react";
+import EditIcon from "@atlaskit/icon/glyph/edit";
+import Button from "@atlaskit/button";
+import MoreIcon from "@atlaskit/icon/glyph/more";
+import DropdownMenu, { DropdownItem, DropdownItemGroup } from "@atlaskit/dropdown-menu";
 
 type Row = {
 	key: string;
@@ -57,14 +61,19 @@ const createHead = (withWidth: boolean) => {
 				width: withWidth ? 30 : undefined,
 			},
 			{
-				key: "party",
+				key: "repos",
 				content: "Repos",
 				width: withWidth ? 30 : undefined,
 			},
 			{
-				key: "term",
+				key: "status",
 				content: "Status",
 				width: withWidth ? 30 : undefined,
+			},
+			{
+				key: "settings",
+				content: "Settings",
+				width: withWidth ? 10: undefined
 			}
 		],
 	};
@@ -106,9 +115,19 @@ export const getGHSubscriptionsRows = (
 					content: (
 						<div css={rowWrapperStyle}>
 							<span>
-								{cloudConnection.isGlobalInstall
-									? `All repos`
-									: `Only select repos`}
+								<a
+									href="#"
+									onClick={() => {
+										AP.navigator.go("addonmodule", {
+											moduleKey: "gh-addon-subscription-repos",
+											customData: {subscriptionId: cloudConnection.subscriptionId},
+										});
+									}}
+								>
+									{cloudConnection.isGlobalInstall
+										? `All repos`
+										: `Only select repos`}
+								</a>
 							</span>
 							<Badge>
 								{ifAllReposSynced(
@@ -116,6 +135,13 @@ export const getGHSubscriptionsRows = (
 									cloudConnection.totalNumberOfRepos
 								)}
 							</Badge>
+							<Button
+							href={cloudConnection.html_url}
+							target="_blank"
+							appearance="subtle"
+							iconBefore={<EditIcon label="" size="small"/>}
+							>
+							</Button>
 						</div>
 					),
 				},
@@ -147,6 +173,31 @@ export const getGHSubscriptionsRows = (
 							</div>
 						</div>
 					),
+				},{
+					key: cloudConnection.id,
+					content: (
+						<div css={rowWrapperStyle}>
+							<DropdownMenu
+								trigger={({ triggerRef, ...props }) => (
+									<Button
+										{...props}
+										appearance="subtle"
+										iconBefore={<MoreIcon label="more" size="small"/>}
+										ref={triggerRef}
+									/>
+								)}
+							>
+								<DropdownItemGroup>
+									<DropdownItem
+										href={cloudConnection.html_url}
+										target="_blank"
+									>
+										Configure
+									</DropdownItem>
+								</DropdownItemGroup>
+							</DropdownMenu>
+						</div>
+					)
 				}
 			],
 		})
