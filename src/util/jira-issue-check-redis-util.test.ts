2c663f91a44e31f9b90d5555b78de23156184252
Gary Xue <gxue@atlassian.com>
2023-12-12 15:52:40 +1100

    fix

--- src/util/jira-issue-check-redis-util.test.ts
+++ src/util/jira-issue-check-redis-util.test.ts
@@ -6,23 +6,24 @@ jest.mock("config/feature-flags");
 
 describe("Redis for jira issue status", () => {
 	const TIMEOUT = 10_000;
-	const ISSUE_KEY = "ABC-" + Math.floor(Math.random() * 10000);
+	let issueKey: string;
 	beforeEach(async () => {
 		when(numberFlag).calledWith(NumberFlags.SKIP_PROCESS_QUEUE_IF_ISSUE_NOT_FOUND_TIMEOUT, expect.anything(), expect.anything())
 			.mockResolvedValue(TIMEOUT);
+		issueKey = "ABC-" + Math.floor(Math.random() * 10000);
 	});
 	it("should save and successfully retried last status (exist)", async () => {
-		await saveIssueStatusToRedis(jiraHost, ISSUE_KEY, "exist");
-		const status = await getIssueStatusFromRedis(jiraHost, ISSUE_KEY);
+		await saveIssueStatusToRedis(jiraHost, issueKey, "exist");
+		const status = await getIssueStatusFromRedis(jiraHost, issueKey);
 		expect(status).toEqual("exist");
 	});
 	it("should save and successfully retried last status (not-exists)", async () => {
-		await saveIssueStatusToRedis(jiraHost, ISSUE_KEY, "not_exist");
-		const status = await getIssueStatusFromRedis(jiraHost, ISSUE_KEY);
+		await saveIssueStatusToRedis(jiraHost, issueKey, "not_exist");
+		const status = await getIssueStatusFromRedis(jiraHost, issueKey);
 		expect(status).toEqual("not_exist");
 	});
 	it("should return null on not found status", async () => {
-		const status = await getIssueStatusFromRedis(jiraHost, ISSUE_KEY);
+		const status = await getIssueStatusFromRedis(jiraHost, issueKey);
 		expect(status).toBeNull();
 	});
 });
