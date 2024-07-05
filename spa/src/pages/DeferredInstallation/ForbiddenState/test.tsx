b5d0d5e920869be5f66a624768b41cecc3b26a13
David Pike <dpike@atlassian.com>
2024-07-05 16:19:19 +1000

    add missing test

--- spa/src/pages/DeferredInstallation/ForbiddenState/test.tsx
+++ spa/src/pages/DeferredInstallation/ForbiddenState/test.tsx
@@ -21,7 +21,7 @@ test("Forbidden State screen", async () => {
 		</BrowserRouter>
 	);
 
-	expect(screen.getByText("Connect Github to Jira")).toBeTruthy();
+	expect(screen.getByText("Connect GitHub to Jira")).toBeTruthy();
 	expect(screen.getByText("Can’t connect this organization because you don’t have owner permissions")).toBeTruthy();
 	expect(screen.getByText("The GitHub account you’ve used doesn’t have owner permissions to connect to the GitHub organization.")).toBeTruthy();
 	expect(screen.getByText("Let the person who sent you the request know to find an owner for that organization.")).toBeTruthy();
